bd5baf09723a291a64745835604914239465ee67
Admin <ggznzn007@naver.com>
2020-08-14 20:09:18 +0900

    20200814

---
+++ csBullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csBullet : MonoBehaviour
+{
+    public float speed = 10f;
+
+    void Update()
+    {
+        this.transform.Translate(Vector3.up * speed * Time.deltaTime);
+        if(this.transform.position.y>10f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
