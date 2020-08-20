b733a807b3679c456247d50aa5eb17423e46de1b
Admin <ggznzn007@naver.com>
2020-08-20 17:23:46 +0900

    20200820

---
+++ csMecanimAnimation.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csMecanimAnimation : MonoBehaviour
+{
+    Animator anim;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+   
+    public void doIdle()
+    {
+        anim.SetInteger("aniStep", 0);
+    }
+    public void doWalk()
+    {
+        anim.SetInteger("aniStep", 1);
+    }
+    public void doRun()
+    {
+        anim.SetInteger("aniStep", 2);
+    }
+
+    public void doJump()
+    {
+        StartCoroutine(coJump());
+    }
+
+    IEnumerator coJump()
+    {
+        anim.SetInteger("aniStep", 3);
+        yield return new WaitForSeconds(0.8f);
+        anim.SetInteger("aniStep", 0);
+    }
+}
---
+++ csMoveSpartan.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csMoveSpartan : MonoBehaviour
+{
+    public float walkSpeed = 20f;
+    public float gravity = 20f;
+    public float jumpSpeed = 16f;
+    private Vector3 velocity;
+
+    CharacterController controller;
+    Animation ani;
+    bool isJump = false;
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        ani = GetComponent<Animation>();
+    }
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            velocity = 
+                new Vector3(Input.GetAxis("Horizontal"),
+                            0,
+                            Input.GetAxis("Vertical"));
+            velocity *= walkSpeed;
+
+            if (Input.GetButtonDown("Jump"))
+            {
+                velocity.y = jumpSpeed;
+                if(isJump==false)
+                    StartCoroutine(doJump());
+            }
+            /*방향키를 충분히 눌러서 일정량 이상의 변화값이 생기면*/
+            else if(velocity.magnitude > 0.5f)
+            {
+                ani.CrossFade("walk", 0.1f);
+                transform.LookAt(transform.position + velocity);
+            }
+            else
+            {
+                if(isJump==false)
+                    ani.CrossFade("idle", 0.1f);
+            }
+        }
+
+        velocity.y -= gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    IEnumerator doJump()
+    {
+        isJump = true;
+        ani.CrossFade("victory", 0.2f);
+        yield return new WaitForSeconds(2.7f);
+        isJump = false;
+    }
+}
