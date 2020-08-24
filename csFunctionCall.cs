136e4aee773d4f9ddc6d8c70870622b258b4bc7e
Admin <ggznzn007@naver.com>
2020-08-24 15:30:08 +0900

    20200824

---
+++ csFunctionCall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csFunctionCall : MonoBehaviour
+{
+    GameObject cube;
+    void Start()
+    {
+        cube = GameObject.Find("Cube");
+    }
+    private void OnGUI()
+    {
+        if(GUI.Button(new Rect(30, 50, 180, 30), 
+            "Function Call(Public)"))
+        {
+            csRotateCube script = cube.GetComponent<csRotateCube>();
+            script.Rotate1();
+        }
+        if(GUI.Button(new Rect(30, 100, 180, 30), 
+            "Function Call(Private)"))
+        {
+            cube.SendMessage("Rotate2",
+                SendMessageOptions.DontRequireReceiver);
+        }
+        if(GUI.Button(new Rect(30, 150, 180, 30),
+            "Static"))
+        {
+            Debug.Log("Call Variable : " + csRotateCube.numX);
+            Debug.Log("Call Function : " + csRotateCube.AddTwoNum(3, 5));
+        }
+    }
+}
---
+++ csRotateCube.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class csRotateCube : MonoBehaviour
+{
+    public static int numX = 0;
+
+    public static int AddTwoNum(int x, int y)
+    {
+        return x + y;
+    }
+
+    public void Rotate1()
+    {
+        transform.Rotate(Vector3.up * 90f);
+    }
+
+    private void Rotate2()
+    {
+        transform.Rotate(Vector3.up * -90f);
+    }
+}
