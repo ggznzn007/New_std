d9f5a1c768262bada81c044135de2d9c7f02008a
Admin <ggznzn007@naver.com>
2020-08-26 09:22:50 +0900

    20200826

---
+++ 15_GenericList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _15_GenericList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            ArrayList ar = new ArrayList();
+            ar.Add(3.14);
+            ar.Add("대한민국");
+            ar.Add(10);
+            */
+            List<int> list = new List<int>();
+            for (int i = 0; i < 5; i++)
+                list.Add(i);
+
+            foreach (int element in list)
+                Console.Write("{0} ", element);
+
+            Console.WriteLine();
+            list.RemoveAt(2);
+            foreach (int element in list)
+                Console.Write("{0} ", element);
+        }
+    }
+}
