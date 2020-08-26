7533818b50d77c41f083d178b3aa1b05d9c046b1
Admin <ggznzn007@naver.com>
2020-08-26 09:23:53 +0900

    20200826

---
+++ 17_GenericStack.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _17_GenericStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack =
+                        new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+
+            while (stack.Count > 0)
+                Console.WriteLine(stack.Pop());
+        }
+    }
+}
