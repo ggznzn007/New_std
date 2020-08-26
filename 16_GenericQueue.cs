b54f06b2ed85536971034327cb7762be6c4bc053
Admin <ggznzn007@naver.com>
2020-08-26 09:23:21 +0900

    20200826

---
+++ 16_GenericQueue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _16_GenericQueue
+{
+    class Program
+    {// 통신, 버퍼에서 많이 사용
+        static void Main(string[] args)
+        {
+            Queue<String> queue = new Queue<String>();
+            
+            queue.Enqueue("A");
+            queue.Enqueue("P");
+            queue.Enqueue("P");
+            queue.Enqueue("L");
+            queue.Enqueue("E");
+
+            while (queue.Count > 0)
+                Console.WriteLine(queue.Dequeue());
+        }
+    }
+}
