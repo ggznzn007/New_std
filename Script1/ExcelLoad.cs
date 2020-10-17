03e6fa767b4f47a3fed9c2339173889346a96474
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2020-10-17 14:30:43 +0900

    엑셀을 유니티로 끌어오기

---
+++ Script1/ExcelLoad.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExcelLoad : MonoBehaviour
+{
+    public Entity_TypicalQuestions item;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int cnt = item.sheets[0].list.Count;
+        int ran = Random.Range(0, cnt);
+        Debug.Log(item.sheets[0].list[ran].Question);
+
+        /*for (int i = 0; i<item.sheets[0].list.Count;i++)
+        {
+            Debug.Log(item.sheets[0].list[i].Question);
+            Debug.Log(item.sheets[0].list[i].Answer1);
+            Debug.Log(item.sheets[0].list[i].Answer2);
+            Debug.Log(item.sheets[0].list[i].Answer3);
+            
+        }*/
+    }
+
+   
+}
