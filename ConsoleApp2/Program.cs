9c00596bab301a38538b2329382a6b16f76a87f4
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2020-11-15 10:07:51 +0900

    Cs 커밋

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} + {1} = {2}", 10, 20, 10 + 20);
+        }
+    }
+}
