238e66346a1b6513959b229139380e48d92685dc
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2020-12-18 16:35:17 +0900

    std_Cs_Generic_20201218

---
+++ Cs_Study/Cs_std/01_Generic.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GenericMethod
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            int[] a = { 1, 2, 3 };
+            double[] d = { 0.1, 0.2, 0.3 };
+            string[] s = { "tiger", "lion", "zebra" };
+
+            PrintArray<int>(a);
+            PrintArray<double>(d);
+            PrintArray<string>(s);
+        }
+
+        private static void PrintArray<T>(T[] a)
+        {
+            foreach (var item in a)
+                Console.Write("{0,8}", item);
+            Console.WriteLine();
+        }
+    }
+}
