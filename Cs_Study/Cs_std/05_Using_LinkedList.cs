4f9f87e9b1fa918688ca72608976f58e58dd8e4b
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2020-12-23 18:32:11 +0900

    std_Cs_Using_LKL20201223

--- Cs_Study/Cs_std/05_Using_LinkedList.cs
+++ Cs_Study/Cs_std/05_Using_LinkedList.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            Random r = new Random();
+
+            for (int i = 0; i < 5; i++)
+                list.InsertLast(r.Next(100));
+
+            Console.WriteLine("랜덤한 5개 값의 리스트입니다");
+            list.Print();
+
+            Console.Write("\n 맨 앞에 10, 맨 뒤에 90 삽입. <Enter> 입력하세요");
+            Console.ReadLine();
+            list.InsertFront(10);
+            list.InsertLast(90);
+            list.Print();
+            Console.WriteLine("\nx 노드 뒤에 y값을 저장하려고 합니다. ");
+            Console.Write(" x값을 입력하세요: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write(" y값을 입력하세요: ");
+            int y = int.Parse(Console.ReadLine());
+
+            list.InsertAfter(x, y);
+            list.Print();
+
+            Console.Write("\n삭제할 노드의 값을 입력하세요: ");
+            int z = int.Parse(Console.ReadLine());
+            list.DeleteNode(z);
+            list.Print();
+
+            Console.WriteLine("\n리스트를 뒤집어 출력. <Enter> 입력하세요");
+            Console.ReadLine();
+            list.Reverse();
+            list.Print();
+        }
+    }
+}
