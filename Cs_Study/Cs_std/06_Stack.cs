b8919a455c13fe69ce5f6e043bc657ce9e193d2c
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2020-12-24 17:06:07 +0900

    std_Cs_Stack_Queue_20201224

---
+++ Cs_Study/Cs_std/06_Stack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace StackImplementation
+{
+    class MyStack<T>
+    {
+        const int maxSize = 10;
+        private T[] arr = new T[maxSize];
+        private int top;
+
+        public MyStack()
+        {
+            top = 0;
+        }
+
+        public void Push(T val)
+        {
+            if(top<maxSize)
+            {
+                arr[top] = val;
+                ++top;
+            }
+            else
+            {
+                Console.WriteLine("Stack is Full");
+                return;
+            }
+        }
+
+        public T Pop()
+        {
+            if(top>0)
+            {
+                --top;
+                return arr[top];
+            }
+            else
+            {
+                Console.WriteLine("Stack is Empty");
+                return default(T);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack<int> stack = new MyStack<int>();
+            Random r = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                int val = r.Next(100);
+                stack.Push(val);
+                Console.Write("Push (" + val + ") ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Pop()=" + stack.Pop() + ",");
+            }
+        }
+    }
+}
---
+++ Cs_Study/Cs_std/07_Queue.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Queue
+{
+    class Node<T>
+    {
+        internal T value;
+        internal Node<T> next;
+
+        public Node(T value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+
+    class MyQueue<T>
+    {
+        internal Node<T> first = null;
+        internal Node<T> last = null;
+
+        internal void EnQueue(Node<T> node)
+        {
+            if (last == null)
+                first = last = node;
+            else
+            {
+                last.next = node;
+                last = node;
+            }
+
+        }
+
+        internal T DeQueue()
+        {
+            if (first == null)
+            {
+                Console.WriteLine("Queue is Empty");
+                return default(T);
+            }
+            else
+            {
+                T value = first.value;
+                first = first.next;
+                return value;
+            }
+        }
+
+        internal void Print()
+        {
+            for (Node<T> t = first; t != null; t = t.next)
+                Console.Write(t.value + " -> ");
+            Console.WriteLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            MyQueue<float> que = new MyQueue<float>();
+
+            for (int i = 0; i < 5; i++)
+                que.EnQueue(new Node<float>(r.Next(100) / 100.0F));
+            que.Print();
+
+            for (int i = 0; i < 3; i++)
+                Console.WriteLine("DeQueue: {0}", que.DeQueue());
+            que.Print();
+        }
+    }
+}
