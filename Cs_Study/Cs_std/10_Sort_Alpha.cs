b854653ccd8c201b66758109cb5ac15469842b1e
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2020-12-25 15:37:01 +0900

    std_Cs_Sort_Alphabet_20201225

---
+++ Cs_Study/Cs_std/10_Sort_Alpha.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlphabeticalSort//알파벳순서 정렬
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> lstNames = new List<string>();
+            lstNames.Add("dog");
+            lstNames.Add("cow");
+            lstNames.Add("rabbit");
+            lstNames.Add("goat");
+            lstNames.Add("sheep");
+            lstNames.Sort();
+            foreach (string s in lstNames)
+                Console.Write(s + " ");
+            Console.WriteLine();
+
+            string[] arrNames = new string[100];
+            arrNames[0] = "dog";
+            arrNames[1] = "cow";
+            arrNames[2] = "rabbit";
+            arrNames[3] = "goat";
+            arrNames[4] = "sheep";
+            Array.Sort(arrNames);
+            foreach(string s in lstNames)
+                Console.Write(s + " ");
+            Console.WriteLine();
+
+        }
+    }
+}
