e70fe4d104771064dbefaf5be7ebaa9269067f71
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2020-12-25 15:27:12 +0900

    std_Cs_List_T_20201225

---
+++ Cs_Study/Cs_std/09_List_T.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace List01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>();
+            Random r = new Random();
+
+            PrintValues(a);
+            for (int i = 0; i < 10; i++)
+                //a.Add(r.Next(100));
+                a.Add(r.Next(10));
+
+            PrintValues(a);
+            a.Sort();
+            PrintValues(a);
+
+            a.RemoveAt(3);
+            PrintValues(a);
+
+        }
+
+        private static void PrintValues(List<int> a)
+        {
+            Console.WriteLine("Print Values in List<int>");
+            Console.WriteLine("  Count = {0}", a.Count);
+            Console.WriteLine("  Capacity = {0}", a.Capacity);
+            foreach (var i in a)
+                Console.Write("  {0}", i);
+            Console.WriteLine();
+        }
+    }
+}
