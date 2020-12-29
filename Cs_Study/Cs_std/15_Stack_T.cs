03f0a7b6d1b83c76e1401da89d6a5ba80697cfe2
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2020-12-29 18:06:23 +0900

    std_Cs_Stack_T_20201229

--- Cs_Study/Cs_std/15_Stack_T.cs
+++ Cs_Study/Cs_std/15_Stack_T.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack_T
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("계산할 수식을 Polish 표기법으로 입력하세요: ");
+            string[] token = Console.ReadLine().Split();
+
+            foreach (var i in token)
+                Console.Write(" {0}", i);
+            Console.Write(" = ");
+
+            Stack<double> nStack = new Stack<double>();
+            
+        }
+    }
+}
