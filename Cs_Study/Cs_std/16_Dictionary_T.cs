f2c71cbd7dc589f29b0d641db47b139075ac3b7c
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2020-12-30 15:29:07 +0900

    std_Cs_Dictionary_T_20201230

---
+++ Cs_Study/Cs_std/16_Dictionary_T.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary_T
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> colorTable = new Dictionary<string, string>();
+
+        }
+    }
+}
