0ccf9dd7e8607c143f7a50146c169eab3f261074
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2020-12-31 15:29:47 +0900

    std_Cs_SortedList_T_20201231

---
+++ Cs_Study/Cs_std/17_SortedList_T.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortedList_T
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int, string> s1 = new SortedList<int, string>();
+            s1.Add(3, "Three");
+            s1.Add(4, "Four");
+            s1.Add(1, "One");
+            s1.Add(2, "Two");
+
+            for (int i = 0; i < s1.Count; i++)
+                Console.Write("k: {0}, v: {1} / ", s1.Keys[i], s1.Values[i]);
+            Console.WriteLine();
+
+            foreach (var kvp in s1)
+                Console.Write("{0, -10} ", kvp);
+            Console.WriteLine();
+
+            SortedList<string, int> s2 = new SortedList<string, int>();
+            s2.Add("one", 1);
+            s2.Add("two", 2);
+            s2.Add("three", 3);
+            s2.Add("four", 4);
+
+            Console.WriteLine(s2["two"]);
+
+            foreach (var kvp in s2)
+                Console.WriteLine("{0, -10} ", kvp);
+        }
+    }
+}
