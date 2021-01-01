884d24b5af570224cc2d8e08bf85e16bd026a260
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-01 15:33:33 +0900

    std_Cs_Indexer_20210101

--- Cs_Study/Cs_std/18_Indexer_T.cs
+++ Cs_Study/Cs_std/18_Indexer_T.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+namespace Indexer_T
+{
+    class MyCollection<T>
+    {
+        private T[] array = new T[100];
+
+        public T this[int i]// 인덱스 정의
+        {
+            get { return array[i]; }
+            set { array[i] = value; }
+        }
+    }
+}
