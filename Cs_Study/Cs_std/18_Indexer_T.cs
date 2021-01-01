076d2dd1751de70f3488852a49ccc7362aca0543
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-01 15:52:30 +0900

    std_Cs_Indexer_20210101

--- Cs_Study/Cs_std/18_Indexer_T.cs
+++ Cs_Study/Cs_std/18_Indexer_T.cs
@@ -6,7 +6,7 @@ namespace Indexer_T
     {
         private T[] array = new T[100];// 클래스의 필드로 T형 자료를 100개 저장 가능한 배열
 
-        public T this[int i]// 인덱스 정의
+        public T this[int i]// 인덱서 정의
         {//this 키워드를 사용해서 정수 인덱스를 사용가능
             get { return array[i]; }//array[i]를 리턴
             set { array[i] = value; }//value 키워드를 사용하여 array[i]를 설정 
@@ -17,12 +17,12 @@ namespace Indexer_T
     {
         static void Main(string[] args)
         {
-            var myString = new MyCollection<string>();
-            myString[0] = "Hello, World!";
-            myString[1] = "Hello, C#";
-            myString[1] = "Hello, Indexer!";
+            var myString = new MyCollection<string>();//인스턴스 myString 정의
+            myString[0] = "Hello, World!";//myString[0]에 헬로 월드를 넣고
+            myString[1] = "Hello, C#";//MyCollection 클래스에 인덱서를 만들어서 배열과같이[]연산자 사용가능
+            myString[2] = "Hello, Indexer!";
 
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < 3; i++)//인덱서 i를 0~2까지 변화하면서 myString[i]를 출력
                 Console.WriteLine(myString[i]);
         }
     }
