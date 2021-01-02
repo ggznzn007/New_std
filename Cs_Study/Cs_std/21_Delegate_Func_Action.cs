b7fadfa7dd0a94d61752c1443082ae53b26f4faa
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-02 15:19:49 +0900

    std_Cs_Delegate_FnA_20210102

--- Cs_Study/Cs_std/21_Delegate_Func_Action.cs
+++ Cs_Study/Cs_std/21_Delegate_Func_Action.cs
@@ -11,7 +11,18 @@ namespace Func_Action
             var arr = new[] { 3, 34, 6, 34, 7, 8, 24, 3, 675, 8, 23 };
 
             int n = Count(arr, delegate (int x) { return x % 2 == 0; });
+            Console.WriteLine("짝수의 개수: " + n);
+        }
 
+        private static int Count(int[] arr, Func<int, bool>testMethod)
+        {
+            int cnt = 0;
+            foreach(var n in arr)
+            {
+                if (testMethod(n))
+                    cnt++;
+            }
+            return cnt;
         }
     }
 }
