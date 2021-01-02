1ce29474339c4b038badc284655f0b2e7c52668b
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-02 14:18:34 +0900

    std_Cs_Delegate_Basic_20210102

---
+++ Cs_Study/Cs_std/19_Delegate_basic.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Delegate_B
+{
+    class Program
+    {
+        delegate bool MemberTest(int a);
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { 3, 5, 4, 2, 6, 4, 6, 8, 54, 23, 4, 6, 4 };
+
+            Console.WriteLine("짝수의 개수: " + Count(arr, IsEven));
+            Console.WriteLine("홀수의 개수: " + Count(arr, IsOdd));
+        }
+
+        static int Count(int[] a, MemberTest testMethod)
+        {
+            int cnt = 0;
+            foreach(var n in a)
+            {
+                if (testMethod(n) == true)
+                    cnt++;
+            }
+            return cnt;
+        }
+    }
+}
