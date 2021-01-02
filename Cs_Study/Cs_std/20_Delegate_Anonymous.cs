cbfed1ce71a72337ae603e916f9cefbe28994188
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-02 15:10:21 +0900

    std_Cs_Delegate_Basic_20210102

--- Cs_Study/Cs_std/20_Delegate_Anonymous.cs
+++ Cs_Study/Cs_std/20_Delegate_Anonymous.cs
@@ -10,22 +10,23 @@ namespace Delegate_Anonymous
         {
             var arr = new[] { 3, 34, 6, 34, 7, 8, 24, 3, 675, 8, 23 };
 
-            int n = Count(arr, delegate (int x) { return x % 2 == 0; });
+            int n = Count(arr, delegate (int x) { return x % 2 == 0; });//배열과 델리게이트가 매개변수
             Console.WriteLine("짝수의 개수: " + n);
-
+            //델리게이트를 이름없이 인라인으로 직접 정의 가능 == 무명 델리게이트
+            //따라서 따로 짝수 홀수 메소드를 정의할 필요없음
             n = Count(arr, delegate (int x) { return x % 2 != 0; });
             Console.WriteLine("홀수의 개수: " + n);
         }
 
         private static int Count(int[] arr, MemberTest testMethod)
-        {
-            int cnt = 0;
+        {//메소드 정의
+            int cnt = 0;//배열의 각 요소에 대해 인수로 전달받은 델리게이트의 메소드를 호출하여
             foreach(var n in arr)
             {
                 if (testMethod(n))
-                    cnt++;
+                    cnt++;//리턴값이 트루면 cnt를 하나씩 증가시켜 리턴
             }
-            return cnt;
+            return cnt;//Main에서 출력
         }
     }
 }
