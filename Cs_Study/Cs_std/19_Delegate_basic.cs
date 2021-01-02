1509d2a1ffbd5784f80b96f947b1639434f9f533
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-02 14:30:31 +0900

    std_Cs_Delegate_Basic_20210102

--- Cs_Study/Cs_std/19_Delegate_basic.cs
+++ Cs_Study/Cs_std/19_Delegate_basic.cs
@@ -9,23 +9,24 @@ namespace Delegate_B
         static void Main(string[] args)
         {
             int[] arr = new int[] { 3, 5, 4, 2, 6, 4, 6, 8, 54, 23, 4, 6, 4 };
-
+            //카운트 메소드는 배열과 델리게이트 메소드를 매개변수로 사용
             Console.WriteLine("짝수의 개수: " + Count(arr, IsEven));
             Console.WriteLine("홀수의 개수: " + Count(arr, IsOdd));
         }
 
         static int Count(int[] a, MemberTest testMethod)
-        {
-            int cnt = 0;
+        {//메소드 정의
+            int cnt = 0;//배열의 각 요소에 대해 인수로 전달받은 델리게이트의 메소드를 호출하여
             foreach(var n in a)
             {
-                if (testMethod(n) == true)
+                if (testMethod(n) == true)//리턴값이 트루면 cnt를 하나씩 증가시켜 리턴
                     cnt++;
             }
-            return cnt;
+            return cnt;//Main에서 출력
         }
 
         static public bool IsOdd(int n) { return n % 2 != 0; }
         static public bool IsEven(int n) { return n % 2 == 0; }
+        //정수를 매개변수로 해서 부울값을 리턴
     }
 }
