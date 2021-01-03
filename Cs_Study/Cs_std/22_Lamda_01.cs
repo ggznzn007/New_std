0644bcfcc6cb7bec102514dd9bc3c081a42fee3d
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-03 15:51:58 +0900

    std_Cs_Lamda_20210103

--- Cs_Study/Cs_std/22_Lamda_01.cs
+++ Cs_Study/Cs_std/22_Lamda_01.cs
@@ -8,7 +8,7 @@ namespace Lamda_1
         {
             var arr = new[] { 3, 34, 6, 34, 7, 8, 24, 3, 675, 8, 23 };
 
-            int n = Count(arr, x => x % 2 == 0);
+            int n = Count(arr, x => x % 2 == 0);//카운트 메소드는 배열과 람다식을 매개변수로 사용
             Console.WriteLine("짝수의 개수: " + n);
 
             n = Count(arr, x => x % 2 == 1);
@@ -16,7 +16,8 @@ namespace Lamda_1
         }
 
         private static int Count(int[] arr, Func<int,bool> testMethod)
-        {
+        {//카운트 메소드 정의
+            //전달된 람다식은 Func<>로 받음
             int cnt = 0;
             foreach(var n in arr)
             {
