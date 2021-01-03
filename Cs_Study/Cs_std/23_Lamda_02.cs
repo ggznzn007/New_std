7dbae7d37fa33604d475a26497c193ef814b423c
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-03 16:13:07 +0900

    std_Cs_Lamda_20210103

--- Cs_Study/Cs_std/23_Lamda_02.cs
+++ Cs_Study/Cs_std/23_Lamda_02.cs
@@ -21,7 +21,34 @@ namespace Lamda_02
             Action line = () => Console.WriteLine();
             line();
 
+            CalcMethod add = (a, b) => a + b;
+            CalcMethod sub = (a, b) => a - b;
 
+            Console.WriteLine(add(10, 20));
+            Console.WriteLine(sub(10.5, 20));
+
+            IsTeenager isTeen
+                = delegate (Student s) { return s.Age > 12 && s.Age < 20; };
+
+            Student s1 = new Student() { Name = "John", Age = 18 };
+            Console.WriteLine("{0}은 {1}.",
+                s1.Name, isTeen(s1) ? "청소년입니다" : "청소년이 아닙니다");
+
+            IsAdult isAdult = (s) =>
+            {
+                int adultAge = 18;
+                return s.Age >= adultAge;
+            };
+
+            Student s2 = new Student() { Name = "Robin", Age = 20 };
+            Console.WriteLine("{0}은 {1}.",
+                s2.Name, isAdult(s2) ? "성인입니다" : "성인이 아닙니다");
+        }
+
+        public class Student
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
         }
     }
 }
