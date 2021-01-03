e8103caa9e6b1557e6ba327562646dae80967415
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-03 16:03:20 +0900

    std_Cs_Lamda_20210103

--- Cs_Study/Cs_std/23_Lamda_02.cs
+++ Cs_Study/Cs_std/23_Lamda_02.cs
@@ -9,6 +9,19 @@ namespace Lamda_02
         delegate bool IsTeenager(Student student);
         delegate bool IsAdult(Student student);
 
+        static void Main(string[] args)
+        {
+            Func<int, int> square = x => x * x;
+            Console.WriteLine(square(5));
 
+            int[] numbers = { 2, 3, 4, 5 };
+            var squaredNumbers = numbers.Select(x => x * x);
+            Console.WriteLine(string.Join(" ", squaredNumbers));
+
+            Action line = () => Console.WriteLine();
+            line();
+
+
+        }
     }
 }
