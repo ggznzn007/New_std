9b266b4c2718628cb754c5a26256e138958e335c
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-03 15:48:21 +0900

    std_Cs_Lamda_20210103

--- Cs_Study/Cs_std/22_Lamda_01.cs
+++ Cs_Study/Cs_std/22_Lamda_01.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+
+namespace Lamda_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = new[] { 3, 34, 6, 34, 7, 8, 24, 3, 675, 8, 23 };
+
+            int n = Count(arr, x => x % 2 == 0);
+            Console.WriteLine("짝수의 개수: " + n);
+
+            n = Count(arr, x => x % 2 == 1);
+            Console.WriteLine("홀수의 개수: " + n);
+        }
+
+
+    }
+}
