e2bab850f13812d3e5ac78591d2d30feddae747a
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-03 16:24:26 +0900

    std_Cs_Lamda_20210103

--- Cs_Study/Cs_std/23_Lamda_02.cs
+++ Cs_Study/Cs_std/23_Lamda_02.cs
@@ -14,25 +14,25 @@ namespace Lamda_02
             Func<int, int> square = x => x * x;
             Console.WriteLine(square(5));
 
-            int[] numbers = { 2, 3, 4, 5 };
-            var squaredNumbers = numbers.Select(x => x * x);
+            int[] numbers = { 2, 3, 4, 5 };//정수배열의 각 요소를 제곱하여 squaredNumbers에 할당
+            var squaredNumbers = numbers.Select(x => x * x);//string.Join메소드로 빈칸을 사용
             Console.WriteLine(string.Join(" ", squaredNumbers));
 
             Action line = () => Console.WriteLine();
-            line();
+            line();//Action델리게이트로 매개변수없고 빈줄 하나를 출력
 
-            CalcMethod add = (a, b) => a + b;
-            CalcMethod sub = (a, b) => a - b;
-
+            CalcMethod add = (a, b) => a + b;//CalcMethod 델리게이트
+            CalcMethod sub = (a, b) => a - b;//CalcMethod 델리게이트
+            //두 개의 더블 값을 매개변수로 하여 두수를 연산함
             Console.WriteLine(add(10, 20));
             Console.WriteLine(sub(10.5, 20));
 
-            IsTeenager isTeen
+            IsTeenager isTeen//Student 객체를 매개변수로 하여 12보다크고 20보다 작으면 트루 리턴
                 = delegate (Student s) { return s.Age > 12 && s.Age < 20; };
 
-            Student s1 = new Student() { Name = "John", Age = 18 };
+            Student s1 = new Student() { Name = "John", Age = 18 };// s1객체 생성
             Console.WriteLine("{0}은 {1}.",
-                s1.Name, isTeen(s1) ? "청소년입니다" : "청소년이 아닙니다");
+                s1.Name, isTeen(s1) ? "청소년입니다" : "청소년이 아닙니다");//isTeen값 리턴
 
             IsAdult isAdult = (s) =>
             {
