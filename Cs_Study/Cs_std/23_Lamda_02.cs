85670b7d49c94d102764d0004dc81e1ce7abe40e
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-03 15:59:14 +0900

    std_Cs_Lamda_20210103

---
+++ Cs_Study/Cs_std/23_Lamda_02.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace Lamda_02
+{
+    class Program
+    {
+        delegate double CalcMethod(double a, double b);
+        delegate bool IsTeenager(Student student);
+        delegate bool IsAdult(Student student);
+
+
+    }
+}
