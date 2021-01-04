8fcb275ddb7161af897bab68c8a92fdfbb487a71
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-04 16:39:25 +0900

    std_Cs_Linq_Basic_20210104

--- Cs_Study/Cs_std/26_Linq_basic.cs
+++ Cs_Study/Cs_std/26_Linq_basic.cs
@@ -8,7 +8,30 @@ namespace LinqBasic
     {
         static void Main(string[] args)
         {
+            List<int> data = new List<int> { 123, 45, 12, 89, 456, 1, 4, 74, 46 };
+            List<int> lstSortedEven = new List<int>();
 
+            foreach(var item in data)
+            {
+                if (item % 2 == 0)
+                    lstSortedEven.Add(item);
+            }
+            lstSortedEven.Sort();
+
+            Console.WriteLine("Using foreach: ");
+            foreach (var item in lstSortedEven)
+                Console.Write(item + " ");
+            Console.WriteLine();
+
+            var sortedEven = from item in data
+                             where item % 2 == 0
+                             orderby item
+                             select item;
+
+            Console.WriteLine("\nUsing Linq: ");
+            foreach (var item in sortedEven)
+                Console.Write(item + " ");
+            Console.WriteLine();
         }
     }
 }
