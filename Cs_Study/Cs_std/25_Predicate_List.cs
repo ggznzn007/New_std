c08e03e756c1066950d04680a567e11c0ff9b798
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-04 16:18:32 +0900

    std_Cs_Predicate_List_20210104

---
+++ Cs_Study/Cs_std/25_Predicate_List.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListAndLamda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> myList = new List<String>
+            { "mouse","cow","tiger","rabbit","dragon","snake"};
+
+            bool n = myList.Exists(s => s.Contains("x"));
+            Console.WriteLine("이름에 'x'를 포함하는 동물이 있나요: " + n);
+        }
+    }
+}
