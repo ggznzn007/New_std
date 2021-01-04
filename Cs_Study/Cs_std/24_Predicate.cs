4c23e4a085fc4bad008a6e82255d9b69a838801d
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-04 16:08:50 +0900

    std_Cs_Predicate_T_20210104

---
+++ Cs_Study/Cs_std/24_Predicate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Predicate01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> isEven = n => n % 2 == 0;
+            Console.WriteLine(isEven(6));
+
+            Predicate<string> isLowerCase = s => s.Equals(s.ToLower());
+            Console.WriteLine(isLowerCase("This is test"));
+        }
+    }
+}
