40d9d0ab72779945c13aabcb45640108502c44e0
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-04 16:42:47 +0900

    std_Cs_Linq_Basic_20210104

--- Cs_Study/Cs_std/26_Linq_basic.cs
+++ Cs_Study/Cs_std/26_Linq_basic.cs
@@ -9,14 +9,14 @@ namespace LinqBasic
         static void Main(string[] args)
         {//정수 리스트 data를 정의하고 초기화
             List<int> data = new List<int> { 123, 45, 12, 89, 456, 1, 4, 74, 46 };
-            List<int> lstSortedEven = new List<int>();
+            List<int> lstSortedEven = new List<int>();//정렬된 짝수의 리스트를 정의
 
             foreach(var item in data)
-            {
+            {//data리스트에 각 요소가 짝수이면 lstSortedEven에 추가한다
                 if (item % 2 == 0)
                     lstSortedEven.Add(item);
             }
-            lstSortedEven.Sort();
+            lstSortedEven.Sort();//lstSortedEven을 Sort메소드를 사용하여 정렬
 
             Console.WriteLine("Using foreach: ");
             foreach (var item in lstSortedEven)
