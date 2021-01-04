875e5495b210357476bbde40f9f352cdf22d5fe8
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-04 16:21:58 +0900

    std_Cs_Predicate_List_20210104

--- Cs_Study/Cs_std/25_Predicate_List.cs
+++ Cs_Study/Cs_std/25_Predicate_List.cs
@@ -12,6 +12,17 @@ namespace ListAndLamda
 
             bool n = myList.Exists(s => s.Contains("x"));
             Console.WriteLine("이름에 'x'를 포함하는 동물이 있나요: " + n);
+
+            Console.WriteLine("이름이 3글자인 첫 번째 동물: ");
+            string name = myList.Find(s => s.Length == 3);
+            Console.WriteLine(name);
+
+            Console.Write("이름이 6글자 이상의 동물들: ");
+            List<string> longName = myList.FindAll(s => s.Length > 5);
+            foreach(var item in longName)
+            {
+
+            }
         }
     }
 }
