5e86f82bc7f8eded4d200698bd6b65a323ed23be
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-04 16:11:59 +0900

    std_Cs_Predicate_T_20210104

--- Cs_Study/Cs_std/24_Predicate.cs
+++ Cs_Study/Cs_std/24_Predicate.cs
@@ -6,11 +6,13 @@ namespace Predicate01
     {
         static void Main(string[] args)
         {
-            Predicate<int> isEven = n => n % 2 == 0;
-            Console.WriteLine(isEven(6));
+            Predicate<int> isEven = n => n % 2 == 0;// 람다식으로 정의
+            Console.WriteLine(isEven(6));//정수하나를 매개변수로 부울값 리턴
 
             Predicate<string> isLowerCase = s => s.Equals(s.ToLower());
             Console.WriteLine(isLowerCase("This is test"));
+            /*isLowerCase는 string s를 매개변수로 보내고 s를 소문자로 바꾼 결과와 같은지를
+              리턴값으로 보내준다. 맨 앞글자가 대문자 T이므로 false 출력*/
         }
     }
 }
