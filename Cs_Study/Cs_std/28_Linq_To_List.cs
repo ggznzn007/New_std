fb99bf4d70d72f8f2ba9b776c097561bf9c661d1
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-05 16:11:23 +0900

    std_Cs_Linq_To_List_20210105

--- Cs_Study/Cs_std/28_Linq_To_List.cs
+++ Cs_Study/Cs_std/28_Linq_To_List.cs
@@ -7,17 +7,17 @@ namespace LinqToList
     class Program
     {
         static void Main(string[] args)
-        {
+        {// 정수리스트 lstData를 정의하고 초기화
             List<int> lstData = new List<int> { 123, 456, 132, 96, 13, 465, 321 };
-            Print("Data: ", lstData);
+            Print("Data: ", lstData);//Print메소드를 이용하여 lstData내용 출력
 
-            List<int> lstOdd = new List<int>();
-            lstOdd = SelectOddAndSort(lstData);
-            Print("Ordered Odd: ", lstOdd);
+            List<int> lstOdd = new List<int>();// 정수리스트 lstOdd 정의
+            lstOdd = SelectOddAndSort(lstData);// SelectOddAndSort메소드 호출하여 결과값 할당
+            Print("Ordered Odd: ", lstOdd);//Print메소드를 이용하여 lstOdd내용 출력
 
-            int[] arrEven;
-            arrEven = SelectEvenAndSort(lstData);
-            Print("Ordered Even: ", arrEven);
+            int[] arrEven;//정수배열 arrEven 정의
+            arrEven = SelectEvenAndSort(lstData);// SelectOddAndSort메소드 호출하여 결과값 할당
+            Print("Ordered Even: ", arrEven);//Print메소드를 이용하여 arrEven내용 출력
         }
 
         private static List<int> SelectOddAndSort(List<int> lstData)
