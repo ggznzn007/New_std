3f98988140e0e08cdf28215650c0a0f5623090b7
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-05 16:39:20 +0900

    std_Cs_String_20210105

--- Cs_Study/Cs_Basic/02_String.cs
+++ Cs_Study/Cs_Basic/02_String.cs
@@ -6,7 +6,16 @@ namespace _string
     {
         static void Main(string[] args)
         {
+            string a = "hello";
+            string b = "h";
 
+            b = b + "ello";
+            Console.WriteLine(a == b);
+            Console.WriteLine("b = " + b);
+
+            int x = 10;
+            string c = b + '!' + " " + x;
+            Console.WriteLine("c = " + c);
         }
     }
 }
