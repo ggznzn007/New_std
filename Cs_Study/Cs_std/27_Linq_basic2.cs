8e2a706a97b7c9535f2c6d1c108eaf3a32065738
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-05 15:44:02 +0900

    std_Cs_Linq_Basic2_20210105

--- Cs_Study/Cs_std/27_Linq_basic2.cs
+++ Cs_Study/Cs_std/27_Linq_basic2.cs
@@ -16,22 +16,22 @@ namespace LinqBasic2
                           where (item > 20 && item % 2 == 0)
                           orderby item descending
                           select item;
-
+            //data 요소 중 20보다 크고 짝수이면 lstEven에 추가하고 출력
             Print("20보다 큰 짝수 검색결과: ", lstEven);
 
             var lstSorted = from item in lstEven
                             orderby item ascending
                             select item * 2;
-
+            //lstEven 요소에 2를 곱하고 오름차순으로 정렬하여 lstSorted에 추가하고 출력
             Print("2를 곱해서 오름차순 정렬: ", lstSorted);
         }
 
         private static void Print(string s, IEnumerable<int> data)
-        {
+        {//Print 메소드 정의하고 string과 IEnumerable<int>를 매개변수로 사용
             Console.WriteLine(s);
             foreach (var i in data)
                 Console.Write(" " + i);
             Console.WriteLine();
-        }
+        }//문자열을 출력하고 data내용에서 해당내용 출력
     }
 }
