fdd6da46bd9b06a924f3030de801a433ac1e0762
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-05 16:01:03 +0900

    std_Cs_Linq_To_List_20210105

--- Cs_Study/Cs_std/28_Linq_To_List.cs
+++ Cs_Study/Cs_std/28_Linq_To_List.cs
@@ -4,5 +4,22 @@ using System.Linq;
 
 namespace LinqToList
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lstData = new List<int> { 123, 456, 132, 96, 13, 465, 321 };
+            Print("Data: ", lstData);
 
+            List<int> lstOdd = new List<int>();
+            lstOdd = SelectOddAndSort(lstData);
+            Print("Ordered Odd: ", lstOdd);
+
+            int[] arrEven;
+            arrEven = SelectOddAndSort(lstData);
+            Print("Ordered Even: ", arrEven);
+        }
+
+
+    }
 }
