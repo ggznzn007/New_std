48bfeecd9746f19c61c0b9abcea34fa15bc4dff1
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-05 15:30:14 +0900

    std_Cs_Linq_Basic2_20210105

--- Cs_Study/Cs_std/27_Linq_basic2.cs
+++ Cs_Study/Cs_std/27_Linq_basic2.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqBasic2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
