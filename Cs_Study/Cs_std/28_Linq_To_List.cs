ffafb38b47ce4710d270bb8f531226d0d4ae80b9
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-05 16:16:15 +0900

    std_Cs_Linq_To_List_20210105

--- Cs_Study/Cs_std/28_Linq_To_List.cs
+++ Cs_Study/Cs_std/28_Linq_To_List.cs
@@ -16,27 +16,27 @@ namespace LinqToList
             Print("Ordered Odd: ", lstOdd);//Print메소드를 이용하여 lstOdd내용 출력
 
             int[] arrEven;//정수배열 arrEven 정의
-            arrEven = SelectEvenAndSort(lstData);// SelectOddAndSort메소드 호출하여 결과값 할당
+            arrEven = SelectEvenAndSort(lstData);// SelectEvenAndSort메소드 호출하여 결과값 할당
             Print("Ordered Even: ", arrEven);//Print메소드를 이용하여 arrEven내용 출력
         }
 
         private static List<int> SelectOddAndSort(List<int> lstData)
-        {
+        {//SelectOddAndSort메소드 정의
             return (from item in lstData
                     where item % 2 == 1
                     orderby item
                     select item).ToList<int>();
-        }
+        }//List<int>를 매개변수로 받아서 홀수인 자료를 정렬 후 ToList메소드를 통해 리스트로 변환
 
         private static int[] SelectEvenAndSort(List<int> lstData)
-        {
+        {//SelectEvenAndSort메소드 정의
             return (from item in lstData
                     where item % 2 == 0
                     orderby item
                     select item).ToArray<int>();
-        }
+        }//List<int>를 매개변수로 받아서 짝수인 자료를 정렬 후 ToArray메소드를 통해 배열로 변환
 
-        private static void Print(string s, IEnumerable<int> data)
+        private static void Print(string s, IEnumerable<int> data)//Print 메소드 정의
         {
             Console.WriteLine(s);
             foreach (var i in data)
