ae89ae9a9ceeb571f3de2cc104a0ff3282a9b116
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-05 15:32:57 +0900

    std_Cs_Linq_Basic2_20210105

--- Cs_Study/Cs_std/27_Linq_basic2.cs
+++ Cs_Study/Cs_std/27_Linq_basic2.cs
@@ -8,6 +8,17 @@ namespace LinqBasic2
     {
         static void Main(string[] args)
         {
+            List<int> data = new List<int> { 123, 45, 12, 89, 456, 1, 4, 74, 46 };
+
+            Print("data : ", data);
+
+            var lstEven = from item in data
+                          where (item > 20 && item % 2 == 0)
+                          orderby item descending
+                          select item;
+
+            Print("20보다 큰 짝수 검색결과: ", lstEven);
+
 
         }
     }
