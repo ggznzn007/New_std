de2234d066535a91a3c3b167db76aad3213e26c9
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-06 16:45:08 +0900

    std_Cs_B_TypeConversion_20210106

--- Cs_Study/Cs_Basic/04_Flot_Dble_Decml.cs
+++ Cs_Study/Cs_Basic/04_Flot_Dble_Decml.cs
@@ -17,7 +17,7 @@ namespace Float_Double_Decimal
             Console.WriteLine("double : {0}~{1}", double.MinValue, double.MaxValue);
             Console.WriteLine("decimal : {0}~{1}", decimal.MinValue, decimal.MaxValue);*/
 
-            /*//TypeConversion // 02
+            /*// 02 TypeConversion
             int num = 2147483647;
             long bigNum = num;// 암시적 형변환
             Console.WriteLine(bigNum);
@@ -28,7 +28,31 @@ namespace Float_Double_Decimal
             a = (int)x; // 명시적 형변환
             Console.WriteLine(a);*/
 
+            // 03 String to Number
+            string input;
+            int value;
 
+            Console.WriteLine("1. int로 변환할 문자열을 입력하세요: ");
+            input = Console.ReadLine();
+            bool result = Int32.TryParse(input, out value);
+
+            if (!result)
+                Console.WriteLine("' {0}'는 int로 변환될 수 없습니다.\n", input);
+            else
+                Console.WriteLine("int ' {0}' 으로 변환되었습니다.\n", value);
+
+            Console.WriteLine("2. double로 변환할 문자열을 입력하세요: ");
+            input = Console.ReadLine();
+            try
+            {
+                double m = Double.Parse(input);
+                //double m = Convert.ToDouble(input);
+                Console.WriteLine("double '{0}'으로 변환되었습니다.\n", m);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
