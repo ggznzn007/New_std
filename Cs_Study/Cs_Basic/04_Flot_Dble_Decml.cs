4087453cb9f5f7a53e8d1aa9f4d2bfcbfe34a2b1
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-06 16:54:54 +0900

    std_Cs_B_Convert_20210106

--- Cs_Study/Cs_Basic/04_Flot_Dble_Decml.cs
+++ Cs_Study/Cs_Basic/04_Flot_Dble_Decml.cs
@@ -54,6 +54,24 @@ namespace Float_Double_Decimal
                 Console.WriteLine(e.Message);
             }*/
 
+            // 04 Convert
+            int x, y;
+
+            Console.WriteLine("첫번째 숫자를 입력하세요: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("두번째 숫자를 입력하세요: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} + {1} = {2}", x, y, x + y);
+
+            // 2진수, 8진수, 10진수, 16진수로 출력하기
+            short value = short.MaxValue; // Int16.MaxValue
+            Console.WriteLine("\n2진수, 8진수, 10진수, 16진수로 출력하기");
+
+            int baseNum = 2;
+            string s = Convert.ToString(value, baseNum);
+            int i = Convert.ToInt32(s, baseNum);
+            Console.WriteLine("i = {0}, {1,2}진수= {2,16}", i, baseNum, s);
+
 
         }
     }
