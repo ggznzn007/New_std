be6b35958cc2c33c0bbd80ca5a454c5e1e8fa80c
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-06 16:47:49 +0900

    std_Cs_B_TypeConversion_20210106

--- Cs_Study/Cs_Basic/04_Flot_Dble_Decml.cs
+++ Cs_Study/Cs_Basic/04_Flot_Dble_Decml.cs
@@ -28,7 +28,7 @@ namespace Float_Double_Decimal
             a = (int)x; // 명시적 형변환
             Console.WriteLine(a);*/
 
-            // 03 String to Number
+            /*// 03 String to Number
             string input;
             int value;
 
@@ -37,9 +37,9 @@ namespace Float_Double_Decimal
             bool result = Int32.TryParse(input, out value);
 
             if (!result)
-                Console.WriteLine("' {0}'는 int로 변환될 수 없습니다.\n", input);
+                Console.WriteLine("'{0}'는 int로 변환될 수 없습니다.\n", input);
             else
-                Console.WriteLine("int ' {0}' 으로 변환되었습니다.\n", value);
+                Console.WriteLine("int '{0}' 으로 변환되었습니다.\n", value);
 
             Console.WriteLine("2. double로 변환할 문자열을 입력하세요: ");
             input = Console.ReadLine();
@@ -49,10 +49,12 @@ namespace Float_Double_Decimal
                 //double m = Convert.ToDouble(input);
                 Console.WriteLine("double '{0}'으로 변환되었습니다.\n", m);
             }
-            catch(FormatException e)
+            catch (FormatException e)
             {
                 Console.WriteLine(e.Message);
-            }
+            }*/
+
+
         }
     }
 }
