a23fbd4d98c60daa93863f36a6f05241b541378f
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-06 15:41:29 +0900

    std_Cs_B_Console_20210106

--- Cs_Study/Cs_Basic/03_Console_WriteLine.cs
+++ Cs_Study/Cs_Basic/03_Console_WriteLine.cs
@@ -1,21 +1,37 @@
 ﻿using System;
 
-namespace CS_
+namespace CS_01
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int i;
+            /*int i; //01
             double x;
 
             i = 5;
             x = 3.141592;
             Console.WriteLine(" i == " + i + ", x == " + x);
 
-            x = i;
+            x = i;//암시적 형변환
             i = (int)x;
-            Console.WriteLine(" i == " + i + ", x == " + x);
+            Console.WriteLine(" i == " + i + ", x == " + x);*/
+
+            bool b = true;
+            char c = 'A';
+            decimal d = 1.234m;//m은 decimal 형의 접미사
+            double e = 1.23456789;
+            float f = 1.23456789f;//f는 float 형의 접미사
+            int i = 1234;
+            string s = "Hello";
+
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(i);
+            Console.WriteLine(s);
         }
     }
 }
