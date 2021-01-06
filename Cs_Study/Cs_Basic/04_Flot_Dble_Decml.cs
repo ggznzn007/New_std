5cef0ba042e5c6f15887cbc1991881692201d601
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-06 16:34:39 +0900

    std_Cs_B_FlotDecml_20210106

--- Cs_Study/Cs_Basic/04_Flot_Dble_Decml.cs
+++ Cs_Study/Cs_Basic/04_Flot_Dble_Decml.cs
@@ -6,7 +6,7 @@ namespace Float_Double_Decimal
     {
         static void Main(string[] args)
         {
-            float flt = 1F / 3;
+            /*float flt = 1F / 3; // 01
             double dbl = 1D / 3;
             decimal dcm = 1M / 3;
 
@@ -15,7 +15,20 @@ namespace Float_Double_Decimal
                 sizeof(float), sizeof(double), sizeof(decimal));
             Console.WriteLine("float : {0}~{1}", float.MinValue, float.MaxValue);
             Console.WriteLine("double : {0}~{1}", double.MinValue, double.MaxValue);
-            Console.WriteLine("decimal : {0}~{1}", decimal.MinValue, decimal.MaxValue);
+            Console.WriteLine("decimal : {0}~{1}", decimal.MinValue, decimal.MaxValue);*/
+
+            /*//TypeConversion // 02
+            int num = 2147483647;
+            long bigNum = num;// 암시적 형변환
+            Console.WriteLine(bigNum);
+
+            double x = 1234.5;
+            int a;
+
+            a = (int)x; // 명시적 형변환
+            Console.WriteLine(a);*/
+
+
         }
     }
 }
