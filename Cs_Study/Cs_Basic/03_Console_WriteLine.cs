267a1247d1c6eaa1642c37471e135765a503f35a
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-06 15:44:57 +0900

    std_Cs_B_Console_20210106

--- Cs_Study/Cs_Basic/03_Console_WriteLine.cs
+++ Cs_Study/Cs_Basic/03_Console_WriteLine.cs
@@ -6,7 +6,7 @@ namespace CS_01
     {
         static void Main(string[] args)
         {
-            /*int i; //01
+            /*int i; // 01
             double x;
 
             i = 5;
@@ -17,21 +17,30 @@ namespace CS_01
             i = (int)x;
             Console.WriteLine(" i == " + i + ", x == " + x);*/
 
-            bool b = true;
-            char c = 'A';
-            decimal d = 1.234m;//m은 decimal 형의 접미사
-            double e = 1.23456789;
-            float f = 1.23456789f;//f는 float 형의 접미사
-            int i = 1234;
-            string s = "Hello";
+            /* bool b = true;// 02
+             char c = 'A';
+             decimal d = 1.234m;//m은 decimal 형의 접미사
+             double e = 1.23456789;
+             float f = 1.23456789f;//f는 float 형의 접미사
+             int i = 1234;
+             string s = "Hello";
 
-            Console.WriteLine(b);
-            Console.WriteLine(c);
-            Console.WriteLine(d);
-            Console.WriteLine(e);
-            Console.WriteLine(f);
-            Console.WriteLine(i);
-            Console.WriteLine(s);
+             Console.WriteLine(b);
+             Console.WriteLine(c);
+             Console.WriteLine(d);
+             Console.WriteLine(e);
+             Console.WriteLine(f);
+             Console.WriteLine(i);
+             Console.WriteLine(s);*/
+
+            /*// 03
+            Console.WriteLine("10 이하의 소수: {0}, {1}, {2}, {3}", 2, 3, 5, 7);
+
+            string primes;
+            primes = String.Format("10 이하의 소수: {0}, {1}, {2}, {3}", 2, 3, 5, 7);
+            Console.WriteLine(primes);*/
+
+
         }
     }
 }
