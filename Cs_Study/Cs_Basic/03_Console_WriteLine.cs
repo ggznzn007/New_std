f21805eb583eadc328260e6054fbac3774e497a6
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-06 15:35:25 +0900

    std_Cs_B_Console_20210106

--- Cs_Study/Cs_Basic/03_Console_WriteLine.cs
+++ Cs_Study/Cs_Basic/03_Console_WriteLine.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+
+namespace CS_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            double x;
+
+            i = 5;
+            x = 3.141592;
+            Console.WriteLine(" i == " + i + ", x == " + x);
+
+            x = i;
+            i = (int)x;
+            Console.WriteLine(" i == " + i + ", x == " + x);
+        }
+    }
+}
