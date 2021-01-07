78c2c1b90c4c35d06da64aca15393e501304e862
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-07 18:15:25 +0900

    std_Cs_B_Operators_20210107

--- Cs_Study/Cs_Basic/05_Operators.cs
+++ Cs_Study/Cs_Basic/05_Operators.cs
@@ -6,13 +6,18 @@ namespace _Operators
     {
         static void Main(string[] args)
         {
+            /*// 01 Operators
             Console.WriteLine(3 + 4 * 5);
             Console.WriteLine((3 + 4) * 5);
             Console.WriteLine(3 * 4 / 5);
             Console.WriteLine(4 / 5 * 3);
 
             int a = 10, b = 20, c;
-            Console.WriteLine(c = a + b);
+            Console.WriteLine(c = a + b);*/
+
+            // 02 ArithmeticOperators
+            Console.WriteLine("정수의 계산");
+
         }
     }
 }
