4c7f98185848b63c31daa40d81db1e6b57417524
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-08 16:23:38 +0900

    std_CS_B_StringMethods_20210108

--- Cs_Study/Cs_Basic/06_StringMethod.cs
+++ Cs_Study/Cs_Basic/06_StringMethod.cs
@@ -12,6 +12,18 @@ namespace _StringMethods
             Console.WriteLine(s.Length);
             Console.WriteLine(s[8]);
             Console.WriteLine(s.Insert(8, "C# "));
+            Console.WriteLine(s.PadLeft(20, '.'));
+            Console.WriteLine(s.PadRight(20, '.'));
+            Console.WriteLine(s.Remove(6));
+            Console.WriteLine(s.Remove(6,7));
+            Console.WriteLine(s.Replace('l','m'));
+            Console.WriteLine(s.ToLower());
+            Console.WriteLine(s.ToUpper());
+            Console.WriteLine('/' + s.Trim() + '/');
+            Console.WriteLine('/' + s.TrimStart() + '/');
+            Console.WriteLine('/' + s.TrimEnd() + '/');
+
+
 
         }
     }
