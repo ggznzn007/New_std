1f5526c37cba87d4f0863b37da4113d066f6fb05
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-08 16:19:58 +0900

    std_CS_B_StringMethods_20210108

--- Cs_Study/Cs_Basic/06_StringMethod.cs
+++ Cs_Study/Cs_Basic/06_StringMethod.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+
+namespace _StringMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hello, World!";
+            string t;
+
+            Console.WriteLine(s.Length);
+            Console.WriteLine(s[8]);
+            Console.WriteLine(s.Insert(8, "C# "));
+
+        }
+    }
+}
