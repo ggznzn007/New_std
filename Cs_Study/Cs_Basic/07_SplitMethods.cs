af66c99eaab6645ac3da23a5c4b677859d708eeb
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-08 17:14:49 +0900

    std_CS_B_String_Format_20210108

--- Cs_Study/Cs_Basic/07_SplitMethods.cs
+++ Cs_Study/Cs_Basic/07_SplitMethods.cs
@@ -60,7 +60,24 @@ namespace _SplitMethods
                 Console.WriteLine("'{0}' is in the string '{1}'", s2, s1);
             }
 
+            // 04 String Format
+            string max = String.Format("0x{0:X} {0:E} {0:N}", Int64.MaxValue);
+            Console.WriteLine(max);
 
+            Decimal exchangeRate = 1129.20m;
+
+            string s = String.Format("현재 원달러 환율은 {0}입니다.", exchangeRate);
+            Console.WriteLine(s);
+
+            s = String.Format("현재 원달러 환율은 {0:C2}입니다.", exchangeRate);
+            Console.WriteLine(s);
+
+            s = String.Format("오늘 날짜는 {0:d}, 시간은 {0:t} 입니다.", DateTime.Now);
+            Console.WriteLine(s);
+
+            TimeSpan duration = new TimeSpan(1, 12, 23, 62);
+            string output = String.Format("소요 시간: {0:c}", duration);
+            Console.WriteLine(output);
         }
     }
 }
