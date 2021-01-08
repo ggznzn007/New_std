31ae59c6e62f3ddd88d5b82f6656b1c3f184f5ae
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-08 16:49:15 +0900

    std_CS_B_SplitMethods_20210108

---
+++ Cs_Study/Cs_Basic/07_SplitMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _SplitMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*// 01 Spilt Method
+            Console.WriteLine("더하고자 하는 숫자들을 입력하세요: ");
+            string s = Console.ReadLine();
+            Console.WriteLine(s);
+
+            int sum = 0;
+            string[] v = s.Split();
+            foreach(var i in v)
+            {
+                sum += int.Parse(i);
+            }
+            Console.WriteLine("결과는 {0}", sum);*/
+
+            // 02 String Concat
+
+        }
+    }
+}
