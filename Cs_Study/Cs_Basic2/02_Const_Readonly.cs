c5d5016fb2f7479e4e5907e6f7c114be2464e4b0
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-09 17:06:18 +0900

    std_Cs_B_Const_Readonly_20210109

---
+++ Cs_Study/Cs_Basic2/02_Const_Readonly.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _Const_Readonly
+{
+    class ConstEx
+    {
+        public const int number = 3;
+    }
+
+    class ReadonlyEx
+    {
+        public readonly int number = 10;
+        public ReadonlyEx()
+        {
+            number = 20;
+        }
+        public ReadonlyEx(int n)
+        {
+            number = n;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
