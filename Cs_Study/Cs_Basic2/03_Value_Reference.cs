9daa4aedf5cbb6d1958e6e9bf0cd786bd91bd204
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-10 15:35:11 +0900

    std_Cs_B_PassingArray_Object_20210110

--- Cs_Study/Cs_Basic2/03_Value_Reference.cs
+++ Cs_Study/Cs_Basic2/03_Value_Reference.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+
+namespace _Val_Ref
+{
+    class Program
+    {// C언어의 포인터와 비슷한 개념 
+        // 값(value) 형식, 참조(reference) 형식
+        static void Main(string[] args)
+        {
+            string s = "before passing";
+            Console.WriteLine(s);
+
+            Test(s);
+            Console.WriteLine(s);
+
+            Test(ref s);
+            Console.WriteLine(s);
+        }
+
+        public static void Test(string s)
+        {
+            s = "after passing";
+        }
+
+        public static void Test(ref string s)
+        {
+            s = "after passing";
+        }
+    }
+}
