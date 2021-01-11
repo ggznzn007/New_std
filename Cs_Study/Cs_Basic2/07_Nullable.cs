7d4d7a5966c210258b1f777a4bbe4b1a992a21df
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-11 15:38:28 +0900

    Update 07_Nullable.cs

--- Cs_Study/Cs_Basic2/07_Nullable.cs
+++ Cs_Study/Cs_Basic2/07_Nullable.cs
@@ -8,6 +8,18 @@ namespace _Nullable
         {
             Nullable<int> i = null;
             Console.WriteLine(i.GetValueOrDefault());
+
+            if (i.HasValue)
+                Console.WriteLine(i.Value);
+            else
+                Console.WriteLine("Null");
+
+            int? x = null;
+            int j = x ?? 0;
+            Console.WriteLine("x = {0}, j = {1}", x, j);
+
+
+            
         }
     }
 }
