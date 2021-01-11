73a84c3354c8dc0568809c13d99f43b3bc60fdbc
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-11 16:35:01 +0900

    std_Cs_BMI_20210111

--- Cs_Study/Cs_Basic2/11_BMI.cs
+++ Cs_Study/Cs_Basic2/11_BMI.cs
@@ -6,15 +6,19 @@ namespace _BMI
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(" < < < B M I 체질량 지수 계산기 > > >");
+            Console.WriteLine();
             while (true)
             {
                 Console.Write("키를 입력하세요(cm) :  ");
                 double height = double.Parse(Console.ReadLine());
                 height /= 100; // m 단위
+                Console.WriteLine();
 
                 Console.Write("체중을 입력하세요(kg) : ");
                 double weight = double.Parse(Console.ReadLine());
                 double bmi = weight / (height * height);
+                Console.WriteLine();
 
                 string comment = null;
                 if (bmi < 18.5)
@@ -29,6 +33,7 @@ namespace _BMI
                     comment = "고도비만입니다. 그만 좀 쳐먹어!!! 운동 좀 해라!!!";
                 //한국 BMI기준 참고
                 Console.WriteLine("BMI = {0:F1}, \"{1}", bmi, comment);
+                Console.WriteLine();
             }
         }
     }
