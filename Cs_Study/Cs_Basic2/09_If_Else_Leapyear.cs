070a379550a98bd8f6e6156a7864587a4f7c3bdd
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-11 15:56:50 +0900

    std_Cs_If_Else_LeapYear_20210111

--- Cs_Study/Cs_Basic2/09_If_Else_Leapyear.cs
+++ Cs_Study/Cs_Basic2/09_If_Else_Leapyear.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System;
+
+namespace _LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year = int.Parse(Console.ReadLine());
+
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+                Console.WriteLine("{0}는 윤년입니다.", year);
+            else
+                Console.WriteLine("{0}는 평년입니다.", year);
+
+            if (DateTime.IsLeapYear(year))
+                Console.WriteLine("{0}은 윤년", year);
+            else
+                Console.WriteLine("{0}은 평년", year);
+        }
+    }
+}
