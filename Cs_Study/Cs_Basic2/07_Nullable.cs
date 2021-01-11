2ef38e57b6c246a9df61364af491d03ab9e485cf
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-11 15:35:43 +0900

    std_Cs_Nullable_20210111

---
+++ Cs_Study/Cs_Basic2/07_Nullable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _Nullable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> i = null;
+            Console.WriteLine(i.GetValueOrDefault());
+        }
+    }
+}
