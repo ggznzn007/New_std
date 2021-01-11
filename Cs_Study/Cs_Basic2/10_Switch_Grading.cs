70f795c0f88d7d1a8424750642f978c3924d87ad
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-11 16:13:33 +0900

    std_Cs_Switch_Grading_20210111

--- Cs_Study/Cs_Basic2/10_Switch_Grading.cs
+++ Cs_Study/Cs_Basic2/10_Switch_Grading.cs
@@ -1,1 +1,48 @@
-﻿
+﻿using System;
+
+namespace _Grading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("해당학생의 점수를 입력하세요:  ");
+            int score = int.Parse(Console.ReadLine());
+            string grade = null;
+
+            if (score >= 90)
+                grade = "A+";
+            else if (score >= 80)
+                grade = "B+";
+            else if (score >= 70)
+                grade = "C";
+            else if (score >= 60)
+                grade = "D";
+            else 
+                grade = "F";
+
+            Console.WriteLine("학점은 {0}", grade);
+
+            switch (score/10)
+            {
+                case 10:
+                case 9:
+                    grade = "A+";
+                    break;
+                case 8:
+                    grade = "B+";
+                    break;
+                case 7:
+                    grade = "C";
+                    break;
+                case 6:
+                    grade = "D";
+                    break;
+                default:
+                    grade = "F";
+                    break;
+            }
+            Console.WriteLine("학점은 {0}", grade);
+        }
+    }
+}
