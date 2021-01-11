1f22b06a149cb062e0225bd8b5589c9730ccb034
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-11 15:44:09 +0900

    std_Cs_Boxing_Unboxing_20210111

---
+++ Cs_Study/Cs_Basic2/08_Object.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _Object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 123;
+            object o = i; // i값을 박싱하여 o로 복사합니다.
+        }
+    }
+}
