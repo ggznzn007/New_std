ad998b322d0edf41c0ccbad47803c2adcb666738
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-11 16:31:12 +0900

    std_Cs_BMI_20210111

--- Cs_Study/Cs_Basic2/11_BMI.cs
+++ Cs_Study/Cs_Basic2/11_BMI.cs
@@ -6,27 +6,30 @@ namespace _BMI
     {
         static void Main(string[] args)
         {
-            Console.Write("키를 입력하세요(cm) :  ");
-            double height = double.Parse(Console.ReadLine());
-            height /= 100; // m 단위
+            while (true)
+            {
+                Console.Write("키를 입력하세요(cm) :  ");
+                double height = double.Parse(Console.ReadLine());
+                height /= 100; // m 단위
 
-            Console.Write("체중을 입력하세요(kg) : ");
-            double weight = double.Parse(Console.ReadLine());
-            double bmi = weight / (height * height);
+                Console.Write("체중을 입력하세요(kg) : ");
+                double weight = double.Parse(Console.ReadLine());
+                double bmi = weight / (height * height);
 
-            string comment = null;
-            if (bmi < 20)
-                comment = "저체중입니다. 많이 드세요.";
-            else if (bmi < 25)
-                comment = "정상체중입니다. 계속 유지하세요.";
-            else if (bmi < 30)
-                comment = "경도비만입니다. 적당한 운동이 필요해요.";
-            else if (bmi < 40)
-                comment = "비만입니다. 운동을 열심히 하세요!!!";
-            else 
-                comment = "고도비만입니다. 그만 좀 쳐먹어!!! 운동 좀 해라!!!";
-
-            Console.WriteLine("BMI = {0:F1}, \"{1}", bmi, comment);
+                string comment = null;
+                if (bmi < 18.5)
+                    comment = "저체중입니다. 많이 드세요.";
+                else if (bmi < 23)
+                    comment = "정상체중입니다. 계속 유지하세요.";
+                else if (bmi < 25)
+                    comment = "경도비만입니다. 적당한 운동이 필요해요.";
+                else if (bmi < 30)
+                    comment = "비만입니다. 운동을 열심히 하세요!!!";
+                else
+                    comment = "고도비만입니다. 그만 좀 쳐먹어!!! 운동 좀 해라!!!";
+                //한국 BMI기준 참고
+                Console.WriteLine("BMI = {0:F1}, \"{1}", bmi, comment);
+            }
         }
     }
 }
