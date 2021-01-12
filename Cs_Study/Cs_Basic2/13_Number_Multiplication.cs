ba136a56a59cbf1a9e4a8cfb7ac285426c054f65
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-12 15:33:27 +0900

    std_Cs_NumMultiTable_20210112

--- Cs_Study/Cs_Basic2/13_Number_Multiplication.cs
+++ Cs_Study/Cs_Basic2/13_Number_Multiplication.cs
@@ -6,6 +6,7 @@ namespace _NumberMultiTable
     {
         static void Main(string[] args)
         {
+            // 01 NumberSystem
             Console.WriteLine("{0,5} {1,8} {2,3} {3,4}", "10진수", "2진수", "8진수", "16진수");
 
             for (int i = 1; i <= 128; i++)
@@ -15,6 +16,12 @@ namespace _NumberMultiTable
                     Convert.ToString(i, 8),
                     Convert.ToString(i, 16));
             }
+
+            // 02 MultiplicationTable (구구단)
+            Console.WriteLine("구구단의 출력할 단수를 입력하세요 : ");
+            int n = int.Parse(Console.ReadLine());
+            for (int j = 1; j <= 9; j++)
+            { Console.WriteLine("{0} X {1} = {2}", n, j, n * j); }
         }
     }
 }
