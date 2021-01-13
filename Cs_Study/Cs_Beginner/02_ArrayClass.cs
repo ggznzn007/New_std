886e624d140ad6a9f5b594bf61634902d2e5cee4
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-13 16:53:39 +0900

    std_Cs_ArrayClass_20210113

--- Cs_Study/Cs_Beginner/02_ArrayClass.cs
+++ Cs_Study/Cs_Beginner/02_ArrayClass.cs
@@ -6,7 +6,18 @@ namespace _ArrayClass
     {
         static void Main(string[] args)
         {
+            int[] a = { 5, 25, 75, 35, 15 }; // 배열을 선언 후 초기화
+            PrintArray(a);
 
+            int[] b;
+            b = (int[])a.Clone(); // 배열 복사 방법 1
+            PrintArray(b);
+
+            int[] c = new int[10];
+            Array.Copy(a, 0, c, 1, 3); // 배열 복사 방법 2
+            PrintArray(c);
+            
+            
         }
         private static void PrintArray(int[] a)
         {
