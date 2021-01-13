5710191bb509675efa919fbe51781341dcc708f4
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-13 16:50:12 +0900

    std_Cs_ArrayClass_20210113

--- Cs_Study/Cs_Beginner/02_ArrayClass.cs
+++ Cs_Study/Cs_Beginner/02_ArrayClass.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using System;
+
+namespace _ArrayClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        private static void PrintArray(int[] a)
+        {
+            foreach (var i in a)
+                Console.Write("{0,5}", i);
+            Console.WriteLine();
+        }
+    }
+}
