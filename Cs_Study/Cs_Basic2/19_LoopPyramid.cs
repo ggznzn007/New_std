46ba7c12f2f796fb3040f033dbbeec4cb9f48cfe
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-13 16:27:40 +0900

    std_Cs_LoopPyramid_20210113

--- Cs_Study/Cs_Basic2/19_LoopPyramid.cs
+++ Cs_Study/Cs_Basic2/19_LoopPyramid.cs
@@ -41,6 +41,28 @@ namespace _LoopPyramid
                 Console.WriteLine();
             }
             Console.WriteLine();
+
+            // 5)
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5 - i; j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            // 6)
+            for (int i = 5; i >= 1; i--)
+            {
+                for (int j = 1; j <= 5 - i; j++)
+                    Console.Write(" ");
+                for (int j = 1; j <= 2 * i - 1; j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
         }
     }
 }
