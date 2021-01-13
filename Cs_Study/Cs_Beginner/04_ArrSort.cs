673dac6ba4ccdcd998aded497d48c579998d03fd
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-13 17:22:40 +0900

    std_Cs_ArrSort_20210113

--- Cs_Study/Cs_Beginner/04_ArrSort.cs
+++ Cs_Study/Cs_Beginner/04_ArrSort.cs
@@ -6,7 +6,27 @@ namespace _ArrSort
     {
         static void Main(string[] args)
         {
+            string[] EngName = {"Mouse","Cow","Tiger","Rabbit","Dragon","Snake","Horse",
+                "Sheep","Monkey","Chiken","Dog","Pig"};
+            string[] KorName = {"똘기","떵이","호치","새촘이","드라고","요롱이","마초",
+            "미미","뭉치","키키","강다리","찡찡이"};
+            PrintArray("Before Sort: ", EngName);
+            PrintArray("Before Sort: ", KorName);
 
+            Array.Reverse(EngName);
+            Array.Reverse(KorName);
+            PrintArray("After Reverse: ", EngName);
+            PrintArray("After Reverse: ", KorName);
+
+            Array.Sort(EngName);
+            Array.Sort(KorName);
+            PrintArray("After Sort: ", EngName);
+            PrintArray("After Sort: ", KorName);
+
+            Array.Reverse(EngName);
+            Array.Reverse(KorName);
+            PrintArray("After Reverse: ", EngName);
+            PrintArray("After Reverse: ", KorName);
         }
 
         private static void PrintArray(string s, string[] name)
