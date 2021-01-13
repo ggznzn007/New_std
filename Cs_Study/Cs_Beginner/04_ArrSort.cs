f5dc3fc4a1d01ff828c157b145349cd5fe6ccde5
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-13 17:10:30 +0900

    std_Cs_ArrSort_20210113

--- Cs_Study/Cs_Beginner/04_ArrSort.cs
+++ Cs_Study/Cs_Beginner/04_ArrSort.cs
@@ -8,5 +8,13 @@ namespace _ArrSort
         {
 
         }
+
+        private static void PrintArray(string s, string[] name)
+        {
+            Console.WriteLine(s);
+            foreach (var n in name)
+                Console.Write("{0} ", n);
+            Console.WriteLine();
+        }
     }
 }
