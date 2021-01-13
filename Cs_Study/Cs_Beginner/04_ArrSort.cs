9fdf0423f744927bb6fbe593fd631d3c6f97474a
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-13 17:24:55 +0900

    std_Cs_ArrSort_20210113

--- Cs_Study/Cs_Beginner/04_ArrSort.cs
+++ Cs_Study/Cs_Beginner/04_ArrSort.cs
@@ -11,22 +11,22 @@ namespace _ArrSort
             string[] KorName = {"똘기","떵이","호치","새촘이","드라고","요롱이","마초",
             "미미","뭉치","키키","강다리","찡찡이"};
             PrintArray("Before Sort: ", EngName);
-            PrintArray("Before Sort: ", KorName);
+            PrintArray("\nBefore Sort: ", KorName);
 
             Array.Reverse(EngName);
             Array.Reverse(KorName);
-            PrintArray("After Reverse: ", EngName);
-            PrintArray("After Reverse: ", KorName);
+            PrintArray("\nAfter Reverse: ", EngName);
+            PrintArray("\nAfter Reverse: ", KorName);
 
             Array.Sort(EngName);
             Array.Sort(KorName);
-            PrintArray("After Sort: ", EngName);
-            PrintArray("After Sort: ", KorName);
+            PrintArray("\nAfter Sort: ", EngName);
+            PrintArray("\nAfter Sort: ", KorName);
 
             Array.Reverse(EngName);
             Array.Reverse(KorName);
-            PrintArray("After Reverse: ", EngName);
-            PrintArray("After Reverse: ", KorName);
+            PrintArray("\nAfter Reverse: ", EngName);
+            PrintArray("\nAfter Reverse: ", KorName);
         }
 
         private static void PrintArray(string s, string[] name)
