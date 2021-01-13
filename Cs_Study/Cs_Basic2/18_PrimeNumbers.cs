557f2db191a522089f7aad955fff84f55024723e
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-13 16:16:02 +0900

    std_Cs_PrimeNumbers_20210113

--- Cs_Study/Cs_Basic2/18_PrimeNumbers.cs
+++ Cs_Study/Cs_Basic2/18_PrimeNumbers.cs
@@ -4,6 +4,24 @@ namespace _PrimeNumbers
 {
     class Program
     {
-
+        static void Main(string[] args)
+        {
+            int index;
+            int primes = 0;
+            for (int i = 2; i < 1000; i++)
+            {
+                for (index = 2; index < i; index++)
+                {
+                    if (i % index == 0)
+                        break;
+                }
+                if(index ==i)// i가 소수라면
+                {
+                    primes++;
+                    Console.Write("{0,5}{1}", i, primes % 15 == 0 ? "\n" : "");
+                }
+            }
+            Console.WriteLine("\n2부터 1000 사이의 소수의 개수: {0}개", primes);
+        }
     }
 }
