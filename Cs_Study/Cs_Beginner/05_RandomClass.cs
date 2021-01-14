51b1f5afefe7a17c4e6f39028e32b40542036ec7
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-14 15:40:16 +0900

    std_Cs_RandomClass_20210114

--- Cs_Study/Cs_Beginner/05_RandomClass.cs
+++ Cs_Study/Cs_Beginner/05_RandomClass.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+
+namespace _RandomClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            Console.Write("{0,-16}", "Random Bytes");
+            Byte[] b = new byte[5];
+            r.NextBytes(b); // 한번 호출로 배열을 랜덤값으로 채움
+
+            foreach (var x in b)
+                Console.Write("{0,12}", x);// 12자리로 출력
+            Console.WriteLine();
+
+
+        }
+    }
+}
