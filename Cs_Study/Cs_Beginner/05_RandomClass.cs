33cc77dce656820aee79699479b44477c3fc7cb8
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-14 15:49:35 +0900

    std_Cs_RandomClass_20210114

--- Cs_Study/Cs_Beginner/05_RandomClass.cs
+++ Cs_Study/Cs_Beginner/05_RandomClass.cs
@@ -12,10 +12,39 @@ namespace _RandomClass
             r.NextBytes(b); // 한번 호출로 배열을 랜덤값으로 채움
 
             foreach (var x in b)
-                Console.Write("{0,12}", x);// 12자리로 출력
+                Console.Write("{0,12}", x); // 12자리로 출력
             Console.WriteLine();
 
+            Console.Write("{0,-16}", "Random Double");
+            double[] d = new double[5];
 
+            for (int i = 0; i < 5; i++) // 5개 double 랜덤값 생성 저장
+                d[i] = r.NextDouble();
+
+            foreach (var x in d)
+                Console.Write("{0,12:F8}", x); // 12자리, 소수점 아래 8자리로 출력
+            Console.WriteLine();
+
+            Console.Write("{0,-16}", "Random Int32");
+            int[] a = new int[5];
+
+            for (int i = 0; i < 5; i++) // 5개 int 랜덤값 생성 저장
+                a[i] = r.Next();
+            PrintArray(a);
+
+            Console.Write("{0,-16}", "Random 0~99");
+            int[] v = new int[5];
+
+            for (int i = 0; i < 5; i++) // 5개 0~99의 랜덤값 생성 저장
+                v[i] = r.Next(100);
+            PrintArray(v);
+        }
+
+        private static void PrintArray(int[] v)
+        {
+            foreach (var value in v)
+                Console.Write("{0,12}", value);
+            Console.WriteLine();
         }
     }
 }
