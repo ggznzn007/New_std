0aceefbb842a975682a51237895a9fb2a103fec2
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-14 15:54:31 +0900

    std_Cs_Array_Random_20210114

---
+++ Cs_Study/Cs_Beginner/06_Array_Random.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _Array_Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static void PrintArray(int[] v)
+        {
+            for (int i = 0; i < v.Length; i++)
+                Console.Write("{0,5}{1}", v[i], (i % 10 == 9) ? "\n" : "");
+        }
+    }
+}
