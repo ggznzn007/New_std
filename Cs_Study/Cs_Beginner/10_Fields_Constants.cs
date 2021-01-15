0ab7e78c98b64b2960147a0e6841dde43389a0c8
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-15 11:08:02 +0900

    std_Cs_Field_Constants_20210115

---
+++ Cs_Study/Cs_Beginner/10_Fields_Constants.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _Fields_Constants
+{
+    class Product
+    {
+        public string name;
+        public int price;
+    }
+
+    class MyMath
+    { public static double PI = 3.14;}
+
+    class MyCalender
+    {
+        public const int months = 12;
+        public const int weeks = 52;
+        public const int days = 365;
+
+        public const double daysPerWeek = (double)days / (double)weeks;
+        public const double daysPerMonth = (double)days / (double)months;
+    }
+
+    class MemberVariables
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
