668924c06a92a2033f0a4ccb50a6f5ed6994191a
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-16 15:16:48 +0900

    std_Cs_Constructor_20210116

--- Cs_Study/Cs_Beginner/12_Constructtor.cs
+++ Cs_Study/Cs_Beginner/12_Constructtor.cs
@@ -1,1 +1,42 @@
-﻿
+﻿using System;
+
+namespace _Constr
+{
+    class Date
+    {
+        private int year, month, day;
+
+        public Date()
+        {
+            year = 1;
+            month = 1;
+            day = 1;
+        }
+
+        public Date(int y, int m, int d)
+        {
+            year = y;
+            month = m;
+            day = d;
+        }
+
+        public void PrintDay()
+        {
+            Console.WriteLine("{0}/{1}/{2}", year, month, day);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Date birthday = new Date(2000, 11, 22);
+            Date chrismas = new Date(2021, 12, 25);
+            Date firstDay = new Date();
+
+            birthday.PrintDay();
+            chrismas.PrintDay();
+            firstDay.PrintDay();
+        }
+    }
+}
