3d17806507026fc4aea919aceb2488646717c4bd
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-16 15:56:21 +0900

    std_Cs_StaticMethod_20210116

--- Cs_Study/Cs_Beginner/14_StaticMethod.cs
+++ Cs_Study/Cs_Beginner/14_StaticMethod.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+
+namespace _StaticMethd
+{
+    class Methods
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 30, c = 20;
+            //Methods x = new Methods();
+            //Console.WriteLine("가장 큰 수는{0}", (x.Larger(a,b),c));
+            Console.WriteLine("가장 큰 수는{0}", Larger(Larger(a,b),c));
+        }
+
+        private static int Larger(int a, int b) // static입니다.
+        {
+            return (a >= b) ? a : b;
+        }
+
+        /*private static int Larger(int a, int b) // static이 아닙니다.
+        {
+            return (a >= b) ? a : b;
+        }*/
+    }
+}
