316cdd8442c47445d59a9200072a1fdf5bbb199a
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-16 17:02:58 +0900

    std_Cs_PyramidMethod_20210116

---
+++ Cs_Study/Cs_Beginner/18_PyramidMethod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _Pyramid
+{
+    class Pyramid
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\t\t@@@ 피라미드 만들기 @@@\t\t\n");
+            Console.Write("피라미드의 범위를 입력해주세요(1 ~ n) >>  ");
+            int n = int.Parse(Console.ReadLine());
+            DrawPyramid(n);
+        }
+
+        static void DrawPyramid(int n)
+        {
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = i; j < n; j++)
+                    Console.Write(" ");
+                for (int k = 1; k <= 2 * i - 1; k++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+    }
+}
