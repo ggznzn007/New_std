36853be8c8ee8f18d7ac7f928ea109820c4da9f5
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-16 16:06:58 +0900

    std_Cs_IsPrime_20210116

--- Cs_Study/Cs_Beginner/15_IsPrime.cs
+++ Cs_Study/Cs_Beginner/15_IsPrime.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+
+namespace _IsPrm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2 ~ n 까지의 소수를 찾는 프로그램
+            Console.WriteLine("\t\t<<<< 소수 탐색 프로그램 >>>>\t\t");
+            Console.WriteLine("원하는 정수n의 범위를 입력하세요: " + "(ex: 2 ~ n)");
+            int n = int.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 2; i <= n; i++)
+                if (IsPrime(i))
+                {
+                    Console.Write("{0} ", i);
+                    count++;
+                }
+            Console.WriteLine("\n2 ~ n까지 소수는 모두 {0}개 있습니다.", count);
+        }
+
+        private static bool IsPrime(int x)
+        {
+            for (int i = 2; i < x; i++)
+            {
+                if (x % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
