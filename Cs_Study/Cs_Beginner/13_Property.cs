ca93e53ced61a6ca457ec8618ada933efa11964f
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-16 15:33:34 +0900

    std_Cs_Property_20210116

--- Cs_Study/Cs_Beginner/13_Property.cs
+++ Cs_Study/Cs_Beginner/13_Property.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+
+namespace _Propt
+{
+    class Rectangle
+    {
+        private double width;
+        private double height;
+
+        public double GetWidth() // Getter
+        { return width; }
+
+        public double GetHeight()
+        { return height; }
+
+        public void SetWidth(double width) // Setter
+        {
+            if (width > 0)
+                this.width = width;
+        }
+
+        public void SetHeight(double height)
+        {
+            if (height > 0)
+                this.height = height;
+        }
+
+
+    }
+}
