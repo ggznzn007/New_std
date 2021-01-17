e131dd47e7b3b6633a1fd8537fe7d3d4376a59a4
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-17 16:21:22 +0900

    std_Cs_AreaOfCircle_20210117
    원의 면적을 계산하는 메소드

---
+++ Cs_Study/Cs_Beginner/22_Area_Circle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _Area_Cir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\n\t\t@@@ 원의 면적을 계산하는 메소드 @@@\t\t\n");
+                Console.Write("원하는 면적을 정수로 입력해주세요 >> :  ");
+                int n = int.Parse(Console.ReadLine());
+                for (double r = 1; r <= n; r++)
+                    Console.WriteLine("{0}까지의 면적은 = {1,7:F2}", r, AreaOfCircle(r));
+
+            }
+        }
+
+        private static double AreaOfCircle(double r)
+        {
+            return Math.PI * r * r;
+        }
+    }
+}
