7f54237fc54925ba21e5ff7815d2132f826477a9
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-17 17:01:38 +0900

    std_Cs_Recursive_Reciprocal_20210117
    역수를 구하는 재귀 메소드

---
+++ Cs_Study/Cs_Beginner/25_Recursive_Reciprocal.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _Recursive_Reci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" 1 ~ n 까지의 역수의 합을 구합니다. n 을 입력하세요: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(" 1 ~ {0} 까지의 역수의 합: {1}", n, SumOfReci(n));
+        }
+
+        private static double SumOfReci(int n)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return 1.0 / n + SumOfReci(n - 1);
+        }
+    }
+}
