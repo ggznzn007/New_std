8198e7eb53b16dc571be255cea43c745e2da92be
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-17 16:36:56 +0900

    std_Cs_Recursive_Power_20210117
    Power 재귀 메소드
    x 의 y 승

---
+++ Cs_Study/Cs_Beginner/23_Recursive_Power.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _Recursive_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\n\t\t<<< Power 재귀 메소드 >>>\t\t\n");
+                Console.WriteLine("Power(x,y)를 계산합니다.\n");
+                Console.Write(" x를 입력하세요: ");
+                double x = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+                Console.Write(" y를 입력하세요: ");
+                double y = double.Parse(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine(" {0}^{1} = {2}", x, y, Power(x, y));
+            }
+        }
+
+        private static double Power(double x, double y)
+        {
+            if (y == 0)
+                return 1;
+            else
+                return x * Power(x, y - 1);
+        }
+    }
+}
