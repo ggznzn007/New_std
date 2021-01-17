334dc64093c0ad5038f8191e4bd21b80852ef966
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-17 16:49:11 +0900

    std_Cs_Recursive_Factorial_20210117

---
+++ Cs_Study/Cs_Beginner/24_Recursive_Factorial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _Recursive_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\t\t$$$ 팩토리얼 재귀 메소드 $$$\t\t\n");
+            Console.WriteLine("팩토리얼이란 1부터 n까지의 곱을 뜻합니다.\n");
+            while (true)
+            {
+                Console.Write("팩토리얼을 계산합니다. 원하는 숫자를 입력하세요 >>  ");
+                double m = double.Parse(Console.ReadLine());
+                Console.WriteLine("\n{0}! = {1}\n", m, Fact(m));
+            }
+        }
+
+        private static double Fact(double x)
+        {
+            // 1! = 1, n! = n(n-1)!
+            if (x == 1)
+                return 1;
+            else
+                return x * Fact(x - 1);
+        }
+    }
+}
