9abd651d49a5b1199c7d9d8f8e73046ebc54be02
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-17 16:00:22 +0900

    std_Cs_Power_20210117
    n의 m승을 계산하는 메소드

---
+++ Cs_Study/Cs_Beginner/21_Power.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\t\t<< n의 m승을 계산하는 메소드 >>\t\t");
+            for (int i = 0; i <= 20; i++)
+                Console.WriteLine("2^ {0,2} = {1,7}", i, Power(2, i));
+        }
+
+        private static int Power(int n, int m)
+        {
+            int p = 1;
+            for (int i = 1; i <= m; i++)
+                p *= n;
+            return p;
+        }
+    }
+}
