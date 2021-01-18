907e092c0fdec39d6362550a42cea39807010a2f
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-18 16:34:59 +0900

    std_Cs_TimeSpanStr_20210118

--- Cs_Study/Cs_Beginner/30_TimeSpanStr.cs
+++ Cs_Study/Cs_Beginner/30_TimeSpanStr.cs
@@ -1,1 +1,32 @@
-﻿
+﻿using System;
+
+namespace _TimeSpanStr
+{
+    class Pro
+    {
+        static void Main(string[] args)
+        {
+            DateTime christmas = new DateTime(2021, 12, 25);
+            DateTime newYearDay = new DateTime(2022, 1, 1);
+
+            TimeSpan span = newYearDay - christmas;
+            //TimeSpan span = newYearDay.Subtract(christmas);
+            Console.WriteLine("크리스마스와 1월 1일의 시간 간격");
+            Console.WriteLine("{0,14}", span);
+            Console.WriteLine("{0,14} days", span.Days);
+            Console.WriteLine("{0,14} hours", span.Hours);
+            Console.WriteLine("{0,14} minutes", span.Minutes);
+            Console.WriteLine("{0,14} seconds", span.Seconds);
+            Console.WriteLine("{0,14} milliseconds", span.Milliseconds);
+
+            Console.WriteLine("또는");
+            Console.WriteLine("{0,14}", span);
+            Console.WriteLine("{0,14} days", span.TotalDays);
+            Console.WriteLine("{0,14} hours", span.TotalHours);
+            Console.WriteLine("{0,14} minutes", span.TotalMinutes);
+            Console.WriteLine("{0,14} seconds", span.TotalSeconds);
+            Console.WriteLine("{0,14} milliseconds", span.TotalMilliseconds);
+            Console.WriteLine("{0,14} ticks", span.Ticks);
+        }
+    }
+}
