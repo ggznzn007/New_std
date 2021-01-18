d6e07848478ea4fe1353752b344dd9b4e5944b2a
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-18 16:20:50 +0900

    std_Cs_DateTimeStr_20210118

--- Cs_Study/Cs_Beginner/29_DateTimeStr.cs
+++ Cs_Study/Cs_Beginner/29_DateTimeStr.cs
@@ -1,1 +1,53 @@
-﻿
+﻿using System;
+
+namespace _DateTimeStr
+{
+    class Pro
+    {
+        static void Main(string[] args)
+        {
+            DateTime date1 = new DateTime(1992, 7, 22, 8, 44, 0);
+            DateTime date2 = new DateTime(1988, 7, 06, 8, 44, 0);
+
+            Console.WriteLine(date1);
+            Console.WriteLine(date2);
+
+            Console.WriteLine("{0}과 {1}의 차이는 {2}일입니다",
+                date1.ToString("yyyy년 M월 d일"),
+                date2.ToString("yyyy년 M월 d일"),
+                date1.Subtract(date2).Days);
+
+            Console.WriteLine("\n오늘: {0}", DateTime.Today);
+
+            DateTime y = DateTime.Today.AddDays(-1);  // 어제
+            Console.WriteLine("어제: {0}",y.ToShortDateString());
+
+            DateTime t = DateTime.Today.AddDays(1); // 내일
+            Console.WriteLine("내일: {0}", t.ToShortDateString());
+
+            Console.WriteLine("\n2020년은 {0}입니다",
+                DateTime.IsLeapYear(2020) ? "윤년" : "평년");
+            Console.WriteLine("2020년 2월은 {0}일입니다.\n",
+                DateTime.DaysInMonth(2020, 2));
+
+            // Parse and TryParse
+            string date = "1988-7-6 12:6";
+            DateTime aDay = DateTime.Parse(date);
+            Console.WriteLine(aDay);
+
+            string input = "1992/7/22 8:44";
+            DateTime bDay;
+            if(DateTime.TryParse(input,out bDay))
+            {
+                Console.WriteLine(bDay);
+            }
+            Console.WriteLine();
+
+            DateTime d1 = DateTime.Now;
+            DateTime d2 = DateTime.UtcNow;
+
+            Console.WriteLine(d1);
+            Console.WriteLine(d2);
+        }
+    }
+}
