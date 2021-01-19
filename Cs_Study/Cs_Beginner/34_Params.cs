2ccc9331db3fc4788ffc66bc8ee031a1f384137f
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-19 15:30:44 +0900

    std_Cs_Params_20210119

---
+++ Cs_Study/Cs_Beginner/34_Params.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _Params // 가변길이 매개변수 params의 사용방법
+{
+    class Pro
+    {
+        public static void PrintIntParams(params int[] arr)
+        {
+            for(int i = 0; i<arr.Length;i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+
+        public static void PrintObjectParams(params object[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            PrintIntParams(1, 2, 3, 4);
+            PrintObjectParams(1, 1.234, 'a', "test");
+            PrintObjectParams();
+
+            int[] myIntArray = { 5, 6, 7, 8, 9 };
+            PrintIntParams(myIntArray);
+
+            object[] myObjArray = { 2, 2.345, 'b', "test", "again" };
+            PrintObjectParams(myObjArray);
+
+            PrintObjectParams(myIntArray);
+        }
+    }
+}
