08ab0896d2379c713b97825775084c38e32d17fe
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-19 15:38:52 +0900

    std_Cs_Op_Named_Argument_20210119

---
+++ Cs_Study/Cs_Beginner/35_OptionNameArg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _Op_Named_Arg // Optional Argument - 선택적 인수
+                        // Named Argument    - 명명된 인수
+{
+    class Pro
+    {
+        static int MyPower(int x,int y =2)
+        {
+            int result = 1;
+            for (int i = 0; i < y; i++)
+                result *= x; // x의 y승
+            return result;
+        }
+
+        static int Area(int h, int w)
+        {
+            return h * w;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MyPower(4, 2));
+            Console.WriteLine(MyPower(4));
+            Console.WriteLine(MyPower(3,4));
+
+            Console.WriteLine(Area(w: 5, h: 6));
+            Console.WriteLine(Area(h: 6, w: 5));
+        }
+    }
+}
