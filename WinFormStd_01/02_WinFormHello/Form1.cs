9f8a604c16aae2011f35de324f0f95c5fe55a5d5
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-19 16:48:00 +0900

    std_WF_Hello_20210119

---
+++ WinFormStd_01/02_WinFormHello/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _02_WinFormHello
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello! Windows Forms Application!";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "";
+        }
+    }
+}
