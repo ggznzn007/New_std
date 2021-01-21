5255d3bccc32483f05a65a54df92b5e2927be6b4
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-21 17:08:49 +0900

    std_WF_ChechedListBox_20210121

---
+++ WinFormStd_01/15_CheckedListBox/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace _15_CheckedListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cLstBox = new System.Windows.Forms.CheckedListBox();
+            this.lstBox = new System.Windows.Forms.ListBox();
+            this.btnTo = new System.Windows.Forms.Button();
+            this.btnAll = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.btnDelAll = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cLstBox
+            // 
+            this.cLstBox.FormattingEnabled = true;
+            this.cLstBox.Location = new System.Drawing.Point(19, 27);
+            this.cLstBox.Name = "cLstBox";
+            this.cLstBox.Size = new System.Drawing.Size(336, 464);
+            this.cLstBox.TabIndex = 0;
+            // 
+            // lstBox
+            // 
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.ItemHeight = 15;
+            this.lstBox.Location = new System.Drawing.Point(496, 27);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(307, 364);
+            this.lstBox.TabIndex = 1;
+            // 
+            // btnTo
+            // 
+            this.btnTo.Location = new System.Drawing.Point(381, 151);
+            this.btnTo.Name = "btnTo";
+            this.btnTo.Size = new System.Drawing.Size(90, 36);
+            this.btnTo.TabIndex = 2;
+            this.btnTo.Text = ">";
+            this.btnTo.UseVisualStyleBackColor = true;
+            this.btnTo.Click += new System.EventHandler(this.btnTo_Click);
+            // 
+            // btnAll
+            // 
+            this.btnAll.Location = new System.Drawing.Point(381, 209);
+            this.btnAll.Name = "btnAll";
+            this.btnAll.Size = new System.Drawing.Size(90, 36);
+            this.btnAll.TabIndex = 2;
+            this.btnAll.Text = ">>";
+            this.btnAll.UseVisualStyleBackColor = true;
+            this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(381, 269);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(90, 36);
+            this.btnBack.TabIndex = 2;
+            this.btnBack.Text = "<";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // btnDelAll
+            // 
+            this.btnDelAll.Location = new System.Drawing.Point(575, 423);
+            this.btnDelAll.Name = "btnDelAll";
+            this.btnDelAll.Size = new System.Drawing.Size(228, 56);
+            this.btnDelAll.TabIndex = 2;
+            this.btnDelAll.Text = "모두 삭제";
+            this.btnDelAll.UseVisualStyleBackColor = true;
+            this.btnDelAll.Click += new System.EventHandler(this.btnDelAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(831, 521);
+            this.Controls.Add(this.btnDelAll);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnAll);
+            this.Controls.Add(this.btnTo);
+            this.Controls.Add(this.lstBox);
+            this.Controls.Add(this.cLstBox);
+            this.Name = "Form1";
+            this.Text = "Travel Wish List";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox cLstBox;
+        private System.Windows.Forms.ListBox lstBox;
+        private System.Windows.Forms.Button btnTo;
+        private System.Windows.Forms.Button btnAll;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Button btnDelAll;
+    }
+}
+
---
+++ WinFormStd_01/15_CheckedListBox/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _15_CheckedListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cLstBox.Items.Add("대한민국, 제주도");
+            cLstBox.Items.Add("오스트리아, 빈");
+            cLstBox.Items.Add("호주, 시드니");
+            cLstBox.Items.Add("캐나다, 벤쿠버");
+            cLstBox.Items.Add("호주, 브리즈번");
+            cLstBox.Items.Add("캐나다, 토론토");
+            cLstBox.Items.Add("덴마크, 코펜하겐");
+            cLstBox.Items.Add("중국, 사천");
+            cLstBox.Items.Add("태국, 방콕");
+            cLstBox.Items.Add("뉴질랜드, 웰링턴");
+            cLstBox.Items.Add("이탈리아, 나폴리");
+            cLstBox.Items.Add("영국, 런던");
+            cLstBox.Items.Add("아일랜드, 더블린");
+            lstBox.SelectionMode = SelectionMode.MultiExtended;
+        }
+
+        private void btnTo_Click(object sender, EventArgs e)
+        {
+            foreach (var city in cLstBox.CheckedItems)
+                lstBox.Items.Add(city);
+        }
+
+        private void btnAll_Click(object sender, EventArgs e)
+        {
+            foreach (var city in cLstBox.Items)
+                lstBox.Items.Add(city);
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            List<string> lstRemove = new List<string>();
+
+            foreach (string city in lstBox.SelectedItems)
+                lstRemove.Add(city);
+
+            foreach (string city in lstRemove)
+                lstBox.Items.Remove(city);
+        }
+
+        private void btnDelAll_Click(object sender, EventArgs e)
+        {
+            lstBox.Items.Clear();
+        }
+    }
+}
