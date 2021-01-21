c868332fe5bc08e0092350b60b6915495aca2255
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-21 15:41:14 +0900

    std_WF_RGBscr_20210121

---
+++ WinFormStd_01/12_RgbScrollBar/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace _12_RgbScrollBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.scrR = new System.Windows.Forms.HScrollBar();
+            this.scrG = new System.Windows.Forms.HScrollBar();
+            this.scrB = new System.Windows.Forms.HScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtR = new System.Windows.Forms.TextBox();
+            this.txtG = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(43, 21);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(722, 222);
+            this.panel1.TabIndex = 0;
+            // 
+            // scrR
+            // 
+            this.scrR.Location = new System.Drawing.Point(143, 278);
+            this.scrR.Name = "scrR";
+            this.scrR.Size = new System.Drawing.Size(472, 41);
+            this.scrR.TabIndex = 1;
+            this.scrR.Tag = "";
+            this.scrR.Scroll += new System.Windows.Forms.ScrollEventHandler(this.scr_Scroll);
+            // 
+            // scrG
+            // 
+            this.scrG.Location = new System.Drawing.Point(143, 344);
+            this.scrG.Name = "scrG";
+            this.scrG.Size = new System.Drawing.Size(472, 41);
+            this.scrG.TabIndex = 1;
+            this.scrG.Tag = "";
+            this.scrG.Scroll += new System.Windows.Forms.ScrollEventHandler(this.scr_Scroll);
+            // 
+            // scrB
+            // 
+            this.scrB.Location = new System.Drawing.Point(143, 414);
+            this.scrB.Name = "scrB";
+            this.scrB.Size = new System.Drawing.Size(472, 41);
+            this.scrB.TabIndex = 1;
+            this.scrB.Tag = "";
+            this.scrB.Scroll += new System.Windows.Forms.ScrollEventHandler(this.scr_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("돋움", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(48, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Red";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("돋움", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(48, 344);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Green";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("돋움", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(48, 414);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Blue";
+            // 
+            // txtR
+            // 
+            this.txtR.Font = new System.Drawing.Font("돋움", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtR.Location = new System.Drawing.Point(648, 278);
+            this.txtR.Name = "txtR";
+            this.txtR.Size = new System.Drawing.Size(117, 34);
+            this.txtR.TabIndex = 3;
+            this.txtR.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            // 
+            // txtG
+            // 
+            this.txtG.Font = new System.Drawing.Font("돋움", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtG.Location = new System.Drawing.Point(648, 344);
+            this.txtG.Name = "txtG";
+            this.txtG.Size = new System.Drawing.Size(117, 34);
+            this.txtG.TabIndex = 3;
+            this.txtG.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            // 
+            // txtB
+            // 
+            this.txtB.Font = new System.Drawing.Font("돋움", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtB.Location = new System.Drawing.Point(648, 414);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(117, 34);
+            this.txtB.TabIndex = 3;
+            this.txtB.TextChanged += new System.EventHandler(this.txt_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 503);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtG);
+            this.Controls.Add(this.txtR);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.scrB);
+            this.Controls.Add(this.scrG);
+            this.Controls.Add(this.scrR);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "RGB ScrollBar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.HScrollBar scrR;
+        private System.Windows.Forms.HScrollBar scrG;
+        private System.Windows.Forms.HScrollBar scrB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtR;
+        private System.Windows.Forms.TextBox txtG;
+        private System.Windows.Forms.TextBox txtB;
+    }
+}
+
