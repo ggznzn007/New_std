facb752e17536272d62d7cf3f32acb7da232c99b
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-21 16:08:48 +0900

    std_WF_ListBox_20210121

---
+++ WinFormStd_01/13_ListBox/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace _13_ListBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.txtSIndex1 = new System.Windows.Forms.TextBox();
+            this.txtSIndex2 = new System.Windows.Forms.TextBox();
+            this.txtSIndex3 = new System.Windows.Forms.TextBox();
+            this.txtSItem1 = new System.Windows.Forms.TextBox();
+            this.txtSItem2 = new System.Windows.Forms.TextBox();
+            this.txtSItem3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Items.AddRange(new object[] {
+            "미국",
+            "러시아",
+            "중국",
+            "영국",
+            "독일",
+            "프랑스",
+            "일본",
+            "이스라엘",
+            "사우디아라비아",
+            "UAE",
+            "한국",
+            "태국",
+            "필리핀",
+            "인도",
+            "방글라데시",
+            "캐나다",
+            "뉴질랜드"});
+            this.listBox1.Location = new System.Drawing.Point(167, 58);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(217, 289);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 15;
+            this.listBox2.Location = new System.Drawing.Point(409, 58);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(217, 289);
+            this.listBox2.TabIndex = 0;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 15;
+            this.listBox3.Location = new System.Drawing.Point(644, 58);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(217, 289);
+            this.listBox3.TabIndex = 0;
+            this.listBox3.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            // 
+            // txtSIndex1
+            // 
+            this.txtSIndex1.Font = new System.Drawing.Font("맑은 고딕", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtSIndex1.Location = new System.Drawing.Point(167, 397);
+            this.txtSIndex1.Name = "txtSIndex1";
+            this.txtSIndex1.Size = new System.Drawing.Size(221, 38);
+            this.txtSIndex1.TabIndex = 1;
+            // 
+            // txtSIndex2
+            // 
+            this.txtSIndex2.Font = new System.Drawing.Font("맑은 고딕", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtSIndex2.Location = new System.Drawing.Point(409, 397);
+            this.txtSIndex2.Name = "txtSIndex2";
+            this.txtSIndex2.Size = new System.Drawing.Size(221, 38);
+            this.txtSIndex2.TabIndex = 1;
+            // 
+            // txtSIndex3
+            // 
+            this.txtSIndex3.Font = new System.Drawing.Font("맑은 고딕", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtSIndex3.Location = new System.Drawing.Point(644, 397);
+            this.txtSIndex3.Name = "txtSIndex3";
+            this.txtSIndex3.Size = new System.Drawing.Size(221, 38);
+            this.txtSIndex3.TabIndex = 1;
+            // 
+            // txtSItem1
+            // 
+            this.txtSItem1.Font = new System.Drawing.Font("맑은 고딕", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtSItem1.Location = new System.Drawing.Point(167, 453);
+            this.txtSItem1.Name = "txtSItem1";
+            this.txtSItem1.Size = new System.Drawing.Size(221, 38);
+            this.txtSItem1.TabIndex = 1;
+            // 
+            // txtSItem2
+            // 
+            this.txtSItem2.Font = new System.Drawing.Font("맑은 고딕", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtSItem2.Location = new System.Drawing.Point(409, 453);
+            this.txtSItem2.Name = "txtSItem2";
+            this.txtSItem2.Size = new System.Drawing.Size(221, 38);
+            this.txtSItem2.TabIndex = 1;
+            // 
+            // txtSItem3
+            // 
+            this.txtSItem3.Font = new System.Drawing.Font("맑은 고딕", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtSItem3.Location = new System.Drawing.Point(644, 453);
+            this.txtSItem3.Name = "txtSItem3";
+            this.txtSItem3.Size = new System.Drawing.Size(221, 38);
+            this.txtSItem3.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("돋움", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(12, 397);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 19);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SelectedIndex : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("돋움", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(12, 465);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "SelectedItem : ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 517);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSIndex3);
+            this.Controls.Add(this.txtSIndex2);
+            this.Controls.Add(this.txtSItem3);
+            this.Controls.Add(this.txtSItem2);
+            this.Controls.Add(this.txtSItem1);
+            this.Controls.Add(this.txtSIndex1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "List Box";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.TextBox txtSIndex1;
+        private System.Windows.Forms.TextBox txtSIndex2;
+        private System.Windows.Forms.TextBox txtSIndex3;
+        private System.Windows.Forms.TextBox txtSItem1;
+        private System.Windows.Forms.TextBox txtSItem2;
+        private System.Windows.Forms.TextBox txtSItem3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WinFormStd_01/13_ListBox/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _13_ListBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox2.Items.Add("미국, 뉴욕");
+            listBox2.Items.Add("러시아, 모스크바");
+            listBox2.Items.Add("중국, 베이징");
+            listBox2.Items.Add("영국, 런던");
+            listBox2.Items.Add("독일, 베를린");
+            listBox2.Items.Add("프랑스, 파리");
+            listBox2.Items.Add("일본, 도쿄");
+            listBox2.Items.Add("이스라엘, 예루살렘");
+            listBox2.Items.Add("사우디아라비아, 리야드");
+            listBox2.Items.Add("UAE, 아부다비");
+            listBox2.Items.Add("한국, 서울");
+            listBox2.Items.Add("태국, 방콕");
+            listBox2.Items.Add("필리핀, 마닐라");
+            listBox2.Items.Add("방글라데시, 다카");
+            listBox2.Items.Add("캐나다, 오타와");
+            listBox2.Items.Add("뉴질랜드, 웰링턴");
+           
+
+            List<string> lstGDP = new List<string> { "미국","러시아","중국",
+                "영국","독일","프랑스","일본","이스라엘","사우디아라비아","UAE",
+            "한국","태국","필리핀","인도","방글라데시","캐나다","뉴질랜드"};
+            listBox3.DataSource = lstGDP;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox lst = sender as ListBox;
+            txtSIndex1.Text = lst.SelectedIndex.ToString();
+            txtSItem1.Text = lst.SelectedItem.ToString();
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox lst = sender as ListBox;
+            txtSIndex2.Text = lst.SelectedIndex.ToString();
+            txtSItem2.Text = lst.SelectedItem.ToString();
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox lst = sender as ListBox;
+            txtSIndex3.Text = lst.SelectedIndex.ToString();
+            txtSItem3.Text = lst.SelectedItem.ToString();
+        }
+    }
+}
