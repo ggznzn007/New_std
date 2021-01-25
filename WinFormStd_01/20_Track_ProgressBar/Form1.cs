61c5e9cebd11882dffc313673fdaf041b3994641
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-25 16:18:49 +0900

    std_WF_Track_Progress_20210125

---
+++ WinFormStd_01/20_Track_ProgressBar/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20_Track_ProgressBar
+{
+    public partial class Form1 : Form
+    {
+        Timer timer1 = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+
+            timer1.Interval = 100; // 0.1초
+
+        }
+    }
+}
