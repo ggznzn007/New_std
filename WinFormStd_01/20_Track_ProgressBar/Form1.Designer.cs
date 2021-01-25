6c2fccdd53a3661f2c95db81729b82a9270ccdc2
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-25 16:24:35 +0900

    std_WF_Track_Progress_20210125

--- WinFormStd_01/20_Track_ProgressBar/Form1.Designer.cs
+++ WinFormStd_01/20_Track_ProgressBar/Form1.Designer.cs
@@ -59,6 +59,7 @@ namespace _20_Track_ProgressBar
             this.button1.TabIndex = 2;
             this.button1.Text = "시작";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // lblTrack
             // 
--- WinFormStd_01/20_Track_ProgressBar/Form1.cs
+++ WinFormStd_01/20_Track_ProgressBar/Form1.cs
@@ -18,7 +18,35 @@ namespace _20_Track_ProgressBar
             InitializeComponent();
 
             timer1.Interval = 100; // 0.1초
+            timer1.Tick += Timer1_Tick;
 
+            trackBar1.Minimum = 0;
+            trackBar1.Maximum = 100;
+            trackBar1.Value = 0;
+
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = 100;
+            progressBar1.Value = 0;
+        }
+
+        private void Timer1_Tick(object sender,EventArgs e)
+        {
+            if(trackBar1.Value<100)
+            {
+                trackBar1.Value++;
+                progressBar1.Value++;
+                lblTrack.Text = trackBar1.Value.ToString();
+                lblProgress.Text = progressBar1.Value.ToString();
+            }
+            else
+            {
+                timer1.Stop();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
         }
     }
 }
