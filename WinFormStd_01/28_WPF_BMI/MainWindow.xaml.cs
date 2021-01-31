99fa4ecc91f6b15c479c830835fe0e45a023198d
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-01-31 15:42:31 +0900

    std_WPF_BMI_20210131

--- WinFormStd_01/28_WPF_BMI/MainWindow.xaml.cs
+++ WinFormStd_01/28_WPF_BMI/MainWindow.xaml.cs
@@ -24,5 +24,19 @@ namespace _28_WPF_BMI
         {
             InitializeComponent();
         }
+
+        private void btnBMI_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtHeight.Text == ""||txtWeight.Text=="")
+            {
+                lblResult.Content = "키와 체중을 입력하세요";
+                return;
+            }
+            double h = Convert.ToDouble(txtHeight.Text) / 100.0;
+            double w = Double.Parse(txtWeight.Text);
+            double bmi = w / (h * h);
+            // Form에서는 Label.Text 인데, WPF에서는 label.Content
+            lblResult.Content = string.Format("당신의 비만도는 {0:F2} 입니다", bmi);
+        }
     }
 }
