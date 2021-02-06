de644b9b87861b179b53aa5ec1c5013abff2271c
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-02-06 15:03:30 +0900

    std_WF_Analog_Clock_20210206

---
+++ WinFormStd_01/34_WF_Analog_Clock/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _34_WF_Analog_Clock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
