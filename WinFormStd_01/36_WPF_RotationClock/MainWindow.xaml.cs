305c2c80e7d47acc5e79342eeef8a1c11a6263f0
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-02-08 15:09:55 +0900

    std_WPF_RotationClock_20210208

--- WinFormStd_01/36_WPF_RotationClock/MainWindow.xaml.cs
+++ WinFormStd_01/36_WPF_RotationClock/MainWindow.xaml.cs
@@ -20,9 +20,45 @@ namespace _36_WPF_RotationClock
     /// </summary>
     public partial class MainWindow : Window
     {
+        // 시침, 분침, 초침의 각도 (12시 방향 기준, 시계 방향)
+        private double hDeg;
+        private double sDeg;
+        private double mDeg;
+        
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        // 시계판 그리기
+        private void DrawFace()
+        {
+            // 눈금 60개를 Line 배열로 만든다
+            Line[] marking = new Line[60];
+            int W = 300; // 시계 넗이
+
+            for(int i = 0; i<60;i++)
+            {
+                marking[i] = new Line();
+                marking[i].Stroke = Brushes.HotPink;
+                marking[i].X1 = W / 2;
+                marking[i].Y1 = 2;
+                marking[i].X2 = W / 2;
+                if(i%5==0) // 매 다섯번째 눈금은 큰 눈금으로 한다
+                {
+                    marking[i].StrokeThickness = 5;
+                    marking[i].Y2 = 20;
+                }
+                else
+                {
+                    marking[i].StrokeThickness = 2;
+                    marking[i].Y2 = 10;
+                }
+
+                // 눈금 하나 당 중심점을 기준으로 6도씩 회전(RotationTransform)
+                RotateTransform rt = new RotateTransform(6 * i);
+                rt.CenterX = 150; // 회전 중심점
+            }
         }
     }
 }
