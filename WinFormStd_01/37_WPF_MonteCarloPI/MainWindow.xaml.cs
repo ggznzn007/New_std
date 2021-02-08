e3142166c5f035564a10c528caa327f5bc0d1f70
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-02-08 16:33:00 +0900

    std_WPF_MonteCarloPI_20210208

--- WinFormStd_01/37_WPF_MonteCarloPI/MainWindow.xaml.cs
+++ WinFormStd_01/37_WPF_MonteCarloPI/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
+
+
 
 namespace _37_WPF_MonteCarloPI
 {
@@ -20,9 +23,53 @@ namespace _37_WPF_MonteCarloPI
     /// </summary>
     public partial class MainWindow : Window
     {
+        int iCnt = 0;
+        int oCnt = 0;
+
+        DispatcherTimer timer = new DispatcherTimer();
+        Random r = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            timer.Interval = new TimeSpan(10000); // 1ms
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            /*Rectangle rect = new Rectangle();
+            rect.Width = 5;
+            rect.Height = 5;*/
+            Ellipse elps = new Ellipse();
+            elps.Width = 4;
+            elps.Height = 4;
+
+
+            int x = r.Next(0, 400);
+            int y = r.Next(0, 400);
+
+            if ((x - 200) * (x - 200) + (y - 200) * (y - 200) <= 40000)
+            {
+                elps.Stroke = Brushes.Red;
+                elps.Fill = Brushes.Red;
+                iCnt++;
+            }
+            else
+            {
+                elps.Stroke = Brushes.Blue;
+                elps.Fill = Brushes.Blue;
+                oCnt++;
+            }
+            int count = iCnt + oCnt;
+            double pi = (double)iCnt / count * 4;
+            txtStatus.Text = "n = " + count + ", In: " + iCnt + "," +
+                "Out:  " + oCnt + ", PI = " + pi;
+            Canvas.SetLeft(elps, x);
+            Canvas.SetTop(elps, y);
+            canvas1.Children.Add(elps);
         }
     }
 }
