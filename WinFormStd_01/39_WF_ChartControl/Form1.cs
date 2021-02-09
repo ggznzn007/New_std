fd75a259b4750b7f14b139980b02b8658f21c2cd
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-02-09 16:01:57 +0900

    std_WF_ChartControl_20210209

--- WinFormStd_01/39_WF_ChartControl/Form1.cs
+++ WinFormStd_01/39_WF_ChartControl/Form1.cs
@@ -25,7 +25,7 @@ namespace _39_WF_ChartControl
             chart1.Titles.Add("중간고사 성적");
             for(int i = 0;i<10;i++)
             {
-                chart1.Series["Series1"].Points.Add(r.Next(100));
+                chart1.Series["Series1"].Points.Add(r.Next(50,100));
             }
             chart1.Series["Series1"].LegendText = "영어";
             chart1.Series["Series1"].ChartType = SeriesChartType.Line;
