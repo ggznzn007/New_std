50dc0f167649af82ae50416af57b3afdd05ea9c7
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-02-09 15:59:22 +0900

    std_WF_ChartControl_20210209

---
+++ WinFormStd_01/39_WF_ChartControl/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace _39_WF_ChartControl
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Using Chart Control";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            chart1.Titles.Add("중간고사 성적");
+            for(int i = 0;i<10;i++)
+            {
+                chart1.Series["Series1"].Points.Add(r.Next(100));
+            }
+            chart1.Series["Series1"].LegendText = "영어";
+            chart1.Series["Series1"].ChartType = SeriesChartType.Line;
+        }
+    }
+}
