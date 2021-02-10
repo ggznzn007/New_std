01bcda23c8fcfef7f753cb6118c6686b79e2f1f4
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-02-10 16:07:09 +0900

    std_WF_GraphWithChart_20210210

--- WinFormStd_01/41_WF_GraphWithChart/Form1.cs
+++ WinFormStd_01/41_WF_GraphWithChart/Form1.cs
@@ -16,6 +16,40 @@ namespace _41_WF_GraphWithChart
         public Form1()
         {
             InitializeComponent();
+            this.Text = "Graph using Chart";
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            chart1.ChartAreas[0].BackColor = Color.Black;
+
+            // ChartArea X축과 Y축을 설정
+            chart1.ChartAreas[0].AxisX.Minimum = -20;
+            chart1.ChartAreas[0].AxisX.Maximum = 20;
+            chart1.ChartAreas[0].AxisX.Interval = 2;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineColor = Color.Pink;
+            chart1.ChartAreas[0].AxisX.MajorGrid.LineDashStyle = 
+                ChartDashStyle.Dash;
+
+            chart1.ChartAreas[0].AxisY.Minimum = -2;
+            chart1.ChartAreas[0].AxisY.Maximum = 2;
+            chart1.ChartAreas[0].AxisY.Interval = 0.5;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineColor = Color.HotPink;
+            chart1.ChartAreas[0].AxisY.MajorGrid.LineDashStyle =
+                ChartDashStyle.Dash;
+
+            // Series 설정(Sin)
+            chart1.Series[0].ChartType = SeriesChartType.Line;
+            chart1.Series[0].Color = Color.LightBlue;
+            chart1.Series[0].BorderWidth = 2;
+            chart1.Series[0].LegendText = "sin(x)/x";
+
+            // Series 추가, 설정(Cos)
+            if(chart1.Series.Count==1)
+            {
+                chart1.Series.Add("Cos");
+                chart1.Series["Cos"].ChartType = SeriesChartType.Line;
+            }
         }
     }
 }
