bd5b4eedc2bc1f403da74805a4455573fab03f99
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-02-11 15:22:20 +0900

    std_WPF_MatchingGame_20210211

--- WinFormStd_01/44_WPF_MatchingGame/MainWindow.xaml.cs
+++ WinFormStd_01/44_WPF_MatchingGame/MainWindow.xaml.cs
@@ -29,7 +29,14 @@ namespace _44_WPF_MatchingGame
         public MainWindow()
         {
             InitializeComponent();
+            BoardSet();
+            myTimer.Interval = new TimeSpan(0, 0, 0, 0, 750); // 0.75초
+            myTimer.Tick += MyTimer_Tick;
+        }
 
+        private void MyTimer_Tick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
         }
 
         private void BoardSet() // 16버튼 초기화
@@ -39,7 +46,45 @@ namespace _44_WPF_MatchingGame
                 Button c = new Button();
                 c.Background = Brushes.White;
                 c.Margin = new Thickness(10);
+                c.Content = MakeImage("../../Images/check.png");
+                c.Tag = TagSet(); // 그림의 인덱스 세팅
+                c.Click += C_Click;
+                board.Children.Add(c);
             }
+        }
+
+        private void C_Click(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private Image MakeImage(string v) // v는 이미지 파일의 경로
+        {
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.UriSource = new Uri(v, UriKind.Relative);
+            bi.EndInit();
+            Image myImage = new Image();
+            myImage.Margin = new Thickness(10);
+            myImage.Stretch = Stretch.Fill;
+            myImage.Source = bi;
+            return myImage;
+        }
+
+        private int TagSet() // 중복되지 않는 i = 0 ~ 15 생성, i % 8 리턴
+        {
+            int i;
+            Random r = new Random();
+            while(true)
+            {
+                i = r.Next(16); // 0 ~ 15까지
+                if(rnd[i]==0)
+                {
+                    rnd[i] = 1;
+                    break;
+                }
+            }
+            return i % 8; // 태그는 0~7까지, 8개의 그림을 표시
         }
     }
 }
