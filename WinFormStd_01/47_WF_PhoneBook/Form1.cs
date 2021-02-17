3fe1a0475467f49d3773825f30a01c630502e3a2
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-02-17 14:24:26 +0900

    std_WF_AccessDB_20210217

--- WinFormStd_01/47_WF_PhoneBook/Form1.cs
+++ WinFormStd_01/47_WF_PhoneBook/Form1.cs
@@ -6,9 +6,42 @@ namespace _47_WF_PhoneBook
 {
     public partial class Form1 : Form
     {
+        OleDbConnection conn = null;
+        OleDbCommand comm = null;
+        OleDbDataReader reader = null;
+
+        string connStr = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\ggznz\reposit\New\WinFormStd_01\47_WF_PhoneBook\Student.accdb";
         public Form1()
         {
             InitializeComponent();
+            DisplayStudents();
+        }
+
+        private void DisplayStudents()
+        {
+            if(conn==null)
+            {
+                conn = new OleDbConnection(connStr);
+                conn.Open();
+            }
+
+            string sql = "SELECT * FROM StudentTable";
+            comm = new OleDbCommand(sql, conn);
+
+            reader = comm.ExecuteReader();
+            while(reader.Read())
+            {
+                string x = "";
+                x += reader["ID"] + "\t";
+                x += reader["SID"] + "\t";
+                x += reader["SName"] + "\t";
+                x += reader["Phone"];
+
+                listBox1.Items.Add(x);
+            }
+            reader.Close();
+            conn.Close();
+            conn = null;
         }
     }
 }
