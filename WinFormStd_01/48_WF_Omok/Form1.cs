a84599c31ead09c85b9065a4a4ef5eba70f42539
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-02-18 15:48:52 +0900

    std_WF_Omok_20210218

--- WinFormStd_01/48_WF_Omok/Form1.cs
+++ WinFormStd_01/48_WF_Omok/Form1.cs
@@ -27,5 +27,34 @@ namespace _48_WF_Omok
             this.ClientSize = new Size(2 * margin + 18 * squareSize,
                 2 * margin + 18 * squareSize + menuStrip1.Height);
         }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            DrawBoard();
+        }
+
+        private void DrawBoard()
+        {
+            g = panel1.CreateGraphics();
+
+            for(int i =0; i<19;i++)
+            {
+                g.DrawLine(pen, new Point(margin + i * squareSize, margin),// 세로선
+                    new Point(margin + i * squareSize, margin + 18 * squareSize));
+                g.DrawLine(pen, new Point(margin, margin + i * squareSize),// 가로선
+                    new Point(margin + 18 * squareSize, margin + i * squareSize));
+            }
+
+            // 화점 그리기
+            for(int x = 3; x<=15;x+=6)
+                for(int y = 3; y<=15;y+=6)
+                {
+                    g.FillEllipse(bBrush,
+                        margin + squareSize * x - pointSize / 2,
+                        margin + squareSize * y - pointSize / 2,
+                        pointSize, pointSize);
+                }
+        }
     }
 }
