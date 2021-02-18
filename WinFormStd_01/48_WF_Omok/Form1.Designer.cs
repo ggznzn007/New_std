502bfe2520f028b2b2dd1c9e32efd5caf9fb105f
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-02-18 15:40:13 +0900

    std_WF_Omok_20210218

--- WinFormStd_01/48_WF_Omok/Form1.Designer.cs
+++ WinFormStd_01/48_WF_Omok/Form1.Designer.cs
@@ -29,13 +29,77 @@ namespace _48_WF_Omok
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.보기ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.수순ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.파일ToolStripMenuItem,
+            this.보기ToolStripMenuItem,
+            this.수순ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // 파일ToolStripMenuItem
+            // 
+            this.파일ToolStripMenuItem.Name = "파일ToolStripMenuItem";
+            this.파일ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.파일ToolStripMenuItem.Text = "파일";
+            // 
+            // 보기ToolStripMenuItem
+            // 
+            this.보기ToolStripMenuItem.Name = "보기ToolStripMenuItem";
+            this.보기ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.보기ToolStripMenuItem.Text = "보기";
+            // 
+            // 수순ToolStripMenuItem
+            // 
+            this.수순ToolStripMenuItem.Name = "수순ToolStripMenuItem";
+            this.수순ToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
+            this.수순ToolStripMenuItem.Text = "수순";
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 501);
+            this.panel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 529);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "오목";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 파일ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 보기ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 수순ToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- WinFormStd_01/48_WF_Omok/Form1.cs
+++ WinFormStd_01/48_WF_Omok/Form1.cs
@@ -1,20 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace _48_WF_Omok
 {
     public partial class Form1 : Form
     {
+        int margin = 40;
+        int squareSize = 30; // 눈금 크기
+        int stoneSize = 28; // 바둑돌 크기
+        int pointSize = 10; // 화점 크기
+
+        Graphics g;
+        Pen pen;
+        Brush wBrush, bBrush;
         public Form1()
         {
             InitializeComponent();
+
+            this.BackColor = Color.Orange;
+
+            pen = new Pen(Color.Black);
+            bBrush = new SolidBrush(Color.Black);
+            wBrush = new SolidBrush(Color.White);
+
+            this.ClientSize = new Size(2 * margin + 18 * squareSize,
+                2 * margin + 18 * squareSize + menuStrip1.Height);
         }
     }
 }
