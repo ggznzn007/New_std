b25ee621d69bfb2f75e4b120e54b490d97d87148
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-02-20 17:24:51 +0900

    std_Arduino_20210220

--- WinFormStd_01/49_ArduinoSensor/Form1.Designer.cs
+++ WinFormStd_01/49_ArduinoSensor/Form1.Designer.cs
@@ -29,25 +29,27 @@ namespace _49_ArduinoSensor
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.파일ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.시뮬레이션ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.시작ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.끝ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnPortValue = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
-            this.btnConnect = new System.Windows.Forms.Button();
-            this.btnDisconnect = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
+            this.btnViewAll = new System.Windows.Forms.Button();
+            this.btnZoom = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -73,9 +75,26 @@ namespace _49_ArduinoSensor
             // 
             // 시뮬레이션ToolStripMenuItem
             // 
+            this.시뮬레이션ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.시작ToolStripMenuItem,
+            this.끝ToolStripMenuItem});
             this.시뮬레이션ToolStripMenuItem.Name = "시뮬레이션ToolStripMenuItem";
             this.시뮬레이션ToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
             this.시뮬레이션ToolStripMenuItem.Text = "시뮬레이션";
+            // 
+            // 시작ToolStripMenuItem
+            // 
+            this.시작ToolStripMenuItem.Name = "시작ToolStripMenuItem";
+            this.시작ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.시작ToolStripMenuItem.Text = "시작";
+            this.시작ToolStripMenuItem.Click += new System.EventHandler(this.시작ToolStripMenuItem_Click);
+            // 
+            // 끝ToolStripMenuItem
+            // 
+            this.끝ToolStripMenuItem.Name = "끝ToolStripMenuItem";
+            this.끝ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.끝ToolStripMenuItem.Text = "끝";
+            this.끝ToolStripMenuItem.Click += new System.EventHandler(this.끝ToolStripMenuItem_Click);
             // 
             // comboBox1
             // 
@@ -84,10 +103,11 @@ namespace _49_ArduinoSensor
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(119, 20);
             this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.btnPortValue);
             this.groupBox1.Controls.Add(this.btnDisconnect);
             this.groupBox1.Controls.Add(this.btnConnect);
             this.groupBox1.Controls.Add(this.comboBox1);
@@ -97,6 +117,35 @@ namespace _49_ArduinoSensor
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Port";
+            // 
+            // btnPortValue
+            // 
+            this.btnPortValue.Location = new System.Drawing.Point(25, 159);
+            this.btnPortValue.Name = "btnPortValue";
+            this.btnPortValue.Size = new System.Drawing.Size(116, 71);
+            this.btnPortValue.TabIndex = 2;
+            this.btnPortValue.Text = "button1";
+            this.btnPortValue.UseVisualStyleBackColor = true;
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(22, 106);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(116, 37);
+            this.btnDisconnect.TabIndex = 2;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(24, 63);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(116, 37);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
             // 
             // label1
             // 
@@ -142,72 +191,47 @@ namespace _49_ArduinoSensor
             // 
             // chart1
             // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
             this.chart1.Location = new System.Drawing.Point(28, 300);
             this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Series1";
-            this.chart1.Series.Add(series1);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
             this.chart1.Size = new System.Drawing.Size(515, 283);
             this.chart1.TabIndex = 7;
             this.chart1.Text = "chart1";
             // 
-            // btnConnect
+            // btnViewAll
             // 
-            this.btnConnect.Location = new System.Drawing.Point(24, 63);
-            this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(116, 37);
-            this.btnConnect.TabIndex = 2;
-            this.btnConnect.Text = "Connect";
-            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnViewAll.Location = new System.Drawing.Point(141, 598);
+            this.btnViewAll.Name = "btnViewAll";
+            this.btnViewAll.Size = new System.Drawing.Size(116, 37);
+            this.btnViewAll.TabIndex = 2;
+            this.btnViewAll.Text = "View All";
+            this.btnViewAll.UseVisualStyleBackColor = true;
+            this.btnViewAll.Click += new System.EventHandler(this.btnViewAll_Click);
             // 
-            // btnDisconnect
+            // btnZoom
             // 
-            this.btnDisconnect.Location = new System.Drawing.Point(22, 106);
-            this.btnDisconnect.Name = "btnDisconnect";
-            this.btnDisconnect.Size = new System.Drawing.Size(116, 37);
-            this.btnDisconnect.TabIndex = 2;
-            this.btnDisconnect.Text = "Disconnect";
-            this.btnDisconnect.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(25, 159);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(116, 71);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(141, 598);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(116, 37);
-            this.button4.TabIndex = 2;
-            this.button4.Text = "View All";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(263, 598);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(116, 37);
-            this.button5.TabIndex = 2;
-            this.button5.Text = "Zoom";
-            this.button5.UseVisualStyleBackColor = true;
+            this.btnZoom.Location = new System.Drawing.Point(263, 598);
+            this.btnZoom.Name = "btnZoom";
+            this.btnZoom.Size = new System.Drawing.Size(116, 37);
+            this.btnZoom.TabIndex = 2;
+            this.btnZoom.Text = "Zoom";
+            this.btnZoom.UseVisualStyleBackColor = true;
+            this.btnZoom.Click += new System.EventHandler(this.btnZoom_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(556, 656);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnZoom);
+            this.Controls.Add(this.btnViewAll);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.groupBox2);
@@ -235,7 +259,7 @@ namespace _49_ArduinoSensor
         private System.Windows.Forms.ToolStripMenuItem 시뮬레이션ToolStripMenuItem;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnPortValue;
         private System.Windows.Forms.Button btnDisconnect;
         private System.Windows.Forms.Button btnConnect;
         private System.Windows.Forms.Label label1;
@@ -244,8 +268,10 @@ namespace _49_ArduinoSensor
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnViewAll;
+        private System.Windows.Forms.Button btnZoom;
+        private System.Windows.Forms.ToolStripMenuItem 시작ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 끝ToolStripMenuItem;
     }
 }
 
--- WinFormStd_01/49_ArduinoSensor/Form1.cs
+++ WinFormStd_01/49_ArduinoSensor/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.IO.Ports;
 using System.Linq;
@@ -17,13 +18,18 @@ namespace _49_ArduinoSensor
         SerialPort sPort;
         private double xCount = 200; // 차트에 보여지는 데이터 개수
         List<SensorData> myData = new List<SensorData>(); // 리스트 자료구조
-        
+
+
+        string connString = @"";/*@"Data Source=" +
+            "(LocalDB)/MSSQLLocalDB;AttachDbFilename="+
+            "C:[/Users/ggznz/reposit/New/WinFormStd_01/49_ArduinoSensor]/SensorData.mdf; Integrated Secrity=true";*/
+
         public Form1()
         {
             InitializeComponent();
 
             // ComboBox
-            foreach(var ports in SerialPort.GetPortNames())
+            foreach (var ports in SerialPort.GetPortNames())
             {
                 comboBox1.Items.Add(ports);
             }
@@ -37,10 +43,10 @@ namespace _49_ArduinoSensor
             ChartSetting();
 
             // 숫자 버튼
-            button1.BackColor = Color.Blue;
-            button1.ForeColor = Color.White;
-            button1.Text = "";
-            button1.Font = new Font("맑은 고딕", 16, FontStyle.Bold);
+            btnPortValue.BackColor = Color.Blue;
+            btnPortValue.ForeColor = Color.White;
+            btnPortValue.Text = "";
+            btnPortValue.Font = new Font("맑은 고딕", 16, FontStyle.Bold);
 
             label1.Text = "Connection Time : ";
             textBox1.TextAlign = HorizontalAlignment.Center;
@@ -54,7 +60,7 @@ namespace _49_ArduinoSensor
             chart1.ChartAreas.Add("draw");
             chart1.ChartAreas["draw"].AxisX.Minimum = 0;
             chart1.ChartAreas["draw"].AxisX.Maximum = xCount;
-            chart1.ChartAreas["draw"].AxisX.Interval = xCount/4;
+            chart1.ChartAreas["draw"].AxisX.Interval = xCount / 4;
             chart1.ChartAreas["draw"].AxisX.MajorGrid.LineColor = Color.White;
             chart1.ChartAreas["draw"].AxisX.MajorGrid.LineDashStyle =
                 ChartDashStyle.Dash;
@@ -84,6 +90,142 @@ namespace _49_ArduinoSensor
                 chart1.Legends.RemoveAt(0);
 
 
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox cb = sender as ComboBox;
+            sPort = new SerialPort(cb.SelectedItem.ToString());
+            sPort.Open();
+            sPort.DataReceived += SPort_DataReceived;
+
+            label1.Text = "Connection Time : " + DateTime.Now.ToString();
+        }
+
+        // 시리얼 포트의 Data_Received 이벤트
+        private void SPort_DataReceived(object sender,SerialDataReceivedEventArgs e)
+        {
+            string s = sPort.ReadLine();
+            this.BeginInvoke((new Action(delegate { ShowValue(s); })));
+        }
+
+        // 시리얼 포트로 받은 값을 보여주는 delegate 메소드
+        private void ShowValue(string s)
+        {
+            int v = Int32.Parse(s);
+            if (v < 0 || v > 1023) // 처음 시작할 때 이상한 값이 들어오는 경우
+                return;
+
+            SensorData data = new SensorData(
+                DateTime.Now.ToShortDateString(),
+                DateTime.Now.ToString("HH:mm:ss"), v);
+            myData.Add(data);
+            DBInsert(data); // 다음 장에서 추가
+
+            textBox1.Text = myData.Count.ToString(); // myData의 개수를 표시
+            progressBar1.Value = v;
+
+            // ListBox에 시간과 값을 표시
+            string item = DateTime.Now.ToString() + "\t" + s;
+            listBox1.Items.Add(item);
+            listBox1.SelectedIndex = listBox1.Items.Count - 1;
+
+            // Chart 표시
+            chart1.Series["PhotoCell"].Points.Add(v);
+
+            // zoom을 위해 200개까지는 기본으로 표시
+            // 데이터 개수가 많아지면 200개만 보이지만, 스크롤 나타남
+            chart1.ChartAreas["draw"].AxisX.Minimum = 0;
+            chart1.ChartAreas["draw"].AxisX.Maximum
+                = (myData.Count >= xCount) ? myData.Count : xCount;
+
+            // change chart range : Zoom 사용
+            if (myData.Count>xCount)
+            {
+                chart1.ChartAreas["draw"].AxisX.ScaleView.Zoom(
+                    myData.Count - xCount, myData.Count);
+            }
+            else
+            {
+                chart1.ChartAreas["draw"].AxisX.ScaleView.Zoom(0, xCount);
+            }
+            btnPortValue.Text = sPort.PortName + "\n" + s;
+
+            // 숫자 버튼 표시
+            if (simulationFlag == false)
+                btnPortValue.Text = sPort.PortName + "\n" + s;
+            else
+                btnPortValue.Text = s;
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            sPort.Close();
+            btnConnect.Enabled = true;
+            btnDisconnect.Enabled = false;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            sPort.Open();
+            btnConnect.Enabled = false;
+            btnDisconnect.Enabled = true;
+        }
+
+        // SensorData를 DB에 저장
+        private void DBInsert(SensorData data)
+        {
+            string sql = string.Format("Insert into SensorTable" +
+                "(Date, Time, Value) Values('{0}','{1}',{2})",
+                data.Date, data.Time, data.Value);
+
+            using (SqlConnection conn = new SqlConnection(connString))
+                using (SqlCommand comm = new SqlCommand(sql,conn))
+            {
+                conn.Open();
+                comm.ExecuteNonQuery();
+            }
+        }
+
+        private void btnViewAll_Click(object sender, EventArgs e)
+        {
+            chart1.ChartAreas["draw"].AxisX.Minimum = 0;
+            chart1.ChartAreas["draw"].AxisX.Maximum = myData.Count;
+            chart1.ChartAreas["draw"].AxisX.ScaleView.Zoom(0, myData.Count);
+            chart1.ChartAreas["draw"].AxisX.Interval = myData.Count / 4;
+        }
+
+        private void btnZoom_Click(object sender, EventArgs e)
+        {
+            chart1.ChartAreas["draw"].AxisX.Minimum = 0;
+            chart1.ChartAreas["draw"].AxisX.Maximum = myData.Count;
+            chart1.ChartAreas["draw"].AxisX.ScaleView.Zoom(
+                myData.Count - xCount, myData.Count);
+            chart1.ChartAreas["draw"].AxisX.Interval = xCount / 4;
+        }
+
+        Timer t = new Timer();
+        Random r = new Random();
+        private bool simulationFlag;
+
+        private void 시작ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            simulationFlag = true;
+            t.Interval = 1000;
+            t.Tick += T_Tick;
+            t.Start();
+        }
+
+        private void T_Tick(object sender, EventArgs e)
+        {
+            int value = r.Next(1024);
+            ShowValue(value.ToString());
+        }
+
+        private void 끝ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            t.Stop();
+            simulationFlag = false;
         }
     }
 }
