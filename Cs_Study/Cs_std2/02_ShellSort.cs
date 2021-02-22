7a77010359f5f59f56d8b87f3f3d4e24325c5a78
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-02-22 15:46:00 +0900

    std_Cs_ShellSort_20210222

---
+++ Cs_Study/Cs_std2/02_ShellSort.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ShellSort01
+{
+    class Pro
+    {
+        public static void ShellSort(int[] array)
+        {
+            int step = array.Length / 2;
+
+            while(step>0)
+            {
+                for(int i = 0; i<(array.Length-step);i++)
+                {
+                    int j = i;
+                    while(j>=0&&array[j]>array[j+step])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + step];
+                        array[j + step] = temp;
+                        j--;
+                    }
+                }
+                step = step / 2;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = { 0, 5, 1, 6, 8, 2, 3 };
+            foreach (int num in array)
+                Console.Write(" " + num);
+            Console.WriteLine();
+
+            ShellSort(array);
+            foreach (int num in array)
+                Console.Write("  " + num);
+        }
+    }
+}
