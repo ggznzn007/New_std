f95a7622fe3a1d5fb3ff3950d5579025d7fc4a6f
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-02-22 15:28:40 +0900

    std_Cs_BubbleSort_20210222

---
+++ Cs_Study/Cs_std2/01_BubbleSort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BubbleSort01
+{
+    class Pro
+    {
+        static void BubbleSort(int[] array)
+        {
+            int count = 0;
+            for(int i = array.Length - 1;i > 0;i--)
+            {
+                for( int j =0; j<i;j++)
+                    if(array[j]>array[j+1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                        count++;
+                    }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = { 0, 5, 1, 6, 8, 2, 3 };
+            foreach (int num in array)
+                Console.Write(" " + num);
+            Console.WriteLine();
+
+            BubbleSort(array);
+            foreach (int num in array)
+                Console.Write(" " + num);
+        }
+    }
+}
