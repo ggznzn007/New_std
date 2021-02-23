24e467b32777d205d5628ff8387a4462461d6ae8
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-02-23 14:20:26 +0900

    std_Cs_HeapSort_20210223

---
+++ Cs_Study/Cs_std2/03_HeapSort.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HeapSort01
+{
+    class Program
+    {
+        static void heapSort(int[] arr)
+        {
+            int i, temp, length = arr.Length;
+            for (i = length / 2 - 1; i >= 0; i--) heapHeapify(arr, length, i);
+            for (i = length - 1; i >= 0; i--)
+            {
+                temp = arr[0]; arr[0] = arr[i]; arr[i] = temp;
+                heapHeapify(arr, i, 0);
+            }
+        }
+        static void heapHeapify(int[] arr, int length, int i)
+        {
+            int left = 2 * i + 1, right = 2 * i + 2;
+            int temp, largest = i;
+            if (left < length && arr[left] > arr[largest]) largest = left;
+            if (right < length && arr[right] > arr[largest]) largest = right;
+            if (largest != i)
+            {
+                temp = arr[i]; arr[i] = arr[largest]; arr[largest] = temp;
+                heapHeapify(arr, length, largest);
+            }
+        }
+        static void Main()
+        {
+            int[] arr = { 9, 1, 22, 4, 0, -1, 1, 22, 100, 10 };
+            heapSort(arr);
+            Console.Write(string.Join(",", arr));
+            // -1,0,1,1,4,9,10,22,22,100
+        }
+    }
+
+}
