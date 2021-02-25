a532465e8fde26b805e215960f4303fb997e2b8e
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-02-25 16:44:04 +0900

    std_Cs_GnomeSort_20210225

---
+++ Cs_Study/Cs_std2/05_GnomeSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace GnomeSort01
+{
+    class program
+    {
+        // method for exchanging elements
+        static void Swap(ref int item1, ref int item2)
+        {
+            var temp = item1;
+            item1 = item2;
+            item2 = temp;
+        }
+
+        // Gnome sort
+        static int[] GnomeSort(int[] unsortedArray)
+        {
+            var index = 1;
+            var nextIndex = index + 1;
+
+            while (index < unsortedArray.Length)
+            {
+                if (unsortedArray[index - 1] < unsortedArray[index])
+                {
+                    index = nextIndex;
+                    nextIndex++;
+                }
+                else
+                {
+                    Swap(ref unsortedArray[index - 1], ref unsortedArray[index]);
+                    index--;
+                    if (index == 0)
+                    {
+                        index = nextIndex;
+                        nextIndex++;
+                    }
+                }
+            }
+
+            return unsortedArray;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gnome sorting");
+            Console.Write("Enter elements of the array:");
+            var parts = Console.ReadLine().Split(new[] { "", ",", ";" }, StringSplitOptions.RemoveEmptyEntries);
+            var array = new int[parts.Length];
+            for (int i = 0; i < parts.Length; i++)
+            {
+                array[i] = Convert.ToInt32(parts[i]);
+            }
+
+            Console.WriteLine("Sorted array: {0}", string.Join(",", GnomeSort(array)));
+
+            Console.ReadLine();
+        }
+    }
+}
