a06049943b4af0f953093816e09761f269fb45cb
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-01 17:33:41 +0900

    std_Cs_Create_Arr_20210301

---
+++ Cs_Study/Cs_std3/01_Create_Arr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CreateArr
+{
+    class Pro
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, };
+
+            int arrLength = 5;
+            int[] arr1 = new int[arrLength];
+
+            for (int i = 0; i < arrLength; i++)
+                arr1[i] = i;
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(" " + arr[i]);
+            Console.WriteLine();
+            for (int i = 0; i < arr1.Length; i++)
+                Console.Write(" " + arr1[i]);
+        }
+    }
+}
