38c64c04892db7af789afaec1d20e18e449b6d50
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-02 19:21:21 +0900

    std_Cs_Random_Val_20210302

---
+++ Cs_Study/Cs_std3/02_Random_Val.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RandomVal01
+{
+    class Pro
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int[] arr = new int[10];
+            int minVal = 0;
+            int maxVal = 10;
+
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = r.Next(minVal, maxVal + 1);
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(" " + arr[i]);
+        }
+    }
+}
