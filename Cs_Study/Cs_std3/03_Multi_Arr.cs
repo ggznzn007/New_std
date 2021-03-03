820a947dc6a9d625b303765c01ee82b1a3464a1a
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-03 16:40:06 +0900

    std_Cs_MulArr_20210303

---
+++ Cs_Study/Cs_std3/03_Multi_Arr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MulArr01
+{
+    class Pro
+    {
+        static void Main()
+        {
+            int n = 3;
+            var m = 5;
+
+            int[,] arr1 = new int[n, m];
+
+            int[,] arr2 = { { 0, 1, 2, 3, 4 }, { 5, 6, 7, 8, 9 } };
+
+            for(int i = 0;i<arr2.GetLength(0);i++)
+            {
+                for (int j = 0; j < arr2.GetLongLength(1); j++)
+                    Console.Write(" " + arr2[i, j]);
+                Console.WriteLine();
+            }
+        }
+    }
+}
