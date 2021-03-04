486e481363870d606be3320754439070756812af
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-04 16:23:43 +0900

    std_Cs_multiArr_20210304

---
+++ Cs_Study/Cs_std3/04_Multi_Arr.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace multiArr
+{
+    class Pro
+    {
+        static void Main()
+        {
+            int n = 3;
+            int m = 5;
+
+            int[,] arr = new int[n, m];
+
+            int lengthN = arr.GetLength(0);
+            int lengthM = arr.GetLength(1);
+
+
+            Console.WriteLine($"Arr is { lengthN}*{lengthM} size");
+        }
+    }
+}
