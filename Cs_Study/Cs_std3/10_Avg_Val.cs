aecabb74832b0b55f5a58d7a0444ce9451ad22a0
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-05 16:12:02 +0900

    std_Cs_Avg_20210305

---
+++ Cs_Study/Cs_std3/10_Avg_Val.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Avg_Val
+{
+    class Pro
+    {
+        static void Main()
+        {
+            double[] arr = { 1, 3, 5, 67, 8, 76, 88, 98 };
+
+            double avgVal = arr.Average();
+
+            double sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+                sum += arr[i];
+
+            double avg = sum / arr.Length;
+
+            Console.WriteLine("Avg counted by arr.Average: " + avgVal);
+            Console.WriteLine("Avg counted manally: " + avg);
+        }
+    }
+}
