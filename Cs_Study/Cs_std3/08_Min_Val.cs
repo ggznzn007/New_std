6ae74b772d5bafba27f185e2082ef0956ac36873
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-05 16:00:53 +0900

    std_Cs_Min_20210305

---
+++ Cs_Study/Cs_std3/08_Min_Val.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Min_Val
+{
+    class Pro
+    {
+        static void Main()
+        {
+            int[] arr = { 0, 12, 34, 3, 5, 6, 1 };
+
+            int minVal = arr.Min();
+
+            int min = arr[0];
+
+            for (int i = 1; i < arr.Length; i++)
+                if (arr[i] < min)
+                    min = arr[i];
+
+            Console.WriteLine("Min val: " + minVal);
+            Console.WriteLine("Min val: " + min);
+        }
+    }
+}
