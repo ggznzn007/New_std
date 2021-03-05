9b1f21af8347dc0a640e570810cfad419f47f070
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-05 15:43:05 +0900

    std_Cs_Element_20210305

---
+++ Cs_Study/Cs_std3/05_Element_Occurs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Arr_Occurs
+{
+    class Pro
+    {
+        static void Main()
+        {
+            int[] arr = { 0, 1, 1, 1, 1, 1, 1, 2, 3, 4, 5, 6, 7 };
+            int element = 1;
+
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] == element)
+                    count++;
+
+            Console.WriteLine("Element " + element + " occurs " + count + " times.");
+        }
+    }
+}
