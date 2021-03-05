ea2f4a2a4618586030afa4a7bfe985734339e399
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-05 15:48:33 +0900

    std_Cs_Delete_Element_20210305

---
+++ Cs_Study/Cs_std3/06_Delete_Element.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Delete_Element
+{
+    class Pro
+    {
+        static void Main()
+        {
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7 };
+            int idx = 3;
+
+            int[] copyArr = new int[arr.Length];
+            arr.CopyTo(copyArr, 0);
+
+            arr = new int[copyArr.Length - 1];
+
+            for(int i = 0, j=0;i<copyArr.Length;i++)
+                if(i!=idx)
+                {
+                    arr[j] = copyArr[i];
+                    j++;
+                }
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(" " + arr[i]);
+        }
+    }
+}
