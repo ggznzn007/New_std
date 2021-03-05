24a8d47adc688dcf473006c6dc55f827d180f840
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-05 15:54:46 +0900

    std_Cs_Remove_Element_20210305

---
+++ Cs_Study/Cs_std3/07_Remove_Element.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Remove_Element
+{
+    class Pro
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int element = 1;
+
+            int[] copyArr = new int[arr.Length];
+            arr.CopyTo(copyArr, 0);
+
+            arr = new int[copyArr.Length - 1];
+
+            bool isDeleted = false;
+
+            for (int i = 0, j = 0; i < copyArr.Length; i++)
+                if (isDeleted == false && copyArr[i] == element)
+                    isDeleted = true;
+                else
+                {
+                    arr[j] = copyArr[i];
+                    j++;
+                }
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(" " + arr[i]);
+        }
+    }
+}
