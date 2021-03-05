7335797b68d62aa3266d993ece8411da9f3b2b6b
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-05 16:06:31 +0900

    std_Cs_Max_20210305

---
+++ Cs_Study/Cs_std3/09_Max_Val.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Max_Val
+{
+    class Pro
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 98 };
+
+            int maxVal = arr.Max();
+
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+                if (arr[i] > max)
+                    max = arr[i];
+
+            Console.WriteLine("Max val: " + maxVal);
+            Console.WriteLine("Max val: " + max);
+        }
+    }
+}
