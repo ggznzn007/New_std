8e0da26481d5ec4e3cb32ed4528b163ccefab83d
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-10 17:10:31 +0900

    std_Cs_Length_20210310

---
+++ Cs_Study/Cs_std3/15_arrLength.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace arrLength
+{
+    class Pro
+    {
+        static void Main()
+        {
+            int[] arrA = new int[10];
+            int lengthA = arrA.Length;
+
+            int[] arrB = { 0, 1, 2, 3, 4 };
+            int lengthB = arrB.Length;
+
+            Console.WriteLine("First arr's length: " + lengthA);
+            Console.WriteLine("Second arr's length: " + lengthB);
+        }
+    }
+}
