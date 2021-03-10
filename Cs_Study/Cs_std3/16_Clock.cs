b06628dff168c75a9cd15f523ffb29d3efda174b
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-10 17:15:34 +0900

    std_Cs_Clock_20210310

---
+++ Cs_Study/Cs_std3/16_Clock.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateClock
+{
+    class Pro
+    {
+        static void Main()
+        {
+            DateTime time = DateTime.Now;
+
+            int h = time.Hour;
+            int m = time.Minute;
+            int s = time.Second;
+
+            Console.WriteLine($"Time from midnight {h}:{m}:{s}");
+        }
+    }
+}
