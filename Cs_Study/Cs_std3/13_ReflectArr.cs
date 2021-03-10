04b1968a6c543972936b417b189e92238115b6d0
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-10 16:58:28 +0900

    std_Cs_Reflect_20210310

---
+++ Cs_Study/Cs_std3/13_ReflectArr.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReflecArr
+{
+    class Pro
+    {
+        static void Main()
+        {
+            int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            int temp = 0;
+
+            for(int i = 0; i< arr.Length/2;i++)
+            {
+                temp = arr[i];
+                arr[i] = arr[arr.Length - (i + 1)];
+                arr[arr.Length - (i + 1)] = temp;
+            }
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(" " + arr[i]);
+        }
+    }
+}
