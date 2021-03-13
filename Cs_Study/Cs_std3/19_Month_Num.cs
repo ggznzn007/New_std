fe199a0343ef05bed124dcf1e56b9fe476ac90ec
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-13 16:04:49 +0900

    std_Cs_Month_Num_20210313

---
+++ Cs_Study/Cs_std3/19_Month_Num.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace MonthNum
+{
+    class Pro
+    {
+        static void Main()
+        {
+            Console.Write("Enter the month: ");
+            string month = Console.ReadLine();
+
+            Calendar cal = new GregorianCalendar();
+            string[] months = CultureInfo.CurrentCulture.DateTimeFormat.MonthNames;
+
+            for(int i = 0; i < months.Length;i++)
+                if(months[i].Equals(month))
+                {
+                    Console.WriteLine(i + 1);
+                    break;
+                }
+        }
+    }
+}
