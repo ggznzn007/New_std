f325856e931a329411f01059192066cb5b4c94f4
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-13 16:13:12 +0900

    std_Cs_SW_20210313

---
+++ Cs_Study/Cs_std3/20_StopWatch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StopWatch01
+{
+    class Pro
+    {
+        static void Main()
+        {
+            while(true)
+            {
+
+            Console.WriteLine("Press enter to start.");
+
+            Console.ReadLine();
+            DateTime start = DateTime.Now;
+
+            Console.WriteLine("Press enter to stop.");
+
+            Console.ReadLine();
+            DateTime stop = DateTime.Now;
+
+            TimeSpan distance = stop - start;
+            double time = distance.TotalSeconds;
+            Console.WriteLine("Time distance: " + time + " seconds");
+            }
+        }
+    }
+}
