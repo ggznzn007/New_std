69ff1679b179e86f6cbaae51be0f608e397ce55c
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-13 15:55:13 +0900

    std_Cs_Day_Week_20210313

---
+++ Cs_Study/Cs_std3/18_Day_Week.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Day_Week
+{
+    class Pro
+    {
+        static void Main()
+        {
+            DateTime date = new DateTime(2021, 3, 13);
+
+            string day = WeekDay(date.Day, date.Month, date.Year);
+            Console.WriteLine(day);
+        }
+
+        public static string WeekDay(int day, int month, int year)
+        {
+            string[] days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            int a = (14 - month) / 12, y = year - a, m = month + 12 * a - 2;
+            return days[(7000 + (day + y + y / 4 - y / 100 + y / 100 + y / 400 + (31 * m) / 12)) % 7];
+        }
+    }
+}
