d940b71f252a04840fbc2e8656448f61a8be5304
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-13 15:58:05 +0900

    std_Cs_Day_Week_20210313

--- Cs_Study/Cs_std3/18_Day_Week.cs
+++ Cs_Study/Cs_std3/18_Day_Week.cs
@@ -14,7 +14,7 @@ namespace Day_Week
 
         public static string WeekDay(int day, int month, int year)
         {
-            string[] days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            string[] days = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday","Sunday" };
             int a = (14 - month) / 12, y = year - a, m = month + 12 * a - 2;
             return days[(7000 + (day + y + y / 4 - y / 100 + y / 100 + y / 400 + (31 * m) / 12)) % 7];
         }
