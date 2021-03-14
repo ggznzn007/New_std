8fd36e9024f9769d97973ec86968f42f6ea4c089
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-14 17:18:55 +0900

    std_Cs_Except01_20210314

--- Cs_Study/Cs_std3/21_Except01.cs
+++ Cs_Study/Cs_std3/21_Except01.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+
+namespace except01
+{
+    class Pro
+    {
+        static void Main()
+        {
+            try
+            {
+                int[] arr = new int[3];
+                arr[10] = 1;
+            }
+            catch(IndexOutOfRangeException except)
+            {
+                Console.WriteLine(except.Message);
+            }
+            finally
+            {
+                Console.WriteLine("The finally block always executes when try block exits.");
+            }
+        }
+    }
+}
