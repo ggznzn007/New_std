6c32b5c1148149065f3ae3e0372582ac5e21b0a6
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-15 20:44:56 +0900

    std_Cs_Multi_Except_20210315

---
+++ Cs_Study/Cs_std3/22_Except02.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Except02_Multi
+{
+    class Pro
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 2, 4, 8, 16, 32, 64, 128 };
+            int[] denom = { 1, 2, 0, 0, 4 };
+
+            for( int i = 0; i<num.Length;i++)
+            {
+                try
+                {
+                    Console.WriteLine($"{num[i]}/{denom[i]}={num[i] / denom[i]}");
+                }
+                catch(DivideByZeroException e)
+                {
+                    Console.WriteLine("Divide by zero.");
+                }
+                catch(IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("Array is out of bounds.");
+                }
+            }
+        }
+    }
+}
