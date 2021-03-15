1c69f68673759994244c5d6424b8abb933d7df9f
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-15 20:48:42 +0900

    std_Cs_Throw_20210315

---
+++ Cs_Study/Cs_std3/23_Throw.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Throw01
+{
+    class Pro
+    {
+        static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            for(int i = 0;i<arr.Length;i++)
+            {
+                try
+                {
+                    if (arr[i] > 5)
+                        throw new Exception("The number > 5.");
+                    else
+                        Console.WriteLine(arr[i]);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
