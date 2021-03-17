3e88579cf7a254e9cc98ea9edf64b320a42e57ea
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-03-17 16:28:20 +0900

    std_Cs_Loops_20210317

--- Cs_Study/Cs_std3/24_Loops.cs
+++ Cs_Study/Cs_std3/24_Loops.cs
@@ -2,12 +2,25 @@
 
 namespace loops
 {
-    class For
+    /*class For
     {
         static void Main()
         {
             for (int i = 1; i <= 10; i++)
                 Console.Write(" " + i);
+        }
+    }*/
+
+    class While
+    {
+        static void Main()
+        {
+            int i = 1;
+            while(i<=10)
+            {
+                Console.Write(" " + i);
+                i++;
+            }
         }
     }
 }
