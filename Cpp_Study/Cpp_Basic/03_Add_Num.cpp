0ccc26ec867888bc277228ac3351382d5e6f0040
Pooleun Jeong <ggznzn007@naver.com>
2021-03-29 17:08:23 +0900

    std_Cpp_Add_20210329

---
+++ Cpp_Study/Cpp_Basic/03_Add_Num.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num1, num2, result;
+
+	cout << "Enter first Number: ";
+	cin >> num1;
+
+	cout << "\nEnter second Number: ";
+	cin >> num2;
+
+	result = num1 + num2;
+
+	cout << "\n Addition is : " << result;
+
+	return 0;
+}
