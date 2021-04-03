b8ca7677a7c7caac5995f2c61a0fce652d47d1de
Pooleun Jeong <ggznzn007@naver.com>
2021-04-03 21:50:02 +0900

    std_Cs_Even_Odd_20210403

---
+++ Cs_Study/Cs_std4/01_Even_Odd.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class EO
+{
+    public static void Main()
+    {
+        int x;
+
+        Console.Write("Enter an integer : ");
+        x = Convert.ToInt32(Console.ReadLine());
+        if(x%2==0)
+        {
+            Console.WriteLine("Number is even.");
+        }
+        else
+        {
+            Console.WriteLine("Number is odd.");
+        }
+        Console.ReadKey(true);
+    }
+}
