768a2f1e001cf6c1fbb4310a359359056b86474d
Pooleun Jeong <ggznzn007@naver.com>
2021-04-04 17:18:36 +0900

    std_Cs_Fact_20210404

---
+++ Cs_Study/Cs_std4/02_Factorial.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Facto
+{
+    class Pro
+    {
+        public static void Main()
+        {
+            int n, fact = 1;
+
+            Console.Write("Enter the number: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                fact = fact * i;
+            }
+
+            Console.Write("Factorial of " + n + " is " + fact);
+            Console.ReadKey(true);
+        }
+    }
+}
