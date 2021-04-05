b091fb2e77005590e4a708479d8ef37d9b76ead4
Pooleun Jeong <ggznzn007@naver.com>
2021-04-05 21:01:45 +0900

    std_Cs_List_20210405

---
+++ Cs_Study/Cs_std4/03_List.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ListD
+{
+    public static void Main()
+    {
+        var countries = new List<string>() { "Korea", "America" };
+
+        countries.Add("UK");
+        countries.Add("India");
+
+        foreach(var country in countries)
+        {
+            Console.WriteLine(country);
+        }
+    }
+}
