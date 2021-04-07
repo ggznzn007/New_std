661db1e9c5330e4ca7fd37134c1d31f0c914b331
Pooleun Jeong <ggznzn007@naver.com>
2021-04-07 18:07:21 +0900

    std_Cs_Arr01_20210407

---
+++ Cs_Study/Cs_std4/04_Arr01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Arr01
+{
+    public static void Main()
+    {
+        int[] arr01 = new int[] { 10, 20, 30, 40, 50 };
+        Console.WriteLine("Contents of the arr - ");
+
+        for(int i = 0; i<5;i++)
+        {
+            Console.WriteLine("Value at arr[" + i + "]is" + arr01[i]);
+        }
+        Console.ReadKey(true);
+    }
+}
