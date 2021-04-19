ae7824dee6edd9390d99efb3a4d6132d7253b997
Pooleun Jeong <ggznzn007@naver.com>
2021-04-19 14:38:24 +0900

    std_Cs_Threa01_20210419

---
+++ Cs_Study/Cs_std4/06_Thread01.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+public class ThreadOne
+{
+    public void Thread1()
+    {
+        for(int i = 0; i<5;i++)
+        {
+            Console.WriteLine("Thread One Runs {0} times", i);
+        }
+    }
+}
+
+public class ThreadTwo
+{
+    public void Thread2()
+    {
+        for(int i = 0; i<5; i++)
+        {
+            Console.WriteLine("Thread Two Runs {0} times", i);
+        }
+    }
+}
+
+public class ThreadDemo
+{
+    public static void Main()
+    {
+        Console.WriteLine("Main Thread starts");
+
+        ThreadOne thr1 = new ThreadOne();
+        ThreadTwo thr2 = new ThreadTwo();
+
+        Thread tid1 = new Thread(new ThreadStart(thr1.Thread1));
+        Thread tid2 = new Thread(new ThreadStart(thr2.Thread2));
+
+        tid1.Start();
+        tid2.Start();
+
+        Console.WriteLine("Main Thread ends");
+        Console.ReadKey(true);
+    }
+}
