489bc526d32e0c8f908ab3450803e06e8245d59c
Pooleun Jeong <ggznzn007@naver.com>
2021-04-20 13:46:09 +0900

    std_Cs_Without_Loops_20210420

---
+++ Cs_Study/Cs_std4/07_Without_Loop_.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class WithoutLoops
+{
+    static int cnt = 1;
+
+    static void printNums(int n)
+    {
+        Console.WriteLine(cnt);
+        cnt++;
+
+        if(cnt==n+1)
+        { return; }
+        printNums(n);
+    }
+    
+    public static void Main()
+    {
+        int num;
+
+        Console.WriteLine("Enter any Number : ");
+
+        num = Convert.ToInt32(Console.ReadLine());
+
+        printNums(num);
+
+        Console.ReadKey(true);
+    }
+
+}
