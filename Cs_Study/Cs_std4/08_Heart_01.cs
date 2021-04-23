2b88acf2b7c8e646f714285b020e3d3d7c84ac1b
Pooleun Jeong <ggznzn007@naver.com>
2021-04-23 17:12:56 +0900

    std_Cs_Pattern_H_20210423

---
+++ Cs_Study/Cs_std4/08_Heart_01.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+class Heart
+{
+    public static void Main()
+    {
+        int i;
+        int j;
+        int n = 5;
+
+        for(i=n/2;i<=n;i+=2)
+        {
+            for(j=1;j<n-i;j+=2)
+            {
+                Console.Write(" ");
+            }
+            for(j=1;j<=n-i;j++)
+            {
+                Console.Write("*");
+            }
+            for(j=1;j<=n-i;j++)
+            {
+                Console.Write(" ");
+            }
+            for(j=1;j<=i;j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+
+        for(i=n;i>=1;i--)
+        {
+            for(j=i;j<n;j++)
+            {
+                Console.Write(" ");
+            }
+            for(j=1;j<=(i*2)-1;j++)
+            {
+                Console.Write("*");
+            }
+
+            Console.WriteLine();
+        }
+        Console.ReadKey(true);
+    }
+}
