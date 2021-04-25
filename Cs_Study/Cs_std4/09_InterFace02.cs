ecfd906e7514a048e6e5db985b6a4a04395ec97b
Pooleun Jeong <ggznzn007@naver.com>
2021-04-25 11:17:46 +0900

    std_Cs_Interface02_20210425

---
+++ Cs_Study/Cs_std4/09_InterFace02.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _Interface02
+{
+    interface ICar
+    {
+        void HasDoors();
+        void HasAirbags();
+        void HasEngine();
+    }
+
+    class ElectroCar : ICar
+    {
+        public void HasAirbags()
+        {
+            Console.WriteLine(GetType().Name + " has doors.");
+        }
+
+        public void HasDoor()
+        {
+            Console.WriteLine(GetType().Name + " has airbags.");
+        }
+
+        public void HasDoors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HasEngine()
+        {
+            Console.WriteLine(GetType().Name + " has engine.");
+        }
+    }
+
+    class GasolineCar : ICar
+    {
+        public void HasAirbags()
+        {
+            Console.WriteLine(GetType().Name + " has doors.");
+        }
+
+        public void HasDoor()
+        {
+            Console.WriteLine(GetType().Name + " has airbags.");
+        }
+
+        public void HasDoors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HasEngine()
+        {
+            Console.WriteLine(GetType().Name + " has engine.");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            ICar electroCar = new ElectroCar();
+            electroCar.HasEngine();
+            electroCar.HasAirbags();
+            electroCar.HasDoors();
+
+            ICar gasolineCar = new GasolineCar();
+            gasolineCar.HasEngine();
+            gasolineCar.HasAirbags();
+            gasolineCar.HasDoors();
+        }
+    }
+}
