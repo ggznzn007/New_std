1f0bd69249ffe74c23251b3cd5dca8a5a2ef056e
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-05-06 15:42:59 +0900

    std_Unity_20210506

---
+++ Animation_Ctrl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Animation_Ctrl : MonoBehaviour
+{
+    public Animator ani;
+    public void Animation_ctrl(int ani_num)
+    {
+        ani = GetComponent<Animator>();
+
+        ani.SetInteger("motion", ani_num);
+        
+    }
+  
+}
