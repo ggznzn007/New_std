6ef47fb2af1ee2edf5c0ed3cdccbfb93a720509d
Pooleun Jeong <ggznzn007@naver.com>
2021-06-13 11:18:20 +0900

    std_Cs_Data_Class_20210613

---
+++ Cs_Study/Cs_std4/10_Data_Class_01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class NameAndPricePair
+{
+    public string Name { get; set; }
+    public int Price { get; set; }
+}
+
+class Program
+{
+    private static void output(NameAndPricePair data)
+    {
+        Console.WriteLine("{0}은 {1}원입니다.", data.Name, data.Price);
+    }
+    static void Main(string[] args)
+    {
+        var data1 = new NameAndPricePair() { Name = "가방", Price = 1200 };
+        var data2 = new NameAndPricePair() { Name = "지갑", Price = 200 };
+        output(data1);
+        output(data2);
+    }
+}
