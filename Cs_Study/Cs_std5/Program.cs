e8b1aff6afdf413ae3d6bea8d45dd539d489eb59
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-06-17 17:27:35 +0900

    std_Cs_Delegate_20210617

---
+++ Cs_Study/Cs_std5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs_std
+{
+    class DelClass
+    {
+        // 델리게이트 선언
+        private delegate void RunDelegate(int i);
+
+        private void RunThis(int val)
+        {
+            // 콘솔출력 : 1024
+            Console.WriteLine("{0}", val);
+        }
+
+        private void RunThat(int value)
+        {
+            // 콘솔출력 : 0x400
+            Console.WriteLine("0x{0:X}", value);
+        }
+
+        public void Perform()
+        {
+            // 2. 델리게이트 인스턴스 생성
+            RunDelegate run = new RunDelegate(RunThis);
+            // 3. 델리게이트 실행
+            run(1024);
+
+            // run = new RunDelegate(RunThat); 을 줄여서 아래와같이 사용가능
+            run = RunThat;
+            run(1024);
+        }
+
+
+        class Pro
+        {
+            static void Main(string[] args)
+            {
+                DelClass dc = new DelClass();
+                dc.Perform();
+            }
+        }
+    }
+}
