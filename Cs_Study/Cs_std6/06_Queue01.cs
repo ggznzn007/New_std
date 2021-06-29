f800c1aef995efccce8c78dfd136cea210b66546
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-06-29 09:35:21 +0900

    std_Cs_20210629

---
+++ Cs_Study/Cs_std6/06_Queue01.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Queue01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue que = new Queue();
+            for (int i = 1; i <= 100; i++)
+            {
+                que.Enqueue(i);
+            }
+            Console.WriteLine("que.Count: {0}", que.Count);
+            Console.WriteLine();
+
+            while (que.Count > 0)
+            {
+                Console.WriteLine(que.Dequeue());
+            }
+            Console.WriteLine();
+            Console.WriteLine("que.Count: {0}", que.Count);
+        }
+    }
+}
