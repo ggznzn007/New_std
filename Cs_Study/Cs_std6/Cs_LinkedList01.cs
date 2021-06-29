76a2f3a32a01982aa91eac9a99cb13d28d4bfdc0
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-06-29 09:14:43 +0900

    std_Cs_20210629

---
+++ Cs_Study/Cs_std6/Cs_LinkedList01.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<string> list = new LinkedList<string>();
+            list.AddLast("Apple");
+            list.AddLast("Banana");
+            list.AddLast("Lemon");
+            list.AddLast("Orange");
+            list.AddLast("Tomato");
+            list.AddLast("Melon");
+            list.AddLast("Watermelon");
+
+            LinkedListNode<string> node = list.Find("Banana");
+            LinkedListNode<string> newNode = new LinkedListNode<string>("Grape");
+
+            // 새 Grape 노드를 Banana 노드 뒤에 추가
+            list.AddAfter(node, newNode);
+
+            // 리스트 출력
+            list.ToList<string>().ForEach(p => Console.WriteLine(p));
+
+            // Enumerator 리스트 출력
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
