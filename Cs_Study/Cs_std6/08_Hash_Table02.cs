1f2a5165937c63e76047488bb8d8d4d0ed878cb1
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-06-29 15:32:14 +0900

    std_Cs_20210629

---
+++ Cs_Study/Cs_std6/08_Hash_Table02.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTable02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 객체 선언
+            Hashtable ht = new Hashtable();
+
+            // 선언한 객체에 데이터 저장하기
+            ht.Add("하나", "one");
+            ht.Add("둘", "two");
+            ht.Add("셋", "three");
+            ht.Add("넷", "four");
+            ht.Add("다섯", "five");
+            ht.Add("여섯", "six");
+
+            foreach (var value in ht.Keys)
+            {
+                Console.WriteLine("{0} : {1}", value, ht[value]);
+            }
+        }
+    }
+}
