da9b1ad29e93f87efa2c7efe2482a571f889f091
Pooleun Jeong <ggznzn007@naver.com>
2021-07-16 15:50:31 +0900

    std_Cs_20210716

--- Cs_Study/Cs_std6/13_LINQ01.cs
+++ Cs_Study/Cs_std6/13_LINQ01.cs
@@ -7,12 +7,13 @@ namespace LINQ01_Where
     {
         static void Main()
         {
-            int[] array;
+            int[] array = { 1, 2, 3, 4, 5, 15, 23, 33, 56, 444, 333, 66, 345, 356, 1, 4, 22, 65 };
             var answer = from num in array
-                         where num < 10
+                         where num < 100
                          select num;
 
-            foreach(int num)
+            foreach (int num in answer)
+                Console.Write("  " + num);
         }
     }
 }
