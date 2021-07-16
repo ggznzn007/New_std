7a33ef83b2cbc23f2889c6227469cdb44b66ca81
Pooleun Jeong <ggznzn007@naver.com>
2021-07-16 15:19:44 +0900

    std_Cs_LINQ_20210716

---
+++ Cs_Study/Cs_std6/13_LINQ01.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace LINQ01_Where
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array;
+            var answer = from num in array
+                         where num < 10
+                         select num;
+
+            foreach(int num)
+        }
+    }
+}
