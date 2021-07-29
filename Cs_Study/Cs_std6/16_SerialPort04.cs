45a8cc366bb2e33ad6f15483386c7a2b66622e34
Pooleun Jeong <ggznzn007@naver.com>
2021-07-29 14:54:05 +0900

    std_Cs_SerialPort_210729

--- Cs_Study/Cs_std6/16_SerialPort04.cs
+++ Cs_Study/Cs_std6/16_SerialPort04.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO.Ports;
+using System.Threading;
+using System.Threading.Tasks;
 
 /*시리얼포트 작업쓰레드 활용
 시리얼포트 통신에서 이벤트 핸들러를 사용하지 않고 직접 작업쓰레드를 생성해서 처리할 수도 있다.
@@ -8,3 +10,55 @@ using System.IO.Ports;
 할 수 있게 한 것이다. (주: 이 예제는 MSDN에서 제공하는 코드를 간결하게 Refactoring 함과 
 동시에 약간의 버그를 수정한 것이다)*/
 
+namespace SerialChat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: SerialChat {portName}");
+                Console.WriteLine("   Ex: C> SerialChat COM3");
+                return;
+            }
+            string portName = args[0];
+
+            // 시리얼포트 열기
+            SerialPort sp = new SerialPort(portName);
+            sp.Open();
+
+            // 읽기 작업쓰레드: Read from serial port
+            var cancel = new CancellationTokenSource();
+            var readTask = Task.Factory.StartNew(() =>
+            {
+                while (!cancel.IsCancellationRequested)
+                {
+                    string readMsg = sp.ReadLine();
+                    if (readMsg.ToLower() == "quit")
+                    {
+                        Environment.Exit(0);
+                    }
+                    Console.WriteLine(readMsg);
+                }
+            }, cancel.Token);
+
+            // 메인쓰레드: Write to serial port
+            while (true)
+            {
+                string sendMsg = Console.ReadLine();
+                if (sendMsg.ToLower() == "quit")
+                {
+                    sp.WriteLine(sendMsg);
+                    cancel.Cancel();
+                    break;
+                }
+
+                sp.WriteLine(sendMsg);
+            }
+
+            // 시리얼포트 닫기
+            sp.Close();
+        }
+    }
+}
