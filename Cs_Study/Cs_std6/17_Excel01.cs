c86d2a0230831d67b848eab679f9d4f1d5312335
Pooleun Jeong <ggznzn007@naver.com>
2021-08-02 10:03:58 +0900

    std_Cs_Excel01_210802

--- Cs_Study/Cs_std6/17_Excel01.cs
+++ Cs_Study/Cs_std6/17_Excel01.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using System;
+
+/*C#을 이용해 Excel 파일에 데이타를 읽거나 쓸 때, Excel Automation을 이용하거나 OLEDB를 이용할 수 있다.
+
+(1) C#에서 엑셀 오토메이션을 이용하기 위해서는 Excel Interop 을 참조한 후,
+    Office Automation COM API들을 사용하게 된다.
+(2) C#에서 엑셀을 OLEDB로 이용할 경우에는 ADO.NET의 OleDb 클래스들을 사용하여 엑셀 데이타를 핸들링하게 된다.*/
+
