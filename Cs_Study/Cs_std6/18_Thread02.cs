2572a125f7af59d73ba5bca03f64a2ca51b2cbe4
Pooleun Jeong <ggznzn007@naver.com>
2021-08-03 11:22:45 +0900

    std_Cs_Thread02_210802

--- Cs_Study/Cs_std6/18_Thread02.cs
+++ Cs_Study/Cs_std6/18_Thread02.cs
@@ -51,3 +51,26 @@ namespace OtherThdApp
         }
     }
 }
+
+/*C# 쓰레스 생성 예 - 다른 클래스 메서드
+동일 클래스가 아닌 다른 클래스의 메서드를 쓰레드에 호출하기 위해서는 해당 클래스의 객체를 생성한 후
+(혹은 외부로부터 전달 받은 후) 그 객체의 메서드를 델리게이트로 Thread에 전달하면 된다.*/
+
+/*class Helper
+{
+    public void Run()
+    {
+        Console.WriteLine("Helper.Run");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Helper클래스의 Run메서드 호출
+        Helper obj = new Helper();
+        Thread t = new Thread(obj.Run);
+        t.Start();
+    }
+}*/
