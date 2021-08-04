a718de8b8cc192efd37185f9e1daeeed0ebda87f
Pooleun Jeong <ggznzn007@naver.com>
2021-08-04 14:34:28 +0900

    std_Cs_Thread03_210804

--- Cs_Study/Cs_std6/18_Thread03.cs
+++ Cs_Study/Cs_std6/18_Thread03.cs
@@ -56,3 +56,11 @@ namespace Thread03
         }
     }
 }
+
+/*Background 쓰레드 vs Foreground 쓰레드
+
+Thread 클래스 객체를 생성한 후 Start()를 실행하기 전에 IsBackground 속성을 true/false로 지정할 수 있는데,
+만약 true로 지정하면 이 쓰레드는 백그라운드 쓰레드가 된다. 디폴트 값은 false 즉 Foreground 쓰레드이다.
+백그라운드와 Foreground 쓰레드의 기본적인 차이점은 Foreground 쓰레드는 메인 쓰레드가 종료되더라도
+Foreground 쓰레드가 살아 있는 한, 프로세스가 종료되지 않고 계속 실행되고,
+백그라운드 쓰레드는 메인 쓰레드가 종료되면 바로 프로세스를 종료한다는 점이다.*/
