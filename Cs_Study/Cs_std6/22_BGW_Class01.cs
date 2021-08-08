6ae3ddcf89a8ea93b78e18416da44e63dca5ac7f
Pooleun Jeong <ggznzn007@naver.com>
2021-08-08 11:15:10 +0900

    std_Cs_BGW_20210808

--- Cs_Study/Cs_std6/22_BGW_Class01.cs
+++ Cs_Study/Cs_std6/22_BGW_Class01.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using System;
+using System.ComponentModel;
+
+namespace MultiThrdApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Execute();
+            Console.Read();
+        }
+
+        private BackgroundWorker worker;
+
+        public void Execute()
+        {
+            // 쓰레드풀에서 작업쓰레드 시작
+            worker = new BackgroundWorker();
+            worker.DoWork += new DoWorkEventHandler(worker_DoWork);
+            worker.RunWorkerAsync();
+        }
+
+        // 작업쓰레드가 실행할 Task 메서드
+        void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            //긴 처리 가정
+            Console.WriteLine("Long running task");
+        }
+    }
+}
