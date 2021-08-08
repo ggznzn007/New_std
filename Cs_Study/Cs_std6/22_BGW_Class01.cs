1c5d410c152997e1512cab01d8e94a8be9d7371c
Pooleun Jeong <ggznzn007@naver.com>
2021-08-08 11:16:17 +0900

    std_Cs_BGW_20210808

--- Cs_Study/Cs_std6/22_BGW_Class01.cs
+++ Cs_Study/Cs_std6/22_BGW_Class01.cs
@@ -1,6 +1,12 @@
 ﻿using System;
 using System.ComponentModel;
 
+/*C# BackgroundWorker 클래스
+BackgroundWorker 클래스는 쓰레드풀에서 작업 쓰레드(Worker Thread)를 할당 받아 작업을 실행하는
+Wrapper 클래스이다. BackgroundWorker는 이벤트를 기반으로 비동기 처리를 진행하는 패턴
+(Event-based Asynchronous Pattern)을 구현한 클래스이다. BackgroundWorker로부터 생성된 객체는
+DoWork 이벤트 핸들러를 통해 실제 작업할 내용을 지정하고, RunWorkerAsync() 메서드를 호출하여 작업을 시작한다.*/
+
 namespace MultiThrdApp
 {
     class Program
