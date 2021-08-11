20c728a2c3a012e0c3169eecc280715b8c506727
Pooleun Jeong <ggznzn007@naver.com>
2021-08-11 14:03:35 +0900

    std_Cs_Task01_20210811

--- Cs_Study/Cs_std6/23_Task_T_01.cs
+++ Cs_Study/Cs_std6/23_Task_T_01.cs
@@ -37,3 +37,17 @@ namespace MultiThrdApp
         }
     }
 }
+
+/*Task 작업 취소
+비동기 작업을 취소하기 위해서는 Cancellation Token을 사용하는데, 작업 취소와 관련된 타입은
+CancellationTokenSource 클래스와 CancellationToken 구조체이다.
+CancellationTokenSource 클래스는 Cancellation Token을 생성하고 Cancel 요청을
+Cancellation Token들에게 보내는 일을 담당하고, CancellationToken은 현재 Cancel 상태를
+모니터링하는 여러 Listener들에 의해 사용되는 구조체이다.
+작업을 취소하는 일반적인 절차는 (1) 먼저 CancellationTokenSource 필드를 선언하고,
+(2) CancellationTokenSource 객체를 생성하며, (3) 비동기 작업 메서드 안에서 작업이 취소되었는지를
+체크하는 코드를 넣으며, (4) 취소 버튼이 눌러지면 CancellationTokenSource의 Cancel() 메서드를
+호출해 작업 취소를 요청한다. 아래 예제는 시작버튼을 누르면 작업을 비동기 Task를 100초 동안 실행하다가
+그 이전에 취소 버튼을 누르면 작업을 중간에 취소하는 코드이다.
+*/
+
