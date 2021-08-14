da7f11478e5799a1c4199b26e2b6ba05bdc17dc9
Pooleun Jeong <ggznzn007@naver.com>
2021-08-14 18:52:07 +0900

    std_Cs_Func01_20210814

---
+++ Cs_Study/Cs_std07/02_Func01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+/*함수의 선언
+관례적으로 함수의 이름은 대문자로 시작.
+//함수 선언
+[접근지정자] [리턴 타입] [함수 이름]([매개변수])
+{
+    //실행 코드
+    return [리턴 결과];
+}*/
+
+
+
+public int Plus(int a, int b)
+{
+    int result = a + b;
+    return result;
+}
+/*함수의 호출
+함수의 이름을 호출하면, 함수 내 코드 실행.
+*/
+Plus(3,5);
+> 8
