008a1e0ed5f82e81419f8ae27904e6a3a831018e
Pooleun Jeong <ggznzn007@naver.com>
2021-08-18 11:39:07 +0900

    std_Cs_Basic01_20210818

---
+++ Cs_Study/Cs_Basic3/04_Str02.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _11_StructorCtor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student b = new Student("정푸른", 880706);
+            b.OutInfo();
+        }
+    }
+
+    struct Student
+    {
+        public string Name;
+        public int StNum;
+        public Student(string aName, int aStNum)
+        {
+            Name = aName;
+            StNum = aStNum;
+        }
+        public void OutInfo()
+        {
+            Console.WriteLine("{0}, {1}", Name, StNum);
+        }
+    }
+}
