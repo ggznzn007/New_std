584c7bd6f222309c6472c324c169ba68053930e8
Pooleun Jeong <ggznzn007@naver.com>
2021-08-18 11:44:55 +0900

    std_Cs_Basic01_20210818

---
+++ Cs_Study/Cs_Basic3/05_Enum02.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _8_EnumInt
+{
+    class Program
+    {
+        enum SEASON { Spring, Summer, Fall, Winter }
+        static void Main(string[] args)
+        {
+            SEASON season;
+            season = SEASON.Fall;
+            string name = season.ToString();
+            Console.WriteLine(name);
+            season = (SEASON)Enum.Parse(typeof(SEASON), "Spring");
+            Console.WriteLine(season);
+            int Value = (int)season;
+            Console.WriteLine(Value);
+        }
+    }
+}
