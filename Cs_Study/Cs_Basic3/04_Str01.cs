d7c0ca544c40a4d4d45f97c11bbd37b3754d8ecf
Pooleun Jeong <ggznzn007@naver.com>
2021-08-18 11:34:00 +0900

    std_Cs_Basic01_20210818

---
+++ Cs_Study/Cs_Basic3/04_Str01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10_StudentStruct
+{
+    struct Student
+    {
+        public string Name;
+        public int StNum;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student b;
+            b.Name = "정푸른";
+            b.StNum = 880706;
+            Console.WriteLine("{0}, {1}", b.Name, b.StNum);
+        }
+    }
+}
