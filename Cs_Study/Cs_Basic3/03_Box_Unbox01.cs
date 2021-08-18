d825c5592873205769f708290c22d49c633beacf
Pooleun Jeong <ggznzn007@naver.com>
2021-08-18 11:30:33 +0900

    std_Cs_Basic01_20210818

---
+++ Cs_Study/Cs_Basic3/03_Box_Unbox01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _6_Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1234;
+            object box = i;
+            Console.WriteLine(box);
+            int j = (int)box;
+            Console.WriteLine(j);
+
+            double d = 3.14;
+            object box1 = d;
+            Console.WriteLine(box1);
+            double d1 = (double)box1;
+            Console.WriteLine(d1);
+        }
+    }
+}
