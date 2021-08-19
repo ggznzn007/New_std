023f057262e54fc95d94f1eb7620f24bca7cafaa
Pooleun Jeong <ggznzn007@naver.com>
2021-08-19 10:50:22 +0900

    std_Cs_Basic02_20210819

---
+++ Cs_Study/Cs_Basic3/07_Switch_Case02.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class CSTest
+{
+    static void Main()
+    {
+        string s = "Albert";
+        switch(s)
+        {
+            case "one":
+                Console.WriteLine(1);
+                break;
+            case "two":
+                Console.WriteLine(2);
+                break;
+            case "three":
+                Console.WriteLine(3);
+                break;
+            default:
+                Console.WriteLine(7);
+                break;
+        }
+    }
+}
