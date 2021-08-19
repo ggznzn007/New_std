9f11dfa3fdc57ff4aef3d91f1e797f2a57588d59
Pooleun Jeong <ggznzn007@naver.com>
2021-08-19 10:53:05 +0900

    std_Cs_Basic02_20210819

---
+++ Cs_Study/Cs_Basic3/08_While01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class CSTest
+{
+    static void Main()
+    {
+        int i = 1;
+        int sum = 0;
+        while(i<100)
+        {
+            sum = sum + i;
+            i++;
+        }
+        Console.WriteLine("1 ~ 99의 합 = {0}", sum);
+    }
+}
