7b2c03a63ab4792381325387c03ce7a763fd1a69
Pooleun Jeong <ggznzn007@naver.com>
2021-08-19 10:41:46 +0900

    std_Cs_Basic02_20210819

---
+++ Cs_Study/Cs_Basic3/06_If01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _7_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 27;
+            if (i % 3 == 0)
+                Console.WriteLine("i는 홀수입니다.");
+            else
+                Console.WriteLine("i는 짝수입니다.");
+        }
+    }
+}
