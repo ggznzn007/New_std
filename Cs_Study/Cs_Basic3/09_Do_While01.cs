c61b5b7f8b99c8313a9459995ddfc0780a1e6e6b
Pooleun Jeong <ggznzn007@naver.com>
2021-08-19 10:59:00 +0900

    std_Cs_Basic02_20210819

---
+++ Cs_Study/Cs_Basic3/09_Do_While01.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class CSTest
+{
+    static void Main()
+    {
+        string snum;
+        int num;
+        do
+        {
+            Console.Write("Please Input any Number : ");
+            snum = Console.ReadLine();
+            num = Convert.ToInt32(snum);
+            Console.WriteLine("That is {0}.", num);
+        } while (num != 0);
+    }
+}
