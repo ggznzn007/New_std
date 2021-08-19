beb47d99b1055c746a9c381348d9e4a0e4af09fd
Pooleun Jeong <ggznzn007@naver.com>
2021-08-19 10:46:44 +0900

    std_Cs_Basic02_20210819

---
+++ Cs_Study/Cs_Basic3/07_Switch_Case01.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class CSTest
+{
+    static void Main(string[] args)
+    {
+        int i = 5;
+        switch(i)
+        {
+            case 1:
+                Console.WriteLine("one");
+                break;
+            case 2:
+                Console.WriteLine("two");
+                break;
+            case 3:
+                Console.WriteLine("three");
+                break;
+            default:
+                Console.WriteLine("others");
+                break;
+
+        }
+    }
+}
