15bfed788fb07de79bdf34bebf1acfff2d722198
Pooleun Jeong <ggznzn007@naver.com>
2021-08-22 20:16:50 +0900

    std_Cs_Game_20210822

---
+++ Tetris_WF/Diagram.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_WF
+{
+    class Diagram
+    {
+        internal int X
+        {
+            get;
+            private set;
+        }
+        internal int Y
+        {
+            get;
+            private set;
+        }
+        internal Diagram()
+        {
+            Reset();
+        }
+
+        private void Reset()
+        {
+            X = GameRule.SX;
+            Y = GameRule.SY;
+        }
+
+        internal void MoveLeft()
+        {
+            X--;
+        }
+        internal void MoveRight()
+        {
+            X++;
+        }
+    }
+}
---
+++ Tetris_WF/GameRule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_WF
+{
+    class GameRule
+    {
+        internal const int B_WIDTH = 30;
+        internal const int B_HEIGHT = 30;
+        internal const int BX = 12;
+        internal const int BY = 20;
+        internal const int SX = 4;
+        internal const int SY = 0;
+    }
+}
