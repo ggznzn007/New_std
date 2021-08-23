43b3969bcfdccfd8ab3c54a8d9f3d155e0cdca27
Pooleun Jeong <ggznzn007@naver.com>
2021-08-23 15:18:35 +0900

    std_Cs_Tet_20210823

--- Tetris_WF/Board.cs
+++ Tetris_WF/Board.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -57,6 +58,56 @@ namespace Tetris_WF
                     {
                         board[x + xx, y + yy] += BlockValue.bvals[bn, turn, xx, yy];
                     }
+                }
+            }
+            CheckLines(y + 3);
+        }
+
+        private void CheckLines(int y)
+        {
+            int yy = 0;
+            for(yy=0;yy<4;yy++)
+            {
+                if(yy-yy<GameRule.BY)
+                {
+                    if(CheckLine(y-yy))
+                    {
+                        ClearLine(y - yy);
+                        y--;
+                    }
+                }
+            }
+        }
+
+        private void ClearLine(int y)
+        {
+            for(;y>0;y--)
+            {
+                for(int xx=0;xx<GameRule.BX;xx++)
+                {
+                    board[xx, y] = board[xx, y - 1];
+                }
+            }
+        }
+
+        private bool CheckLine(int y)
+        {
+            for(int xx=0;xx<GameRule.BX;xx++)
+            {
+                if(board[xx,y]==0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        internal void ClearBoard()
+        {
+            for(int xx=0;xx<GameRule.BX;xx++)
+            {
+                for(int yy=0;yy<GameRule.BY;yy++)
+                {
+                    board[xx, yy] = 0;
                 }
             }
         }
