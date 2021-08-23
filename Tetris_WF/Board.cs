e156f9ff6a8b1a43808d8eb43b169dbfb53f7ac5
Pooleun Jeong <ggznzn007@naver.com>
2021-08-23 13:58:22 +0900

    std_Cs_Tet_20210823

---
+++ Tetris_WF/Board.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_WF
+{
+    class Board
+    {
+        internal static Board GameBoard
+        {
+            get;
+            private set;
+        }
+        static Board()
+        {
+            GameBoard = new Board();
+        }
+        Board()
+        {
+
+        }
+        int[,] board = new int[GameRule.BX, GameRule.BY];
+        internal int this[int x,int y]
+        {
+            get
+            {
+                return board[x, y];
+            }
+        }
+
+    }
+}
