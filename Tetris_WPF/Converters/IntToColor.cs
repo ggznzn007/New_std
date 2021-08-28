1d5673c658c38424e579b4b7239f313114bb0974
Pooleun Jeong <ggznzn007@naver.com>
2021-08-28 16:56:25 +0900

    std_Cs_Tet_20210828

---
+++ Tetris_WPF/Converters/IntToColor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+using Tetris_WPF.Models;
+
+namespace Tetris_WPF.Converters
+{
+    public class IntToColor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int type = (int)value;
+
+            switch(type)
+            {
+                case Constants.Block_background:
+                    if (parameter != null)
+                        return new SolidColorBrush(Colors.Transparent);
+                    return new SolidColorBrush(Colors.Black);                    
+                case Constants.Block_wall:
+                    return new SolidColorBrush(Colors.Gray);
+                case Constants.Block_1:
+                    return new SolidColorBrush(Colors.Purple);
+                case Constants.Block_2:
+                    return new SolidColorBrush(Colors.Yellow);
+                case Constants.Block_3:
+                    return new SolidColorBrush(Colors.Blue);
+                case Constants.Block_4:
+                    return new SolidColorBrush(Colors.Red);
+                case Constants.Block_5:
+                    return new SolidColorBrush(Colors.Green);
+                case Constants.Block_6:
+                    return new SolidColorBrush(Colors.HotPink);
+                case Constants.Block_7:
+                    return new SolidColorBrush(Colors.MintCream);
+            }
+            return new SolidColorBrush(Colors.White);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tetris_WPF/Models/Constants.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris_WPF.Models
+{
+    public class Constants
+    {
+        public const int Block_wall = -1;
+        public const int Block_background = 0;
+
+        public const int Block_1 = 1;
+        public const int Block_2 = 2;
+        public const int Block_3 = 3;
+        public const int Block_4 = 4;
+        public const int Block_5 = 5;
+        public const int Block_6 = 6;
+        public const int Block_7 = 7;
+
+        public const int TETRIT_MOVE_RIGHT = 1;
+        public const int TETRIT_MOVE_LEFT = 2;
+        public const int TETRIT_MOVE_DOWN = 3;
+
+    }
+}
