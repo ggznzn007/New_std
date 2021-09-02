4c2d59ee009be6ab67450d21773ef1e2e8a3e17e
Pooleun Jeong <ggznzn007@naver.com>
2021-09-02 13:27:14 +0900

    std_Cs_LottoNum_20210902

---
+++ Lotto_Num_WF/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace Lotto_Num_WF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("맑은 고딕", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(33, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(746, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "로또 번호 생성!!!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(26, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "당첨번호";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("맑은 고딕", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.textBox1.Location = new System.Drawing.Point(163, 165);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(615, 38);
+            this.textBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 272);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "로또번호 생성기";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Lotto_Num_WF/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lotto_Num_WF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // 텍스트 박스 초기화
+            textBox1.Text = "";
+            textBox1.Text = Create_LottoNum();
+        }
+
+        private string Create_LottoNum()
+        {
+            string strTmp = "";
+            string strNum = "";
+            string strBonusNum = "";
+            int[] tmpNum = new int[7];
+
+            for (int i = 0; i < 7; i++)
+            {
+                Random rnd = new Random();
+                // 1부터 45 숫자 중 선택
+                tmpNum[i] = rnd.Next(1, 45);
+                for (int j = 0; j < i; j++)
+                {
+                    if (tmpNum[i] == tmpNum[j])
+                    {
+                        i = i - 1;
+                    }
+                }
+            }
+            strTmp = string.Join(", ", tmpNum);
+            // 로또 번호
+            strNum = strTmp.Substring(0, strTmp.LastIndexOf(',') - 1);
+            // 끝에 있는 보너스 번호
+            strBonusNum = strTmp.Substring(strTmp.LastIndexOf(',') + 1, 2).Trim();
+            return "로또 번호 : " + strNum + ", 보너스번호 : " + strBonusNum;
+        }
+    }
+}
