ebd0728b30c3906b0c3b36a34606c8c03fe29266
Pooleun Jeong <ggznzn007@naver.com>
2021-09-04 14:04:44 +0900

    std_Cs_Dodge_WF_20210904

--- Dodge_WF/Form1.Designer.cs
+++ Dodge_WF/Form1.Designer.cs
@@ -29,13 +29,34 @@ namespace Dodge_WF
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(7, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(789, 445);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Dodge_WF/Form1.cs
+++ Dodge_WF/Form1.cs
@@ -12,10 +12,52 @@ namespace Dodge_WF
 {
     public partial class Form1 : Form
     {
+        Random rs = new Random();
+        const int missile_quantity = 100;
+        int randNum = 0;
+
+        // 볼 사이즈와 컬러 구조체 선언
+        Point[] ballPoint = new Point[missile_quantity];
+        int sizePoint_x = 3;
+        int sizePoint_y = 3;
+        // 비행기 사이즈와 비행기 컬러 구조체 선언
+        int plane_x = 0;
+        int plane_y = 0;
+        int planeSize = 25;
+
+        int ready_x = 0;
+        int ready_y = 0;
+        int readySize_x = 200;
+        int readySize_y = 100;
+
+        int gameOver_x = 0;
+        int gameOver_y = 0;
+        int gameOver_size_x = 0;
+        int gameOver_size_y = 0;
+
+        int gameState = 0;
+
+        // 볼 스피드
+        int[] x_Speed = new int[100];
+        int[] y_Speed = new int[100];
 
         public Form1()
         {
             InitializeComponent();
+            plane_x = (pictureBox1.Width / 2);
+            plane_y = (pictureBox1.Height / 2);
+
+            ready_x = (pictureBox1.Width / 2) - (readySize_x / 2);
+            ready_y = (pictureBox1.Height / 2) - (readySize_y / 2);
+
+            gameOver_x = (pictureBox1.Width / 2) - (gameOver_size_x / 2);
+            gameOver_y = (pictureBox1.Height / 2) - (gameOver_size_y / 2);
+
+            for(int i =0; i<missile_quantity;i++)
+            {
+                ballPoint[i].X = rs.Next(0, pictureBox1.Width);
+                ballPoint[i].Y = rs.Next(0, pictureBox1.Height);
+            }
         }
     }
 }
