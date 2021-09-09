b9802d17b32587e43ee7502b8ab7d1784f8dc9ba
Pooleun Jeong <ggznzn007@naver.com>
2021-09-09 16:11:20 +0900

    std_Cs_20210909

---
+++ Cs_Study/Cs_std08/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs_std08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1000보다 작은 3또는 5의 배수의 합을 구하라
+            int sum = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0) //3의 배수또는 5의배수
+                {
+                    sum += i;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
