1737771ff6a277e869adf99859699395a2f7b5ab
Pooleun Jeong <ggznzn007@naver.com>
2021-09-13 18:23:55 +0900

    std_Unity_210913

---
+++ Unity_Std_01/DongDong.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DongDong : MonoBehaviour
+{
+    public GameManager manager;
+    public int level;
+    public bool isDrag;
+    public bool isMerge;
+    Rigidbody2D rigid;
+    CircleCollider2D circle;
+    Animator anim;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        circle = GetComponent<CircleCollider2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void OnEnable()
+    {
+        anim.SetInteger("Level", level);
+    }
+    void Update()
+    {
+        if (isDrag)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // x축 경계 설정
+            float leftBorder = -4.1f + transform.localScale.x / 2f;
+            float rightBorder = 4.1f - transform.localScale.x / 2f;
+            if (mousePos.x < leftBorder)
+            {
+                mousePos.x = leftBorder;
+            }
+            else if (mousePos.x > rightBorder)
+            {
+                mousePos.x = rightBorder;
+            }
+            mousePos.y = 8.5f;
+            mousePos.z = 0;
+            transform.position = Vector3.Lerp(transform.position, mousePos, 0.2f);
+        }
+    }
+
+    public void Drag()
+    {
+        isDrag = true;
+    }
+
+    public void Drop()
+    {
+        isDrag = false;
+        rigid.simulated = true;
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "DongDong")
+        {
+            DongDong other = collision.gameObject.GetComponent<DongDong>();
+            if (level == other.level && !isMerge && !other.isMerge && level < 7)
+            {
+                // 합치기
+                float meX = transform.position.x;
+                float meY = transform.position.y;
+                float otherX = other.transform.position.x;
+                float otherY = other.transform.position.y;
+                // 1. 내가 아래에 있을 때
+                // 2. 내가 동일한 높이에 있을 때 , 내가 오른쪽에 있을 때
+                if (meY < otherY || (meY == otherY && meX > otherX))
+                {
+                    // 상대방은 사라짐
+                    other.Hide(transform.position);
+                    // 나는 레벨업
+                    LevelUp();
+                }
+            }
+        }
+    }
+
+    public void Hide(Vector3 targetPos)
+    {
+        isMerge = true;
+        rigid.simulated = false;
+        circle.enabled = false;
+
+        StartCoroutine(HideRoutine(targetPos));
+    }
+
+    IEnumerator HideRoutine(Vector3 targetPos)
+    {
+        int frameCount = 0;
+
+        while (frameCount < 20)
+        {
+            frameCount++;
+            transform.position = Vector3.Lerp(transform.position, targetPos, 0.5f);
+            yield return null;
+        }
+
+        isMerge = false;
+        gameObject.SetActive(false);
+    }
+
+    void LevelUp()
+    {
+        isMerge = true;
+
+        rigid.velocity = Vector2.zero;
+        rigid.angularVelocity = 0;
+
+        StartCoroutine(LevelUpRoutine());
+    }
+
+    IEnumerator LevelUpRoutine()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        anim.SetInteger("Level", level + 1);
+
+        yield return new WaitForSeconds(0.1f);
+        level++;
+
+        manager.maxLevel = Mathf.Max(level, manager.maxLevel);
+
+        isMerge = false;
+    }
+}
