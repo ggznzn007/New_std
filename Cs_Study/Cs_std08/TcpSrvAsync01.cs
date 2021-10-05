5c8834e504e46d9993816c623755202bbc1edd44
Pooleun Jeong <ggznzn007@naver.com>
2021-10-05 16:13:38 +0900

    std_Cs_TCP_Asy_20211005

---
+++ Cs_Study/Cs_std08/TcpSrvAsync01.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs_std08
+{
+    class TcpSrvAsync01
+    {
+        static void Main(string[] args)
+        {
+            AysncEchoServer().Wait();
+        }
+
+        async static Task AysncEchoServer()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, 7000);
+            listener.Start();
+            while (true)
+            {
+                // 비동기 Accept                
+                TcpClient tc = await listener.AcceptTcpClientAsync().ConfigureAwait(false);
+
+                // 새 쓰레드에서 처리
+                _ = Task.Factory.StartNew(AsyncTcpProcess, tc);
+            }
+        }
+
+        async static void AsyncTcpProcess(object o)
+        {
+            TcpClient tc = (TcpClient)o;
+
+            int MAX_SIZE = 1024;  // 가정
+            NetworkStream stream = tc.GetStream();
+
+            // 비동기 수신            
+            var buff = new byte[MAX_SIZE];
+            var nbytes = await stream.ReadAsync(buff, 0, buff.Length).ConfigureAwait(false);
+            if (nbytes > 0)
+            {
+                string msg = Encoding.ASCII.GetString(buff, 0, nbytes);
+                Console.WriteLine($"{msg} at {DateTime.Now}");
+
+                // 비동기 송신
+                await stream.WriteAsync(buff, 0, nbytes).ConfigureAwait(false);
+            }
+
+            stream.Close();
+            tc.Close();
+        }
+    }
+}
