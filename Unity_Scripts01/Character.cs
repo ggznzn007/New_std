f22918ff12c65ca28259afba43ae706d967e07e3
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-11-23 17:11:51 +0900

    std_Unity_ChAnim_20211123

---
+++ Unity_Scripts01/Character.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    protected Vector2 direction;
+    private Animator animator;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    protected virtual void Update()
+    {
+        Move();
+        AnimateMovement();
+    }
+
+    public void Move()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    public void AnimateMovement()
+    {
+        //Sets the animation parameter so that he faces the correct direction
+        animator.SetFloat("x", direction.x);
+        animator.SetFloat("y", direction.y);
+    }
+
+}
+ 
