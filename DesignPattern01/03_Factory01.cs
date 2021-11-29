f4d9fcf3f9ad43ee18b848ab5a7f154b719a435f
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-11-29 11:55:29 +0900

    std_DesignPattern_20211129

---
+++ DesignPattern01/03_Factory01.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Factory01
+{
+    public abstract class Shape
+    {
+        public abstract double GetArea();
+    }
+
+    public abstract class ShapeCreator
+    {
+        public abstract Shape Create();
+    }
+}
---
+++ DesignPattern01/03_Shapes01.cs
@@ -0,0 +1,38 @@
+﻿using Factory01;
+using System;
+
+public class Circle : Shape
+{
+    public double Radius;
+
+    public override double GetArea()
+    {
+        return 2 * Math.PI * Radius;
+    }
+
+  
+}
+
+public class Triangle : Shape
+{
+    public override double GetArea()
+    {
+        return 0;
+    }
+}
+
+public class CircleCreator : ShapeCreator
+{
+    public override Shape Create()
+    {
+        return new Circle();
+    }
+}
+
+public class TriangleCreator : ShapeCreator
+{
+    public override Shape Create()
+    {
+        return new Triangle();
+    }
+}
