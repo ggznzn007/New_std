5a2bf453317e986468b9d1993cb87e80a795191c
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-11-30 09:15:51 +0900

    std_DesignPattern_20211130

---
+++ DesignPattern01/05_Adapter01.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<NewUnit> ltUnit = new List<NewUnit>();
+            ltUnit.Add(new NewUnit()); //신규 유닛
+            ltUnit.Add(new Unit()); //기존 유닛
+            foreach (NewUnit newUnit in ltUnit)
+            {
+                //같은 소스로 제어가능
+                newUnit.Move();
+                newUnit.Stop();
+            }
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// 타겟 클래스
+        /// </summary>
+        class NewUnit
+        {
+            public virtual void Move()
+            {
+                Console.WriteLine("New Unit Moved !");
+            }
+            public virtual void Stop()
+            {
+                Console.WriteLine("New Unit Stopped !");
+            }
+        }
+        /// <summary>
+        /// 어뎁터클래스
+        /// </summary>
+        class Unit : NewUnit
+        {
+            private OldUnit _adaptee = new OldUnit();
+            public override void Move()
+            {
+                _adaptee.MoveToPoint();
+            }
+            public override void Stop()
+            {
+                _adaptee.StopMove();
+            }
+        }
+        /// <summary>
+        /// 어뎁티 클래스
+        /// </summary>
+        class OldUnit
+        {
+            public void MoveToPoint()
+            {
+                Console.WriteLine("Old Unit Moved !");
+            }
+            public void StopMove()
+            {
+                Console.WriteLine("Old Unit Stopped !");
+            }
+        }
+    }
+}
