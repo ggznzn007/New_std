291bcdf920534a986d25bf4a1f9ea1fc4296d2f9
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-12-01 09:20:24 +0900

    std_DesignPattern_20211201

---
+++ DesignPattern01/09_Facade01.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*퍼사드 패턴
+    - 클래스 라이브러리 같은 커다란 코드를 사용하기 위한 간략한 인터페이스를 제공
+    - 라이브러리 외부 코드가 라이브러리 안쪽 코드에 의존하는 일을 감소시킨다
+    - 소프트웨어 라이브러리를 쉽게 이해할 수 있게 해준다
+    - 대부분의 외부 코드에서 퍼사드를 이용하기 때문에 시스템을 개발하는데 있어 유연성이 향상된다*/
+
+namespace CSharp_FacedePattern
+{
+    class SubSystemOne
+    {
+        public void MethodOne()
+        {
+            Console.WriteLine(" SubSystemOne Method");
+        }
+    }
+    class SubSystemTwo
+    {
+        public void MethodTwo()
+        {
+            Console.WriteLine(" SubSystemTwo Method");
+        }
+    }
+    class SubSystemThree
+    {
+        public void MethodThree()
+        {
+            Console.WriteLine(" SubSystemThree Method");
+        }
+    }
+    class SubSystemFour
+    {
+        public void MethodFour()
+        {
+            Console.WriteLine(" SubSystemFour Method");
+        }
+    }
+    class Facade
+    {
+        private SubSystemOne _one;
+        private SubSystemTwo _two;
+        private SubSystemThree _three;
+        private SubSystemFour _four;
+
+        public Facade()
+        {
+            _one = new SubSystemOne();
+            _two = new SubSystemTwo();
+            _three = new SubSystemThree();
+            _four = new SubSystemFour();
+        }
+        public void MethodA()
+        {
+            Console.WriteLine("\nMethodA() ---- ");
+            _one.MethodOne();
+            _two.MethodTwo();
+            _four.MethodFour();
+        }
+        public void MethodB()
+        {
+            Console.WriteLine("\nMethodB() ---- ");
+            _two.MethodTwo();
+            _three.MethodThree();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facade facade = new Facade();
+            facade.MethodA();
+            facade.MethodB();
+            Console.ReadKey();
+        }
+    }
+}
