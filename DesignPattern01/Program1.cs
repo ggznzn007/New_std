6a675734f6e19182b8820c2f8b8227f250752bb0
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-12-01 09:36:50 +0900

    std_DesignPattern_20211201

---
+++ DesignPattern01/Program1.cs
@@ -0,0 +1,16 @@
+﻿namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Picture picture = new Picture("언제나 휴일", 100, 100, 100);
+            Picture picture2 = new Picture("언제나 휴일2", 100, 100, 100);
+            SmartManager sm = new SmartManager();
+            sm.AddPicture(picture);
+            sm.AddPicture(picture2);
+            sm.Change("언제나 휴일", 20, 20, -50);
+            sm.View();
+        }
+    }
+}
