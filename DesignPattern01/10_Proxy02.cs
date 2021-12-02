46ecd1f0ab156804757deb1b78f1901ea88210a6
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-12-02 13:31:37 +0900

    std_DesignPattern_20211202

---
+++ DesignPattern01/10_Proxy02.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CSharp_DesignPattern
+{
+    abstract class Subject
+    {
+        public abstract void Request();
+    }
+    class RealSubject : Subject
+    {
+        public override void Request()
+        {
+            Console.WriteLine("Cakked RealSubject.Request");
+        }
+    }
+    class Proxy : Subject
+    {
+        private RealSubject _realSubject;
+
+        public override void Request()
+        {
+            if (_realSubject == null)
+            {
+                _realSubject = new RealSubject();
+            }
+            _realSubject.Request();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proxy proxy = new Proxy();
+            proxy.Request();
+
+            Console.ReadKey();
+        }
+    }
+}
