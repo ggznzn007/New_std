88361d362e73395f9c0ca1afc274a8ef21247834
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-12-03 13:59:44 +0900

    std_DesignPattern_20211203

--- DesignPattern01/12_Visitor/12_Picture.cs
+++ DesignPattern01/12_Visitor/12_Picture.cs
@@ -1,58 +1,28 @@
 ﻿using System;
-
 namespace Visitor
-
 {
-
     class Picture : Element
-
     {
-
         int tone;
-
         int brightness;
-
         int saturation;
-
         public Picture(string name, int tone, int brightness, int saturation) : base(name)
-
         {
-
             SetInfo(tone, brightness, saturation);
-
         }
-
         public void ViewInfo()
-
         {
-
             Console.WriteLine("색조:{0} 명도:{1} 채도:{2}", tone, brightness, saturation);
-
         }
-
         public void SetInfo(int tone, int brightness, int saturation)
-
         {
-
             this.tone = tone;
-
             this.brightness = brightness;
-
             this.saturation = saturation;
-
         }
-
         public override void Accept(IVisit visitor)
-
         {
-
             visitor.VisitPicture(this);
-
         }
-
     }
-
 }
-
-
-
--- DesignPattern01/12_Visitor/Program.cs
+++ DesignPattern01/12_Visitor/Program.cs
@@ -1,35 +1,19 @@
 ﻿namespace Visitor
-
 {
-
     class Program
-
     {
-
         static void Main(string[] args)
-
         {
-
             MyTour mt = new MyTour();
-
             Picture e = new Picture("A", 1, 1, 1);
-
-            Diary d = new Diary("2012.6.1", "언제나 휴일");
-
+            Diary d = new Diary("2021.12.3", "Abert");
             mt.AddElement(e);
-
             mt.AddElement(d);
-
             mt.ViewAll();
-
             mt.Reset();
-
             mt.ViewAll();
-
         }
-
     }
-
 }
 
 
