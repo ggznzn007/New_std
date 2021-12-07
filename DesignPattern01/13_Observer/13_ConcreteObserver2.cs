130c094ca56113c50138b5df21bd9f97ff13bce3
Pooleun Jeong <ggznzn007@naver.com>
2021-12-07 09:34:26 +0900

    std_DesignPattern_20211207

---
+++ DesignPattern01/13_Observer/13_ConcreteObserver2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine;
+
+// 옵저버 구현클래스
+public class ConcreteObserver2 : Observer
+{
+    // 대상타입의 클래스에서 이 메소드를 실행시킴
+    public override void OnNotify()
+    {
+        Console.WriteLine("옵저버 클래스의 메서드 실행 #2");
+        //Debug.Log("옵저버 클래스의 메서드 실행 #2");
+    }
+}
