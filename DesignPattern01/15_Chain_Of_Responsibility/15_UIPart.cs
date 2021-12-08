9d176c0ba77a06383a80bc8d689cb8ae59a8574b
Pooleun Jeong <ggznzn007@naver.com>
2021-12-08 09:25:12 +0900

    std_DesignPattern_20211208

---
+++ DesignPattern01/15_Chain_Of_Responsibility/15_UIPart.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+namespace ChainofResponsibility
+{
+    class UIPart
+    {
+        ChangeHandler head = null;
+        ChangeHandler tail = null;
+        public void AddChangeHandler(ChangeHandler handler)
+        {
+            if (head != null)
+            {
+                tail.Successor = handler;
+                tail = handler;
+            }
+            else
+            {
+                head = tail = handler;
+            }
+        }
+        public string ChangeRequest(List<int> mode, string subject)
+        {
+            if (head != null)
+            {
+                return head.ChangeRequest(mode, subject);
+            }
+            return subject;
+        }
+    }
+}
