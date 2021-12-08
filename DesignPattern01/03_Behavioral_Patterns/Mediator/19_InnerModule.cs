80cf0f0363df04da8fa2791afbcd4248b638a0fd
Pooleun Jeong <ggznzn007@naver.com>
2021-12-08 16:56:06 +0900

    std_DesignPattern_20211208

---
+++ DesignPattern01/03_Behavioral_Patterns/Mediator/19_InnerModule.cs
@@ -0,0 +1,23 @@
+﻿namespace Mediator
+{
+    abstract class InnerModule
+    {
+        IChange mediator;
+        public int Mid
+        {
+            get;
+            private set;
+        }
+        public InnerModule(IChange mediator, int mid)
+        {
+            Mid = mid;
+            this.mediator = mediator;
+        }
+        public void Changed(int value)
+        {
+            mediator.Change(Mid, value);
+        }
+        public abstract void SetValue(int value);
+        public abstract void AlramChanged(int value);
+    }
+}
