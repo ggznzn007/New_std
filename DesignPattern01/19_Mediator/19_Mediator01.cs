0b3aa3d574fb46dea4026dccd027d3696e574485
Pooleun Jeong <ggznzn007@naver.com>
2021-12-08 16:44:30 +0900

    std_DesignPattern_20211208

---
+++ DesignPattern01/19_Mediator/19_Mediator01.cs
@@ -0,0 +1,97 @@
+﻿/*미디에이터(중재자)패턴
+    - 클래스간의 복잡한 로직을 하나의 클래스(중재자)에 위임하여 처리
+    - 클래스 간 복잡도를 떨어뜨릴 목적으로 사용
+    - 유지보수, 재사용, 확장 등에 유리함*/
+using System;
+ 
+namespace CSharp_MediatorPattern
+{
+    abstract class Mediator
+    {
+        public abstract void Send(string message, Colleague colleague);
+    }
+    class ConcreteMediator : Mediator
+    {
+        private ConcreteColleague1 _colleague1;
+        private ConcreteColleague2 _colleague2;
+
+        public ConcreteColleague1 Colleague1
+        {
+            set { _colleague1 = value; }
+        }
+
+        public ConcreteColleague2 Colleague2
+        {
+            set { _colleague2 = value; }
+        }
+
+        public override void Send(string message, Colleague colleague)
+        {
+            if (colleague == _colleague1)
+            {
+                _colleague2.Notify(message);
+            }
+            else
+            {
+                _colleague1.Notify(message);
+            }
+        }
+    }
+    abstract class Colleague
+    {
+        protected Mediator mediator;
+
+        public Colleague(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+    }
+    class ConcreteColleague1 : Colleague
+    {
+        public ConcreteColleague1(Mediator mediator) : base(mediator)
+        {
+        }
+        public void Send(string message)
+        {
+            mediator.Send(message, this);
+        }
+        public void Notify(string message)
+        {
+            Console.WriteLine("Colleague1 gets message: " + message);
+        }
+    }
+    class ConcreteColleague2 : Colleague
+    {
+        public ConcreteColleague2(Mediator mediator) : base(mediator)
+        {
+        }
+
+        public void Send(string message)
+        {
+            mediator.Send(message, this);
+        }
+
+        public void Notify(string message)
+        {
+            Console.WriteLine("Colleague2 gets message: " + message);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteMediator m = new ConcreteMediator();
+
+            ConcreteColleague1 c1 = new ConcreteColleague1(m);
+            ConcreteColleague2 c2 = new ConcreteColleague2(m);
+
+            m.Colleague1 = c1;
+            m.Colleague2 = c2;
+
+            c1.Send("How are you?");
+            c2.Send("Fine, thanks");
+
+            Console.ReadKey();
+        }
+    }
+}
