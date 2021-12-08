5fd5c5d165b041282a48fcd3e7212a6f4b20ad54
Pooleun Jeong <ggznzn007@naver.com>
2021-12-08 16:56:56 +0900

    std_DesignPattern_20211208

---
+++ DesignPattern01/03_Behavioral_Patterns/Mediator/19_Shutter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Mediator
+{
+    class Shutter : InnerModule
+    {
+        int speed;
+        public Shutter(IChange mediator, int mid) : base(mediator, mid)
+        {
+            speed = 10;
+        }
+        public override void SetValue(int value)
+        {
+            this.speed = value;
+            Console.WriteLine("셔터 스피드:{0}", speed);
+            Changed(speed);
+        }
+        public override void AlramChanged(int iris)
+        {
+            Console.WriteLine("조리개 F값 변경 통보 받음");
+            SetValue(10 - iris);
+        }
+    }
+}
