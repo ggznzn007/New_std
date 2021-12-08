207bf6874470ecd4fd427ff4967c935ba23337ac
Pooleun Jeong <ggznzn007@naver.com>
2021-12-08 17:02:50 +0900

    std_DesignPattern_20211208

---
+++ DesignPattern01/03_Behavioral_Patterns/Mediator/AppGroup/Program5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Mediator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Camera camera = new Camera();
+            camera.ChangeMode(0);
+            Console.WriteLine("1");
+            camera.SetShutterSpeed(3);
+            Console.WriteLine("2");
+            camera.SetIris(3);
+            camera.ChangeMode(1);
+            Console.WriteLine("3");
+            camera.SetShutterSpeed(4);
+            Console.WriteLine("4");
+            camera.SetIris(4);
+        }
+    }
+}
