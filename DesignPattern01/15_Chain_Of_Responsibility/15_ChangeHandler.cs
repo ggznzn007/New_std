ad6b49d286b41e7cfacd35563d4ece53701d06f9
Pooleun Jeong <ggznzn007@naver.com>
2021-12-08 09:27:12 +0900

    std_DesignPattern_20211208

---
+++ DesignPattern01/15_Chain_Of_Responsibility/15_ChangeHandler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace ChainofResponsibility
+{
+    abstract class ChangeHandler
+    {
+        public ChangeHandler Successor
+        {
+            get;
+            set;
+        }
+        readonly int hid;
+        public ChangeHandler(int hid)
+        {
+            this.hid = hid;
+            Successor = null;
+        }
+        public abstract string ChangeRequest(List<int> mode, string picture);
+        protected bool IncludeMode(List<int> mode)
+        {
+            foreach (int m in mode)
+            {
+                if (m == hid)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
