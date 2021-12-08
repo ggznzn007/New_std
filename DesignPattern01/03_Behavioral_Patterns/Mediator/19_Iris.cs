f01f9d71901c193fb29ceadda8105fce26758e02
Pooleun Jeong <ggznzn007@naver.com>
2021-12-08 16:57:32 +0900

    std_DesignPattern_20211208

---
+++ DesignPattern01/03_Behavioral_Patterns/Mediator/19_Iris.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Mediator
+{
+    class Iris : InnerModule
+    {
+        int iris;
+        public Iris(IChange mediator, int mid) : base(mediator, mid)
+        {
+            iris = 0;
+        }
+        public override void SetValue(int value)
+        {
+            this.iris = value;
+            Console.WriteLine("조리개 F값:{0}", iris);
+            Changed(iris);
+        }
+        public override void AlramChanged(int speed)
+        {
+            Console.WriteLine("셔터 스피드 변경 통보 받음");
+            SetValue(10 - speed);
+        }
+    }
+}
