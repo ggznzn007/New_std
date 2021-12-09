495d0384dd83c2bec2983331ad5fd57342e45ad4
Pooleun Jeong <ggznzn007@naver.com>
2021-12-09 09:57:52 +0900

    std_Factory_20211209

---
+++ DesignPattern02/01_Creational/01_Factory/Ex_CreditCard/ConcreteProduct/CashBackCreditCard.cs
@@ -0,0 +1,34 @@
+﻿namespace FactoryMethod_ex
+{
+    //구조 상품 추상 클래스
+    class CashBackCreditCard : CreditCard
+    {
+        private readonly string _cardType;
+        private int _creditLimit;
+        private int _annualCharge;
+
+        public CashBackCreditCard(int creditLimit, int annualCharge)
+        {
+            _cardType = "CashBack";
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override string CardType
+        {
+            get { return _cardType; }
+        }
+
+        public override int CreditLimit
+        {
+            get { return _creditLimit; }
+            set { _creditLimit = value; }
+        }
+
+        public override int AnnualCharge
+        {
+            get { return _annualCharge; }
+            set { _annualCharge = value; }
+        }
+    }
+}
