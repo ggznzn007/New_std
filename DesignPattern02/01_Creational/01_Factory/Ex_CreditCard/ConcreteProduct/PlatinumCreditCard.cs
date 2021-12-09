88fd21d5fa84678debf37749c2d373f0125eafa3
Pooleun Jeong <ggznzn007@naver.com>
2021-12-09 10:02:51 +0900

    std_Factory_20211209

---
+++ DesignPattern02/01_Creational/01_Factory/Ex_CreditCard/ConcreteProduct/PlatinumCreditCard.cs
@@ -0,0 +1,34 @@
+﻿namespace FactoryMethod_ex
+{
+    // 구조 상품 추상 클래스
+    class PlatinumCreditCard : CreditCard
+    {
+        private readonly string _cardType;
+        private int _creditLimit;
+        private int _annualCharge;
+
+        public PlatinumCreditCard(int creditLimit, int annualCharge)
+        {
+            _cardType = "Platinum";
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override string CardType
+        {
+            get { return _cardType; }
+        }
+
+        public override int CreditLimit
+        {
+            get { return _creditLimit; }
+            set { _creditLimit = value; }
+        }
+
+        public override int AnnualCharge
+        {
+            get { return _annualCharge; }
+            set { _annualCharge = value; }
+        }
+    }
+}
---
+++ DesignPattern02/01_Creational/01_Factory/Ex_CreditCard/Creator/CardFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace FactoryMethod_ex
+{
+    // 생성 추상 클래스
+    abstract class CardFactory
+    {
+        public abstract CreditCard GetCreditCard();
+    }
+}
