6ee453aa79e598443d4e3b3fbd538aacf6e6b391
Pooleun Jeong <ggznzn007@naver.com>
2021-12-09 10:12:21 +0900

    std_Factory_20211209

---
+++ DesignPattern02/01_Creational/01_Factory/Ex_CreditCard/ConcreteCreator/CashBackFactory.cs
@@ -0,0 +1,20 @@
+﻿namespace FactoryMethod_ex
+{
+    // 상품 구조 클래스
+    class CashBackFactory : CardFactory
+    {
+        private int _creditLimit;
+        private int _annualCharge;
+
+        public CashBackFactory(int creditLimit, int annualCharge)
+        {
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override CreditCard GetCreditCard()
+        {
+            return new CashBackCreditCard(_creditLimit, _annualCharge);
+        }
+    }
+}
---
+++ DesignPattern02/01_Creational/01_Factory/Ex_CreditCard/ConcreteCreator/TitaniumFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace FactoryMethod_ex
+{
+    class TitaniumFactory : CardFactory
+    {
+        private int _creditLimit;
+        private int _annualCharge;
+
+        public TitaniumFactory(int creditLimit, int annualCharge)
+        {
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override CreditCard GetCreditCard()
+        {
+            return new TitaniumCreditCard(_creditLimit, _annualCharge);
+        }
+    }
+}
