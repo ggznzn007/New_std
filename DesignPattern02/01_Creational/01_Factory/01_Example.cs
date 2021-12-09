7784f2197566e0336c5df5f98044284581ff86d7
Pooleun Jeong <ggznzn007@naver.com>
2021-12-09 09:40:14 +0900

    std_Factory_20211209

---
+++ DesignPattern02/01_Creational/01_Factory/01_Example.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FactoryMethod
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //메소드 호출
+            File[] files = new File[2];
+            files[0] = new Furniture();// 가구
+            files[1] = new Crokery();// 그릇
+
+            foreach (File File in files)
+            {
+                Console.WriteLine("\n" + File.GetType().Name + "--");
+                foreach (Items Items in File.Itemss)
+                {
+                    Console.WriteLine(" " + Items.GetType().Name);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+    // 생산상품 추상 클래스 
+    abstract class Items
+    { }
+    // ConcreteProduct classes
+    class SofasetItems : Items
+    { }
+    class PlatesItems : Items
+    { }
+    class BedItems : Items
+    { }
+    class GlassesItems : Items
+    { }
+    class TeasetItems : Items
+    { }
+    class BowlsItems : Items
+    { }
+    class DiningItems : Items
+    { }
+    abstract class File
+    {
+        private List<Items> _Itemss = new List<Items>();
+        // Constructor calls abstract Factory method
+        public File()
+        {
+            this.CreateItemss();
+        }
+        public List<Items> Itemss
+        {
+            get { return _Itemss; }
+        }
+        // Factory Method
+        public abstract void CreateItemss();
+    }
+    class Furniture : File
+    {
+        // Factory Method implementation
+        public override void CreateItemss()
+        {
+            Itemss.Add(new SofasetItems());
+            Itemss.Add(new BedItems());
+            Itemss.Add(new DiningItems());
+        }
+    }
+    class Crokery : File
+    {
+        public override void CreateItemss()
+        {
+            Itemss.Add(new PlatesItems());
+            Itemss.Add(new GlassesItems());
+            Itemss.Add(new TeasetItems());
+            Itemss.Add(new BowlsItems());
+        }
+    }
+}
