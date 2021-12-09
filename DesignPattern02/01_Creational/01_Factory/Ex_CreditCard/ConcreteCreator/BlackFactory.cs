2b471766acf60a4e413ae4a9728208057eb2d0d9
Pooleun Jeong <ggznzn007@naver.com>
2021-12-09 10:14:12 +0900

    std_Factory_20211209

---
+++ DesignPattern02/01_Creational/01_Factory/Ex_CreditCard/ConcreteCreator/BlackFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace FactoryMethod_ex
+{
+    class BlackFactory : CardFactory
+    {
+        private int _creditLimit;
+        private int _annualCharge;
+
+        public BlackFactory(int creditLimit, int annualCharge)
+        {
+            _creditLimit = creditLimit;
+            _annualCharge = annualCharge;
+        }
+
+        public override CreditCard GetCreditCard()
+        {
+            return new BlackCreditCard(_creditLimit, _annualCharge);
+        }
+    }
+}
