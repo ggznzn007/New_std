0bb4418c00b3e2673fb751450c7ab7ebcfe36091
Pooleun Jeong <ggznzn007@naver.com>
2021-12-13 09:43:16 +0900

    std_Unity_20211213

---
+++ Unity_Scripts01/JoystickM.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JoystickM : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    RectTransform rect;
+    Vector2 touch = Vector2.zero;
+    [SerializeField] RectTransform handle;
+    float widethHalf;
+    [SerializeField] JoystickValue value;
+
+    private void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        widethHalf = rect.sizeDelta.x * 0.5f;
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+       touch = (eventData.position - rect.anchoredPosition)/ widethHalf;
+        if(touch.magnitude>1)
+        {
+            touch = touch.normalized;
+        }
+        value.joyTouch = touch;
+        handle.anchoredPosition = touch * widethHalf;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnDrag(eventData);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        value.joyTouch = Vector2.zero; 
+        handle.anchoredPosition = Vector2.zero;
+    }
+}
