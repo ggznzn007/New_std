958cd332653471168cec541cbe8a6943382574b0
Pooleun Jeong <ggznzn007@naver.com>
2021-12-17 11:40:49 +0900

    std_Cs_Builder_20211217

--- DesignPattern02/01_Creational/02_Builder/Builder01.cs
+++ DesignPattern02/01_Creational/02_Builder/Builder01.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+
+namespace Builder01
+{
+    public abstract class Builder
+    {
+        protected House house;
+
+        public void createHouse()
+        {
+            house = new House();
+        }
+
+        public abstract void buildWalls();
+        public abstract void buildDoors();
+        public abstract void buildRoof();
+        public abstract void buildWindows();
+        public abstract void getHouse();
+    }
+}
