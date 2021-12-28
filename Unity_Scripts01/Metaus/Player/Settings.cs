8f59de267d7c49eac390db13a6814767343241df
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2021-12-28 15:58:34 +0900

    std_Unity_20211228

---
+++ Unity_Scripts01/Metaus/Player/Settings.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public bool isJoyStick;
+    public Image touchBtn, joyStickBtn;
+    public Color black;
+    public PlayerCtrl playerCtrl_script;
+    GameObject mainView, missionView;
+
+    private void Start()
+    {
+        mainView = playerCtrl_script.mainView;
+        missionView = playerCtrl_script.missionView;
+    }
+    // 설정 버튼 누르면 호출
+    public void ClickSetting()
+    {
+        gameObject.SetActive(true);
+        playerCtrl_script.isCantMove = true;
+    }
+    // 게임으로 돌아가기 버튼 누르면 호출
+    public void ClickBack()
+    {
+        gameObject.SetActive(false);
+        playerCtrl_script.isCantMove = false;
+
+    }
+
+    // 터치이동을 누르면 호출
+    public void ClickTouch()
+    {
+        isJoyStick = false;
+        touchBtn.color = black;
+        joyStickBtn.color = Color.white;
+    }
+
+    // 조이스틱을 누르면 호출
+    public void ClickJoyStick()
+    {
+        isJoyStick = true;
+        touchBtn.color = Color.white; 
+        joyStickBtn.color = black;
+    }
+
+    // 게임 종료 누르면 호출
+    public void ClickQuit()
+    {
+        mainView.SetActive(true);
+        missionView.SetActive(false);
+
+        //캐릭터 삭제
+        playerCtrl_script.DestoyPlayer();
+    }
+}
