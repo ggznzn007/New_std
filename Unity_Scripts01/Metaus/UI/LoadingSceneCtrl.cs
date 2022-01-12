7a395c62acb4033f812f6e186536ad5ec8422734
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-01-12 18:44:20 +0900

    std_Unity_LoadingSceneCtrl_20220112

---
+++ Unity_Scripts01/Metaus/UI/LoadingSceneCtrl.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingSceneCtrl : MonoBehaviour
+{
+    static string nextScene;
+
+    [SerializeField]
+    Image progressBar;
+
+    public static void LoadScene(string sceneName)
+    {
+        nextScene = sceneName;
+        SceneManager.LoadScene("Loading");
+    }
+
+    private void Start()
+    {
+        StartCoroutine(LoadSceneProgress());
+    }
+
+    IEnumerator LoadSceneProgress()
+    {
+        AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
+        op.allowSceneActivation = false;
+
+        float timer = 0f;
+        while (!op.isDone)
+        {
+            yield return null;
+            if (op.progress < 0.3f)
+            {
+                progressBar.fillAmount = op.progress;
+            }
+            else
+            {
+                timer += Time.unscaledDeltaTime;
+                progressBar.fillAmount = Mathf.Lerp(0.3f, 1f, timer);
+                if (progressBar.fillAmount >= 1f)
+                {
+                    op.allowSceneActivation = true;
+                    yield break;
+                }
+            }
+        }
+    }
+}
