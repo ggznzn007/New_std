38ee7944408eb298b05e8ec242ab77670be197cc
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-01-18 16:07:30 +0900

    std_Unity_FadeIN_20220118

---
+++ Unity_Scripts01/Metaus/UI/Fade_IN.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fade_IN : MonoBehaviour
+{
+    private GameObject fadeInPanel;
+    private Image image;
+
+    private bool isAlpha = false;
+
+    private void Awake()
+    {
+        fadeInPanel = this.gameObject;
+        image = fadeInPanel.GetComponent<Image>();
+    }
+
+     void Update()
+    {
+        StartCoroutine(FadeInScreen());
+        if(isAlpha)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private IEnumerator FadeInScreen()
+    {
+        Color color = image.color;
+
+        for (int i = 100; i >= 0; i--)
+        {
+            color.a -= Time.deltaTime * 0.008f;
+            image.color = color;
+            if(image.color.a<=0)
+            {
+                isAlpha = true;
+            }
+        }
+        yield return null;
+    }
+}
