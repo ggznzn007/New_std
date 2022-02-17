8e5862a3e273abd81bd4e3569cfe55060b3fba62
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-02-17 12:02:30 +0900

    Dev_Update_20220217

--- Unity_Scripts01/Metaus/UI/LeanTween_setting.cs
+++ Unity_Scripts01/Metaus/UI/LeanTween_setting.cs
@@ -5,20 +5,13 @@ using UnityEngine;
 public class LeanTween_setting : MonoBehaviour
 {
     
-    void Start()
+   private void Start()
     {
-        //transform.localScale = Vector2.zero;
-        //
-        transform.LeanScale(new Vector2(.9f,.9f), 0.6f).setLoopPingPong();
-        
-
+        this.transform.LeanScale(new Vector2(.9f,.9f), 0.6f).setLoopPingPong();
     }
 
-    private void Update()
-    {
-        //transform.LeanScale(Vector2.zero, 1f).setEaseInBack();
-       // transform.LeanScale(Vector2.zero, 1f).setEaseInBounce();
-    }
+   
+    
     public void Open()
     {
         //transform.LeanScale(Vector2.one, 1f);
