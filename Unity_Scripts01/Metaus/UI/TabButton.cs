bdb9767d407c178e5acd311b80b44b86640b357d
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-02-27 14:11:23 +0900

    Dev_Update_20220226

---
+++ Unity_Scripts01/Metaus/UI/TabButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TabButton : MonoBehaviour
+{
+    Image backGround;
+    public Sprite idleImg;
+    public Sprite seletedImg;
+    private void Awake()
+    {
+        backGround = GetComponent<Image>();
+    }
+    public void Seleted()
+    {
+        backGround.sprite = seletedImg;
+    }
+    public void Unseleted()
+    {
+        backGround.sprite = idleImg;
+    }
+}
