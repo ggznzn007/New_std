bc3d2046a491d963521ec7bb240de3b3bab17561
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-03-04 14:11:34 +0900

    Dev_Update_20220303

--- Unity_Scripts01/GPGS/GPGSManager2.cs
+++ Unity_Scripts01/GPGS/GPGSManager2.cs
@@ -5,33 +5,25 @@ using UnityEngine.UI;
 using GooglePlayGames;
 using GooglePlayGames.BasicApi;
 
-
 #if UNITY_ANDROID
-public class GPGSManager : MonoBehaviour
+public class GPGSManager2 : MonoBehaviour
 {
     public Text myName;
     public Text myLog;
     public RawImage myImage;
-    public Button start, gsLogin;
-
-    private bool bWaitingForAuth = false;
-
+    public Button fakeLogin, fakeStart;
+    public Button realLogin, realStart, gsLogin;
+   // private bool bWaitingForAuth = false;
     private void Awake()
     {
         myLog.text = "구글 로그인 해주세요";
-        myImage.gameObject.SetActive(false);
+        realLogin.interactable = true;
+        fakeLogin.interactable = true;
         // 구글 게임서비스 활성화 (초기화)
         PlayGamesPlatform.InitializeInstance(new PlayGamesClientConfiguration.Builder().Build());
         PlayGamesPlatform.DebugLogEnabled = true;
         PlayGamesPlatform.Activate();
-    }
-
-    private void Start()
-    {
-        //게임시작시 자동로그인
-        //doAutoLogin();
-    }
-
+    }    
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -40,9 +32,8 @@ public class GPGSManager : MonoBehaviour
             LoadingUIController.Instance.LoadScene("Enter"); // 인트로 씬 불러옴
         }
     }
-
     // 자동로그인
-    public void doAutoLogin()
+    /*public void doAutoLogin()
     {
         myLog.text = "...";
         if (bWaitingForAuth)
@@ -62,7 +53,7 @@ public class GPGSManager : MonoBehaviour
         {
             myLog.text = "로그인이 필요합니다.\n";
         }
-    }
+    }*/
 
     // 수동로그인 
     public void OnBtnLoginClicked()
@@ -71,10 +62,11 @@ public class GPGSManager : MonoBehaviour
         if (Social.localUser.authenticated)
         {
             Debug.Log(Social.localUser.userName);
-            myLog.text = "name : " + "\n" + Social.localUser.userName + "\n";
+            myLog.text = "name : " + "\n" + Social.localUser.userName + "\n";            
+            myLog.transform.LeanScale(Vector3.one, 0.2f);
             StartCoroutine(UserPictureLoad());
-            start.transform.LeanScale(Vector3.one, 0.2f);
-            gsLogin.gameObject.SetActive(true);
+            realStart.transform.LeanScale(Vector3.one, 0.2f);
+            gsLogin.transform.LeanScale(Vector3.one, 0.2f);
         }
         else
             Social.localUser.Authenticate((bool success) =>
@@ -82,69 +74,84 @@ public class GPGSManager : MonoBehaviour
                 if (success)
                 {
                     Debug.Log(Social.localUser.userName);
-                    myLog.text = "name : " + "\n" + Social.localUser.userName + "\n";
+                    myLog.text = "name : " + "\n" + Social.localUser.userName + "\n";                    
+                    myLog.transform.LeanScale(Vector3.one, 0.2f);
                     StartCoroutine(UserPictureLoad());
-                    start.transform.LeanScale(Vector3.one, 0.2f);
-                    gsLogin.gameObject.SetActive(true);
+                    realStart.transform.LeanScale(Vector3.one, 0.2f);
+                    gsLogin.transform.LeanScale(Vector3.one, 0.2f);
                 }
                 else
                 {
-                    Debug.Log("Login Fail");
-                    myLog.text = "로그인 실패! 다시 시도해주세요.\n";
 
-                    start.transform.LeanScale(Vector3.zero, 0.2f).setEaseOutBack();
-                    gsLogin.gameObject.SetActive(false);
+                    Debug.Log("Login Fail");
+                    myLog.text = "로그인 실패! 다시 시도해주세요.\n";                   
+                    myLog.transform.LeanScale(Vector3.one, 0.2f);
+                    realStart.transform.LeanScale(Vector3.zero, 0.2f).setEaseOutBack();
+                    gsLogin.transform.LeanScale(Vector3.zero, 0.2f).setEaseOutBack();
                 }
             });
     }
-
     // 수동 로그아웃 
     public void OnBtnLogoutClicked()
     {
         ((PlayGamesPlatform)Social.Active).SignOut();
         myLog.text = "로그인이 필요합니다.";
     }
-
     // 인증 callback
     void AuthenticateCallback(bool success)
     {
         myLog.text = "Loading";
+        myLog.transform.LeanScale(Vector3.one, 0.2f);        
         if (success)
         {
             // 사용자 이름을 띄어줌 
             myLog.text = "환영합니다!!!" + "\n" + Social.localUser.userName + "\n";
-
+            myLog.transform.LeanScale(Vector3.one, 0.2f);            
             StartCoroutine(UserPictureLoad());
         }
         else
         {
             myLog.text = "로그인이 필요합니다.\n";
+            myLog.transform.LeanScale(Vector3.one, 0.2f);            
         }
     }
-
     // 유저 이미지 받아오기 
     IEnumerator UserPictureLoad()
     {
-        myLog.text = "유저 로그인 정보 로딩중 ...";
+        myLog.text = "유저 로그인 정보 로딩중 ...";        
+        myLog.transform.LeanScale(Vector3.zero, 0.2f).setEaseOutBack();
         // 최초 유저 이미지 가져오기
-        myImage.gameObject.SetActive(true);
+        myImage.transform.LeanScale(Vector3.one, 0.2f);        
         Texture2D pic = Social.localUser.image;
-
         // 구글 아바타 이미지 여부를 확인 후 이미지 생성 
         while (pic == null)
         {
             pic = Social.localUser.image;
-
             yield return null;
         }
-
         myImage.texture = pic;
         myName.text = Social.localUser.userName;
+        myLog.transform.LeanScale(Vector3.one, 0.2f);
         myLog.text = "로그인 완료되었습니다!!!";
+        realLogin.interactable = false;
     }
-
-
-
+    IEnumerator FakeUserInfoLoad()
+    {
+        myImage.texture = myImage.texture;
+        myImage.transform.LeanScale(Vector3.one, 0.2f);
+        myName.text = myName.text + "님";
+        myName.transform.LeanScale(Vector3.one, 0.2f);
+        myLog.text = "환영합니다!!!";       
+        myLog.transform.LeanScale(Vector3.one, 0.2f);
+        gsLogin.transform.LeanScale(Vector3.one, 0.2f);
+        fakeStart.transform.LeanScale(Vector3.one, 0.2f);
+        fakeLogin.interactable = false;
+        yield return null;
+    }
+    public void FakeGoogleLogin()
+    {
+        StartCoroutine(FakeUserInfoLoad());        
+    }
     public void ClickEnter()
     {
         if (Social.localUser.authenticated)
@@ -152,22 +159,18 @@ public class GPGSManager : MonoBehaviour
             LoadingUIController.Instance.LoadScene("Main");
         }
     }
-
+    public void ClickFakeEnter()
+    {
+        LoadingUIController.Instance.LoadScene("Main");
+    }
     public void ClickGSLogin()
     {
         StartCoroutine(ClickStart());
-        /*if (Social.localUser.authenticated)
-        {
-            
-        }*/
     }
-
     public void ClickBack()
     {
-
         LoadingUIController.Instance.LoadScene("Enter");
     }
-
     private IEnumerator ClickStart()
     {
         this.transform.LeanScale(Vector2.zero, 3f).setEaseInBack();
