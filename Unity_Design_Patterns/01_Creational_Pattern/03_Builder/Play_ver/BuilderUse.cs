798266e5ae77a9909d936baa5ffc121f469984f7
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-03-28 15:18:30 +0900

    std_DesignPattern_Unity_Builder_20220328

---
+++ Unity_Design_Patterns/01_Creational_Pattern/03_Builder/Play_ver/BuilderUse.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuilderUse : MonoBehaviour
+{
+    void Start()
+    {
+        // Instantiate the director and builders
+        Engineer engineer = new Engineer();
+        //CarBuilder carBuilder = new CarBuilder();
+        CarBuilder carBuilder = GetComponent<CarBuilder>();
+        carBuilder.makeVehicle();
+        //MotorCycleBuilder motorCycleBuilder = new MotorCycleBuilder();
+        MotorCycleBuilder motorCycleBuilder = GetComponent<MotorCycleBuilder>();
+        motorCycleBuilder.makeVehicle();
+
+        // 빌더를 통해 구성해야 할 제품을 입력받아 제품을 구성한다.
+        engineer.Construct(carBuilder);
+        engineer.Construct(motorCycleBuilder);
+
+        // 최종 생산된 제품을 반환받는다.
+        Vehicle car = carBuilder.getVehicle();
+        Debug.Log(car.GetPartsList());
+
+        Vehicle motorCycle = motorCycleBuilder.getVehicle();
+        Debug.Log(motorCycle.GetPartsList());
+
+        // 최종 생산된 제품의 위치를 지정한다.
+        car.transform.position = new Vector3(-3f, 0, 0);
+        motorCycle.transform.position = new Vector3(3f, 0, 0);
+    }
+
+}
