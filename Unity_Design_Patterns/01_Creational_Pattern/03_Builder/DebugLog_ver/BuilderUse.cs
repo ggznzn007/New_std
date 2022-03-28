8e0d231fe0dbeca322c0932f0e11a25172ad2a65
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-03-28 14:39:11 +0900

    Create BuilderUse.cs

---
+++ Unity_Design_Patterns/01_Creational_Pattern/03_Builder/DebugLog_ver/BuilderUse.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class BuilderUse : MonoBehaviour
+{
+    void Start()
+    {
+        // Instantiate the director and builders
+        Engineer engineer = new Engineer();  // ⭐ 엔지니어 만들기
+        CarBuilder carBuilder = new CarBuilder(); // 빈 Car 만들기
+        MotorCycleBuilder motorCycleBuilder = new MotorCycleBuilder();  // 빈 MotorCycle 만들기
+
+        // 빌더를 통해 구성해야 할 제품을 입력받아 제품을 구성한다.
+        // 부품들을 붙인다.
+        engineer.Construct(carBuilder);
+        engineer.Construct(motorCycleBuilder);
+
+        // 최종 생산된 완성된 제품을 반환받는다.
+        Vehicle car = carBuilder.getVehicle();
+        Debug.Log(car.GetPartsList());
+        Vehicle motorCycle = motorCycleBuilder.getVehicle();
+        Debug.Log(motorCycle.GetPartsList());
+    }
+
+}
