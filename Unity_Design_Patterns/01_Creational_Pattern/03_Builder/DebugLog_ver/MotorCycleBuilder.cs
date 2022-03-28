0cd821164769550f5bbd1f68d292f33ac876bf3f
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-03-28 14:38:30 +0900

    Create MotorCycleBuilder.cs

---
+++ Unity_Design_Patterns/01_Creational_Pattern/03_Builder/DebugLog_ver/MotorCycleBuilder.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+class MotorCycleBuilder : IVehicleBuilder
+{
+    private Vehicle _vehicle;
+
+    public Vehicle getVehicle()
+    {
+        return _vehicle;
+    }
+
+    public MotorCycleBuilder()
+    {
+        // 빌더가 만들어질 때 아무것도 없는 모터싸이클도 하나 만든다.(부품이 붙기 전)
+        _vehicle = new Vehicle(VehicleType.MotorCycle);
+    }
+
+    public void BuildFrame()  // MotorCycle의 프레임 만들기 
+    {
+        _vehicle.AddPart("알루미늄");
+    }
+
+    public void BuildEngine() // MotorCycle의 엔진 만들기 
+    {
+        _vehicle.AddPart("50마력");
+    }
+
+    public void BuildWheels() // MotorCycle의 바퀴 만들기 
+    {
+        _vehicle.AddPart("앞 바퀴");
+        _vehicle.AddPart("뒤 바퀴");
+    }
+}
