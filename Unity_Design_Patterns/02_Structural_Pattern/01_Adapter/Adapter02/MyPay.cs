d3418a9c91031f082417acfacd5856bf3fd302f2
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-03-30 14:31:01 +0900

    std_Unity_DP_Adapter_20220330

---
+++ Unity_Design_Patterns/02_Structural_Pattern/01_Adapter/Adapter02/MyPay.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyPay : MonoBehaviour
+{
+    void Start()
+    {
+
+        // for PayX : 원래 이렇게 사용중 ...
+
+        PayImpl myPay = new PayImpl();
+        myPay.setCreditCardNum("12345");
+        string myCardNum = myPay.getCreditCardNum();
+        // Debug.Log("PayX : " + myCardNum);
+
+        // for PayY : PayX 와 같은 메서드명을 사용
+        //            그러므로 코드를 바꿀 필요가 없다.
+        Debug.Log("PayY : " + myCardNum);
+    }
+}
