937c37264d617619928c31bf4beb91d5aa8c052a
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-03-30 14:25:59 +0900

    std_Unity_DP_Adapter_20220330

---
+++ Unity_Design_Patterns/02_Structural_Pattern/01_Adapter/Adapter01/WildTurkey.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WildTurkey : Turkey
+{
+    public void gobble()
+    {
+        Debug.Log("Ä¥¸éÁ¶ : ¿ï±â(°í¸£¸¤°í¸£¸¤)");
+    }
+
+    public void fly()
+    {
+        Debug.Log("Ä¥¸éÁ¶ : ³¯±â");
+
+    }
+}
