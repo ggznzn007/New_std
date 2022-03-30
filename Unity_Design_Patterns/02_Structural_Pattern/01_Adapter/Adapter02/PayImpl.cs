c8b052d889bf9d239866ddc324be4e4e61b70e3b
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-03-30 14:30:38 +0900

    std_Unity_DP_Adapter_20220330

---
+++ Unity_Design_Patterns/02_Structural_Pattern/01_Adapter/Adapter02/PayImpl.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+// 우리 회사에서 PayY 인터페이스 구현
+public class PayImpl : PayY, PayX // 다중 상속  = 어댑터가 됨
+{
+
+    // for PayY
+    private string customerCardNum;
+
+    public string getCustomerCardNum()
+    {
+        Debug.Log("PayY (Get)");
+        return customerCardNum;
+    }
+
+    public void setCustomerCardNum(string customerCardNum)
+    {
+        Debug.Log("PayY (Set)");
+        this.customerCardNum = customerCardNum;
+    }
+
+    // ------------------------------------------------------
+
+    // for PayX Method
+    public string getCreditCardNum()
+    {
+        return getCustomerCardNum();
+    }
+
+    public void setCreditCardNum(string creditCardNum)
+    {
+        setCustomerCardNum(creditCardNum);
+    }
+}
