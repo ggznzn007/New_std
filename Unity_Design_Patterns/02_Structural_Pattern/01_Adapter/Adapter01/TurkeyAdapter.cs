fb3b97993b3f9e27a60bb26309a333f515030eb7
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-03-30 14:26:23 +0900

    std_Unity_DP_Adapter_20220330

---
+++ Unity_Design_Patterns/02_Structural_Pattern/01_Adapter/Adapter01/TurkeyAdapter.cs
@@ -0,0 +1,23 @@
+
+// Duck 객체가 모자라서 Turkey 객체를 대신 사용해야 하는 상황
+// 인터페이스가 다르기 때문에 Turkey객체를 바로 사용할 수는 없다.
+public class TurkeyAdapter : Duck
+{
+    Turkey turkey;
+
+    public TurkeyAdapter(Turkey turkey)
+    {
+        this.turkey = turkey;
+    }
+
+    public void quack()
+    {
+        turkey.gobble();
+    }
+
+    public void fly()
+    {
+        turkey.fly();
+    }
+
+}
