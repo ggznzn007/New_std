2959638e27b91cae6371bdea32c0d6e9e85a4bc1
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-03-30 14:25:31 +0900

    std_Unity_DP_Adapter_20220330

---
+++ Unity_Design_Patterns/02_Structural_Pattern/01_Adapter/Adapter01/MallardDuck.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MallardDuck : Duck
+{ 
+    public void quack()
+    {
+        Debug.Log("¿À¸® : ¿ï±â(²Ð²Ð)");
+    }
+
+    public void fly()
+    {
+        Debug.Log("¿À¸® : ³¯±â");
+    }
+}
