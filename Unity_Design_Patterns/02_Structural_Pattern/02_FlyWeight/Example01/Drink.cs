4a7321b295e20a8d1071e59689d8a7d1483ac542
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-03-31 16:22:47 +0900

    Unity_DP_FlyWeight_20220331

---
+++ Unity_Design_Patterns/02_Structural_Pattern/02_FlyWeight/Example01/Drink.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface Drink
+{
+    string getName();
+}
+
+public class Cola : Drink
+{
+    string name;
+    int price;
+
+    public Cola(string name)
+    {
+        this.name = name;
+    }
+
+    public string getName() // 오버라이딩
+    {
+        return name;
+    }
+}
