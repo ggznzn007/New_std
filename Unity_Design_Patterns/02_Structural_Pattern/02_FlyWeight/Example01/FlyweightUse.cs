e395ac8f54414c0295e3b8ab1d0e127af749f2c5
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-03-31 16:23:30 +0900

    Create FlyweightUse.cs

---
+++ Unity_Design_Patterns/02_Structural_Pattern/02_FlyWeight/Example01/FlyweightUse.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyweightUse : MonoBehaviour
+{
+
+    void Start()
+    {
+        Cola coke1 = DrinkFactory.getDrink("코카콜라");
+        Cola coke2 = DrinkFactory.getDrink("펩시콜라");
+        Cola coke3 = DrinkFactory.getDrink("코카콜라");
+
+        Debug.Log(coke1 = coke2); // 둘이 다르다
+        Debug.Log(coke1 = coke3); // 둘이 같다
+        
+        Debug.Log("name : " + coke1.getName()); 
+    }
+
+}
