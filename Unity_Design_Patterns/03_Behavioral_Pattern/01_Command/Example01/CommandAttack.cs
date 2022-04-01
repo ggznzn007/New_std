48c3779970ef8fc5b54fcce220cb12e2b5222c4e
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-04-01 16:10:58 +0900

    Unity_DP_Command_20220401

---
+++ Unity_Design_Patterns/03_Behavioral_Pattern/01_Command/Example01/CommandAttack.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Concrete Command 객체 : 직접적으로 동작하는 객체
+public class CommandAttack : CommandKey
+{
+	public CommandAttack(MonoBehaviour _mono, GameObject _shield,
+						  GameObject _cannon, Transform _firePos)
+	{
+		this.shield = _shield;
+		this.cannon = _cannon;
+		this.firePos = _firePos;
+		this.mono = _mono;
+	}
+
+	public override void Execute()
+	{
+		Attack();
+	}
+
+	void Attack()
+	{
+		Debug.Log("Attack");
+		GameObject.Instantiate(cannon, firePos.position, firePos.rotation);
+	}
+}
