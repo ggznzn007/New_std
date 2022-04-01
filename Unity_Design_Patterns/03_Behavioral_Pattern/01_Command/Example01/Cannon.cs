c3d381110c6b6fa9c6006494e26e6e2b09c6b185
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-04-01 16:10:29 +0900

    Unity_DP_Command_20220401

---
+++ Unity_Design_Patterns/03_Behavioral_Pattern/01_Command/Example01/Cannon.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+	float power = 5000.0f;
+	Vector3 velocity;
+	
+
+	void Start()
+	{
+		velocity = transform.right * power;
+
+		GetComponent<Rigidbody>().AddForce(velocity);
+		
+		StartCoroutine("DeleteCannon");
+	}
+
+	IEnumerator DeleteCannon()
+	{
+		yield return new WaitForSeconds(2.5f);
+		Destroy(this.gameObject);
+	}
+
+}
