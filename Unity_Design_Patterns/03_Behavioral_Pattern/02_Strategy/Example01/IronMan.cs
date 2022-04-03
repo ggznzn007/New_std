cf683011f2581aba569c780a2bf6615893d10eae
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-04-03 15:54:43 +0900

    Unity_DP_Strategy_20220403

---
+++ Unity_Design_Patterns/03_Behavioral_Pattern/02_Strategy/Example01/IronMan.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IronMan : MonoBehaviour, IWeapon
+{
+    public void Shoot(GameObject obj)
+    {
+        Vector3 initialPosition = new Vector3(transform.position.x, transform.position.y, 0);
+        GameObject bullet = Instantiate(obj) as GameObject;
+        bullet.transform.position = initialPosition;
+    }
+
+}
