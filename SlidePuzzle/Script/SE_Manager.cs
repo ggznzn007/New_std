3f97afcc89b8ac6126b9aaba7d8df2172b352cfc
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-04-14 17:51:39 +0900

    std_Unity_SlidePuzzle_20220414

---
+++ SlidePuzzle/Script/SE_Manager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SE_Manager : MonoBehaviour
+{
+    public static SE_Manager instance;
+    AudioSource sound;
+
+    [Header("AudioClip")]
+    public AudioClip shuffle;
+    public AudioClip btn;
+    public AudioClip gameClear;
+    
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        sound = GetComponent<AudioSource>();
+    }
+
+    public void Playsound(AudioClip clip)
+    {
+        sound.PlayOneShot(clip); 
+    }
+}
