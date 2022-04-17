00f69cb80e6a263bd73ebd85903e6c58aedc019c
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-04-17 16:26:32 +0900

    std_Unity_1010Puzzle_20220417

--- 1010/Script/SoundManager.cs
+++ 1010/Script/SoundManager.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    public static SoundManager instance;
-    AudioSource sound;
+    public static SoundManager instance; // 싱글톤으로 소리제어
+    AudioSource sound; 
 
     [Header("AudioClip")]
-    public AudioClip blockClick;
-    public AudioClip blockDrop;
-    public AudioClip blockClear;
-    public AudioClip blockSet;
-    public AudioClip gameOver;
+    public AudioClip blockClick;         // 블럭선택
+    public AudioClip blockDrop;          // 블럭놓았을 때
+    public AudioClip blockClear;         // 라인이 없어질 때
+    public AudioClip blockSet;           // 블럭생성
+    public AudioClip gameOver;           // 게임오버
 
     private void Awake()
     {
@@ -22,8 +22,8 @@ public class SoundManager : MonoBehaviour
         sound = GetComponent<AudioSource>();
     }
 
-    public void Playsound(AudioClip clip)
+    public void Playsound(AudioClip clip) // 해당 소리 재생 메소드
     {
-        sound.PlayOneShot(clip);
+        sound.PlayOneShot(clip); 
     }
 }
