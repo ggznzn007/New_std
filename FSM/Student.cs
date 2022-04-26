600974cee10c956845c4ded0487447f88a7a1582
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-04-26 16:04:08 +0900

    std_Unity_FSM_20220426

--- FSM/Student.cs
+++ FSM/Student.cs
@@ -10,9 +10,9 @@ public class Student : BaseGameEntity
 	private	int			totalScore;			// 점수
 	private	Locations	currentLocation;	// 현재 위치
 
-	// Student가 가지고 있는 모든 상태, 현재 상태
-	private	State[]		states;
-	private	State		currentState;
+	// Student가 가지고 있는 모든 상태, 상태를 관리하는 참조 클래스
+	private	State<Student>[]		states;
+	private	StateMachine<Student>	stateMachine;
 
 	public int Knowledge
 	{
@@ -31,7 +31,7 @@ public class Student : BaseGameEntity
 	}
 	public int TotalScore
 	{
-		set => totalScore = Mathf.Clamp(value, 0, 100);
+		set => totalScore = Mathf.Clamp(0, value, 100);
 		get => totalScore;
 	}
 	public Locations CurrentLocation
@@ -49,15 +49,16 @@ public class Student : BaseGameEntity
 		gameObject.name = $"{ID:D2}_Student_{name}";
 
 		// Student가 가질 수 있는 상태 개수만큼 메모리 할당, 각 상태에 클래스 메모리 할당
-		states									= new State[5];
+		states									= new State<Student>[5];
 		states[(int)StudentStates.RestAndSleep]	= new StudentOwnedStates.RestAndSleep();
 		states[(int)StudentStates.StudyHard]	= new StudentOwnedStates.StudyHard();
 		states[(int)StudentStates.TakeAExam]	= new StudentOwnedStates.TakeAExam();
 		states[(int)StudentStates.PlayAGame]	= new StudentOwnedStates.PlayAGame();
 		states[(int)StudentStates.HitTheBottle]	= new StudentOwnedStates.HitTheBottle();
 
-		// 현재 상태를 집에서 쉬는 "RestAndSleep" 상태로 설정
-		ChangeState(StudentStates.RestAndSleep);
+		// 상태를 관리하는 StateMachine에 메모리를 할당하고, 첫 상태를 설정
+		stateMachine = new StateMachine<Student>();
+		stateMachine.Setup(this, states[(int)StudentStates.RestAndSleep]);
 
 		knowledge		= 0;
 		stress			= 0;
@@ -68,26 +69,12 @@ public class Student : BaseGameEntity
 
 	public override void Updated()
 	{
-		if ( currentState != null )
-		{
-			currentState.Execute(this);
-		}
+		stateMachine.Execute();
 	}
 
 	public void ChangeState(StudentStates newState)
 	{
-		// 새로 바꾸려는 상태가 비어있으면 상태를 바꾸지 않는다
-		if ( states[(int)newState] == null ) return;
-
-		// 현재 재생중인 상태가 있으면 Exit() 메소드 호출
-		if ( currentState != null )
-		{
-			currentState.Exit(this);
-		}
-
-		// 새로운 상태로 변경하고, 새로 바뀐 상태의 Enter() 메소드 호출
-		currentState = states[(int)newState];
-		currentState.Enter(this);
+		stateMachine.ChangeState(states[(int)newState]);
 	}
 }
 
