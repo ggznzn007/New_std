87b4a2cab456df6e20a5b3bf9b066a219cf748f7
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-04-26 16:03:26 +0900

    std_Unity_FSM_20220426

--- FSM/State.cs
+++ FSM/State.cs
@@ -1,18 +1,18 @@
-public abstract class State
+public abstract class State<T> where T : class
 {
 	/// <summary>
 	/// 해당 상태를 시작할 때 1회 호출
 	/// </summary>
-	public abstract void Enter(Student entity);
+	public abstract void Enter(T entity);
 
 	/// <summary>
 	/// 해당 상태를 업데이트할 때 매 프레임 호출
 	/// </summary>
-	public abstract void Execute(Student entity);
+	public abstract void Execute(T entity);
 
 	/// <summary>
 	/// 해당 상태를 종료할 때 1회 호출
 	/// </summary>
-	public abstract void Exit(Student entity);
+	public abstract void Exit(T entity);
 }
 
