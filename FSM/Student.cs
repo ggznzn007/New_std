835165333d1125c1ed34b3aec91403faefc1d591
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-04-27 14:53:53 +0900

    std_Unity_FSM_20220427

--- FSM/Student.cs
+++ FSM/Student.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
 
-public enum StudentStates { RestAndSleep = 0, StudyHard, TakeAExam, PlayAGame, HitTheBottle }
+public enum StudentStates { RestAndSleep = 0, StudyHard, TakeAExam, PlayAGame, HitTheBottle, GMessageReceive }
 
 public class Student : BaseGameEntity
 {
-	private	int			knowledge;			// 지식
-	private	int			stress;				// 스트레스
-	private	int			fatigue;			// 피로
-	private	int			totalScore;			// 점수
-	private	Locations	currentLocation;	// 현재 위치
+	private	int				knowledge;			// 지식
+	private	int				stress;				// 스트레스
+	private	int				fatigue;			// 피로
+	private	int				totalScore;			// 점수
+	private	Locations		currentLocation;	// 현재 위치
+	private	StudentStates	currentState;		// 현재 상태
 
 	// Student가 가지고 있는 모든 상태, 상태를 관리하는 참조 클래스
 	private	State<Student>[]		states;
 	private	StateMachine<Student>	stateMachine;
+
+	public	StudentStates	CurrentState => currentState;
 
 	public int Knowledge
 	{
@@ -49,16 +52,19 @@ public class Student : BaseGameEntity
 		gameObject.name = $"{ID:D2}_Student_{name}";
 
 		// Student가 가질 수 있는 상태 개수만큼 메모리 할당, 각 상태에 클래스 메모리 할당
-		states									= new State<Student>[5];
-		states[(int)StudentStates.RestAndSleep]	= new StudentOwnedStates.RestAndSleep();
-		states[(int)StudentStates.StudyHard]	= new StudentOwnedStates.StudyHard();
-		states[(int)StudentStates.TakeAExam]	= new StudentOwnedStates.TakeAExam();
-		states[(int)StudentStates.PlayAGame]	= new StudentOwnedStates.PlayAGame();
-		states[(int)StudentStates.HitTheBottle]	= new StudentOwnedStates.HitTheBottle();
+		states										= new State<Student>[6];
+		states[(int)StudentStates.RestAndSleep]		= new StudentOwnedStates.RestAndSleep();
+		states[(int)StudentStates.StudyHard]		= new StudentOwnedStates.StudyHard();
+		states[(int)StudentStates.TakeAExam]		= new StudentOwnedStates.TakeAExam();
+		states[(int)StudentStates.PlayAGame]		= new StudentOwnedStates.PlayAGame();
+		states[(int)StudentStates.HitTheBottle]		= new StudentOwnedStates.HitTheBottle();
+		states[(int)StudentStates.GMessageReceive]	= new StudentOwnedStates.GlobalMessageReceive();
 
 		// 상태를 관리하는 StateMachine에 메모리를 할당하고, 첫 상태를 설정
 		stateMachine = new StateMachine<Student>();
 		stateMachine.Setup(this, states[(int)StudentStates.RestAndSleep]);
+		// 전역 상태 설정
+		stateMachine.SetGlobalState(states[(int)StudentStates.GMessageReceive]);
 
 		knowledge		= 0;
 		stress			= 0;
@@ -74,7 +80,14 @@ public class Student : BaseGameEntity
 
 	public void ChangeState(StudentStates newState)
 	{
+		currentState = newState;
+
 		stateMachine.ChangeState(states[(int)newState]);
+	}
+
+	public override bool HandleMessage(Telegram telegram)
+	{
+		return stateMachine.HandleMessage(telegram);
 	}
 }
 
