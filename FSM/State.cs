a1115c482f76e53c838a14c834f919346f29bea2
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-04-27 14:53:20 +0900

    std_Unity_FSM_20220427

--- FSM/State.cs
+++ FSM/State.cs
@@ -1,4 +1,4 @@
-public abstract class State<T> where T : class
+public abstract class State<T> where T : BaseGameEntity //class
 {
 	/// <summary>
 	/// 해당 상태를 시작할 때 1회 호출
@@ -14,5 +14,10 @@ public abstract class State<T> where T : class
 	/// 해당 상태를 종료할 때 1회 호출
 	/// </summary>
 	public abstract void Exit(T entity);
+
+	/// <summary>
+	/// 메시지를 받았을 때 1회 호출
+	/// </summary>
+	public abstract bool OnMessage(T entity, Telegram telegram);
 }
 
