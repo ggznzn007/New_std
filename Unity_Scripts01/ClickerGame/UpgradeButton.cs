ad2f29b9c217d91bc1014438e8e399603b344e5e
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-05-10 19:20:55 +0900

    std_Unity_Clicker_20220510

---
+++ Unity_Scripts01/ClickerGame/UpgradeButton.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UpgradeButton : MonoBehaviour
+{
+    public TextMeshProUGUI upgradeDisplayer;
+
+    public string upgradeName;
+
+    [HideInInspector] // 인스펙터 상에서 값을 숨김
+    public int goldByUpgrade;
+   
+    public int startGoldByUpgrade = 1; // 게임 시작 시 기초값
+
+    [HideInInspector]
+    public int currentCost = 1;
+
+    public int startCurrentCost = 1;
+
+    [HideInInspector]
+    public int level = 1;
+
+    public float upgradePow = 1.07f;
+
+    public float costPow = 3.14f;
+
+    private void Start()
+    {
+        currentCost = startCurrentCost;
+        level = 1;
+        goldByUpgrade = startGoldByUpgrade;
+        UpdateUI();
+    }
+
+    public void PurchaseUpgrade()
+    {
+        if(DataController.GetInstance().GetGold()>=currentCost)
+        {
+            DataController.GetInstance().SubGold(currentCost);
+            level++;
+            DataController.GetInstance().AddGoldPerClick(goldByUpgrade);
+
+            UpdateUpgrade();
+            UpdateUI();
+        }
+    }
+
+    public void UpdateUpgrade()
+    {
+        goldByUpgrade = startGoldByUpgrade * (int)Mathf.Pow(upgradePow, level);
+        currentCost = startCurrentCost * (int)Mathf.Pow(costPow, level);
+    }
+
+    public void UpdateUI()
+    {
+        upgradeDisplayer.text = upgradeName + "\nCost : " + currentCost + "\nLevel : " + level +
+            "\nNext New GoldPerClick : " + goldByUpgrade;
+    }
+}
