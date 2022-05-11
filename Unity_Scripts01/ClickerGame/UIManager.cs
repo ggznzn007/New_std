35704beacb9aeb6df03877e76e38de3eb04c79bc
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-05-11 18:37:24 +0900

    std_Unity_Clicker_20220511

--- Unity_Scripts01/ClickerGame/UIManager.cs
+++ Unity_Scripts01/ClickerGame/UIManager.cs
@@ -8,12 +8,12 @@ public class UIManager : MonoBehaviour
 {
     public TextMeshProUGUI goldDisplayer;
     public TextMeshProUGUI goldPerClickDisplayer;
-    public TextMeshProUGUI goldPerSecDisplayer;   
+    public TextMeshProUGUI goldPerSecDisplayer;     
 
     private void Update()
     {
-        goldDisplayer.text = "GOLD : " + DataController.GetInstance().GetGold();
-        goldPerClickDisplayer.text = "GOLD PER CLICK : " + DataController.GetInstance().GetGoldPerClick();
-        goldPerSecDisplayer.text = "GOLD PER SEC : " + DataController.GetInstance().GetGoldPerSec();
+        goldDisplayer.text = " GOLD: " + DataController.Instance.gold;
+        goldPerClickDisplayer.text = " GOLD PER CLICK: " + DataController.Instance.goldPerClick;
+        goldPerSecDisplayer.text = " GOLD PER SEC: " + DataController.Instance.GetGoldPerSec();
     }
 }
