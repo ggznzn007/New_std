02e29a5ed1074746985345beed7fdcf2e9b19058
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-05-11 16:33:55 +0900

    std_Unity_Clicker_20220511

--- Unity_Scripts01/ClickerGame/DataController.cs
+++ Unity_Scripts01/ClickerGame/DataController.cs
@@ -22,6 +22,8 @@ public class DataController : MonoBehaviour
         return instance;
     }
 
+    private ItemButton[] itemButtons;
+
     private int m_gold = 0;
 
     private int m_goldPerClick = 0;
@@ -30,6 +32,8 @@ public class DataController : MonoBehaviour
     {
         m_gold = PlayerPrefs.GetInt("Gold");
         m_goldPerClick = PlayerPrefs.GetInt("GoldPerClick", 1);
+
+        itemButtons = FindObjectsOfType<ItemButton>();
     }
 
     public void SetGold(int newGold)
@@ -70,5 +74,72 @@ public class DataController : MonoBehaviour
     {
         m_goldPerClick += newGoldPerClick;
         SetGoldPerClick(m_goldPerClick);
+    }
+
+    public void LoadUpgradeButton(UpgradeButton upgradeButton)
+    {
+        string key = upgradeButton.upgradeName;
+
+        upgradeButton.level = PlayerPrefs.GetInt(key + "_level", 1);
+        upgradeButton.goldByUpgrade = PlayerPrefs.GetInt(key + "_goldByUpgrade",
+            upgradeButton.startGoldByUpgrade);
+        upgradeButton.currentCost = PlayerPrefs.GetInt(key + "_cost", upgradeButton.startCurrentCost);
+    }
+
+    public void SaveUpgradeButton(UpgradeButton upgradeButton)
+    {
+        string key = upgradeButton.upgradeName;
+
+        PlayerPrefs.SetInt(key + "_level", upgradeButton.level);
+        PlayerPrefs.SetInt(key + "_goldByUpgrade",upgradeButton.goldByUpgrade);
+        PlayerPrefs.SetInt(key + "_cost", upgradeButton.currentCost);
+    }
+
+    public void LoadItemButton(ItemButton itemButton)
+    {
+        string key = itemButton.itemName;
+
+        itemButton.level = PlayerPrefs.GetInt(key + "_level");
+        itemButton.currentCost = PlayerPrefs.GetInt(key + "_cost", itemButton.startCurrentCost);
+        itemButton.goldPerSec = PlayerPrefs.GetInt(key + "_goldPerSec");
+
+        if(PlayerPrefs.GetInt(key+"_isPurchased")==1)
+        {
+            itemButton.isPurchased = true;
+        }
+        else
+        {
+            itemButton.isPurchased = false;
+        }
+    }
+
+    public void SaveItemButton(ItemButton itemButton)
+    {
+        string key = itemButton.itemName;
+
+        PlayerPrefs.SetInt(key + "_level", itemButton.level);
+        PlayerPrefs.SetInt(key + "_cost", itemButton.currentCost);
+        PlayerPrefs.SetInt(key + "_goldPerSec", itemButton.goldPerSec);
+
+        if (itemButton.isPurchased)
+        {
+            PlayerPrefs.SetInt(key + "_isPurchased", 1);
+        }
+        else
+        {
+            PlayerPrefs.SetInt(key + "_isPurchased", 0);
+        }
+    }
+
+    public int GetGoldPerSec()
+    {
+        int goldPerSec = 0;
+        for (int i = 0; i < itemButtons.Length; i++)
+        {
+            goldPerSec += itemButtons[i].goldPerSec;
+
+        }
+
+        return goldPerSec;
     }
 }
