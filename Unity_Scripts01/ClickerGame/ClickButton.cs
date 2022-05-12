90f2daab981ac943bd112df3aef95de079d4a1f1
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-05-12 17:01:25 +0900

    std_Unity_Clicker_20220512

--- Unity_Scripts01/ClickerGame/ClickButton.cs
+++ Unity_Scripts01/ClickerGame/ClickButton.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class ClickButton : MonoBehaviour
 {
+    public Animator anim;
     public void OnMouseDown()
     {
        DataController.Instance.gold+= DataController.Instance.goldPerClick;
+
+        anim.SetTrigger("OnClick");  
     }
 }
