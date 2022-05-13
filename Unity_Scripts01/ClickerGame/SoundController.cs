21538cff8075bb2403bf077dd90c6827dbca6b7f
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-05-13 10:04:26 +0900

    std_Unity_Clicker_20220513

---
+++ Unity_Scripts01/ClickerGame/SoundController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    public static SoundController instance;
+
+    AudioSource sound;
+
+    [Header("AudioClip")]
+    public AudioClip upgradeClick;        
+    public AudioClip playerHit;          
+    public AudioClip heroineClick;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        sound = GetComponent<AudioSource>();
+    }
+
+    public void Playsound(AudioClip clip) // 해당 소리 재생 메소드
+    {
+        sound.PlayOneShot(clip);
+    }
+}
