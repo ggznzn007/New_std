309ac64b4d07117fc33728a3683265625127a06f
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-05-18 15:47:25 +0900

    std_Unity_Clicker_20220518

--- Unity_Scripts01/ClickerGame/BigInteger/UIManager.cs
+++ Unity_Scripts01/ClickerGame/BigInteger/UIManager.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Numerics;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,13 +9,16 @@ public class UIManager : MonoBehaviour
 {
     public TextMeshProUGUI goldDisplayer;
     public TextMeshProUGUI goldPerClickDisplayer;
-    public TextMeshProUGUI goldPerSecDisplayer;
-    
+    public TextMeshProUGUI goldPerSecDisplayer;    
+   
     private void Update()
-    {       
-        goldDisplayer.text = " 총 재산: " + DataController.Instance.GetCommaGold()+"원";
-        goldPerClickDisplayer.text = " 클릭당 추가금액: " + DataController.Instance.GetCommaClick() + "원";
-        goldPerSecDisplayer.text = " 초당 추가금액: " + DataController.Instance.GetCommaSec() + "원";
+    {
+       
+        goldDisplayer.text = " 총 재산: " + DataController.Instance.GetGoldText(DataController.Instance.Gold);
+        goldPerClickDisplayer.text = " 클릭당 추가금액: " + 
+            DataController.Instance.GetGoldText(DataController.Instance.GoldPerClick);
+        goldPerSecDisplayer.text = " 초당 추가금액: " + 
+            DataController.Instance.GetGoldText(DataController.Instance.GetGoldPerSec());
     }
 
     
