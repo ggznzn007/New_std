641067e0a0b4655cfc0eec8875007ae556e02daf
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-05-18 15:47:00 +0900

    std_Unity_Clicker_20220518

--- Unity_Scripts01/ClickerGame/BigInteger/HeroineButton.cs
+++ Unity_Scripts01/ClickerGame/BigInteger/HeroineButton.cs
@@ -15,7 +15,7 @@ public class HeroineButton : MonoBehaviour
 
     public string itemName;
 
-    public int level;
+    public BigInteger level;
 
     [HideInInspector]
     public BigInteger currentCost = 1;
@@ -75,15 +75,15 @@ public class HeroineButton : MonoBehaviour
 
     public void UpdateItem()
     {
-        goldPerSec = (BigInteger)(goldPerSec) + startGoldPerSec * (BigInteger)Mathf.Pow(upgradePow, level);
-        currentCost = (BigInteger)currentCost/2 + startCurrentCost * (BigInteger)Mathf.Pow(costPow, level);
+        goldPerSec = (BigInteger)(goldPerSec) + startGoldPerSec * (BigInteger)Mathf.Pow(upgradePow, (int)level);
+        currentCost = (BigInteger)currentCost/2 + startCurrentCost * (BigInteger)Mathf.Pow(costPow, (int)level);
         
     }
 
     public void UpdateUI()
     {
-        itemDisplayer.text = itemName + " Level " + level + "\n\n구매금액: " + GetCommaGold(currentCost) + "원"+ 
-            "\n초당 추가금액: " + GetCommaGold(goldPerSec) + "원";
+        itemDisplayer.text = itemName + " Level " + (int)level + "\n\n구매금액: "  + GetCurrentCostText2(currentCost)+
+            "\n초당 추가금액: " + DataController.Instance.GetGoldText(goldPerSec);
 
         slider.minValue = 0;
         slider.maxValue =(float)currentCost;
@@ -101,11 +101,38 @@ public class HeroineButton : MonoBehaviour
 
         
     }
-
-    public string GetCommaGold(BigInteger data)
+    public string GetCurrentCostText2(BigInteger data) // 골드 표현 형식을 소수점 까지 표시하는 메서드
     {
-        return string.Format("{0:#,###}", data);
+        int placeN = 3; // 세자리 단위로 끊어서 표현
+        BigInteger value = currentCost; // 빅인티저에 골드를 대입
+        List<int> numList = new List<int>();
+        int p = (int)Mathf.Pow(10, placeN);
+
+        do
+        {
+            numList.Add((int)(value % p));
+            value /= p;
+        }
+        while (value >= 1);
+
+        int num = numList.Count < 2 ? numList[0] : numList[numList.Count - 1] * p + numList[numList.Count - 2];
+        float f = (num / (float)p);
+        return f.ToString("N2") + GetUnitText2(numList.Count - 1);
     }
+
+    private string GetUnitText2(int index)
+    {
+        int idx = index - 1;
+        if (idx < 0) { return ""; }
+        int recallCount = (idx / 26) + 1;
+        string recallString = "";
+        for (int i = 0; i < recallCount; i++)
+        {
+            recallString += (char)(97 + idx % 26);
+        }
+        return recallString;
+    }
+   
     private void Update()
     {
         UpdateUI();
