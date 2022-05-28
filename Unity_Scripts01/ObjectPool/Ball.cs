2d30c5010250a19af214bb35b385f87aaf339bfa
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-05-28 15:34:54 +0900

    std_Unity_ObPool_20220527

---
+++ Unity_Scripts01/ObjectPool/Ball.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField] Rigidbody rbody;
+    [SerializeField] Renderer render;
+
+
+    [SerializeField] float upForce = 1f;
+    [SerializeField] float sideForce = 0.1f;
+
+    private void OnEnable()
+    {
+        float xForce = Random.Range(-sideForce, sideForce);
+        float yForce = Random.Range(upForce,upForce);
+        float zForce = Random.Range(-sideForce, sideForce);
+        Vector3 force = new Vector3(xForce, yForce, zForce);
+        rbody.velocity = force;
+
+        Invoke(nameof(DeactiveDelay), 5);
+    }
+
+    public void Setup(Color color)
+    {
+        render.material.color = color;
+    }
+
+    void DeactiveDelay() => gameObject.SetActive(false);
+
+    private void OnDisable()
+    {
+        ObjectPooler.ReturnToPool(gameObject);
+        CancelInvoke();
+    }
+
+}
