be75222b3f5ab760327c248a4948c9d52729b72f
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-06-09 22:27:12 +0900

    std_Unity_Clicker_20220609

--- Unity_Scripts01/ClickerGame/BigInteger/HeroineButton.cs
+++ Unity_Scripts01/ClickerGame/BigInteger/HeroineButton.cs
@@ -49,10 +49,10 @@ public class HeroineButton : MonoBehaviour
     }
     public void PurchaseItem()
     {
-        if(DataController.Instance.Gold >=currentCost)
+        if (DataController.Instance.Gold >= currentCost)
         {
             isPurchased = true;
-            DataController.Instance.Gold -=currentCost;
+            DataController.Instance.Gold -= currentCost;
             level++;
             UpdateItem();
             UpdateUI();
@@ -62,11 +62,11 @@ public class HeroineButton : MonoBehaviour
 
     IEnumerator AddGoldLoop()
     {
-        while(true)
+        while (true)
         {
-            if(isPurchased)
+            if (isPurchased)
             {
-                DataController.Instance.Gold+=goldPerSec;
+                DataController.Instance.Gold += goldPerSec;
             }
 
             yield return new WaitForSeconds(1.0f);
@@ -76,78 +76,79 @@ public class HeroineButton : MonoBehaviour
     public void UpdateItem()
     {
         goldPerSec = (goldPerSec) + startGoldPerSec * (BigInteger)Mathf.Pow(upgradePow, (int)level);
-        currentCost = currentCost/2 + startCurrentCost * (BigInteger)Mathf.Pow(costPow, (int)level);
-        
+        currentCost = currentCost / 2 + startCurrentCost * (BigInteger)Mathf.Pow(costPow, (int)level);
+
     }
 
     public void UpdateUI()
     {
-        itemDisplayer.text = itemName + " Level " + level + "\n\n구매금액: "  
-            + DataController.Instance.GetGoldText(currentCost)+
+        itemDisplayer.text = itemName + " Level " + level + "\n\n구매금액: "
+            + DataController.Instance.GetGoldText(currentCost) +
             "\n초당 추가금액: " + DataController.Instance.GetGoldText(goldPerSec);
 
         slider.minValue = 0;
-        slider.maxValue =(long)currentCost;
+        slider.maxValue = (long)currentCost;
 
         slider.value = (long)DataController.Instance.Gold;
 
-        if(isPurchased)
+        if (isPurchased)
         {
-           canvasGroup.alpha = 1.0f;
+            canvasGroup.alpha = 1.0f;
         }
         else
         {
-           canvasGroup.alpha = 0.6f;
-        }              
-        
-    }
-   /* public string GetCurrentCostText2(BigInteger data) // 골드 표현 형식을 소수점 까지 표시하는 메서드
-    {
-        int placeN = 4; // 세자리 단위로 끊어서 표현
-        BigInteger value = currentCost; // 빅인티저에 골드를 대입
-        List<BigInteger> numList = new List<BigInteger>();
-        BigInteger p = (int)Mathf.Pow(10, placeN);
-
-        do
-        {
-            numList.Add((int)(value % p));
-            value /= p;
+            canvasGroup.alpha = 0.6f;
         }
-        while (value >= 1);
 
-        BigInteger num = numList.Count < 2 ? numList[0] : numList[numList.Count - 1] * p + numList[numList.Count - 2];
-        float f = ((int)num / (float)p);
-        return f.ToString("N0") + GetUnitText2(numList.Count - 3);
     }
+    /* public string GetCurrentCostText2(BigInteger data) // 골드 표현 형식을 소수점 까지 표시하는 메서드
+     {
+         int placeN = 4; // 세자리 단위로 끊어서 표현
+         BigInteger value = currentCost; // 빅인티저에 골드를 대입
+         List<BigInteger> numList = new List<BigInteger>();
+         BigInteger p = (int)Mathf.Pow(10, placeN);
 
-    private string GetUnitText2(int index)
-    {
-        int idx = index - 1;
-        if (idx < 0) { return ""; }
-        int recallCount = (idx / 26) + 1;
-        string recallString = "";
-        for (int i = 0; i < recallCount; i++)
-        {
-            recallString += (char)(97 + idx % 26);
-        }
-        return recallString;
-    }*/
-   
+         do
+         {
+             numList.Add((int)(value % p));
+             value /= p;
+         }
+         while (value >= 1);
+
+         BigInteger num = numList.Count < 2 ? numList[0] : numList[numList.Count - 1] * p + numList[numList.Count - 2];
+         float f = ((int)num / (float)p);
+         return f.ToString("N0") + GetUnitText2(numList.Count - 3);
+     }
+
+     private string GetUnitText2(int index)
+     {
+         int idx = index - 1;
+         if (idx < 0) { return ""; }
+         int recallCount = (idx / 26) + 1;
+         string recallString = "";
+         for (int i = 0; i < recallCount; i++)
+         {
+             recallString += (char)(97 + idx % 26);
+         }
+         return recallString;
+     }*/
+
     public void PurchaseActiveHero()
     {
         if (DataController.Instance.Gold >= currentCost)
         {
             heroineButton.interactable = true;
-            
+
         }
         else
         {
             heroineButton.interactable = false;
-            
+
         }
     }
     private void FixedUpdate()
     {
+
         PurchaseActiveHero();
         UpdateUI();
     }
