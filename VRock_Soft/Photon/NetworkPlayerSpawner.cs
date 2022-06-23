bd09bb141056692be57cc1815e988a158b8b5d0a
kimjongjae <65937620+ggznzn007@users.noreply.github.com>
2022-06-23 15:33:54 +0900

    VR_Dev_20220622

---
+++ VRock_Soft/Photon/NetworkPlayerSpawner.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+using UnityEngine.UI;
+using PN = Photon.Pun.PhotonNetwork;
+using Random = UnityEngine.Random;
+
+public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
+{
+    private GameObject spawnPlayerPrefab;
+    /*[SerializeField] Transform[] spawnPoints;
+    public int GetIndex
+    {
+        get
+        {
+            for (int i = 0; i < PN.PlayerList.Length; i++)
+            {
+                if (PN.PlayerList[i] == PN.LocalPlayer)
+                    return i;
+            }
+            return -1;
+        }
+    }
+*/
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        spawnPlayerPrefab = PN.Instantiate("Player", transform.position, transform.rotation);
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        PN.Destroy(spawnPlayerPrefab);
+    }
+}
