a8fe31da7fdcbc183799f879e01a240c91895799
kimjongjae <65937620+ggznzn007@users.noreply.github.com>
2022-06-29 18:19:59 +0900

    VR_Dev_20220629

--- VRock_Soft/Photon/NetworkedPlayer.cs
+++ VRock_Soft/Photon/NetworkedPlayer.cs
@@ -5,54 +5,42 @@ using Photon.Pun;
 using Photon.Realtime;
 using System;
 using UnityEngine.UI;
-using PN = Photon.Pun.PhotonNetwork;
+using PN = Photon.Pun.PN;
 using Random = UnityEngine.Random;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
 
 public class NetworkedPlayer : MonoBehaviourPunCallbacks
 {
+    public GameObject LocalXRRigGameObject;
+    public GameObject AvatarHead;
+    public GameObject AvatarBody;   
+    
+    private PhotonView PV;
+    
+    private void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        if(PV.IsMine)
+        {
+            LocalXRRigGameObject.SetActive(true);
+            SetLayerRecursively(AvatarHead, 8);
+            SetLayerRecursively(AvatarHead, 9);
+        }
+        else
+        {
+            LocalXRRigGameObject.SetActive(false);
+            SetLayerRecursively(AvatarHead, 0);
+            SetLayerRecursively(AvatarHead, 0);
+        }
+    }    
 
-	public GameObject avatar;
-	public Transform playerGlobal;
-	public Transform playerLocal;
-	private PhotonView pv;
-	
-
-	void Start () 
-	{
-		pv = GetComponent<PhotonView>();
-		Debug.Log("I am Networked Player");	
-
-		if (pv.IsMine)
-		{
-			Debug.Log("photonview is mine");	
-			//playerGlobal = GameObject.Find("CameraController").transform;
-			//playerLocal = playerGlobal.Find("Dive_Camera");
-
-			this.transform.SetParent(playerLocal);
-			this.transform.localPosition = Vector3.zero;
-
-			//avatar.SetActive(false);
-			//avatar =  Resources.Load("NetworkedPlayer/avatar") as GameObject;
-		}
-	}
-	
-	void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-	{
-		if (stream.IsWriting)
-		{
-			Debug.Log ("send the others my data");
-
-			// stream.SendNext(playerGlobal.position);
-			// stream.SendNext(playerGlobal.rotation);
-			// stream.SendNext(playerLocal.localPosition);
-			// stream.SendNext(playerLocal.localRotation);
-		}
-		else
-		{
-			this.transform.position = (Vector3)stream.ReceiveNext();
-			this.transform.rotation = (Quaternion)stream.ReceiveNext();
-			avatar.transform.localPosition = (Vector3)stream.ReceiveNext();
-			avatar.transform.localRotation = (Quaternion)stream.ReceiveNext();
-		}
-	}
+    void SetLayerRecursively(GameObject go, int layerNum)
+    {
+        if (go == null) return;
+        foreach (Transform trans in go.GetComponentsInChildren<Transform>(true))
+        {
+            trans.gameObject.layer = layerNum;
+        }
+    }
 }
