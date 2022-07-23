1678b7212b271d73468542154c2c518661a984ed
kimjongjae <65937620+ggznzn007@users.noreply.github.com>
2022-07-23 15:22:12 +0900

    VR_Dev_20220722

--- VRock_Soft/GameObject/BulletPool.cs
+++ VRock_Soft/GameObject/BulletPool.cs
@@ -2,24 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum PoolName
-{
-    BulletPool
-}
+
 
 public class BulletPool : MonoBehaviour
 {
-    
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
