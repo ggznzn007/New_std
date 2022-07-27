4bcbdb2bd85d76f3cb206ebb9e3f777a257e1048
kimjongjae <65937620+ggznzn007@users.noreply.github.com>
2022-07-27 16:56:25 +0900

    VR_Dev_20220727

--- VRock_Soft/GameObject/BulletManager.cs
+++ VRock_Soft/GameObject/BulletManager.cs
@@ -4,43 +4,54 @@ using UnityEngine;
 
 
 
-public class BulletManager : MonoBehaviour
+public class BulletManager : MonoBehaviour                                        // 총알 스크립트
 {
+    [SerializeField] GameObject bullet;  // 총알 프리팹
 
-    [SerializeField] ParticleSystem exploreEffect;
-    AudioSource audioSource;
-    public float speed = 30f;
+    AudioSource audioSource;             // 총알 발사 소리
+    public float speed = 40f;            // 총알 속도
+    public int tagCount = 0;
 
-    private void Start()
+    private Rigidbody rb;
+
+
+    private void Awake()
     {
-        // audioSource = GetComponent<AudioSource>();
-        Rigidbody rb = GetComponent<Rigidbody>();
-        rb.velocity = transform.forward * speed;
+        rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+    
+    private void OnEnable()
+    {
+        audioSource.Play();                                                                     // 총알 발사 소리 재생
+        rb.velocity = GameObject.FindGameObjectWithTag("FirePoint").transform.forward * speed;
+        
 
-
+        Invoke(nameof(DeactiveDelay), 1);
+    }
+    private void OnDisable()
+    {
+        ObjectPooler.ReturnToPool(gameObject);
+        CancelInvoke();
     }
 
-    private void Update()
-    {
-        // 앞으로 계속 나감
-        //transform.position += transform.forward * speed*Time.deltaTime;
-
-    }
-
-
-    private void OnTriggrtEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         // 터지는 이펙트 보여지고
-        ParticleSystem explo = Instantiate(exploreEffect);
-        explo.transform.position = transform.position;
-        //Destroy(explo, 2);
-        // 터지는 소리 재생
-        audioSource.Play();
+        if (collision.gameObject.CompareTag("Cube"))
+        {
+            var exEffect = ObjectPooler.SpawnFromPool<ExploreEffect>("ExplorelEffect", this.transform.position);
+            DeactiveDelay();
 
-        // 충돌하면 없어지게
-
+            
+           
+            Debug.Log("태그됨");
+        }
     }
 
 
+    void DeactiveDelay() => gameObject.SetActive(false);
+
+   
 }
 
