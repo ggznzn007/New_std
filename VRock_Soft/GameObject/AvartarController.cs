c6b37abfa64e118aedfc689f679f218a822dc213
kimjongjae <65937620+ggznzn007@users.noreply.github.com>
2022-08-24 11:10:12 +0900

    VR_Dev_20220824

--- VRock_Soft/GameObject/AvartarController.cs
+++ VRock_Soft/GameObject/AvartarController.cs
@@ -17,210 +17,140 @@ using Antilatency.SDK;
 
 public class AvartarController : MonoBehaviourPunCallbacks, IPunObservable
 {
-    public static AvartarController ATC;
-    public Image HP;
-    public Text Nickname;
-    private PhotonView PV;
-    public Renderer head;
-    public Renderer body;
-    public Renderer hair;
-    public Renderer eye_L;
-    public Renderer eye_R;
-    public Renderer glove_R;
-    public Renderer hand_R;
-    public Renderer[] CurRends;
-    public Material DeadRend;
-    public Material[] RespawnRends;
-   
-    /* public GameObject ConHand_L;
-     public GameObject ConHand_R;
-     public GameObject Hand_L;
-     public GameObject Hand_R;
-     public GameObject deathHead;
-     public GameObject deathBody;
-     public GameObject deathHand_L;
-     public GameObject deathHand_R;*/
+    public static AvartarController ATC;                                     // 싱글턴 
+    public Image HP;                                                         // 플레이어 HP
+    public Text Nickname;                                                    // 플레이어 닉네임
+    private PhotonView PV;                                                   // 포톤뷰
+    public MeshRenderer head_Rend;                                           // 아바타 머리     렌더러
+    public MeshRenderer body_Rend;                                           // 아바타 몸      
+    public MeshRenderer hair_Rend;                                           // 아바타 머리색   
+    public MeshRenderer eye_L_Rend;                                          // 아바타 왼쪽눈   
+    public MeshRenderer eye_R_Rend;                                          // 아바타 오른쪽눈  
+    public SkinnedMeshRenderer glove_R_Rend;                                 // 아바타 장갑     
+    public SkinnedMeshRenderer hand_R_Rend;                                  // 아바타 오른손   
+    public Material[] head;                                                  // 아바타 머리     머티리얼
+    public Material[] body;                                                  // 아바타 몸       
+    public Material hair;                                                    // 아바타 머리색
+    public Material eye_L;                                                   // 아바타 왼쪽눈
+    public Material eye_R;                                                   // 아바타 오른쪽눈
+    public Material glove_R;                                                 // 아바타 장갑
+    public Material hand_R;                                                  // 아바타 오른손    
+    public Material DeadRend;                                                // 플레이어 죽음   머티리얼    
+    public List<Collider> playerColls;                                       // 플레이어 콜라이더
+    public bool isDead;                                                      // 플레이어 죽음 판단여부
 
-    public GameObject gunPrefab;
-    /*  public GameObject Controll_L;
-      public GameObject Controll_R;*/
-
-    public List<Collider> playerColls;
-    public bool isDead;
     private void Awake()
     {
         ATC = this;
-        PV = GetComponent<PhotonView>();
-        isDead = false;
-        
-       /* playerRends = GetComponentsInChildren<MeshRenderer>();
-        PlayerRends = GetComponentsInChildren<Renderer>();
-        DeadRends = GetComponentsInChildren<Material>();
-        CurRends = GetComponentsInChildren<Renderer>();
-
-        deathHead.SetActive(false);
-        deathBody.SetActive(false);
-        deathHand_L.SetActive(false);
-        deathHand_R.SetActive(false);
-        Hand_R.SetActive(true);
-        Hand_L.SetActive(true);
-        playerColls = new List<Collision>();
-        materials = new List<Material>();
-         boxRends = GetComponentsInChildren<MeshRenderer>();
-        Controll_R.GetComponentsInChildren<SkinnedMeshRenderer>();*/
+        PV = GetComponent<PhotonView>();             
     }
 
     void Start()
-    {
-       // DeadRend = GetComponentInChildren<Material>();
-        head = GetComponentInChildren<Renderer>();
-        body = GetComponentInChildren<Renderer>();
-        hair = GetComponentInChildren<Renderer>();
-        eye_L = GetComponentInChildren<Renderer>();
-        eye_R = GetComponentInChildren<Renderer>();
-        glove_R = GetComponentInChildren<Renderer>();
-        hand_R = GetComponentInChildren<Renderer>();
+    {        
+        Nickname.text = PV.IsMine ? PN.NickName : PV.Owner.NickName;         // 플레이어 포톤뷰가 자신이면 닉네임을 아니면 오너 닉네임
+        Nickname.color = PV.IsMine ? Color.white : Color.red;                // 플레이어 포톤뷰가 자신이면 흰색 아니면 빨간색
 
-      //  RespawnRends = GetComponentsInChildren<Material>();
-        if (PN.IsConnected)
-        {
-            Nickname.text = PV.IsMine ? PN.NickName : PV.Owner.NickName;
-            Nickname.color = PV.IsMine ? Color.white : Color.red;
-            HP.fillAmount = 1f;
-           
-        }
+        HP.fillAmount = 1f;                                                  // 플레이어 HP 초기화
+        isDead = false;                                                      // 플레이어 죽음 초기화
+
+        head_Rend.materials = head;                                      
+        body_Rend.materials = body;
+        hair_Rend.material = hair;
+        eye_L_Rend.material = eye_L;
+        eye_R_Rend.material = eye_R;
+        glove_R_Rend.material = glove_R;
+        hand_R_Rend.material = hand_R;
+
+        // DeadRend = GetComponentInChildren<Material>();
+        /* head = GetComponentInChildren<MeshRenderer>().materials;
+         body = GetComponentInChildren<MeshRenderer>().materials;
+         hair = GetComponentInChildren<MeshRenderer>().material;
+         eye_L = GetComponentInChildren<MeshRenderer>().material;
+         eye_R = GetComponentInChildren<MeshRenderer>().material;
+         glove_R = GetComponentInChildren<SkinnedMeshRenderer>().material;
+         hand_R = GetComponentInChildren<SkinnedMeshRenderer>().material;*/
     }
 
 
     void Update()
     {
-        if (!PV.IsMine) return;
-    }
-
-    public void HitPlayer()
-    {
-        HP.fillAmount -= 0.6f;
-        if (HP.fillAmount <= 0)
-        {
-            isDead = true;
-            Nickname.gameObject.SetActive(false);
-            HP.gameObject.SetActive(false);
-            //playerColl.enabled = false;            
-            PV.RPC("DestroyPlayer", RpcTarget.AllBuffered);
-            GunManager.gunManager.DestroyGun_Delay();
-            Debug.Log("킬 성공");
-        }
-    }
+        if (!PV.IsMine) return;        
+    }    
 
     private void OnTriggerEnter(Collider col)
     {
-        if (col.CompareTag("Respawn"))
+        if (col.CompareTag("Respawn")&& isDead)
         {
             PlayerRespawn();
+            PV.RPC("RespawnPlayer", RpcTarget.AllBuffered);
             Debug.Log("리스폰");
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.collider.CompareTag("Bullet"))
+        if (collision.collider.CompareTag("Bullet")&& !isDead)
         {
             HitPlayer();
             Debug.Log("총알에 맞음");
         }
 
     }
-
-
-
-    IEnumerator Damaged()
+  
+    public void HitPlayer()
     {
-        // SetPlayerInVisible(false);
-
-        /*Controll_R.modelPrefab = deathHand_R.transform;
-        Controll_L.modelPrefab = deathHand_L.transform;
-        gunPrefab.SetActive(false);
-        deathHead.SetActive(true);
-        deathBody.SetActive(true);
-        deathHand_L.SetActive(true);
-        deathHand_R.SetActive(true);*/
-
-        yield return new WaitForSeconds(5f);
-        /* Controll_R.gameObject.SetActive(true);
-         Controll_L.modelPrefab = Hand_L.transform;
-         deathHead.SetActive(false);
-         deathBody.SetActive(false);
-         deathHand_L.SetActive(false);
-         deathHand_R.SetActive(false);
-         Hand_R.SetActive(true);
-         Hand_L.SetActive(true);*/
-        //  SetPlayerVisible(true);
-        // SetPlayerInVisible(true);
-        HP.gameObject.SetActive(true);
-        Nickname.gameObject.SetActive(true);
-        HP.fillAmount = 1f;
-        gunPrefab.SetActive(true);
-
+        HP.fillAmount -= 0.5f;
+        if (HP.fillAmount <= 0)
+        {           
+            PV.RPC("DeadPlayer", RpcTarget.AllBuffered);
+           if(PV.IsMine)
+            {
+                GunManager.gunManager.DestroyGun_Delay();
+            }
+            Debug.Log("킬 성공");
+        }
     }
-
     public void PlayerDead()
     {
-        head.materials[0]= DeadRend;
-        head.materials[1]= DeadRend;
-        body.materials[0] = DeadRend;
-        body.materials[1] = DeadRend;
-        hair.material = DeadRend;
-        eye_L.material = DeadRend;
-        eye_R.material = DeadRend;
-        glove_R.material = DeadRend;
-        hand_R.material = DeadRend;
+        isDead = true;
+        Nickname.gameObject.SetActive(false);
+        HP.gameObject.SetActive(false);
+        HP.fillAmount = 0f;
+
+        head_Rend.material = DeadRend;                 
+        body_Rend.material = DeadRend;       
+        hair_Rend.material = DeadRend;
+        eye_L_Rend.material = DeadRend;
+        eye_R_Rend.material = DeadRend;
+        glove_R_Rend.material = DeadRend;
+        hand_R_Rend.material = DeadRend;
     }
 
     public void PlayerRespawn()
     {
-        head.materials[0] = RespawnRends[0];
-        head.materials[1] = RespawnRends[1];
-        body.materials[0] = RespawnRends[2];
-        body.materials[1] = RespawnRends[3];
-        hair.material = RespawnRends[4]; ;
-        eye_L.material = RespawnRends[5];
-        eye_R.material = RespawnRends[5];
-        glove_R.material = RespawnRends[6];
-        hand_R.material = RespawnRends[7];
-
+        isDead = false;        
         HP.gameObject.SetActive(true);
         Nickname.gameObject.SetActive(true);
         HP.fillAmount = 1f;
-        isDead = false;        
+
+        head_Rend.materials = head;
+        body_Rend.materials = body;
+        hair_Rend.material = hair;
+        eye_L_Rend.material = eye_L;
+        eye_R_Rend.material = eye_R;
+        glove_R_Rend.material = glove_R;
+        hand_R_Rend.material = hand_R;
     }
-
-
-
-    /*void SetPlayerInVisible(bool isVisible)
-    {
-        for (int i = 0; i < playerRends.Length; i++)
-        {
-           
-           // playerColl.enabled = isVisible;
-        }
-
-    }*/
-
-
-
+    
 
     [PunRPC]
-    public void DestroyPlayer()
-    {
-        // Destroy(gameObject);
-        // StartCoroutine(Damaged());
+    public void DeadPlayer()
+    {       
         PlayerDead();
     }
 
     [PunRPC]
     public void RespawnPlayer()
-    {
-        // Destroy(gameObject);
-        // StartCoroutine(Damaged());
+    {        
         PlayerRespawn();
     }
 
@@ -239,4 +169,34 @@ public class AvartarController : MonoBehaviourPunCallbacks, IPunObservable
 
         }
     }
+
+    /*IEnumerator Damaged()
+    {
+        // SetPlayerInVisible(false);
+
+        Controll_R.modelPrefab = deathHand_R.transform;
+        Controll_L.modelPrefab = deathHand_L.transform;
+        gunPrefab.SetActive(false);
+        deathHead.SetActive(true);
+        deathBody.SetActive(true);
+        deathHand_L.SetActive(true);
+        deathHand_R.SetActive(true);
+
+        yield return new WaitForSeconds(5f);
+        Controll_R.gameObject.SetActive(true);
+        Controll_L.modelPrefab = Hand_L.transform;
+        deathHead.SetActive(false);
+        deathBody.SetActive(false);
+        deathHand_L.SetActive(false);
+        deathHand_R.SetActive(false);
+        Hand_R.SetActive(true);
+        Hand_L.SetActive(true);
+        //  SetPlayerVisible(true);
+        // SetPlayerInVisible(true);
+        HP.gameObject.SetActive(true);
+        Nickname.gameObject.SetActive(true);
+        HP.fillAmount = 1f;
+        gunPrefab.SetActive(true);
+
+    }*/
 }
