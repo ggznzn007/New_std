62acc0e3e6daccae3a2526b19e0440993434d52b
kimjongjae <65937620+ggznzn007@users.noreply.github.com>
2022-08-26 17:44:38 +0900

    VR_Dev_20220826

---
+++ VRock_Soft/Audio_Effect/AudioManager.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager AM; 
+
+    [Header("배경음 재생기")]
+    public AudioSource bgmPlayer;
+
+    [Header("배경음 오디오소스")]
+    public AudioClip[] bgm;
+
+    [Header("효과음 재생기")]
+    public AudioSource[] effectPlayer;
+
+    [Header("효과음 오디오소스")]
+    public AudioClip[] effectClip;
+
+    public enum Effect { PlayerDamaged, PlayerDead, ReSpawn,PlayerKill};
+
+    int effectCursor;
+
+    private void Awake()
+    {
+        AM = this;        
+    }
+
+    public void EffectPlay(Effect type)
+    {
+        switch(type)
+        {
+            case Effect.PlayerDamaged:
+                effectPlayer[effectCursor].clip = effectClip[0];
+                break;
+            case Effect.PlayerDead:
+                effectPlayer[effectCursor].clip = effectClip[1];
+                break;
+            case Effect.ReSpawn:
+                effectPlayer[effectCursor].clip = effectClip[2];
+                break;
+            case Effect.PlayerKill:
+                effectPlayer[effectCursor].clip = effectClip[3];
+                break;
+        }
+
+        effectPlayer[effectCursor].Play();
+        effectCursor = (effectCursor + 1) % effectPlayer.Length;
+    }
+
+    private void Update()
+    {
+        if(!bgmPlayer.isPlaying)
+        {
+            RandomPlay();
+        }
+    }
+
+    public void RandomPlay()
+    {
+        bgmPlayer.clip = bgm[Random.Range(0, bgm.Length)];
+        bgmPlayer.Play();
+    }
+}
