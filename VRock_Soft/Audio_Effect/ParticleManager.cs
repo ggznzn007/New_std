04c410120da7cfe4b3d59db1cc60dd2391523b16
kimjongjae <65937620+ggznzn007@users.noreply.github.com>
2022-08-27 16:22:32 +0900

    VR_Dev_20220826

---
+++ VRock_Soft/Audio_Effect/ParticleManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleManager : MonoBehaviour
+{
+    ParticleSystem particle;
+    ParticleSystem.Particle[] gameEffects;
+    
+    void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+        gameEffects = new ParticleSystem.Particle[particle.main.maxParticles];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
