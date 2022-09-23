1d21b0095c3be714edc4fae06a4de785dd207fac
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2022-09-23 11:26:29 +0900

    VR_Dev_20220923

---
+++ VRock_Soft/Player/DataManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+using UnityEngine.UI;
+using PN = Photon.Pun.PN;
+using Random = UnityEngine.Random;
+
+public enum Team
+{
+    BLUE,RED
+}
+
+public class DataManager : MonoBehaviourPun
+{
+    public static DataManager DM;
+
+    private void Awake()
+    {
+        if(DM == null)DM=this;
+        else if (DM !=null) return;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public Team currentTeam;
+
+    public bool isSelected;
+
+    public int startingNum;
+}
