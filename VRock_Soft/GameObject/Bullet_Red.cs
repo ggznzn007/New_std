f35f8cf3e39cbe286bb3baaa60e3b9f1d88a46f6
Pooleun Jeong <ggznzn007@naver.com>
2022-10-05 17:11:08 +0900

    VR_Dev_20221005

--- VRock_Soft/GameObject/Bullet_Red.cs
+++ VRock_Soft/GameObject/Bullet_Red.cs
@@ -12,25 +12,21 @@ using UnityEngine.SceneManagement;
 using UnityEngine.XR;
 public class Bullet_Red : MonoBehaviourPunCallbacks
 {
-    public PhotonView PV;
-    public float speed;
-    public Rigidbody rb;
-    public ParticleSystem exploreEffect;
-    public Transform firePoint;
-    public int actNumber = -1;
-    // Start is called before the first frame update
-    void Start()
+    public static Bullet_Red BR;
+    [SerializeField] PhotonView PV;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] ParticleSystem exploreEffect;
+    [SerializeField] Transform firePoint;
+    [SerializeField] int actNumber;
+    [SerializeField] float speed;    
+    private void Start()
     {
+        BR = this;
         PV = GetComponent<PhotonView>();
         rb = GetComponent<Rigidbody>();
-        Destroy(gameObject, 1.2f);
+        Destroy(gameObject, 1f);      
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 
     private void OnCollisionEnter(Collision collsion)
     {
@@ -38,9 +34,8 @@ public class Bullet_Red : MonoBehaviourPunCallbacks
         {
             return;
         }
-
         // 터지는 이펙트 보여지고
-        if (collsion.collider.CompareTag("Cube") && PV.IsMine)
+        if (collsion.collider.CompareTag("Cube") && PV.IsMine) // 일반태그
         {
             // 충돌지점의 정보를 추출
             ContactPoint contact = collsion.contacts[0];
@@ -48,28 +43,17 @@ public class Bullet_Red : MonoBehaviourPunCallbacks
             // 법선 벡타가 이루는 회전각도 추출
             Quaternion rot = Quaternion.FromToRotation(-Vector3.forward, contact.normal);
 
-
             // 충돌 지점에 이펙트 생성           
             var effect = Instantiate(exploreEffect, contact.point, rot);
 
             Destroy(effect, 0.5f);
-
+            Destroy(gameObject);
             transform.position = contact.point;
-
-            PV.RPC("DestroyBullet", RpcTarget.AllBuffered);
-            //Enqueue(); // 큐 방식 총알 풀링 => 사용한 총알을 다시 큐에 넣기
-            //OP.PoolDestroy(this.gameObject);
-            // BulletPool.BulletPooling.ReturnBullet(); // 기존 풀링
-            // ShowEffect(collision); // 이펙트 메서드 
-            //StartCoroutine(ExploreEffect(collision));  // 이펙트 코루틴         
-            //StartCoroutine(DeactiveBullet()); // 총알 비활성화 코루틴
-            //Destroy(collision.gameObject); // 총알 맞은 오브젝트가 사라짐 
-
-            // Debug.Log("목표물에 명중");
-
+            AudioManager.AM.EffectPlay(AudioManager.Effect.BulletImpact);
+            PV.RPC("DestroyBulletR", RpcTarget.All);
         }
 
-        if (collsion.collider.CompareTag("BlueTeam"))
+        if (collsion.collider.CompareTag("BlueTeam")|| collsion.collider.CompareTag("RedTeam"))
         {
             // 충돌지점의 정보를 추출
             ContactPoint contact = collsion.contacts[0];
@@ -77,52 +61,26 @@ public class Bullet_Red : MonoBehaviourPunCallbacks
             // 법선 벡타가 이루는 회전각도 추출
             Quaternion rot = Quaternion.FromToRotation(-Vector3.forward, contact.normal);
 
-
             // 충돌 지점에 이펙트 생성           
             var effect = Instantiate(exploreEffect, contact.point, rot);
 
             Destroy(effect, 0.5f);
-            PV.RPC("DestroyBullet", RpcTarget.AllBuffered);
+            Destroy(gameObject);
+            PV.RPC("DestroyBulletR", RpcTarget.All);
             //Debug.Log("플레이어 명중");
         }
-
-
-        if (collsion.collider.CompareTag("Next"))
-        {
-            // 충돌지점의 정보를 추출
-            ContactPoint contact = collsion.contacts[0];
-
-            // 법선 벡타가 이루는 회전각도 추출
-            Quaternion rot = Quaternion.FromToRotation(-Vector3.forward, contact.normal);
-
-
-            // 충돌 지점에 이펙트 생성           
-            var effect = Instantiate(exploreEffect, contact.point, rot);
-
-            Destroy(effect, 0.5f);
-            PV.RPC("DestroyBullet", RpcTarget.AllBuffered);
-
-            PN.Disconnect();
-            //Debug.Log("플레이어 명중");
-        }
-
-
-
-
     }
 
-
     [PunRPC]
-    public void BulletDir(float speed)//,int addSpeed)
+    public void BulletDirR(float speed, int actorNumber)//,int addSpeed)
     {
         this.speed = speed;
-        // this.addSpeed = addSpeed;
+        actNumber = actorNumber;       
     }
 
     [PunRPC]
-    public void DestroyBullet()
+    public void DestroyBulletR()
     {
-        Destroy(gameObject);
-        Debug.Log("총알 파괴");
+        Destroy(gameObject);       
     }
 }
