0fc55c064bdb1b051ca389057b8008ef34e4b8ab
Pooleun Jeong <ggznzn007@naver.com>
2022-10-12 15:00:42 +0900

    VR_Dev_20221012

--- VRock_Soft/Audio_Effect/AudioManager.cs
+++ VRock_Soft/Audio_Effect/AudioManager.cs
@@ -4,33 +4,49 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    public static AudioManager AM; 
+    public static AudioManager AM;
 
-    [Header("배경음 재생기")]
+    [Header("배경음 스피커")]
     public AudioSource bgmPlayer;
-        
+
     [Header("배경음 오디오소스")]
     public AudioClip[] bgm;
-    
 
-    [Header("효과음 재생기")]
+
+    [Header("효과음 스피커")]
     public AudioSource[] effectPlayer;
 
     [Header("효과음 오디오소스")]
     public AudioClip[] effectClip;
 
-    public enum Effect { PlayerDamaged, PlayerDead, ReSpawn,PlayerKill};
+    public enum Effect
+    {
+        PlayerDamaged,
+        PlayerDead,
+        ReSpawn,
+        PlayerKill,
+        BulletImpact,
+        GAMESTART,
+        START,
+        Three,
+        Two,
+        One,
+        GAMEOVER,
+        END,
+        PlayerHit,
+        HeadShot
+    };
 
     int effectCursor;
 
     private void Awake()
     {
-        AM = this;        
+        AM = this;
     }
 
     public void EffectPlay(Effect type)
     {
-        switch(type)
+        switch (type)
         {
             case Effect.PlayerDamaged:
                 effectPlayer[effectCursor].clip = effectClip[0];
@@ -44,6 +60,36 @@ public class AudioManager : MonoBehaviour
             case Effect.PlayerKill:
                 effectPlayer[effectCursor].clip = effectClip[3];
                 break;
+            case Effect.BulletImpact:
+                effectPlayer[effectCursor].clip = effectClip[4];
+                break;
+            case Effect.GAMESTART:
+                effectPlayer[effectCursor].clip = effectClip[5];
+                break;
+            case Effect.START:
+                effectPlayer[effectCursor].clip = effectClip[6];
+                break;
+            case Effect.Three:
+                effectPlayer[effectCursor].clip = effectClip[7];
+                break;
+            case Effect.Two:
+                effectPlayer[effectCursor].clip = effectClip[8];
+                break;
+            case Effect.One:
+                effectPlayer[effectCursor].clip = effectClip[9];
+                break;
+            case Effect.GAMEOVER:
+                effectPlayer[effectCursor].clip = effectClip[10];
+                break;
+            case Effect.END:
+                effectPlayer[effectCursor].clip = effectClip[11];
+                break;
+            case Effect.PlayerHit:
+                effectPlayer[effectCursor].clip = effectClip[12];
+                break;
+            case Effect.HeadShot:
+                effectPlayer[effectCursor].clip = effectClip[13];
+                break;
         }
 
         effectPlayer[effectCursor].Play();
@@ -52,15 +98,15 @@ public class AudioManager : MonoBehaviour
 
     private void Update()
     {
-        if(!bgmPlayer.isPlaying)
+        if (!bgmPlayer.isPlaying)
         {
             RandomPlay();
-        } 
-       
-       /* if(gunBgmPlayer.isPlaying)
-        {
-            RandomPlayStop();
-        }*/
+        }
+
+        /* if(gunBgmPlayer.isPlaying)
+         {
+             RandomPlayStop();
+         }*/
     }
 
     public void RandomPlay()
@@ -68,15 +114,15 @@ public class AudioManager : MonoBehaviour
         bgmPlayer.clip = bgm[Random.Range(0, bgm.Length)];
         bgmPlayer.Play();
     }
-   /* public void RandomPlayStop()
-    {
-       // bgmPlayer.clip = bgm[Random.Range(0, bgm.Length)];
-        bgmPlayer.Stop();
-    }
+    /* public void RandomPlayStop()
+     {
+        // bgmPlayer.clip = bgm[Random.Range(0, bgm.Length)];
+         bgmPlayer.Stop();
+     }
 
-    public void NormalPlay()
-    {
-        gunBgmPlayer.clip = gunBgm;
-        gunBgmPlayer.Play();
-    }*/
+     public void NormalPlay()
+     {
+         gunBgmPlayer.clip = gunBgm;
+         gunBgmPlayer.Play();
+     }*/
 }
