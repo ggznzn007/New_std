30e896ca74df6a7e0e859d540939d9e992a9b3ee
Pooleun Jeong <ggznzn007@naver.com>
2022-11-30 17:09:46 +0900

    VR_Dev_20221130

---
+++ VRock_Archery/Object/Bow.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Bow : XRGrabInteractable
+{
+    public Transform notch;
+    private PullMeasurer pullMeasurer;
+    private LineRenderer lineRenderer;
+
+    [Header("Sound")]
+    public AudioClip grabClip;
+
+    [Header("Quiver")]
+    public Transform quiver;
+    public Vector2 quiverOffset;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        pullMeasurer = GetComponentInChildren<PullMeasurer>();
+        lineRenderer = GetComponentInChildren<LineRenderer>();
+    }
+
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {
+        base.ProcessInteractable(updatePhase);
+        if (updatePhase == XRInteractionUpdateOrder.UpdatePhase.Dynamic)
+            if (isSelected)
+                UpdateBow(pullMeasurer.PullAmount);
+    }
+    private void UpdateBow(float value)
+    {
+        Vector3 linePosition = Vector3.forward * Mathf.Lerp(-0.25f, -0.5f, value);
+        notch.localPosition = linePosition;
+        lineRenderer.SetPosition(1, linePosition);
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs interactor)
+    {
+        base.OnSelectEntered(interactor);
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs interactor)
+    {
+        base.OnSelectExited(interactor);
+        Destroy(gameObject,1f);
+    }
+
+    //public void BowHaptic(XRBaseInteractor interactor)
+    //{
+    //    if (interactor.TryGetComponent(out XRController controller))
+    //        HapticManager.Impulse(1, .05f, controller.inputDevice);
+    //}
+
+    public void OffsetQuiver(XRBaseInteractor interactor)
+    {
+        if (interactor.TryGetComponent(out XRController controller))
+        {
+            bool right = (controller.inputDevice.role == UnityEngine.XR.InputDeviceRole.RightHanded);
+            quiver.localPosition = new Vector3(quiverOffset.x * (right ? -1 : 1), quiverOffset.y, quiver.localPosition.z);
+        }
+    }
+}
---
+++ VRock_Archery/Object/PullMeasurer.cs
@@ -0,0 +1,83 @@
+﻿using Photon.Pun;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class PullMeasurer : XRBaseInteractable
+{
+    [SerializeField] private Transform start;
+    [SerializeField] private Transform end;
+    [SerializeField] private GameObject arrowPrefab;
+    [SerializeField] private XRSocketInteractor notch;
+    private GameObject myArrow;
+
+    public float PullAmount { get; private set; } = 0.0f;
+
+    public Vector3 PullPosition => Vector3.Lerp(start.position, end.position, PullAmount);
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        base.OnSelectExited(args);
+        PullAmount = 0;
+    }
+
+    public override void ProcessInteractable(XRInteractionUpdateOrder.UpdatePhase updatePhase)
+    {
+        base.ProcessInteractable(updatePhase);
+        if (isSelected)
+        {
+            // Update pull values while the measurer is grabbed
+            if (updatePhase == XRInteractionUpdateOrder.UpdatePhase.Fixed)
+                UpdatePull();
+        }
+    }
+
+    private void UpdatePull()
+    {
+        // Use the interactor's position to calculate amount
+        Vector3 interactorPosition = firstInteractorSelecting.transform.position;
+
+        // Figure out the new pull value, and it's position in space
+        PullAmount = CalculatePull(interactorPosition);
+    }
+
+    private float CalculatePull(Vector3 pullPosition)
+    {
+        // Direction, and length
+        Vector3 pullDirection = pullPosition - start.position;
+        Vector3 targetDirection = end.position - start.position;
+
+        // Figure out out the pull direction
+        float maxLength = targetDirection.magnitude;
+        targetDirection.Normalize();
+
+        // What's the actual distance?
+        float pullValue = Vector3.Dot(pullDirection, targetDirection) / maxLength;
+        return Mathf.Clamp(pullValue, 0.0f, 1.0f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        // Draw line from start to end point
+        if (start && end)
+            Gizmos.DrawLine(start.position, end.position);
+    }
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+        CreateAndSelectArrow(args);
+    }
+    private void CreateAndSelectArrow(SelectEnterEventArgs args)
+    {
+        // Create arrow, force into interacting hand
+        Arrow arrow = CreateArrow(args.interactorObject.transform);
+        interactionManager.SelectEnter(args.interactorObject, arrow);
+        notch.socketActive = true;
+    }
+
+    private Arrow CreateArrow(Transform orientation)
+    {
+        // Create arrow, and get arrow component
+        myArrow = PN.Instantiate(arrowPrefab.name, orientation.position, orientation.rotation);
+        return myArrow.GetComponent<Arrow>();
+    }
+}
