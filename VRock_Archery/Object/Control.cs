9f7a41682365c8af79686727736d48f3feefee21
Pooleun Jeong <ggznzn007@naver.com>
2023-01-05 15:15:39 +0900

    VR_Dev_20230105

---
+++ VRock_Archery/Object/Control.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+using UnityEngine.UI;
+using PN = Photon.Pun.PN;
+using Random = UnityEngine.Random;
+public class Control : MonoBehaviourPunCallbacks
+{
+   public XRRayInteractor interactor;
+    public PhotonView PV;
+    
+    void Start()
+    {   
+        PV = GetComponent<PhotonView>();
+        interactor = GetComponent<XRRayInteractor>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        if(!PV.IsMine)
+        {
+            if (NetworkedGrabbing.NG.isBeingHeld)
+            {
+                interactor.enabled = false;
+            }
+            else
+            {
+                interactor.enabled = true;
+            }
+        }
+        
+        
+    }
+}
---
+++ VRock_Archery/Object/EMP_Effect.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+using UnityEngine.UI;
+using PN = Photon.Pun.PN;
+using Random = UnityEngine.Random;
+
+public class EMP_Effect : MonoBehaviour
+{
+    public CapsuleCollider coll;
+    
+    private void Start()
+    {
+        StartCoroutine(CollOnOff());
+       
+    }
+    private void Awake()
+    {
+        coll = GetComponent<CapsuleCollider>();        
+    }  
+
+    public IEnumerator CollOnOff()
+    {
+        coll.enabled = true;
+        yield return new WaitForSeconds(0.004f);
+        coll.enabled = false;        
+    }
+
+    /*[System.Obsolete]
+    public IEnumerator DestroyEX()
+    {
+        yield return new WaitForSeconds(GetComponent<ParticleSystem>().duration);
+        Destroy(gameObject);
+    }*/
+}
