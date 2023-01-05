b909a05a290948957ce2b016703a9338f1f3f0fb
Pooleun Jeong <ggznzn007@naver.com>
2023-01-05 15:14:04 +0900

    VR_Dev_20230105

---
+++ VRock_Archery/Archery/FollowTransform.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour
+{
+    public Transform FollowTarget;
+    public bool MatchRotation = true;
+
+    void Update()
+    {
+        if (FollowTarget)
+        {
+            transform.position = FollowTarget.position;
+
+            if (MatchRotation)
+            {
+                transform.rotation = FollowTarget.rotation;
+            }
+        }
+    }
+}
