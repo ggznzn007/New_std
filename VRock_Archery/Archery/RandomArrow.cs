4e28bee484e3e1e6dd3901c9986ff0c019ccf176
Pooleun Jeong <ggznzn007@naver.com>
2023-01-17 11:09:21 +0900

    VR_Dev_20230117

---
+++ VRock_Archery/Archery/RandomArrow.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class RandomArrow
+{
+    public static bool RandArrow1(float Chance)
+    {
+        if (Chance < 0.0000001f)
+        {
+            Chance = 0.0000001f;
+        }
+
+        bool Success = false;
+        int RandAccuracy = 10000000;
+        float RandHitRange = Chance * RandAccuracy;
+        int Rand = UnityEngine.Random.Range(1, RandAccuracy + 1);
+        if (Rand <= RandHitRange)
+        {
+            Success = true;
+        }
+        return Success;
+    }
+
+    public static bool RandArrowPer(float Percentage_Chance)
+    {
+        if (Percentage_Chance < 0.0000001f)
+        {
+            Percentage_Chance = 0.0000001f;
+        }
+
+        Percentage_Chance = Percentage_Chance / 100;
+
+        bool Success = false;
+        int RandAccuracy = 10000000;
+        float RandHitRange = Percentage_Chance * RandAccuracy;
+        int Rand = UnityEngine.Random.Range(1, RandAccuracy + 1);
+        if (Rand <= RandHitRange)
+        {
+            Success = true;
+        }
+        return Success;
+    }
+}
