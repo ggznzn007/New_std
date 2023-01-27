d3cfc350fc6c4fe8ea5d1185ed16f58fb1d86966
Pooleun Jeong <ggznzn007@naver.com>
2023-01-27 14:56:59 +0900

    VR_Dev_20230127

--- VRock_Soft/GameObject/RevolverManager.cs
+++ VRock_Soft/GameObject/RevolverManager.cs
@@ -19,12 +19,14 @@ public class RevolverManager : MonoBehaviourPun, IPunObservable
     [Header("총알 속도")][SerializeField] float speed;
     [Header("총알 소유권")][SerializeField] bool isBulletMine;
     [Header("액터넘버")][SerializeField] int actorNumber;
+    public PhotonView PV;                           // 포톤뷰
+    public bool isBeingHeld = false;
+    public bool isGrip;
 
     private RaycastHit hit;                          // 레이캐스트광선 히트
     private Ray ray;                                 // 레이캐스트 광선
     private ParticleSystem muzzleFlash;              // 총구 이펙트                                           
     private AudioSource audioSource;                 // 총알 발사 소리
-    private PhotonView PV;                           // 포톤뷰
     private GameObject myBull;                       // 자기 총알    
     private float fireTime = 0;                      // 총알 딜레이 타임 
     private readonly float delayfireTime = 0.3f;    // 총알 딜레이 제한시간
@@ -33,18 +35,21 @@ public class RevolverManager : MonoBehaviourPun, IPunObservable
     private bool triggerBtnL;
     private Vector3 remotePos;
     private Quaternion remoteRot;
+    private Rigidbody rb;
 
     private void Awake()
     {
-        RM = this;
+        RM = this;        
     }
     
     void Start()
     {
         PV = GetComponent<PhotonView>();
         audioSource = GetComponent<AudioSource>();
+        rb = GetComponent<Rigidbody>();
         muzzleFlash = firePoint.GetComponentInChildren<ParticleSystem>();  // 하위 컴포넌트 추출 
-        actorNumber = PV.OwnerActorNr;     
+        actorNumber = PV.OwnerActorNr;
+        isGrip = true;
     }
 
     private void FixedUpdate()
@@ -53,54 +58,60 @@ public class RevolverManager : MonoBehaviourPun, IPunObservable
         {
             transform.SetPositionAndRotation(Vector3.Lerp(transform.position, remotePos, 30 * Time.deltaTime)
                 , Quaternion.Lerp(transform.rotation, remoteRot, 30 * Time.deltaTime));
-        }
-       
+        }       
         GetTarget();       // 표적에 레이캐스트를 쏴서 타겟팅하는 메서드
         Reload();          // 총을 재장전하는 시간 메서드       
         //WhenDead();       // 플레이어가 죽었을때 총이 사라지는 메서드
-        ActivateHaptic();        
-    }
-       
-   /* private void OnCollisionStay(Collision collision)
-    {        
-        if (collision.collider.CompareTag("Cube"))
-        {
-            try
-            {
-                if (!SpawnWeapon_RW.RW.weaponInIt && !SpawnWeapon_LW.LW.weaponInIt)
-                {
-                    PV.RPC(nameof(DestroyRevol), RpcTarget.All);
-                    Debug.Log("양손 놓고 총이 정상적으로 파괴됨");
-                }
-                if (!SpawnWeapon_RW.RW.weaponInIt || SpawnWeapon_LW.LW.weaponInIt)
-                {
-                    PV.RPC(nameof(DestroyRevol), RpcTarget.All);
-                    Debug.Log("왼쪽총이 정상적으로 파괴됨");
-                }
-                if (SpawnWeapon_RW.RW.weaponInIt || !SpawnWeapon_LW.LW.weaponInIt)
-                {
-                    PV.RPC(nameof(DestroyRevol), RpcTarget.All);
-                    Debug.Log("오른쪽총이 정상적으로 파괴됨");
-                }
+        ActivateHaptic();
 
+        if (isBeingHeld)               // 총의 입장에서 손에 잡혀있음
+        {
+            isGrip = true;
+            rb.isKinematic = true;
+        }
+        else
+        {
+            isGrip = false;
+            rb.isKinematic = false;
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.collider.CompareTag("Cube") || collision.collider.CompareTag("FloorBox") || collision.collider.CompareTag("Shield"))
+        {
+            if (PV.IsMine)
+            {
+                if (!isGrip)
+                {
+                    try
+                    {
+                        PV.RPC(nameof(DestroyGun), RpcTarget.AllBuffered);
+                    }
+                    finally
+                    {
+                        PV.RPC(nameof(DestroyGun), RpcTarget.AllBuffered);
+                    }                    
+                }
+            }
+            /*try
+            {
+                if (PV.IsMine)
+                {
+                    if (!isBeingHeld && !isGrip)
+                    {
+                        PV.RPC(nameof(DestroyGun), RpcTarget.AllBuffered);
+                    }
+                }
             }
             finally
             {
-                PV.RPC(nameof(DestroyRevol), RpcTarget.All);
-            }
-        }
-    }*/
-
-    public void GetTarget()
-    {
-        ray = new Ray(firePoint.position, firePoint.forward);
-        ray.origin = firePoint.position;
-        ray.direction = firePoint.forward;
-    }
-
-    private void OnDrawGizmos()
-    {
-        Debug.DrawRay(ray.origin, ray.direction * fireDistance, Color.red);
+                if (PV.IsMine)
+                {
+                    PV.RPC(nameof(DestroyGun), RpcTarget.AllBuffered);
+                }
+            }*/
+        }        
     }
 
     public void FireBullet()                                              // 컨트롤러 트리거를 이용한 총알 발사로직
@@ -116,7 +127,7 @@ public class RevolverManager : MonoBehaviourPun, IPunObservable
             fireTime = 0;
         }
     }
-
+    
     void ActivateHaptic()
     {    // 앞에 수는 진동의 진폭
          // 뒤에 수는 진동의 강도
@@ -145,10 +156,52 @@ public class RevolverManager : MonoBehaviourPun, IPunObservable
             remoteRot = (Quaternion)stream.ReceiveNext();
         }
     }
-
     void Reload()                                   // 총알 재장전 시간
     {
         fireTime += Time.deltaTime;
+    }
+
+    public void GetTarget()
+    {
+        ray = new Ray(firePoint.position, firePoint.forward);
+        ray.origin = firePoint.position;
+        ray.direction = firePoint.forward;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(ray.origin, ray.direction * fireDistance, Color.red);
+    } 
+
+    [PunRPC]
+    public void DestroyGun()
+    {
+        Destroy(gameObject);
+        //Destroy(PV.gameObject);
+    }
+
+    [PunRPC]
+    public void StartGrabbing()
+    {
+        isBeingHeld = true;
+    }
+
+    [PunRPC]
+    public void StopGrabbing()
+    {
+        isBeingHeld = false;
+    }
+
+    public void OnSelectedEntered()
+    {
+        Debug.Log("잡았다");
+        PV.RPC(nameof(StartGrabbing), RpcTarget.AllBuffered);
+    }
+
+    public void OnSelectedExited()
+    {
+        Debug.Log("놓았다");
+        PV.RPC(nameof(StopGrabbing), RpcTarget.AllBuffered);
     }
 
     [PunRPC]
@@ -158,9 +211,5 @@ public class RevolverManager : MonoBehaviourPun, IPunObservable
         muzzleFlash.Play();
     }
    
-  /*  [PunRPC]
-    public void DestroyRevol()
-    {
-        Destroy(gameObject);
-    }*/
+    
 }
