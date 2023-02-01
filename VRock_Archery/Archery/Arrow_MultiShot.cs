45b1b5fb88fbef6b88e90e81b3bd3d3316783552
Pooleun Jeong <ggznzn007@naver.com>
2023-02-01 15:39:32 +0900

    VR_Dev_20230201

--- VRock_Archery/Archery/Arrow_MultiShot.cs
+++ VRock_Archery/Archery/Arrow_MultiShot.cs
@@ -12,51 +12,65 @@ using Photon.Pun.Demo.Procedural;
 using UnityEngine.InputSystem.HID;
 public class Arrow_MultiShot : Arrow
 {
-    public static Arrow_MultiShot ArrowMul;
-    public GameObject effects;
+    // public static Arrow_MultiShot ArrowMul;
+    public SphereCollider tagColl;
+    public ParticleSystem typing;
+    public ParticleSystem[] effects;
     public GameObject[] arrowMesh;
     public TrailRenderer[] tails;
+    private bool isRotate;
 
-    private void Start()
+    protected override void Awake()
     {
-        ArrowMul = this;
+        base.Awake();
+        isRotate = true;
+        typing.gameObject.SetActive(true);
+        tagColl.tag = "Skilled";
     }
+    /* private void Start()
+     {
+         //ArrowMul = this;
+
+         isRotate = true;
+
+     }*/
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
         base.OnSelectEntered(args);
         arrowM.OnSelectedEntered();
+        //PV.RequestOwnership();
         DataManager.DM.grabArrow = true;
-        PV.RequestOwnership();        
         DataManager.DM.arrowNum = 2;
-        rotSpeed = 0;
+        isRotate = false;
+        //rotSpeed = 0;
+
+        if (PV.IsMine)
+        {
+            //if (!PV.IsMine) return;
+            if (DataManager.DM.grabArrow)
+            {
+                PV.RPC(nameof(HideType), RpcTarget.AllBuffered);
+                PV.RPC(nameof(Multipack), RpcTarget.AllBuffered);
+            }
+        }
     }
     protected override void OnSelectExited(SelectExitEventArgs args)
     {
         base.OnSelectExited(args);
-        if (PV.IsMine) { if (!PV.IsMine) return; PV.RPC(nameof(Multipack), RpcTarget.AllBuffered); }       
-        
+        tagColl.tag = "Arrow";
         if (args.interactorObject is Notch notch)
         {
-            //GetTarget();    
+            //GetTarget();
+            tagColl.tag = "Arrow";
             if (notch.CanRelease)
             {
-                LaunchArrow(notch);                
+                LaunchArrow(notch);
                 arrowM.OnSelectedExited();
-                
-                if(PV.IsMine)
+                if (PV.IsMine)
                 {
-                    if (!PV.IsMine) return;
+                    // if (!PV.IsMine) return;
                     PV.RPC(nameof(Tailpack), RpcTarget.AllBuffered);
-                }
-                
-                if (effects != null)
-                {
-                    if (!PV.IsMine) return;                    
-                    PV.RPC(nameof(DelayEX), RpcTarget.AllBuffered);                    
-                }
-                else
-                {
-                    effects = null;
+                    PV.RPC(nameof(DelayEX), RpcTarget.AllBuffered);
                 }
 
             }
@@ -66,13 +80,20 @@ public class Arrow_MultiShot : Arrow
 
     private void Update()
     {
-        transform.Rotate(rotSpeed * Time.deltaTime * new Vector3(0, 0, 1));
+        if (isRotate)
+        {
+            transform.Rotate(rotSpeed * Time.deltaTime * new Vector3(0, 0, 1));
+        }
+        else
+        {
+            rotSpeed = 0;
+        }
     }
 
     [PunRPC]
     public void DelayEX()
     {
-        StartCoroutine(nameof(DelayEffect));
+        StartCoroutine(DelayEffect());
     }
 
     [PunRPC]
@@ -80,21 +101,34 @@ public class Arrow_MultiShot : Arrow
     {
         arrowMesh[0].SetActive(true);
         arrowMesh[1].SetActive(true);
-        arrowMesh[0].transform.rotation= Quaternion.identity;
-        arrowMesh[1].transform.rotation= Quaternion.identity;
     }
 
     [PunRPC]
     public void Tailpack()
     {
-        tail.gameObject.SetActive(true);
-        tails[0].gameObject.SetActive(true);
-        tails[1].gameObject.SetActive(true);       
+        StartCoroutine(TailCtrl());
+    }
+
+    [PunRPC]
+    public void HideType()
+    {
+        typing.gameObject.SetActive(false);
     }
 
     public IEnumerator DelayEffect()
     {
-        yield return new WaitForSecondsRealtime(0.04f);
-        effects.SetActive(true);                  
+        yield return new WaitForSecondsRealtime(0.05f);
+        effects[0].gameObject.SetActive(true);
+        effects[1].gameObject.SetActive(true);
+        effects[2].gameObject.SetActive(true);
+    }
+
+    public IEnumerator TailCtrl()
+    {
+        tails[0].gameObject.SetActive(true);
+        tails[1].gameObject.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        tails[0].gameObject.SetActive(false);
+        tails[1].gameObject.SetActive(false);
     }
 }
