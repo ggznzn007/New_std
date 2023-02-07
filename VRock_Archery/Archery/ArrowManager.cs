818802300fdfe1d960b907e6fb35e8ba89e8ae50
Pooleun Jeong <ggznzn007@naver.com>
2023-02-07 15:06:22 +0900

    VR_Dev_20230207

--- VRock_Archery/Archery/ArrowManager.cs
+++ VRock_Archery/Archery/ArrowManager.cs
@@ -10,66 +10,36 @@ using UnityEngine.UI;
 using PN = Photon.Pun.PN;
 
 public class ArrowManager : MonoBehaviourPunCallbacks//, IPunObservable
-{
-    public static ArrowManager ArrowM;
-    public PhotonView PV;
-    public Collider dmColl;
-    public Arrow arrow;
-    public Rigidbody rb;
-    //private Vector3 remotePos;
-    //private Quaternion remoteRot;
+{   
+    public PhotonView PV;    
     public bool isBeingHeld = false;
     public bool isGrip;    
-    //public string hit;
-    public int actNumber;
-    
+  
 
-    /* public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-     {
-
-         if (stream.IsWriting)
-         {
-             stream.SendNext(transform.position);
-             stream.SendNext(transform.rotation);
-         }
-         else
-         {
-             transform.SetPositionAndRotation((Vector3)stream.ReceiveNext(), (Quaternion)stream.ReceiveNext());
-         }
-     }*/
+   
 
     private void Awake()
     {
-        ArrowM = this;
+       
         isGrip = true;
     }
 
     void Start()
     {
-        PV = GetComponent<PhotonView>();
-        arrow= GetComponent<Arrow>();
-        rb = arrow.rigidbody;
+        PV = GetComponent<PhotonView>();       
         isGrip = true;        
     }
 
 
     void Update()
-    {
-      /*  if (!PV.IsMine)
-        {
-            transform.SetPositionAndRotation(Vector3.Lerp(transform.position, remotePos, 10 * Time.deltaTime)
-                , Quaternion.Lerp(transform.rotation, remoteRot, 10 * Time.deltaTime));
-        }*/
-
+    {     
         if (isBeingHeld)
         {
-            isGrip = true;            
-            //rb.isKinematic = true;
+            isGrip = true;   
         }
         else
         {
             isGrip = false;
-            // rb.isKinematic = false;
         }
     }
 
@@ -152,49 +122,6 @@ public class ArrowManager : MonoBehaviourPunCallbacks//, IPunObservable
     {
         Debug.Log("화살을 놓았습니다.");
         PV.RPC(nameof(StopGrabbing), RpcTarget.AllBuffered);       
-    }
-
-    /* private void OnCollisionEnter(Collision collision)
-     {
-         if (!AvartarController.ATC.isAlive)
-         {
-             return;
-         }
-
-         if (collision.collider.CompareTag("Cube") && PV.IsMine) // 일반태그
-         {
-             PV.RPC(nameof(DestroyArrow), RpcTarget.AllBuffered);
-         }
-
-         if (collision.collider.CompareTag("BlueTeam") || collision.collider.CompareTag("RedTeam") && !PV.IsMine)
-         {
-             PV.RPC(nameof(DestroyArrow), RpcTarget.AllBuffered);
-         }
-
-         if (collision.collider.CompareTag("Head") && !PV.IsMine)
-         {
-             PV.RPC(nameof(DestroyArrow), RpcTarget.AllBuffered);
-         }
-
-         if (collision.collider.CompareTag("Body") && !PV.IsMine)
-         {
-             PV.RPC(nameof(DestroyArrow), RpcTarget.AllBuffered);
-         }
-
-     }*/
-
-   
-
-   /* [PunRPC]
-    public void BeepSound()
-    {
-        AudioManager.AM.PlaySX(bombBeep);
-    }*/
-    /*[PunRPC]
-    public void ExploSound()
-    {
-        AudioManager.AM.PlaySX("Explo");
-    }*/
-
+    }       
    
 }
