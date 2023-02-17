01a19aaca7ee31da0b7daea33c6f6da4fbc057fa
Pooleun Jeong <ggznzn007@naver.com>
2023-02-17 16:35:29 +0900

    VR_Dev_20230217

--- VRock_Archery/Object/EMP_Effect.cs
+++ VRock_Archery/Object/EMP_Effect.cs
@@ -10,29 +10,36 @@ using UnityEngine.UI;
 using PN = Photon.Pun.PN;
 using Random = UnityEngine.Random;
 
-public class EMP_Effect : MonoBehaviour
+public class EMP_Effect : MonoBehaviourPun
 {
     public Collider coll;
     
+    //public PhotonView PV;
+
     private void Awake()
     {
-        coll = GetComponent<Collider>();        
-    }  
+        coll = GetComponent<Collider>();   
+        //PV = GetComponent<PhotonView>();
+    }
+
     private void Start()
     {
-        StartCoroutine(CollOnOff());       
+        StartCoroutine(CollOnOff());
     }
 
     public IEnumerator CollOnOff()
     {
         coll.enabled = true;
-        yield return new WaitForSeconds(0.005f);
+        //yield return new WaitForSeconds(0.005f);
+        yield return new WaitForSeconds(0.03f);
         coll.enabled = false;        
     }
 
+   
     /*[System.Obsolete]
-    public IEnumerator DestroyEX()
+    private IEnumerator Start()
     {
+        StartCoroutine(CollOnOff());
         yield return new WaitForSeconds(GetComponent<ParticleSystem>().duration);
         Destroy(gameObject);
     }*/
