b2811f3c71e1efcf23cf1176eb56121cc56936e1
Pooleun Jeong <ggznzn007@naver.com>
2023-02-21 16:37:18 +0900

    VR_Dev_20230221

---
+++ VRock_Archery/GameManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+using UnityEngine.UI;
+using PN = Photon.Pun.PN;
+using Random = UnityEngine.Random;
+using Photon.Pun.Demo.PunBasics;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+
+    void LoadArena()
+    {
+        if (!PN.IsMasterClient)
+        {
+            Debug.LogError("PhotonNetwork : Trying to Load a level but we are not the master Client");
+        }
+        Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PN.CurrentRoom.PlayerCount);
+        PN.LoadLevel("Room for " + PN.CurrentRoom.PlayerCount);
+    }
+
+    public override void OnPlayerEnteredRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
+
+
+        if (PN.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerEnteredRoom IsMasterClient {0}", PN.IsMasterClient); // called before OnPlayerLeftRoom
+
+
+            LoadArena();
+        }
+    }
+
+
+    public override void OnPlayerLeftRoom(Player other)
+    {
+        Debug.LogFormat("OnPlayerLeftRoom() {0}", other.NickName); // seen when other disconnects
+
+
+        if (PN.IsMasterClient)
+        {
+            Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PN.IsMasterClient); // called before OnPlayerLeftRoom
+
+
+            LoadArena();
+        }
+    }
+}
