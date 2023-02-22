ee7b72bff4f1e04f72b79aefbb70e533e2b6a257
Pooleun Jeong <ggznzn007@naver.com>
2023-02-22 15:02:21 +0900

    VR_Dev_20230222

---
+++ VRock_Archery/Archery/ArrowBasicM.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+using UnityEngine.UI;
+using PN = Photon.Pun.PN;
+public class ArrowBasicM : MonoBehaviourPunCallbacks, IPunObservable
+{
+    public PhotonView PV;
+    public bool isBeingHeld;
+    private bool isGrip;
+    private Vector3 remotePos;
+    private Quaternion remoteRot;
+
+    private void Awake()
+    {
+        isBeingHeld = false;
+        isGrip = false;
+    }
+
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!PV.IsMine)
+        {
+            transform.SetPositionAndRotation(Vector3.Lerp(transform.position, remotePos, 30 * Time.deltaTime)
+                , Quaternion.Lerp(transform.rotation, remoteRot, 30 * Time.deltaTime));
+            return;
+        }
+        if (isBeingHeld)
+        {
+            isGrip = true;
+        }
+        else
+        {
+            isGrip = false;
+        }
+    }
+
+    [PunRPC]
+    public void StartGrabbing()
+    {
+        isBeingHeld = true;
+    }
+
+    [PunRPC]
+    public void StopGrabbing()
+    {
+        isBeingHeld = false;
+    }
+
+    public void OnSelectedEntered()
+    {
+        Debug.Log("화살을 잡았습니다.");
+        PV.RPC(nameof(StartGrabbing), RpcTarget.AllBuffered);       
+    }
+
+    public void OnSelectedExited()
+    {
+        Debug.Log("화살을 놓았습니다.");
+        PV.RPC(nameof(StopGrabbing), RpcTarget.AllBuffered);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            remotePos = (Vector3)stream.ReceiveNext();
+            remoteRot = (Quaternion)stream.ReceiveNext();
+        }
+    }
+}
