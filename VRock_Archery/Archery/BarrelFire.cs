6a2fdb60f808cfc1881ed6c9a23fcfdae636d089
Pooleun Jeong <ggznzn007@naver.com>
2023-02-22 15:02:27 +0900

    VR_Dev_20230222

---
+++ VRock_Archery/Archery/BarrelFire.cs
@@ -0,0 +1,62 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+using UnityEngine.UI;
+using PN = Photon.Pun.PN;
+using Random = UnityEngine.Random;
+using TMPro;
+using UnityEngine.SceneManagement;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+using UnityEngine;
+using System.Collections;
+
+public class BarrelFire : MonoBehaviourPunCallbacks, IPunObservable
+{
+    public Collider exColl;
+    private PhotonView PV;
+    private Vector3 remotePos;
+    private Quaternion remoteRot;
+
+    [System.Obsolete]
+    private IEnumerator Start()
+    {
+        PV = GetComponent<PhotonView>();        
+        StartCoroutine(CollOnOff());
+        yield return new WaitForSeconds(GetComponent<ParticleSystem>().duration);
+        Destroy(PV.gameObject);
+    }
+
+    public IEnumerator CollOnOff()
+    {
+        while(true)
+        {
+            exColl.enabled = true;
+            yield return new WaitForSeconds(0.05f);
+            exColl.enabled = false;
+            yield return new WaitForSeconds(0.4f);
+        }        
+    }
+    private void Update()
+    {
+        if (!PV.IsMine)
+        {
+            transform.SetPositionAndRotation(Vector3.Lerp(transform.position, remotePos, 30 * Time.deltaTime)
+                , Quaternion.Lerp(transform.rotation, remoteRot, 30 * Time.deltaTime));
+        }
+    }
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            remotePos = (Vector3)stream.ReceiveNext();
+            remoteRot = (Quaternion)stream.ReceiveNext();
+        }
+    }
+
+
+}
