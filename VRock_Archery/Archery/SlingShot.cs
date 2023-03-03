e6ee7c71bd9d224ab6665f049cef2b83a2cbe874
Pooleun Jeong <ggznzn007@naver.com>
2023-03-03 11:26:45 +0900

    VR_Dev_20230303

---
+++ VRock_Archery/Archery/SlingShot.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SlingShot : XRGrabInteractable
+{
+    protected override void OnSelectEntered(SelectEnterEventArgs interactor)
+    {
+        base.OnSelectEntered(interactor);       
+    }
+    protected override void OnSelectExited(SelectExitEventArgs interactor)
+    {
+        base.OnSelectExited(interactor);       
+    }
+}
