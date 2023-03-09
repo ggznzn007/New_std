8c79dddb4db67521df4194d8bee6755b65625548
Pooleun Jeong <ggznzn007@naver.com>
2023-03-09 16:14:08 +0900

    VR_Dev_20230309

--- VRock_Archery/Archery/IceBlock.cs
+++ VRock_Archery/Archery/IceBlock.cs
@@ -9,76 +9,18 @@ using System;
 using UnityEngine.UI;
 using PN = Photon.Pun.PN;
 using UnityEditor;
-public class IceBlock : XRGrabInteractable,IPunObservable, IPunOwnershipCallbacks
+public class IceBlock : XRGrabInteractable
 {
     public static IceBlock IB;
     public PhotonView PV;
     public GameObject DestroyEX;
-    Rigidbody rb;
-    public bool isBeingHeld = false;
-    private Vector3 remotePos;
-    private Quaternion remoteRot;
+    
     void Start()
     {
         IB = this;
-        PV = GetComponent<PhotonView>();
-        rb = GetComponent<Rigidbody>();
+        PV = GetComponent<PhotonView>();       
     }
-
-
-    void Update()
-    {
-        if (!PV.IsMine)
-        {
-            transform.SetPositionAndRotation(Vector3.Lerp(transform.position, remotePos, 30 * Time.deltaTime)
-                , Quaternion.Lerp(transform.rotation, remoteRot, 30 * Time.deltaTime));
-            return;
-        }
-
-        if (isBeingHeld)
-        {
-            rb.isKinematic = true;
-        }
-        else
-        {
-            rb.isKinematic = false;
-        }
-    }
-
-    protected override void OnSelectEntered(SelectEnterEventArgs args)
-    {
-        base.OnSelectEntered(args);        
-        Debug.Log("아이스블럭 잡았다");        
-        PV.RPC(nameof(StartGrabbing), RpcTarget.AllBuffered);
-        if (PV.Owner == PN.LocalPlayer)
-        {
-            Debug.Log("이미 소유권이 나에게 있습니다.");
-        }
-        else
-        {
-            TransferOwnership();
-        }
-    }
-
-    protected override void OnSelectExited(SelectExitEventArgs args)
-    {
-        base.OnSelectExited(args);
-        Debug.Log("아이스블럭 놓았다");        
-        PV.RPC(nameof(StopGrabbing), RpcTarget.AllBuffered);
-    }
-
-    [PunRPC]
-    public void StartGrabbing()
-    {
-        isBeingHeld = true;
-    }
-
-    [PunRPC]
-    public void StopGrabbing()
-    {
-        isBeingHeld = false;
-    }
-
+    
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.CompareTag("Icicle")|| collision.collider.CompareTag("FloorBox"))
@@ -100,42 +42,5 @@ public class IceBlock : XRGrabInteractable,IPunObservable, IPunOwnershipCallback
         Destroy(gameObject);
     }       
 
-    private void TransferOwnership()
-    {
-        PV.RequestOwnership();
-    }
-
-    public void OnOwnershipRequest(PhotonView targetView, Player requestingPlayer)
-    {
-        if (targetView != PV)
-        {
-            return;
-        }
-        Debug.Log("소유권 요청 : " + targetView.name + "from " + requestingPlayer.NickName);
-        PV.TransferOwnership(requestingPlayer);
-    }
-
-    public void OnOwnershipTransfered(PhotonView targetView, Player previousOwner)
-    {
-        Debug.Log("현재소유한 플레이어: " + targetView.name + "from " + previousOwner.NickName);
-    }
-
-    public void OnOwnershipTransferFailed(PhotonView targetView, Player senderOfFailedRequest)
-    {
-
-    }
-
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting)
-        {
-            stream.SendNext(transform.position);
-            stream.SendNext(transform.rotation);
-        }
-        else
-        {
-            remotePos = (Vector3)stream.ReceiveNext();
-            remoteRot = (Quaternion)stream.ReceiveNext();
-        }
-    }
+  
 }
