72330807f9eda56d890c2bf52e65c852ce767688
Pooleun Jeong <ggznzn007@naver.com>
2023-03-10 16:46:00 +0900

    VR_Dev_20230310

--- VRock_Archery/Archery/BlockManager.cs
+++ VRock_Archery/Archery/BlockManager.cs
@@ -32,8 +32,9 @@ public class BlockManager : MonoBehaviourPunCallbacks, IPunOwnershipCallbacks, I
     {
         if (!PV.IsMine)
         {
-            transform.SetPositionAndRotation(Vector3.Lerp(transform.position, remotePos, 30 * Time.deltaTime)
-                , Quaternion.Lerp(transform.rotation, remoteRot, 30 * Time.deltaTime));
+            //float t = Mathf.Clamp(Time.deltaTime * 10, 0f, 0.99f);
+            transform.SetPositionAndRotation(Vector3.Lerp(transform.position, remotePos, Time.deltaTime * 30)
+                , Quaternion.Lerp(transform.rotation, remoteRot, Time.deltaTime * 30));
             return;
         }
         if (isBeingHeld)
