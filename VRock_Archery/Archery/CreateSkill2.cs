117fac658ca46fd8c6987e0dacccb48a7a947614
Pooleun Jeong <ggznzn007@naver.com>
2023-03-21 16:00:23 +0900

    VR_Dev_20230321

--- VRock_Archery/Archery/CreateSkill2.cs
+++ VRock_Archery/Archery/CreateSkill2.cs
@@ -6,16 +6,16 @@ using Photon.Realtime;
 
 public class CreateSkill2 : MonoBehaviourPun                                               // 스노우 특수 눈덩이 생성 슬롯
 {
-    public GameObject snowSkilled;
-    public GameObject snowStone;
-    public Transform spawnPoint;
-    private readonly float limitTime = 5;
-    private readonly int perCent = 60;         // 돌덩이 1, 얼음 2
-    private ParticleSystem _particleSystem;
-    private AudioSource _audioSource;
-    private PhotonView PV;
-    private GameObject curBall;
-    private float curTime;
+    public GameObject snowSkilled;                                                        // 고드름 프리팹
+    public GameObject snowStone;                                                          // 돌덩이 프리팹
+    public Transform spawnPoint;                                                          // 특수 눈덩이 생성 포인트
+    private readonly float limitTime = 2;                                                // 특수 눈덩이 생성 제한시간
+    private readonly int perCent = 60;                                                    // 특수눈덩이 확률 돌덩이 1순위, 고드름 2순위  // 수치가 높으면 돌덩이 확률이 높다
+    private ParticleSystem _particleSystem;                                               // 생성 슬롯 파티클
+    private AudioSource _audioSource;                                                     // 생성 슬롯 오디오
+    private PhotonView PV;                                                                // 포톤뷰
+    private GameObject curBall;                                                           // 현재 눈덩이
+    private float curTime;                                                                // 현재 시간
 
     private void Start()
     {
@@ -26,21 +26,21 @@ public class CreateSkill2 : MonoBehaviourPun                                    
 
     private void Update()
     {
-        if (PN.IsMasterClient)
+        if (PN.IsMasterClient)                                                         // 마스터 만 화살 생성(여러사람이 생성하면 에러)
         {
             SpawnBomb();
         }
     }
 
-    public void SpawnBomb()
+    public void SpawnBomb()                                                           // 눈덩이 생성 메서드
     {
-        if (curBall == null)
+        if (curBall == null)                                                          // 현재 슬롯이 비었을 때만 생성
         {
-            if (curBall != null) return;
+            if (curBall != null) return;                                              // 아니면 리턴
             curTime += Time.deltaTime;
-            if (curTime >= limitTime)
+            if (curTime >= limitTime)                                                 // 시간이 제한시간보다 같거나 클 때 생성
             {
-                bool skilled = RandomArrow.RandArrowPer(perCent);
+                bool skilled = RandomArrow.RandArrowPer(perCent);                     // 확률 계산하는 메서드 (따로 만든 클래스)
                 if (skilled)
                 {
                     curBall = PN.InstantiateRoomObject(snowStone.name, spawnPoint.position, spawnPoint.rotation, 0);
@@ -59,12 +59,12 @@ public class CreateSkill2 : MonoBehaviourPun                                    
 
     private void OnTriggerEnter(Collider coll)
     {
-        if (coll.CompareTag("Stoneball")|| coll.CompareTag("Icicle"))
+        if (coll.CompareTag("Stoneball")|| coll.CompareTag("Icicle"))  // 특수눈덩이 태그 중 돌, 고드름이 슬롯에 태그 시작 시 
         {
             if (PV.IsMine)
             {
                 //if (!PV.IsMine) return;
-                PV.RPC(nameof(FxPlay), RpcTarget.AllBuffered);
+                PV.RPC(nameof(FxPlay), RpcTarget.AllBuffered); // 슬롯 파티클 재생 , RPC로 모든 플레이어에게 공유
             }
 
         }
@@ -72,12 +72,12 @@ public class CreateSkill2 : MonoBehaviourPun                                    
 
     private void OnTriggerExit(Collider coll)
     {
-        if (coll.CompareTag("Stoneball") || coll.CompareTag("Icicle"))
+        if (coll.CompareTag("Stoneball") || coll.CompareTag("Icicle")) // 특수눈덩이 태그 중 돌, 고드름이 슬롯에 태그 중 나갈 때 
         {
             if (PV.IsMine)
             {
                 //if (!PV.IsMine) return;
-                PV.RPC(nameof(FxStop), RpcTarget.AllBuffered);
+                PV.RPC(nameof(FxStop), RpcTarget.AllBuffered); // 슬롯 파티클 정지 , RPC로 모든 플레이어에게 공유
             }
 
         }
