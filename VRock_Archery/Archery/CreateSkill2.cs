434fee7a3b24d9ab2d349c89516a7fbe825aaf2b
Pooleun Jeong <ggznzn007@naver.com>
2023-03-24 14:47:03 +0900

    VR_Dev_20230324

--- VRock_Archery/Archery/CreateSkill2.cs
+++ VRock_Archery/Archery/CreateSkill2.cs
@@ -9,7 +9,7 @@ public class CreateSkill2 : MonoBehaviourPun                                    
     public GameObject snowSkilled;                                                        // 고드름 프리팹
     public GameObject snowStone;                                                          // 돌덩이 프리팹
     public Transform spawnPoint;                                                          // 특수 눈덩이 생성 포인트
-    private readonly float limitTime = 2;                                                // 특수 눈덩이 생성 제한시간
+    private readonly float limitTime = 20;                                                // 특수 눈덩이 생성 제한시간
     private readonly float perCent = 60;                                                    // 특수눈덩이 확률 돌덩이 1순위, 고드름 2순위  // 수치가 높으면 돌덩이 확률이 높다
     private ParticleSystem _particleSystem;                                               // 생성 슬롯 파티클
     private AudioSource _audioSource;                                                     // 생성 슬롯 오디오
