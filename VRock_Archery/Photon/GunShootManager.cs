51320960cc0e40774a90d88feafc58fea8d41a7e
Pooleun Jeong <ggznzn007@naver.com>
2023-03-28 12:01:45 +0900

    VR_Dev_20230328

--- VRock_Archery/Photon/GunShootManager.cs
+++ VRock_Archery/Photon/GunShootManager.cs
@@ -17,7 +17,7 @@ using UnityEngine.LowLevel;
 using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using static Unity.XR.PXR.PXR_HandPoseConfig.BonesRecognizer;
 
-public class GunShootManager : MonoBehaviourPunCallbacks                                      // 토이
+public class GunShootManager : MonoBehaviourPunCallbacks      // 아처 게임 관리 스크립트
 {
     public static GunShootManager GSM;
 
@@ -32,50 +32,44 @@ public class GunShootManager : MonoBehaviourPunCallbacks                        
     [SerializeField] GameObject blueTeam;
     [Header("관리자")]
     public GameObject admin;
-    [Header("Eagle NPC 프리팹")]                                            // 폭탄을 생성해주는 NPC => 크레인
+    [Header("Eagle NPC 프리팹")]                                           
     public GameObject eagleNPC;
     public Transform[] wayPos;
     public GameObject eagleBomb;
     public Transform spawnPoint;
-    public GameObject myBomb = null;
-   /* [Header("스킬화살 프리팹")]
-    public ParticleSystem[] arrowSpawnFX;
-    public Transform[] aSpawnPosition;                         // 특수화살 생성위치
-    public GameObject arrowSkilled;                            // 스킬 화살
-    public GameObject arrowMulti;                              // 멀티샷
-    public GameObject arrowBomb;                               // 폭탄 화살*/
-
-    private GameObject spawnPlayer;                                  // 생성되는 플레이어
+    public GameObject myBomb = null; 
     [SerializeField] bool count = false;                             // 타이머 카운트 스위치
     [SerializeField] int limitedTime;                                // 게임제한시간
-    Hashtable setTime = new Hashtable();                             // 세팅시간
-    PhotonView PV;
     public Transform adminPoint;                                     // 관리자 생성 위치
-    public string GameInfo1;
-    public string GameInfo2;
-    public string one;
-    public string two;
-    public string three;
-    public string startGame;
-    public string gameover;
-    public int kills;
-    private ExitGames.Client.Photon.Hashtable playerProp = new ExitGames.Client.Photon.Hashtable();
-    public Image[] gameOverImage;
-    public Image[] bluewinImg;
-    public Image[] redwinImg;
-    public Image[] blueloseImg;
-    public Image[] redloseImg;
-    public Image[] drawImg;
-    public TMP_Text[] blueScore;
-    public TMP_Text[] redScore;
-    public int score_BlueKill;
-    public int score_RedKill;
-    /*private GameObject curArrowB;
-    private GameObject curArrowBB;
-    private GameObject curArrowR;
-    private GameObject curArrowRR;
-    private GameObject curEagle;              */       
 
+    public string GameInfo1;                                         // 게임 알림 오디오재생을 위한 문자열 "게임이 3초뒤에 시작됩니다."
+    public string GameInfo2;                                         // 게임 알림 오디오재생을 위한 문자열 "3초 뒤에 다음스테이지로 이동합니다"
+    public string GameInfo3;                                         // 게임 알림 오디오재생을 위한 문자열 "게임이 곧 종료됩니다."
+    public string GameInfo4;                                         // 게임 알림 오디오재생을 위한 문자열 "블루팀이 승리하였습니다."
+    public string GameInfo5;                                         // 게임 알림 오디오재생을 위한 문자열 "레드팀이 승리하였습니다."
+    public string GameInfo6;                                         // 게임 알림 오디오재생을 위한 문자열 "무승부입니다."
+    public string GameInfo7;                                         // 게임 알림 오디오재생을 위한 문자열 "score_result."
+    public string GameInfo9;                                         // 게임 알림 오디오재생을 위한 문자열 "게임이 1분 남았습니다."
+    public string one;                                               // 게임 알림 오디오재생을 위한 문자열 "1"
+    public string two;                                               // 게임 알림 오디오재생을 위한 문자열 "2"
+    public string three;                                             // 게임 알림 오디오재생을 위한 문자열 "3"
+    public string gameStart;                                         // 게임 알림 오디오재생을 위한 문자열 "스타트"
+    public string gameover;                                          // 게임 알림 오디오재생을 위한 문자열 "게임오버"
+    public int kills;                                                // 킬 수
+    public Image[] gameOverImage;                                    // 양쪽 UI에 나오는 게임오버 이미지
+    public Image[] bluewinImg;                                       // 양쪽 UI에 나오는 블루팀 이겼을 때 이미지
+    public Image[] redwinImg;                                        // 양쪽 UI에 나오는 레드팀 이겼을 때 이미지
+    public Image[] blueloseImg;                                      // 양쪽 UI에 나오는 블루팀 졌을 때 이미지
+    public Image[] redloseImg;                                       // 양쪽 UI에 나오는 레드팀 졌을 때 이미지
+    public Image[] drawImg;                                          // 양쪽 UI에 나오는 비겼을 때 이미지
+    public TMP_Text[] blueScore;                                     // 양쪽 UI에 나오는 블루팀 점수 텍스트
+    public TMP_Text[] redScore;                                      // 양쪽 UI에 나오는 레드팀 점수 텍스트
+    public int score_BlueKill;                                       // 양쪽 UI에 나오는 블루팀 점수
+    public int score_RedKill;                                        // 양쪽 UI에 나오는 레드팀 점수  
+    PhotonView PV;                                                   // 포톤뷰
+    Hashtable setTime = new Hashtable();                             // 게임시간
+    private GameObject spawnPlayer;                                  // 생성되는 플레이어
+    private ExitGames.Client.Photon.Hashtable playerProp = new ExitGames.Client.Photon.Hashtable();
 
     private void Awake()
     {
@@ -175,13 +169,13 @@ public class GunShootManager : MonoBehaviourPunCallbacks                        
     public void SetScore()
     {
         blueScore[0].text = score_BlueKill.ToString();   // 블루팀 점수
-        blueScore[1].text = score_RedKill.ToString();     // 레드팀 점수
-        redScore[0].text = score_BlueKill.ToString();   // 블루팀 점수
+        blueScore[1].text = score_RedKill.ToString();    // 레드팀 점수
+        redScore[0].text = score_BlueKill.ToString();    // 블루팀 점수
         redScore[1].text = score_RedKill.ToString();     // 레드팀 점수
     }
     public void UpdateStats()
     {
-        playerProp["kills"] = kills;             // 개인 킬 수                                                
+        playerProp["kills"] = kills;             // 킬 수                                                
 
         PN.LocalPlayer.CustomProperties = playerProp;
         PN.SetPlayerCustomProperties(playerProp);
@@ -353,7 +347,7 @@ public class GunShootManager : MonoBehaviourPunCallbacks                        
     }
 
     [PunRPC]
-    public void EndGameT()
+    public void EndGameT()                   // "게임 종료"
     {
         StartCoroutine(LeaveGame());
     }
@@ -361,13 +355,13 @@ public class GunShootManager : MonoBehaviourPunCallbacks                        
     [PunRPC]
     public void Notice()
     {
-        AudioManager.AM.PlaySE("GameInfo3");
+        AudioManager.AM.PlaySE(GameInfo3); // "게임이 곧 종료됩니다." 알림 오디오 재생
     }
 
     [PunRPC]
     public void Notice1min()
     {
-        AudioManager.AM.PlaySE("GameInfo9");
+        AudioManager.AM.PlaySE(GameInfo9); // "게임이 1분 남았습니다." 알림 오디오 재생
     }
 
     IEnumerator PlayTimer()
@@ -380,10 +374,10 @@ public class GunShootManager : MonoBehaviourPunCallbacks                        
 
         if (limitedTime == 60)
         {
-            PV.RPC(nameof(Notice1min), RpcTarget.AllViaServer);   // 게임 끝나기 60초전 알림
+            PV.RPC(nameof(Notice1min), RpcTarget.AllViaServer);   // 게임 끝나기 1분 전 알림 RPC로 전체 알림
         }
 
-        if (limitedTime == 8)                                 // 게임 끝나기 8초전에 알림
+        if (limitedTime == 8)                                     // 게임 끝나기 8초전에 알림 RPC로 전체 알림
         {
             PV.RPC(nameof(Notice), RpcTarget.AllViaServer);
         }
@@ -392,105 +386,104 @@ public class GunShootManager : MonoBehaviourPunCallbacks                        
         {
             count = false;
             limitedTime = 0;
-            PV.RPC(nameof(EndGameT), RpcTarget.AllViaServer);
-            Debug.Log("타임오버");
+            PV.RPC(nameof(EndGameT), RpcTarget.AllViaServer);     // 게임 종료를 RPC로 전체 알림            
         }
     }
 
     public IEnumerator StartTimer()
     {
-        yield return new WaitForSeconds(4);
-        AudioManager.AM.PlaySE("GameInfo1");
-        countText[0].text = string.Format("게임이 3초 뒤에 시작됩니다.");
-        countText[1].text = string.Format("게임이 3초 뒤에 시작됩니다.");
-        yield return new WaitForSeconds(3);
-        AudioManager.AM.PlaySE("Three");
-        countText[0].text = string.Format("3");
-        countText[1].text = string.Format("3");
+        yield return new WaitForSeconds(4);                            
+        AudioManager.AM.PlaySE(GameInfo1);                             // "게임이 3초 뒤에 시작됩니다." 알림 오디오 재생
+        countText[0].text = string.Format("게임이 3초 뒤에 시작됩니다.");  // 블루팀 UI에 문자로 띄우기
+        countText[1].text = string.Format("게임이 3초 뒤에 시작됩니다.");  // 레드팀 UI에 문자로 띄우기
+        yield return new WaitForSeconds(3);                            
+        AudioManager.AM.PlaySE(three);                                 // "Three" 알림 오디오 재생
+        countText[0].text = string.Format("3");                        // 블루팀 UI에 문자로 띄우기
+        countText[1].text = string.Format("3");                        // 레드팀 UI에 문자로 띄우기
+        yield return new WaitForSeconds(1);                            
+        AudioManager.AM.PlaySE(two);                                   // "Two" 알림 오디오 재생
+        countText[0].text = string.Format("2");                        // 블루팀 UI에 문자로 띄우기
+        countText[1].text = string.Format("2");                        // 레드팀 UI에 문자로 띄우기
+        yield return new WaitForSeconds(1);                            
+        AudioManager.AM.PlaySE(one);                                   // "One" 알림 오디오 재생
+        countText[0].text = string.Format("1");                        // 블루팀 UI에 문자로 띄우기
+        countText[1].text = string.Format("1");                        // 레드팀 UI에 문자로 띄우기
         yield return new WaitForSeconds(1);
-        AudioManager.AM.PlaySE("Two");
-        countText[0].text = string.Format("2");
-        countText[1].text = string.Format("2");
+        AudioManager.AM.PlaySE(gameStart);                             // "스타트" 알림 오디오 재생
+        countText[0].text = string.Format("게임 스타트!!!");             // 블루팀 UI에 문자로 띄우기
+        countText[1].text = string.Format("게임 스타트!!!");             // 레드팀 UI에 문자로 띄우기
         yield return new WaitForSeconds(1);
-        AudioManager.AM.PlaySE("One");
-        countText[0].text = string.Format("1");
-        countText[1].text = string.Format("1");
-        yield return new WaitForSeconds(1);
-        AudioManager.AM.PlaySE("Start");
-        countText[0].text = string.Format("게임 스타트!!!");
-        countText[1].text = string.Format("게임 스타트!!!");
-        yield return new WaitForSeconds(1);
-        count = true;
-        DataManager.DM.inGame = true;
-        timerText[0].gameObject.SetActive(true);
-        timerText[1].gameObject.SetActive(true);
-        countText[0].gameObject.SetActive(false);
-        countText[1].gameObject.SetActive(false);
+        count = true;                                                  // 게임 타이머 시작
+        DataManager.DM.inGame = true;                                  // 게임 중 여부 데이터 저장
+        timerText[0].gameObject.SetActive(true);                       // 블루팀 UI에 타이머 on
+        timerText[1].gameObject.SetActive(true);                       // 레드팀 UI에 타이머 on
+        countText[0].gameObject.SetActive(false);                      // 블루팀 UI에 안내문구 off
+        countText[1].gameObject.SetActive(false);                      // 레드팀 UI에 안내문구 off
     }
 
     public IEnumerator LeaveGame()
     {
-        timerText[0].gameObject.SetActive(false);
-        timerText[1].gameObject.SetActive(false);
-        resultText[0].gameObject.SetActive(true);
-        resultText[1].gameObject.SetActive(true);
-        DataManager.DM.inGame = false;
-        DataManager.DM.gameOver = true;
-        AudioManager.AM.PlaySE("Gameover");
-        gameOverImage[0].gameObject.SetActive(true);        
-        gameOverImage[1].gameObject.SetActive(true);        
+        timerText[0].gameObject.SetActive(false);                      // 블루팀 UI에 타이머 off
+        timerText[1].gameObject.SetActive(false);                      // 레드팀 UI에 타이머 off
+        resultText[0].gameObject.SetActive(true);                      // 블루팀 UI에 결과 on
+        resultText[1].gameObject.SetActive(true);                      // 레드팀 UI에 결과 on
+        DataManager.DM.inGame = false;                                 // 게임 중 여부 데이터 해제
+        DataManager.DM.gameOver = true;                                // 게임오버 여부 true
+        AudioManager.AM.PlaySE(gameover);                              // "게임오버" 알림 오디오 재생
+        gameOverImage[0].gameObject.SetActive(true);                   // 블루팀 UI에 게임오버 이미지 on
+        gameOverImage[1].gameObject.SetActive(true);                   // 레드팀 UI에 게임오버 이미지 on   
         yield return new WaitForSeconds(2);
-        VictoryTeam();
+        VictoryTeam();                                                 // 게임 결과 판정 메서드 호출(승리,무승부)
         yield return new WaitForSeconds(3);
-        AudioManager.AM.PlaySE("GameInfo7");
-        gameOverImage[0].gameObject.SetActive(false);
-        gameOverImage[1].gameObject.SetActive(false);
+        AudioManager.AM.PlaySE(GameInfo7);                             // "게임결과 발표 후" 알림 오디오 재생
+        gameOverImage[0].gameObject.SetActive(false);                  // 블루팀 UI에 게임오버 이미지 off
+        gameOverImage[1].gameObject.SetActive(false);                  // 레드팀 UI에 게임오버 이미지 off  
         yield return new WaitForSeconds(3);
-        countText[0].gameObject.SetActive(true);
-        countText[1].gameObject.SetActive(true);
-        AudioManager.AM.PlaySE("GameInfo2");
+        countText[0].gameObject.SetActive(true);                       // 블루팀 UI에 안내문구 on
+        countText[1].gameObject.SetActive(true);                       // 레드팀 UI에 안내문구 on
+        AudioManager.AM.PlaySE(GameInfo2);                             // "3초 뒤에 다음 스테이지로 이동합니다" 알림 오디오 재생
         countText[0].text = string.Format("3초 뒤에 다음 스테이지로 이동합니다");
         countText[1].text = string.Format("3초 뒤에 다음 스테이지로 이동합니다");
         yield return new WaitForSeconds(5);
         PN.LeaveRoom();
-        StopCoroutine(LeaveGame());
+        StopCoroutine(LeaveGame());                                    // 방 나가기 메서드 호출
     }
 
     public void VictoryTeam()
     {
-        if (score_BlueKill > score_RedKill)
+        if (score_BlueKill > score_RedKill)                                   // 블루팀 승리 시
         {
-            AudioManager.AM.PlaySE("GameInfo4");            
-            blueScore[0].gameObject.SetActive(false);
-            blueScore[1].gameObject.SetActive(false);
-            redScore[0].gameObject.SetActive(false);
-            redScore[1].gameObject.SetActive(false);
-            bluewinImg[0].gameObject.SetActive(true);
-            bluewinImg[1].gameObject.SetActive(true);
-            redloseImg[0].gameObject.SetActive(true);
-            redloseImg[1].gameObject.SetActive(true);
+            AudioManager.AM.PlaySE(GameInfo4);                                // "블루팀이 승리하였습니다." 알림 오디오 재생            
+            blueScore[0].gameObject.SetActive(false);                         // 블루팀 UI에 블루팀 스코어 off
+            blueScore[1].gameObject.SetActive(false);                         // 레드팀 UI에 블루팀 스코어 off
+            redScore[0].gameObject.SetActive(false);                          // 블루팀 UI에 레드팀 스코어 off
+            redScore[1].gameObject.SetActive(false);                          // 레드팀 UI에 레드팀 스코어 off 
+            bluewinImg[0].gameObject.SetActive(true);                         // 블루팀 UI에 블루팀 Win 이미지 on
+            bluewinImg[1].gameObject.SetActive(true);                         // 레드팀 UI에 블루팀 Win 이미지 on
+            redloseImg[0].gameObject.SetActive(true);                         // 블루팀 UI에 레드팀 Lose 이미지 on
+            redloseImg[1].gameObject.SetActive(true);                         // 레드팀 UI에 레드팀 Lose 이미지 on
         }
-        else if (score_BlueKill < score_RedKill)
+        else if (score_BlueKill < score_RedKill)                              // 레드팀 승리 시
         {
-            AudioManager.AM.PlaySE("GameInfo5");
-            blueScore[0].gameObject.SetActive(false);
-            blueScore[1].gameObject.SetActive(false);
-            redScore[0].gameObject.SetActive(false);
-            redScore[1].gameObject.SetActive(false);
-            blueloseImg[0].gameObject.SetActive(true);
-            blueloseImg[1].gameObject.SetActive(true);
-            redwinImg[0].gameObject.SetActive(true);
-            redwinImg[1].gameObject.SetActive(true);
+            AudioManager.AM.PlaySE(GameInfo5);                                // "레드팀이 승리하였습니다." 알림 오디오 재생 
+            blueScore[0].gameObject.SetActive(false);                         // 블루팀 UI에 블루팀 스코어 off
+            blueScore[1].gameObject.SetActive(false);                         // 레드팀 UI에 블루팀 스코어 off
+            redScore[0].gameObject.SetActive(false);                          // 블루팀 UI에 레드팀 스코어 off
+            redScore[1].gameObject.SetActive(false);                          // 레드팀 UI에 레드팀 스코어 off 
+            blueloseImg[0].gameObject.SetActive(true);                        // 블루팀 UI에 블루팀 Lose 이미지 on
+            blueloseImg[1].gameObject.SetActive(true);                        // 레드팀 UI에 블루팀 Lose 이미지 on
+            redwinImg[0].gameObject.SetActive(true);                          // 블루팀 UI에 레드팀 Win 이미지 on
+            redwinImg[1].gameObject.SetActive(true);                          // 레드팀 UI에 레드팀 Win 이미지 on
         }
-        else if (score_BlueKill == score_RedKill)
+        else if (score_BlueKill == score_RedKill)                             // 무승부 시
         {
-            AudioManager.AM.PlaySE("GameInfo6");
-            blueScore[0].gameObject.SetActive(false);
-            blueScore[1].gameObject.SetActive(false);
-            redScore[0].gameObject.SetActive(false);
-            redScore[1].gameObject.SetActive(false);
-            drawImg[0].gameObject.SetActive(true);
-            drawImg[1].gameObject.SetActive(true);
+            AudioManager.AM.PlaySE(GameInfo6);                                // "무승부입니다." 알림 오디오 재생 
+            blueScore[0].gameObject.SetActive(false);                         // 블루팀 UI에 블루팀 스코어 off
+            blueScore[1].gameObject.SetActive(false);                         // 레드팀 UI에 블루팀 스코어 off
+            redScore[0].gameObject.SetActive(false);                          // 블루팀 UI에 레드팀 스코어 off
+            redScore[1].gameObject.SetActive(false);                          // 레드팀 UI에 레드팀 스코어 off
+            drawImg[0].gameObject.SetActive(true);                            // 블루팀 UI에 레드팀 Draw 이미지 on
+            drawImg[1].gameObject.SetActive(true);                            // 레드팀 UI에 레드팀 Draw 이미지 on
         }
     }
 
