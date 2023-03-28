200c0e130e026ae972cb0b4e63bfe0360f48dc36
Pooleun Jeong <ggznzn007@naver.com>
2023-03-28 12:01:10 +0900

    VR_Dev_20230328

--- VRock_Archery/Object/BodyDamaged_S.cs
+++ VRock_Archery/Object/BodyDamaged_S.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
@@ -9,11 +7,17 @@ using PN = Photon.Pun.PN;
 using Random = UnityEngine.Random;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR;
-public class BodyDamaged_S : MonoBehaviourPun
+
+public class BodyDamaged_S : MonoBehaviourPun  // 스노우 플레이어 몸통 콜라이더 스크립트 - 바디 대미지
 {
     public AvartarController AT;
 
-    private void OnCollisionEnter(Collision collision)                         // 기본 화살이 몸에 맞았을 때 노멀 대미지 
+    private void Start()
+    {
+        AT = GetComponentInParent<AvartarController>();
+    }
+
+    private void OnCollisionEnter(Collision collision)                         // 기본 눈덩이 맞았을 때 노멀 대미지 
     {
         if (collision.collider.CompareTag("Snowball") && AT.isAlive && DataManager.DM.inGame)
         {
@@ -24,11 +28,11 @@ public class BodyDamaged_S : MonoBehaviourPun
         }
 
         if ((collision.collider.CompareTag("Stoneball") || collision.collider.CompareTag("Icicle"))
-           && AT.isAlive && DataManager.DM.inGame)                                                  // 특수 눈덩이 머리에 맞을 때 대미지
+           && AT.isAlive && DataManager.DM.inGame)                                                  // 특수 눈덩이 맞을 때 대미지
         {
             if (!AT.isDamaged)
             {
-                AT.HeadShotDamage();
+                AT.HeadShotDamage(); // 특수 눈덩이는 헤드샷과 같은 대미지 적용
             }
         }
 
@@ -36,21 +40,19 @@ public class BodyDamaged_S : MonoBehaviourPun
 
     private void OnTriggerEnter(Collider coll)
     {
-        if (coll.CompareTag("Bomb") && AT.isAlive && DataManager.DM.inGame)             // 폭탄 화살 대미지
+        if (coll.CompareTag("Bomb") && AT.isAlive && DataManager.DM.inGame)             // 아이스 폭탄 대미지
         {
             if (!AT.isDamaged)
             {
-                AT.IceDamage();
-                Debug.Log("아이스폭탄대미지!");
+                AT.IceDamage();               
             }
         }
 
-        if (coll.CompareTag("Effect") && AT.isAlive && DataManager.DM.inGame)             // 폭탄 화살 대미지
+        if (coll.CompareTag("Effect") && AT.isAlive && DataManager.DM.inGame)           // 고드름 스킬 대미지
         {
             if (!AT.isDamaged)
             {
-                AT.SkillDamage();
-                Debug.Log("아이스폭탄대미지!");
+                AT.SkillDamage();                
             }
         }
     }
