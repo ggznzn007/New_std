07e05e7af6c784d6ab22864ae5ea9b1e234e0726
Pooleun Jeong <ggznzn007@naver.com>
2023-03-28 12:02:08 +0900

    VR_Dev_20230328

--- VRock_Archery/Photon/WesternManager.cs
+++ VRock_Archery/Photon/WesternManager.cs
@@ -32,60 +32,65 @@ public class WesternManager : MonoBehaviourPunCallbacks
     public GameObject admin;
     [Header("스노우블럭 프리팹")]
     public GameObject snowBlock;
-    [Header("NPC 프리팹")]
+    [Header("Eagle NPC 프리팹")]
     public GameObject eagleNPC;  
 
-    public Transform[] blockPoint;
-    private GameObject spawnPlayer;
-    [SerializeField] bool count = false;
-    [SerializeField] bool bCount = false;
-    [SerializeField] int limitedTime;
-    [SerializeField] float buildTime;
-    Hashtable setTime = new Hashtable();
-    PhotonView PV;
-    private float curTime;
-    public Transform adminPoint;
-    public string GameInfo1;
-    public string GameInfo2;
-    public string one;
-    public string two;
-    public string three;
-    public string startGame;
-    public string gameover;
-    public int kills;
+    public Transform[] blockPoint;                                   // 스노우 블럭 생성 위치
+    [SerializeField] bool count = false;                             // 타이머 카운트 스위치
+    [SerializeField] bool bCount = false;                            // 타이머 카운트 스위치_빌드타임
+    [SerializeField] int limitedTime;                                // 게임제한시간
+    [SerializeField] float buildTime;                                // 빌드제한시간_ 스노우 블럭 쌓는시간
+    public Transform adminPoint;                                     // 관리자 생성 위치
+    public string BuildTime;                                         // 게임 알림 오디오재생을 위한 문자열 "빌드타임에 대한 설명 멘트"
+    public string BuildTime2;                                        // 게임 알림 오디오재생을 위한 문자열 "빌드 타임!"
+    public string GameInfo1;                                         // 게임 알림 오디오재생을 위한 문자열 "게임이 3초뒤에 시작됩니다."
+    public string GameInfo3;                                         // 게임 알림 오디오재생을 위한 문자열 "게임이 곧 종료됩니다."
+    public string GameInfo4;                                         // 게임 알림 오디오재생을 위한 문자열 "블루팀이 승리하였습니다."
+    public string GameInfo5;                                         // 게임 알림 오디오재생을 위한 문자열 "레드팀이 승리하였습니다."
+    public string GameInfo6;                                         // 게임 알림 오디오재생을 위한 문자열 "무승부입니다."
+    public string GameInfo7;                                         // 게임 알림 오디오재생을 위한 문자열 "score_result."
+    public string GameInfo8;                                         // 게임 알림 오디오재생을 위한 문자열 "게임이 종료되었습니다. 헤드셋을 벗어주세요."
+    public string GameInfo9;                                         // 게임 알림 오디오재생을 위한 문자열 "게임이 1분 남았습니다."
+    public string one;                                               // 게임 알림 오디오재생을 위한 문자열 "1"
+    public string two;                                               // 게임 알림 오디오재생을 위한 문자열 "2"
+    public string three;                                             // 게임 알림 오디오재생을 위한 문자열 "3"
+    public string gameStart;                                         // 게임 알림 오디오재생을 위한 문자열 "스타트"
+    public string gameover;                                          // 게임 알림 오디오재생을 위한 문자열 "게임오버"
+    public int kills;                                                // 킬 수
+    public Image[] buildTImage;                                      // 양쪽 UI에 나오는 빌드타임 이미지
+    public Image[] gameOverImage;                                    // 양쪽 UI에 나오는 게임오버 이미지
+    public Image[] bluewinImg;                                       // 양쪽 UI에 나오는 블루팀 이겼을 때 이미지
+    public Image[] redwinImg;                                        // 양쪽 UI에 나오는 레드팀 이겼을 때 이미지
+    public Image[] blueloseImg;                                      // 양쪽 UI에 나오는 블루팀 졌을 때 이미지
+    public Image[] redloseImg;                                       // 양쪽 UI에 나오는 레드팀 졌을 때 이미지
+    public Image[] drawImg;                                          // 양쪽 UI에 나오는 비겼을 때 이미지
+    public TMP_Text[] blueScore;                                     // 양쪽 UI에 나오는 블루팀 점수 텍스트
+    public TMP_Text[] redScore;                                      // 양쪽 UI에 나오는 레드팀 점수 텍스트
+    public int score_BlueKill;                                       // 양쪽 UI에 나오는 블루팀 점수
+    public int score_RedKill;                                        // 양쪽 UI에 나오는 레드팀 점수    
+    PhotonView PV;                                                   // 포톤뷰
+    Hashtable setTime = new Hashtable();                             // 게임시간
+    private GameObject spawnPlayer;                                  // 생성되는 플레이어
     private ExitGames.Client.Photon.Hashtable playerProp = new ExitGames.Client.Photon.Hashtable();
-    public Image[] buildTImage;
-    public Image[] gameOverImage;
-    public Image[] bluewinImg;
-    public Image[] redwinImg;
-    public Image[] blueloseImg;
-    public Image[] redloseImg;
-    public Image[] drawImg;
-    public TMP_Text[] blueScore;
-    public TMP_Text[] redScore;
-    public int score_BlueKill;
-    public int score_RedKill;
 
     private void Awake()
     {
         WM = this;
         DataManager.DM.currentMap = Map.WESTERN;
         SetScore();
-
+        
     }
     void Start()
     {
-
+        DataManager.DM.inBuild = true;
         PV = GetComponent<PhotonView>();
         if (PN.IsConnectedAndReady && PN.InRoom)
         {
             SpawnPlayer();
 
             if (PN.IsMasterClient)
-            {
-                //PV.RPC(nameof(BuildTimer), RpcTarget.AllViaServer);                              // 타이머 동기화
-                PV.RPC(nameof(StartBtnW), RpcTarget.AllViaServer);                              // 타이머 동기화
-                                                                                                // BuildTimer();
+            {                
+                PV.RPC(nameof(StartBtnW), RpcTarget.AllViaServer);                              // 타이머 동기화                                                                                                
                 SpawnBlock();                                                                  // 스노우블럭 시작시  지정위치에 생성 총 10개
             }
 
@@ -95,43 +100,6 @@ public class WesternManager : MonoBehaviourPunCallbacks
             }
         }
     }
-
-    /*[PunRPC]
-    public void BuildTimer()
-    {       
-       StartCoroutine(BuildT());        
-    }
-
-    public IEnumerator BuildT()
-    {
-        AudioManager.AM.PlaySE("GameInfo1");
-        buildTime = 30;
-        yield return new WaitForSeconds(4);
-        if (buildTime>0)
-        {
-            buildTime -= 1;
-        }
-        if(buildTime<=0)
-        {
-            StartCoroutine(StartTimer());
-        }
-        else
-        {
-            yield break;
-        }
-        
-        photonView.RPC(nameof(ShowBuildT), RpcTarget.All,buildTime);
-
-        yield return new WaitForSeconds(1);
-        StartCoroutine(BuildT());        
-    }
-
-    [PunRPC]
-    public void ShowBuildT(int number)
-    {
-        btimerText[0].text = number.ToString();
-        btimerText[1].text = number.ToString();
-    }*/
 
     public void SpawnPlayer()
     {
@@ -156,7 +124,7 @@ public class WesternManager : MonoBehaviourPunCallbacks
                 break;
             // 윈도우 프로그램 빌드 시
             case Team.ADMIN:
-                if (Application.platform == RuntimePlatform.WindowsPlayer)//|| Application.platform == RuntimePlatform.WindowsEditor)
+                if (Application.platform == RuntimePlatform.WindowsPlayer)
                 {
                     if (Application.platform != RuntimePlatform.WindowsPlayer) { return; }
                     PN.AutomaticallySyncScene = true;
@@ -178,12 +146,7 @@ public class WesternManager : MonoBehaviourPunCallbacks
     {
         if (Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter)) { PV.RPC("StartBtnW", RpcTarget.All); }
         if (Input.GetKeyDown(KeyCode.Backspace)) { PV.RPC("EndGameW", RpcTarget.All); }
-        if (Input.GetKeyDown(KeyCode.Escape)) { StartCoroutine(nameof(ExitGame)); }
-
-        /*if (PN.IsMasterClient)
-        {
-            SpawnBlock();
-        }*/
+        if (Input.GetKeyDown(KeyCode.Escape)) { StartCoroutine(nameof(ExitGame)); }      
 
         /*if (PN.IsConnectedAndReady && PN.InRoom && PN.IsMasterClient)  // 윈도우 프로그램 빌드 시
         {
@@ -193,11 +156,12 @@ public class WesternManager : MonoBehaviourPunCallbacks
             }
         }*/
     }
+
     void FixedUpdate()
     {
-        SetScore();
-        TimerW();
-        TimerB();
+        SetScore(); // 스코어 셋팅
+        TimerW();   // 게임 타이머 메서드 호출
+        TimerB();   // 빌드 타이머 메서드 호출 
     }
 
     public void SetScore()
@@ -270,13 +234,7 @@ public class WesternManager : MonoBehaviourPunCallbacks
         for (int i = 0; i < blockPoint.Length; i++)
         {
             PN.InstantiateRoomObject(snowBlock.name, blockPoint[i].position, blockPoint[i].rotation, 0);          
-        }
-      /*  curTime += Time.deltaTime;
-        if (curTime >= limit)
-        {
-           
-        }
-*/
+        }      
     }
 
     [PunRPC]
@@ -286,7 +244,7 @@ public class WesternManager : MonoBehaviourPunCallbacks
     }
 
     [PunRPC]
-    public void EndGameW()
+    public void EndGameW()                   // "게임 종료"
     {
         StartCoroutine(LeaveGame());
     }
@@ -294,13 +252,13 @@ public class WesternManager : MonoBehaviourPunCallbacks
     [PunRPC]
     public void NoticeW()
     {
-        AudioManager.AM.PlaySE("GameInfo3");
+        AudioManager.AM.PlaySE(GameInfo3); // "게임이 곧 종료됩니다." 알림 오디오 재생
     }
 
     [PunRPC]
     public void Notice1min()
     {
-        AudioManager.AM.PlaySE("GameInfo9");
+        AudioManager.AM.PlaySE(GameInfo9); // "게임이 1분 남았습니다." 알림 오디오 재생
     }
 
     public IEnumerator PlayTimer()
@@ -313,10 +271,10 @@ public class WesternManager : MonoBehaviourPunCallbacks
 
         if (limitedTime == 60)
         {
-            PV.RPC(nameof(Notice1min), RpcTarget.AllViaServer);   // 게임 끝나기 60초전 알림
+            PV.RPC(nameof(Notice1min), RpcTarget.AllViaServer);   // 게임 끝나기 1분 전 알림 RPC로 전체 알림
         }
 
-        if (limitedTime == 8)
+        if (limitedTime == 8)                                     // 게임 끝나기 8초전에 알림 RPC로 전체 알림
         {
             PV.RPC(nameof(NoticeW), RpcTarget.AllViaServer);
         }
@@ -325,8 +283,7 @@ public class WesternManager : MonoBehaviourPunCallbacks
         {
             count = false;
             limitedTime = 0;
-            PV.RPC(nameof(EndGameW), RpcTarget.AllViaServer);
-            Debug.Log("타임오버");
+            PV.RPC(nameof(EndGameW), RpcTarget.AllViaServer);     // 게임 종료를 RPC로 전체 알림            
         }
     }
 
@@ -345,51 +302,53 @@ public class WesternManager : MonoBehaviourPunCallbacks
 
     }
 
-    IEnumerator StartTimer()
+    IEnumerator StartTimer()                                           // 빌드 타임 타이머
     {
         yield return new WaitForSeconds(4);
-        AudioManager.AM.PlaySE("BuildTime2");
-        countText[0].gameObject.SetActive(false);
-        countText[1].gameObject.SetActive(false);
-        buildTImage[0].gameObject.SetActive(true);
-        buildTImage[1].gameObject.SetActive(true);
+        AudioManager.AM.PlaySE(BuildTime2);                            // "빌드 타임 !" 알림 오디오 재생
+        countText[0].gameObject.SetActive(false);                      // 블루팀 UI에 알림문구 off
+        countText[1].gameObject.SetActive(false);                      // 레드팀 UI에 알림문구 off
+        buildTImage[0].gameObject.SetActive(true);                     // 블루팀 UI에 빌드타임 이미지 on
+        buildTImage[1].gameObject.SetActive(true);                     // 레드팀 UI에 빌드타임 이미지 on
+        DataManager.DM.inBuild = true;                                 // 빌드 타임 중 여부 데이터 저장
         yield return new WaitForSeconds(1);
-        AudioManager.AM.PlaySE("BuildTime");        
-        buildTime = 30;
-        bCount = true;        
+        AudioManager.AM.PlaySE(BuildTime);                             // "빌드 타임에 대한 멘트 재생" 알림 오디오 재생
+        buildTime = 30;                                                // 빌드제한시간
+        bCount = true;                                                 // 빌드 타이머 시작
     }
 
-    public IEnumerator RealTimer()
+    public IEnumerator RealTimer()                                     // 게임 타이머
     {
-        buildTImage[0].gameObject.SetActive(false);
-        buildTImage[1].gameObject.SetActive(false);
-        btimerText[0].gameObject.SetActive(false);
-        btimerText[1].gameObject.SetActive(false);
-        countText[0].gameObject.SetActive(true);
-        countText[1].gameObject.SetActive(true);
+        buildTImage[0].gameObject.SetActive(false);                    // 블루팀 UI에 빌드타임 이미지 off
+        buildTImage[1].gameObject.SetActive(false);                    // 레드팀 UI에 빌드타임 이미지 off
+        btimerText[0].gameObject.SetActive(false);                     // 블루팀 UI에 빌드타임 타이머 문자 off
+        btimerText[1].gameObject.SetActive(false);                     // 레드팀 UI에 빌드타임 타이머 문자 off
+        countText[0].gameObject.SetActive(true);                       // 블루팀 UI에 안내문구 on
+        countText[1].gameObject.SetActive(true);                       // 레드팀 UI에 안내문구 on
         yield return new WaitForSeconds(1);
-        AudioManager.AM.PlaySE("GameInfo1");
+        AudioManager.AM.PlaySE(GameInfo1);                             // "게임이 3초 뒤에 시작됩니다." 알림 오디오 재생
         countText[0].text = string.Format("게임이 3초 뒤에 시작됩니다.");
         countText[1].text = string.Format("게임이 3초 뒤에 시작됩니다.");
         yield return new WaitForSeconds(3);
-        AudioManager.AM.PlaySE("Three");
+        AudioManager.AM.PlaySE(three);                                 // "Three" 알림 오디오 재생
         countText[0].text = string.Format("3");
         countText[1].text = string.Format("3");
         yield return new WaitForSeconds(1);
-        AudioManager.AM.PlaySE("Two");
+        AudioManager.AM.PlaySE(two);                                   // "Two" 알림 오디오 재생
         countText[0].text = string.Format("2");
         countText[1].text = string.Format("2");
         yield return new WaitForSeconds(1);
-        AudioManager.AM.PlaySE("One");
+        AudioManager.AM.PlaySE(one);                                   // "One" 알림 오디오 재생
         countText[0].text = string.Format("1");
         countText[1].text = string.Format("1");
         yield return new WaitForSeconds(1);
-        AudioManager.AM.PlaySE("Start");
+        AudioManager.AM.PlaySE(gameStart);                             // "스타트" 알림 오디오 재생
         countText[0].text = string.Format("게임 스타트!!!");
         countText[1].text = string.Format("게임 스타트!!!");
+        DataManager.DM.inBuild = false;                                // 빌드 타임 중 여부 데이터 해제
         yield return new WaitForSeconds(1);
-        count = true;                                             // 게임 타이머 스타트
-        DataManager.DM.inGame = true;
+        count = true;                                                  // 게임 타이머 시작
+        DataManager.DM.inGame = true;                                  // 게임 중 여부 데이터 저장
         timerText[0].gameObject.SetActive(true);
         timerText[1].gameObject.SetActive(true);
         countText[0].gameObject.SetActive(false);
@@ -402,33 +361,33 @@ public class WesternManager : MonoBehaviourPunCallbacks
         timerText[1].gameObject.SetActive(false);
         resultText[0].gameObject.SetActive(true);
         resultText[1].gameObject.SetActive(true);
-        DataManager.DM.inGame = false;
-        DataManager.DM.gameOver = true;
-        AudioManager.AM.PlaySE("Gameover");
+        DataManager.DM.inGame = false;                                 // 게임 중 여부 데이터 해제
+        DataManager.DM.gameOver = true;                                // 게임오버 여부 true
+        AudioManager.AM.PlaySE(gameover);                              // "게임오버" 알림 오디오 재생
         gameOverImage[0].gameObject.SetActive(true);
         gameOverImage[1].gameObject.SetActive(true);
         yield return new WaitForSeconds(2);
-        VictoryTeam();
+        VictoryTeam();                                                 // 게임 결과 판정 메서드 호출(승리,무승부)
         yield return new WaitForSeconds(3);
-        AudioManager.AM.PlaySE("GameInfo7");
+        AudioManager.AM.PlaySE(GameInfo7);                             // "게임결과 발표 후" 알림 오디오 재생
         gameOverImage[0].gameObject.SetActive(false);
         gameOverImage[1].gameObject.SetActive(false);
         yield return new WaitForSeconds(3);
         countText[0].gameObject.SetActive(true);
         countText[1].gameObject.SetActive(true);
-        AudioManager.AM.PlaySE("GameInfo8");
+        AudioManager.AM.PlaySE(GameInfo8);                           // "게임이 종료되었습니다." 알림 오디오 재생
         countText[0].text = string.Format("게임이 종료되었습니다\n 헤드셋을 벗어주세요");
         countText[1].text = string.Format("게임이 종료되었습니다\n 헤드셋을 벗어주세요");
         yield return new WaitForSeconds(4);
         PN.LeaveRoom();
-        StopCoroutine(LeaveGame());
+        StopCoroutine(LeaveGame());                                    // 방 나가기 메서드 호출
     }
 
     public void VictoryTeam()
     {
-        if (score_BlueKill > score_RedKill)
+        if (score_BlueKill > score_RedKill)                                   // 블루팀 승리 시
         {
-            AudioManager.AM.PlaySE("GameInfo4");
+            AudioManager.AM.PlaySE(GameInfo4);                                // "블루팀이 승리하였습니다." 알림 오디오 재생
             blueScore[0].gameObject.SetActive(false);
             blueScore[1].gameObject.SetActive(false);
             redScore[0].gameObject.SetActive(false);
@@ -439,9 +398,9 @@ public class WesternManager : MonoBehaviourPunCallbacks
             redloseImg[1].gameObject.SetActive(true);
 
         }
-        else if (score_BlueKill < score_RedKill)
+        else if (score_BlueKill < score_RedKill)                              // 레드팀 승리 시
         {
-            AudioManager.AM.PlaySE("GameInfo5");
+            AudioManager.AM.PlaySE(GameInfo5);                                // "레드팀이 승리하였습니다." 알림 오디오 재생 
             blueScore[0].gameObject.SetActive(false);
             blueScore[1].gameObject.SetActive(false);
             redScore[0].gameObject.SetActive(false);
@@ -451,9 +410,9 @@ public class WesternManager : MonoBehaviourPunCallbacks
             redwinImg[0].gameObject.SetActive(true);
             redwinImg[1].gameObject.SetActive(true);
         }
-        else if (score_BlueKill == score_RedKill)
+        else if (score_BlueKill == score_RedKill)                             // 무승부 시
         {
-            AudioManager.AM.PlaySE("GameInfo6");
+            AudioManager.AM.PlaySE(GameInfo6);                                // "무승부입니다." 알림 오디오 재생 
             blueScore[0].gameObject.SetActive(false);
             blueScore[1].gameObject.SetActive(false);
             redScore[0].gameObject.SetActive(false);
