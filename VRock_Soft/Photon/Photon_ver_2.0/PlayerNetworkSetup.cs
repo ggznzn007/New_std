2e6dd20da24a5fd86e4bd620d4cd85b9a3926078
Pooleun Jeong <ggznzn007@naver.com>
2023-03-29 13:42:20 +0900

    VR_Dev_20230329

---
+++ VRock_Soft/Photon/Photon_ver_2.0/PlayerNetworkSetup.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+using System.Linq;
+using System.IO;
+using UnityEngine.UI;
+using PN = Photon.Pun.PN;
+using Random = UnityEngine.Random;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR;
+using Antilatency;
+using Antilatency.TrackingAlignment;
+using Antilatency.DeviceNetwork;
+using Antilatency.Alt;
+using Antilatency.SDK;
+using static ObjectPooler;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
+public class PlayerNetworkSetup : MonoBehaviourPunCallbacks
+{
+    public GameObject LocalXRRigGameObject;
+    public GameObject AvatarHead;
+    public GameObject AvatarBody;
+    public GameObject AvatarHand_L;
+    public GameObject AvatarHand_R;
+    public MeshRenderer at_L;
+    public MeshRenderer at_R;
+
+    private void Start()
+    {
+        if (photonView.IsMine)                                 // 로컬 플레이어 
+        {
+            LocalXRRigGameObject.SetActive(true);
+            SetLayerRecursively(go: AvatarHead, 8);
+            SetLayerRecursively(go: AvatarBody, 9);            
+            //SetLayerRecursively(go: AvatarHand_L, 0);
+            //SetLayerRecursively(go: AvatarHand_R, 0);
+            //Camera.main.cullingMask = Camera.main.cullingMask & ~(1 << LayerMask.NameToLayer("LocalAvatarHands"));// 레이어 제거
+            
+        }
+        else                                                   // 리모트 플레이어
+        {
+            LocalXRRigGameObject.SetActive(false);
+            SetLayerRecursively(go: AvatarHead, 0);
+            SetLayerRecursively(go: AvatarBody, 0);
+            //Camera.main.cullingMask |= 1 << LayerMask.NameToLayer("LocalAvatarHands"); // 레이어 추가
+            // SetLayerRecursively(go: AvatarHand_L, 10);
+            // SetLayerRecursively(go: AvatarHand_R, 10);
+        }
+    }
+    private void Update()
+    {
+        if (!photonView.IsMine) return;
+    }
+
+    void SetLayerRecursively(GameObject go, int layerNum)
+    {
+        if (go == null) return;
+        foreach (Transform trans in go.GetComponentsInChildren<Transform>(true))
+        {
+            trans.gameObject.layer = layerNum;
+        }
+    }
+
+    public void Hide_L()
+    {
+        if (photonView.IsMine)
+            photonView.RPC(nameof(HideL), RpcTarget.AllBuffered);
+    }
+    public void Show_L()
+    {
+        if (photonView.IsMine)
+            photonView.RPC(nameof(ShowL), RpcTarget.AllBuffered);
+    }
+    public void Hide_R()
+    {
+        if (photonView.IsMine)
+            photonView.RPC(nameof(HideR), RpcTarget.AllBuffered);
+    }
+    public void Show_R()
+    {
+        if (photonView.IsMine)
+            photonView.RPC(nameof(ShowR), RpcTarget.AllBuffered);
+    }
+
+    [PunRPC]
+    public void HideL()
+    {
+        at_L.forceRenderingOff = true;
+    }
+
+    [PunRPC]
+    public void ShowL()
+    {
+        at_L.forceRenderingOff = false;
+    }
+
+
+    [PunRPC]
+    public void HideR()
+    {
+        at_R.forceRenderingOff = true;
+    }
+
+    [PunRPC]
+    public void ShowR()
+    {
+        at_R.forceRenderingOff = false;
+    }
+
+}
