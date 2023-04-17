2c55ee974c8060a683bb5b06401356b8f3a144e9
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-04-17 16:58:21 +0900

    Std_20230417

---
+++ Multi2D_PUN2/NetworkManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviourPunCallbacks
+{
+    public InputField NickNameInput;
+    public GameObject DisconnectPanel;
+    public GameObject RespawnPanel;
+
+
+    void Awake()
+    {
+        Screen.SetResolution(960, 540, false);
+        PhotonNetwork.SendRate = 60;
+        PhotonNetwork.SerializationRate = 30;
+    }
+
+    public void Connect() => PhotonNetwork.ConnectUsingSettings();
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.LocalPlayer.NickName = NickNameInput.text;
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions { MaxPlayers = 6 }, null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        DisconnectPanel.SetActive(false);
+        StartCoroutine("DestroyBullet");
+        Spawn();
+    }
+
+    IEnumerator DestroyBullet()
+    {
+        yield return new WaitForSeconds(0.2f);
+        foreach (GameObject GO in GameObject.FindGameObjectsWithTag("Bullet")) GO.GetComponent<PhotonView>().RPC("DestroyRPC", RpcTarget.All);
+    }
+
+    public void Spawn()
+    {
+        PhotonNetwork.Instantiate("Player", new Vector3(Random.Range(-6f, 19f), 4, 0), Quaternion.identity);
+        RespawnPanel.SetActive(false);
+    }
+
+    void Update() { if (Input.GetKeyDown(KeyCode.Escape) && PhotonNetwork.IsConnected) PhotonNetwork.Disconnect(); }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        DisconnectPanel.SetActive(true);
+        RespawnPanel.SetActive(false);
+    }
+}
