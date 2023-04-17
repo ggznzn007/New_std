6d149839f52ab7994c9d7a4f7fa908b000600c44
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-04-17 16:58:31 +0900

    Std_20230417

---
+++ Multi2D_PUN2/BulletScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class BulletScript : MonoBehaviourPunCallbacks
+{
+    public PhotonView PV;
+    int dir;
+
+
+    void Start() => Destroy(gameObject, 3.5f);
+
+    void Update() => transform.Translate(Vector3.right * 7 * Time.deltaTime * dir);
+
+
+    void OnTriggerEnter2D(Collider2D col) // col을 RPC의 매개변수로 넘겨줄 수 없다
+    {
+        if (col.tag == "Ground") PV.RPC("DestroyRPC", RpcTarget.AllBuffered);
+        if (!PV.IsMine && col.tag == "Player" && col.GetComponent<PhotonView>().IsMine) // 느린쪽에 맞춰서 Hit판정
+        {
+            col.GetComponent<PlayerScript>().Hit();
+            PV.RPC("DestroyRPC", RpcTarget.AllBuffered);
+        }
+    }
+
+
+    [PunRPC]
+    void DirRPC(int dir) => this.dir = dir;
+
+    [PunRPC]
+    void DestroyRPC() => Destroy(gameObject);
+}
