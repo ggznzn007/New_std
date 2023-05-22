d268a49d06e0f78d5a621f9edf2cc467316ae017
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-05-22 15:06:49 +0900

    RTS_Control_Std_20230522

---
+++ RTS_Control_std/UnitSpawner.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitSpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject unitPrefab;
+    [SerializeField]
+    private int maxUnitCount;
+
+    private Vector2 minSize = new Vector2(-22, -22);
+    private Vector2 maxSize = new Vector2(22, 22);
+
+    public List<UnitController> SpawnUnits()
+    {
+        List<UnitController> unitList = new List<UnitController>(maxUnitCount);
+
+        for (int i = 0; i < maxUnitCount; ++i)
+        {
+            Vector3 pos = new Vector3(Random.Range(minSize.x, maxSize.x), 1, Random.Range(minSize.y, maxSize.y));
+
+            GameObject clone = Instantiate(unitPrefab, pos, Quaternion.identity);
+            UnitController unit = clone.GetComponent<UnitController>(); 
+
+            unitList.Add(unit);
+        }
+
+        return unitList;
+
+    }
+}
