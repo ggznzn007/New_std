39b56c14e66ddb8e89659d99307ef8ccacd5a1eb
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-05-23 16:16:53 +0900

    ScriptableObject_std_230523

---
+++ ScriptableObject/UI_Setting.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class UI_Setting : ScriptableObject
+{
+    private const string SettingFileDirectory = "Assets/Resources";
+    private const string SettingFilePath = "Assets/Resources/UI_Setting.asset";
+
+    private static UI_Setting _instance;
+
+    public static UI_Setting Instance
+    {
+        get
+        {
+            if(_instance != null)
+            {
+                return _instance;
+            }
+
+            _instance = Resources.Load<UI_Setting>("UI_Setting");
+
+#if UNITY_EDITOR
+
+            if (_instance == null)
+            {
+                if(!AssetDatabase.IsValidFolder(SettingFileDirectory))
+                {
+                    AssetDatabase.CreateFolder("Assets", "Resources");
+                }
+
+                _instance = AssetDatabase.LoadAssetAtPath<UI_Setting>(SettingFilePath);
+
+                if(_instance == null)
+                {
+                   _instance =  CreateInstance<UI_Setting>();
+                    AssetDatabase.CreateAsset(_instance, SettingFilePath);
+                }
+            }
+
+#endif
+            return _instance;
+        }
+    }
+
+
+    public string language = "KOR";
+
+    public Color themeColor;
+    public Sprite emptyThumbnailSprite;
+    public GameObject popupPrefab;
+
+    public Font defaultFont;
+    public int defaultFontSize = 100;
+    public Color defaultFontColor = Color.blue;
+}
