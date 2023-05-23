a97a5db4edab6eff31007f6f4c41a1d13aef7890
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-05-23 16:16:46 +0900

    ScriptableObject_std_230523

---
+++ ScriptableObject/TextSetter.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextSetter : MonoBehaviour
+{
+    public Text text;
+
+    
+    void Start()
+    {
+        text.font = UI_Setting.Instance.defaultFont;
+        text.fontSize = UI_Setting.Instance.defaultFontSize;
+        text.color = UI_Setting.Instance.defaultFontColor;
+    }
+
+   
+    void Update()
+    {
+        
+    }
+}
