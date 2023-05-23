b4efe882ae6bacea8b2bb3e3a617cf804013d5a2
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-05-23 16:16:59 +0900

    ScriptableObject_std_230523

---
+++ ScriptableObject/UI_SettingEditor.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(UI_Setting))]
+
+public class UI_SettingEditor : Editor
+{
+    [MenuItem("Assets/Open UI Setting")]
+    public static void OpenInspector()
+    {
+        Selection.activeObject = UI_Setting.Instance;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        if(GUI.changed)
+        {
+            EditorUtility.SetDirty(target);
+            AssetDatabase.SaveAssets();
+        }
+    }
+}
