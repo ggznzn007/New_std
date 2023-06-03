8dc3b9a5c5694946c1e28b8e901349e60bc29fb6
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-06-03 17:04:29 +0900

    WPF_Std_Talk_20230603

---
+++ WPF_Kakaotalk/Converters/WindowStateIconConverter.cs
@@ -0,0 +1,33 @@
+﻿using FontAwesome6;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace AlTalk.Converters
+{
+    internal class WindowStateIconConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            WindowState state = (WindowState)value;
+            if (state==WindowState.Normal)
+            {
+                return EFontAwesomeIcon.Regular_Square;
+            }
+            else
+            {
+                return EFontAwesomeIcon.Solid_DownLeftAndUpRightToCenter;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
