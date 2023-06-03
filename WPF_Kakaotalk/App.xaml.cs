f8caf90a13f3b3432dd9366e2f3a3f1912c87b62
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-06-03 17:03:59 +0900

    WPF_Std_Talk_20230603

---
+++ WPF_Kakaotalk/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using AlTalk.Views;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AlTalk
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Services = ConfigureServices();
+            Startup += App_Startup;
+        }
+
+        private void App_Startup(object sender, StartupEventArgs e)
+        {
+            var mainView = App.Current.Services.GetService<MainView>();
+            mainView.Show();
+        }
+
+        public new static App Current => (App)Application.Current;
+
+        public IServiceProvider Services { get; }
+
+        private static IServiceProvider ConfigureServices()
+        {
+            var services = new ServiceCollection();
+
+            // Services
+
+
+            // Views
+            services.AddSingleton<MainView>();
+           
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
