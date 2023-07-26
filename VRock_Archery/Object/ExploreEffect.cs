4d2e70f01079cb184b2734bc0f7280e3f308ee84
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-07-26 15:40:12 +0900

    Update ExploreEffect.cs

--- VRock_Archery/Object/ExploreEffect.cs
+++ VRock_Archery/Object/ExploreEffect.cs
@@ -22,8 +22,7 @@ public class ExploreEffect : MonoBehaviour                      // ÃÑ¾Ë ÀÓÆÑÆ® È
         ObjectPooler.ReturnToPool(gameObject);    // ÇÑ °´Ã¼¿¡ ÇÑ¹ø¸¸
         CancelInvoke();    // Monobehaviour¿¡ Invoke°¡ ÀÖ´Ù¸é 
     }
-
-    
+      
 
     void DeactiveDelay() => gameObject.SetActive(false);*/
 }
