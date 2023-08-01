ff1497878d38427318dd58988770eb9776abc99f
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-08-01 17:10:46 +0900

    Create Block.cs

---
+++ ColorSelect/Block.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class Block : MonoBehaviour, IPointerDownHandler
+{
+    private Image image;
+    private GameController gameController;
+
+    public Color Color
+    {
+        set => image.color = value;
+        get => image.color;
+    }
+
+
+    public void Setup(GameController gameController)
+    {
+        image = GetComponent<Image>();
+        this.gameController = gameController;
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        gameController.CheckBlock(Color);
+    }
+}
