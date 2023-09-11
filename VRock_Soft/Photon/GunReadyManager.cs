badc02817ba49874af62f9b80683ed9e6b84b9dd
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-09-11 11:42:58 +0900

    Update GunReadyManager.cs

--- VRock_Soft/Photon/GunReadyManager.cs
+++ VRock_Soft/Photon/GunReadyManager.cs
@@ -41,12 +41,14 @@ public class GunReadyManager : MonoBehaviourPunCallbacks
         }
         GRM = this;
     }
+
     private void Start()
     {
         PN.SendRate = 60;
         PN.SerializationRate = 30;
         StartToServer();
     }
+
     public void StartToServer()                                                     // 서버연결 메서드
     {
         //PN.ConnectUsingSettings();                                                // 디폴트 연결
