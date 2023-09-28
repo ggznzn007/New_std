2750039a88b402fa4c89ccd4977e962a9edcc36e
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-09-28 15:43:22 +0900

    Create Utils.cs

---
+++ Supersell/Code/CloudRoad/Utils.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Utils:MonoBehaviour
+{   
+    public static float move_Speed = 7f;
+    public static float scroll_Speed = 2000.0f;
+
+    public static float minPosX = -22f; 
+    public static float maxPosX = 22f; 
+    public static float minPosZ = -4f; 
+    public static float maxPosZ = 4f; 
+
+    public static float maxZoom = 87.5f; 
+    public static float minZoom = 30f;    
+
+    /*public float Move_Speed() { return move_Speed; }
+    public float Scroll_Speed() { return scroll_Speed; }
+    public float Min_PosX() { return minPosX; }
+    public float Max_PosX() { return maxPosX; }
+    public float Min_PosZ() { return minPosZ; }
+    public float Max_PosZ() { return maxPosZ; }
+    public float Max_Zoom() { return maxZoom; }
+    public float Min_Zoom() { return minZoom; }*/
+}
