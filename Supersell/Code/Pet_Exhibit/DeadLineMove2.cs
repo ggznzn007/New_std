722f5d9d66159e9c2067617267c40bbe1526b08c
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-10-12 13:45:59 +0900

    Create DeadLineMove2.cs

---
+++ Supersell/Code/Pet_Exhibit/DeadLineMove2.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeadLineMove2 : MonoBehaviour
+{
+    public static DeadLineMove2 DLM2;
+    public float speed;
+    public float disSpeed;
+    public Vector3 startPoint;
+    public Vector3 endPoint;
+
+    private void Start()
+    {
+        DLM2 = this;
+        startPoint = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (FootPrint.FPM.isStart && !FootPrint.FPM.gamePaused)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, endPoint, speed / disSpeed);
+        }
+    }
+   
+   /* private void OnTriggerEnter(Collider coll)
+    {       
+        if (coll.CompareTag("Player"))
+        {            
+            FootPrint.FPM.FailWalk();
+        }
+    }*/
+}
