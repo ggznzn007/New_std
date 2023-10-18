55302e3f255c44261cac6cf11e036b7c319f1368
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-10-18 17:23:32 +0900

    Create Make_Button.cs

---
+++ Supersell/Code/Earth/Make_Button.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class Make_Button : MonoBehaviour
+{
+    public UnityEvent unityEvent = new();
+    public GameObject button;
+    void Start()
+    {
+        button = this.gameObject;
+    }
+
+    void Update()
+    {
+        if(!DataManager.DM.isInfoOpen)
+        {
+            if (DataManager.DM.isInfoOpen) { return; }
+            ButtonClick();
+        }
+    }
+
+   public void ButtonClick()
+    {        
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {            
+            if (Physics.Raycast(ray, out RaycastHit hit) && hit.collider.gameObject == gameObject)
+            {
+                unityEvent.Invoke();
+            }
+        }
+        
+    }
+
+  
+
+}
