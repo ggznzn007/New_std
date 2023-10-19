bccc0a137818b72a492be148ea9f23081252c8c5
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-10-19 16:23:22 +0900

    Create URL_Impot.cs

---
+++ Supersell/Code/Earth/URL_Impot.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URL_Impot : MonoBehaviour
+{
+   public void ImportingURL()
+    {
+        Application.OpenURL("https://earth.google.com/web/@-0.68496394,128.80936941,1166.51793291a,27985888.26438904d,35y,-0h,0t,0r/data=OgMKATA");
+    }
+}
