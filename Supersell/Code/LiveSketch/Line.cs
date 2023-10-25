a5422c34a89a3b94609f34080e5205ca2f212224
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-10-25 17:23:32 +0900

    Create Line.cs

---
+++ Supersell/Code/LiveSketch/Line.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Line : MonoBehaviour
+{
+    [SerializeField] private LineRenderer lineRenderer;
+
+    List<Vector2> points;
+
+    public void UpdateLine(Vector2 position)
+    {
+        if(points == null)
+        {
+            points = new List<Vector2>();
+            SetPoint(position);
+            return;
+        }
+
+        if(Vector2.Distance(points.Last(), position)>.1f)
+        {
+            SetPoint(position);
+        }
+    }
+
+    void SetPoint(Vector2 point)
+    {
+        points.Add(point);
+
+        lineRenderer.positionCount = points.Count;
+        lineRenderer.SetPosition(points.Count - 1, point);
+    }
+}
