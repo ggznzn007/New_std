67e33eb2ceb6c8f70d74f03c8f2c41dc8371c50a
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-10-25 17:23:46 +0900

    Create LineGenerator.cs

---
+++ Supersell/Code/LiveSketch/LineGenerator.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineGenerator : MonoBehaviour
+{
+    [SerializeField] private GameObject linePrefab;
+
+    Line activeLine;
+   
+    void Update()
+    {
+        DrawLines();
+    }
+   
+    public void DrawLines()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newLine = Instantiate(linePrefab);
+            activeLine = newLine.GetComponent<Line>();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+        }
+
+        if (activeLine != null)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            activeLine.UpdateLine(mousePos);
+        }
+    }
+}
