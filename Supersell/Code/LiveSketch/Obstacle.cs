0190ada6c39f7ca171eac3a446be7385ad33d68b
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-11-01 13:05:35 +0900

    Create Obstacle.cs

---
+++ Supersell/Code/LiveSketch/Obstacle.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Obstacle : MonoBehaviour, IComparable<Obstacle>
+{
+    public SpriteRenderer mySpriteRenderer { get; set; }
+
+    public int CompareTo(Obstacle other)
+    {
+        if (mySpriteRenderer.sortingOrder > other.mySpriteRenderer.sortingOrder)
+        {
+            return 1;
+        }
+        else if (mySpriteRenderer.sortingOrder < other.mySpriteRenderer.sortingOrder)
+        {
+            return -1;
+        }
+        return 0;
+    }
+
+    void Start()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+}
