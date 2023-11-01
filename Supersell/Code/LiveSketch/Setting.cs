70840411535942d8522c8ff7af0d023d744be23d
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-11-01 13:05:37 +0900

    Update Setting.cs

--- Supersell/Code/LiveSketch/Setting.cs
+++ Supersell/Code/LiveSketch/Setting.cs
@@ -2,19 +2,23 @@ using UnityEngine;
 public class Setting
 {
     public static float delayTime = 3;
+    public static float DelTime = 10;
     public static WaitForSeconds delay;
-    public static float speed = 5;
-    public static float startWaitTime = 1.5f;
-    public static float minX = -20f;
-    public static float maxX = 20f;
-    public static float minY = -20f;
-    public static float maxY = 20f;
+    public static float speed = 1.5f;
+    public static float speed_Baro = 1.2f;
+    public static float speed_Brac = 1.2f;
+    public static float startWaitTime = 1.2f;
+    public static float minX = -118f;
+    public static float maxX = 118f;
+    public static float minY = -17f;
+    public static float maxY = 0f;
     public static float minZ = 0f;
-    public static float maxZ = 10f;
+    public static float maxZ = 100f;
     public static float scale = 2f;
     public static float distance = 1;
     public static string movingSpots = "MovingSpotGroup";
     public static float posVal = 2.5f;
+    
 
     public static int[] RandomNumbers(int maxCount, int n)
     {
