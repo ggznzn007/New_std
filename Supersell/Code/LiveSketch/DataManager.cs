9228a22f5b96a93b83b31de6d7308060defae024
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-11-01 13:05:30 +0900

    Create DataManager.cs

---
+++ Supersell/Code/LiveSketch/DataManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager DM;
+    public int activeToco = 0;
+
+    void Start()
+    {
+        DM = this;
+        DontDestroyOnLoad(DM);
+    }
+
+}
