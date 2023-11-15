445c96eb83aef877c040ef54b66f2bdb2179051f
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-11-15 16:21:11 +0900

    Create ClickMouse.cs

---
+++ Supersell/Code/Earth/ClickMouse.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickMouse : MonoBehaviour
+{
+    float interval = 0.25f;
+    float doubleClickedTime = -1.0f;
+    bool isDoubleClicked = false;
+
+    private void OnMouseUp()
+    {
+        if ((Time.time - doubleClickedTime) < interval)
+        {
+            isDoubleClicked = true;
+            doubleClickedTime = -1.0f;
+        }
+        else
+        {
+            isDoubleClicked = false;
+            doubleClickedTime = Time.time;
+        }
+    }
+
+    void Update()
+    {
+        if (isDoubleClicked)
+        {
+            Debug.Log("double click");
+            isDoubleClicked = false;
+        }
+    }
+}
