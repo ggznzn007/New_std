5ea0e4847e35ad1b7a38a63171f3c00a46ce99f2
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-11-15 16:21:16 +0900

    Create Rot.cs

---
+++ Supersell/Code/Earth/Rot.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rot : MonoBehaviour
+{
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if(Input.GetKeyDown(KeyCode.Return))
+        //this.transform.rotation = new Quaternion(0,0,0,0);
+       //this.transform.localEulerAngles = new Vector3(0, 0, 90);
+    }
+}
