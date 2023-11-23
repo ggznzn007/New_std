a9f982ea12614410300c1de7388e414d2564ae93
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-11-23 11:50:23 +0900

    Create SceneFader.cs

---
+++ Supersell/Code/FourSeasons/SceneFader.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneFader : Singleton<SceneFader>
+{
+   
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
