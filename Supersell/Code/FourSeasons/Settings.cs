40c33502ec77d0f340c06363551f419d9fb94ae6
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-11-23 11:50:24 +0900

    Create Settings.cs

---
+++ Supersell/Code/FourSeasons/Settings.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings
+{
+    public static float SceneTime = 30;
+    public static float DelayTime = 5;    
+}
