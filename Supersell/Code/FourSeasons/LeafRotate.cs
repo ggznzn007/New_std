bf230e83a75377e5a27102b01ad5e2ffde87093b
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-11-23 11:50:20 +0900

    Create LeafRotate.cs

---
+++ Supersell/Code/FourSeasons/LeafRotate.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeafRotate : MonoBehaviour
+{
+    Vector3 startPos;
+    float rotSpeed = 70f;
+    LeafMove lM;
+    //float newRotSpeed;
+    void Start()
+    {
+        startPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        lM = this.transform.parent.GetComponent<LeafMove>();
+        //newRotSpeed = rotSpeed;
+    }
+
+    void Update()
+    {
+       
+        if (startPos != transform.position)
+        {
+            if (Vector3.Distance(transform.position, startPos) < 10)
+            {               
+               transform.parent.Rotate(Vector3.up, rotSpeed * Time.deltaTime);
+                rotSpeed = 70;
+                StartCoroutine(DelRot());
+            }
+            else
+            {
+                this.transform.Rotate(Vector3.forward, rotSpeed * Time.deltaTime);
+                rotSpeed = 1;
+            }
+            
+        }
+        
+    }
+
+    IEnumerator DelRot()
+    {
+        yield return new WaitForSeconds(0.8f);
+        startPos = this.transform.position;
+        //this.transform.Rotate(Vector3.forward, rotSpeed * Time.deltaTime);
+    }
+
+    IEnumerator DelayStopRotate()
+    {       
+        yield return new WaitForSeconds(2);
+        rotSpeed = 90;
+    }
+
+  /*  private void OnTriggerEnter(Collider coll)
+    {
+        if(coll.CompareTag("Player"))
+        {
+            rotSpeed = 70;          
+            Debug.Log("플레이어 태그 시작");
+        }       
+    }
+
+    private void OnTriggerStay(Collider coll)
+    {
+        if (coll.CompareTag("Player"))
+        {
+            rotSpeed = 70;
+            Debug.Log("플레이어 태그 중");
+        }
+    }
+
+    private void OnTriggerExit(Collider coll)
+    {
+        if(coll.CompareTag("Player"))
+        {
+            rotSpeed = 0;
+            Debug.Log("플레이어 아웃");
+        }
+    }*/
+}
