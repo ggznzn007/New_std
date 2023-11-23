e4782025dfb089846631fcac3d87e85389552d71
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-11-23 11:50:26 +0900

    Create Singleton.cs

---
+++ Supersell/Code/FourSeasons/Singleton.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = (T)FindObjectOfType(typeof(T));
+
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject(typeof(T).Name, typeof(T));
+                    instance = obj.GetComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (transform.parent != null && transform.root != null)
+        {
+            DontDestroyOnLoad(this.transform.root.gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    }
+}
