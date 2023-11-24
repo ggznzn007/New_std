fb62815486f2936dd13376213ca217b3c6461533
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-11-24 16:02:31 +0900

    Update Settings.cs

--- Supersell/Code/FourSeasons/Settings.cs
+++ Supersell/Code/FourSeasons/Settings.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class Settings
 {
-    public static float SceneTime = 30;
+    public static float SceneTime_Spring = 10;
+    public static float SceneTime_Summer = 10;
+    public static float SceneTime_Fall = 10;
+    public static float SceneTime_Winter = 10;
+    public static float SceneTime_Fish = 10;
+    public static float SceneTime = 10;
     public static float DelayTime = 5;    
 }
