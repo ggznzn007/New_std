33943c13e23444d5dcdee977ab464862b963bdf4
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-11-24 16:02:28 +0900

    Create Setting.cs

---
+++ Supersell/Code/FourSeasons/Setting.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Setting 
+{
+    public static float size1 = 3;
+    public static float size2 = 4;
+    public static float size3 = 5;
+    public static float size4 = 6;
+    public static float defaultSize = 4;
+}
