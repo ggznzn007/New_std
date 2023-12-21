4506fb28f4af67d85facc1575a3bc94ee7351d31
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-12-21 15:56:30 +0900

    Update Control_Brac.cs

--- Supersell/Code/LiveSketch/Control_Brac.cs
+++ Supersell/Code/LiveSketch/Control_Brac.cs
@@ -18,10 +18,8 @@ public class Control_Brac : PlayerControl
         moveSpot = mySp.GetComponent<Transform>();
         anim = GetComponent<Animator>();
 
-
         moveSpot.position = new Vector3(Random.Range(Setting.minX, Setting.maxX), Random.Range(Setting.minY, Setting.maxY)
          , Random.Range(Setting.minZ, Setting.maxZ));
-
     }
 
     private void Update()
@@ -38,33 +36,17 @@ public class Control_Brac : PlayerControl
             }
             else
             {
-                waitTime -= Time.deltaTime;
-                // anim.SetBool("IsRight", false);
-                // anim.SetBool("IsLeft", false);
+                waitTime -= Time.deltaTime;               
             }
         }
 
         if (moveSpot.position.x < transform.position.x)
         {
-            sr.flipY = false;
-            //sr.flipY = false;
-            // anim.SetBool("IsRight", true);
-            // anim.SetBool("IsLeft", false);
+            sr.flipY = false;          
         }
         else if (moveSpot.position.x > transform.position.x)
         {
-            sr.flipY = true;
-            // sr.flipY = true;
-            //  anim.SetBool("IsRight", false);
-            // anim.SetBool("IsLeft", true);
+            sr.flipY = true;          
         }
-
-       // StartCoroutine(DeActive_Brac());
-    }
-
-    public IEnumerator DeActive_Brac()
-    {
-        yield return new WaitForSeconds(Setting.DelTime);
-        ObjectPool.OP.ReturnToPool_Brac(gameObject);
     }
 }
