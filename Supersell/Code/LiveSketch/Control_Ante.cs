a0617a98d882b7c2bfeb117e097a9f1b206bc087
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-12-21 15:55:39 +0900

    Update Control_Ante.cs

--- Supersell/Code/LiveSketch/Control_Ante.cs
+++ Supersell/Code/LiveSketch/Control_Ante.cs
@@ -2,9 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.U2D.Animation;
+using UnityEngine.UI;
 
 public class Control_Ante : PlayerControl
 {
+   
+    public float speed =1;
     private void Awake()
     {
         sr = GetComponent<SpriteRenderer>();
@@ -18,16 +21,14 @@ public class Control_Ante : PlayerControl
         moveSpot = mySp.GetComponent<Transform>();
         anim = GetComponent<Animator>();
 
-
         moveSpot.position = new Vector3(Random.Range(Setting.minX, Setting.maxX), Random.Range(Setting.minY, Setting.maxY)
-          , Random.Range(Setting.minZ, Setting.maxZ));
-
-    }
+          , Random.Range(Setting.minZ, Setting.maxZ));        
+    }   
 
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, moveSpot.position, Setting.speed * Time.deltaTime);
-
+        transform.position = Vector3.MoveTowards(transform.position, moveSpot.position, speed * Time.deltaTime);
+        
         if (Vector2.Distance(transform.position, moveSpot.position) < Setting.distance)
         {
             if (waitTime <= 0)
@@ -38,33 +39,17 @@ public class Control_Ante : PlayerControl
             }
             else
             {
-                waitTime -= Time.deltaTime;
-                // anim.SetBool("IsRight", false);
-                // anim.SetBool("IsLeft", false);
+                waitTime -= Time.deltaTime;             
             }
         }
 
         if (moveSpot.position.x < transform.position.x)
         {
-            sr.flipY = false;
-            //sr.flipY = false;
-            // anim.SetBool("IsRight", true);
-            // anim.SetBool("IsLeft", false);
+            sr.flipY = false;          
         }
         else if (moveSpot.position.x > transform.position.x)
         {
-            sr.flipY = true;
-            // sr.flipY = true;
-            //  anim.SetBool("IsRight", false);
-            // anim.SetBool("IsLeft", true);
+            sr.flipY = true;           
         }
-
-        //StartCoroutine(DeActive_Ante());
-    }
-
-    public IEnumerator DeActive_Ante()
-    {
-        yield return new WaitForSeconds(Setting.DelTime);
-        ObjectPool.OP.ReturnToPool_Ante(gameObject);
     }
 }
