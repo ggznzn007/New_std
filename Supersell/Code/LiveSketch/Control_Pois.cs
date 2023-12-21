2db8902719ec7d370dc38157c65b4d3f6b221013
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-12-21 15:56:19 +0900

    Update Control_Pois.cs

--- Supersell/Code/LiveSketch/Control_Pois.cs
+++ Supersell/Code/LiveSketch/Control_Pois.cs
@@ -21,7 +21,6 @@ public class Control_Pois : PlayerControl
 
         moveSpot.position = new Vector3(Random.Range(Setting.minX, Setting.maxX), Random.Range(Setting.minY, Setting.maxY)
           , Random.Range(Setting.minZ, Setting.maxZ));
-
     }
 
     private void Update()
@@ -38,33 +37,17 @@ public class Control_Pois : PlayerControl
             }
             else
             {
-                waitTime -= Time.deltaTime;
-                // anim.SetBool("IsRight", false);
-                // anim.SetBool("IsLeft", false);
+                waitTime -= Time.deltaTime;               
             }
         }
 
         if (moveSpot.position.x < transform.position.x)
         {
-            sr.flipX = true;
-            //sr.flipY = false;
-            // anim.SetBool("IsRight", true);
-            // anim.SetBool("IsLeft", false);
+            sr.flipX = true;           
         }
         else if (moveSpot.position.x > transform.position.x)
         {
-            sr.flipX = false;
-            // sr.flipY = true;
-            //  anim.SetBool("IsRight", false);
-            // anim.SetBool("IsLeft", true);
-        }
-
-      //  StartCoroutine(DeActive_Pois());
-    }
-
-    public IEnumerator DeActive_Pois()
-    {
-        yield return new WaitForSeconds(Setting.DelTime);
-        ObjectPool.OP.ReturnToPool_Pois(gameObject);
+            sr.flipX = false;           
+        }    
     }
 }
