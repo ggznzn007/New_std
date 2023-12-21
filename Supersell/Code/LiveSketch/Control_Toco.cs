7f082baf050af940b01493fdc24f582c21f6a235
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-12-21 15:56:08 +0900

    Update Control_Toco.cs

--- Supersell/Code/LiveSketch/Control_Toco.cs
+++ Supersell/Code/LiveSketch/Control_Toco.cs
@@ -14,53 +14,11 @@ public class Control_Toco : PlayerControl
     }
     private void Start()
     {
-        mySp = Instantiate(mySpot).transform;
-        moveSpot = mySp.GetComponent<Transform>();
         anim = GetComponent<Animator>();
-
-        /*
-                moveSpot.position = new Vector3(Random.Range(Setting.minX, Setting.maxX), Random.Range(Setting.minY, Setting.maxY)
-                    , Random.Range(Setting.minZ, Setting.maxZ));*/
-
     }
 
     private void Update()
     {
-        /*  transform.position = Vector3.MoveTowards(transform.position, moveSpot.position, Setting.speed_Baro * Time.deltaTime);
-
-          if (Vector2.Distance(transform.position, moveSpot.position) < Setting.distance)
-          {
-              if (waitTime <= 0)
-              {
-                  moveSpot.position = new Vector3(Random.Range(Setting.minX, Setting.maxX), Random.Range(Setting.minY, Setting.maxY)
-                      , Random.Range(Setting.minZ, Setting.maxZ));
-                  waitTime = Setting.startWaitTime;
-              }
-              else
-              {
-                  waitTime -= Time.deltaTime;
-                  // anim.SetBool("IsRight", false);
-                  // anim.SetBool("IsLeft", false);
-              }
-          }
-
-          if (moveSpot.position.x < transform.position.x)
-          {
-              sr.flipX = true;
-              //sr.flipY = false;
-              // anim.SetBool("IsRight", true);
-              // anim.SetBool("IsLeft", false);
-          }
-          else if (moveSpot.position.x > transform.position.x)
-          {
-              sr.flipX = false;
-              // sr.flipY = true;
-              //  anim.SetBool("IsRight", false);
-              // anim.SetBool("IsLeft", true);
-          }*/
-
-      //   StartCoroutine(DeActive());
-    }
-
     
+    }    
 }
