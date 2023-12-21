81528c74c869c8765f5f6677ea6dba2870487e87
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2023-12-21 15:56:01 +0900

    Update Control_Slot.cs

--- Supersell/Code/LiveSketch/Control_Slot.cs
+++ Supersell/Code/LiveSketch/Control_Slot.cs
@@ -13,58 +13,12 @@ public class Control_Slot : PlayerControl
         waitTime = Setting.startWaitTime;
     }
     private void Start()
-    {
-        mySp = Instantiate(mySpot).transform;
-        moveSpot = mySp.GetComponent<Transform>();
+    {       
         anim = GetComponent<Animator>();
-
-        /*
-                moveSpot.position = new Vector3(Random.Range(Setting.minX, Setting.maxX), Random.Range(Setting.minY, Setting.maxY)
-                    , Random.Range(Setting.minZ, Setting.maxZ));*/
-
     }
 
     private void Update()
     {
-        /*  transform.position = Vector3.MoveTowards(transform.position, moveSpot.position, Setting.speed_Baro * Time.deltaTime);
-
-          if (Vector2.Distance(transform.position, moveSpot.position) < Setting.distance)
-          {
-              if (waitTime <= 0)
-              {
-                  moveSpot.position = new Vector3(Random.Range(Setting.minX, Setting.maxX), Random.Range(Setting.minY, Setting.maxY)
-                      , Random.Range(Setting.minZ, Setting.maxZ));
-                  waitTime = Setting.startWaitTime;
-              }
-              else
-              {
-                  waitTime -= Time.deltaTime;
-                  // anim.SetBool("IsRight", false);
-                  // anim.SetBool("IsLeft", false);
-              }
-          }
-
-          if (moveSpot.position.x < transform.position.x)
-          {
-              sr.flipX = true;
-              //sr.flipY = false;
-              // anim.SetBool("IsRight", true);
-              // anim.SetBool("IsLeft", false);
-          }
-          else if (moveSpot.position.x > transform.position.x)
-          {
-              sr.flipX = false;
-              // sr.flipY = true;
-              //  anim.SetBool("IsRight", false);
-              // anim.SetBool("IsLeft", true);
-          }*/
-
-      //  StartCoroutine(DeActive_Cham());
-    }
-
-    public IEnumerator DeActive_Cham()
-    {
-        yield return new WaitForSeconds(Setting.DelTime);
-        ObjectPool.OP.ReturnToPool_Cham(gameObject);
+      
     }
 }
