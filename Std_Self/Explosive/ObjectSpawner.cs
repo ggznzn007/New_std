e2aa8d8d2c90d98fc2c0b6c6c173e003c84e25bb
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-05-07 15:38:46 +0900

    Create ObjectSpawner.cs

---
+++ Std_Self/Explosive/ObjectSpawner.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject[] objects;
+    [SerializeField] private Vector2 minPoint = new(-14f, -14f);
+    [SerializeField] private Vector2 maxPoint = new(14f, 14f);
+
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.Return))
+        {
+            SpawnObject(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnObject(1);
+        }
+    }
+
+    void SpawnObject(int index)
+    {
+        float x = Random.Range(minPoint.x, maxPoint.x);
+        float y = 10f;
+        float z = Random.Range(minPoint.y, maxPoint.y);
+        Color color = Random.ColorHSV();
+
+        GameObject clone = Instantiate(objects[index], new Vector3(x,y,z),Quaternion.identity);
+
+        clone.GetComponent<MeshRenderer>().material.color = color;
+
+        if(clone.TryGetComponent<TrailRenderer>(out var trail))
+        {
+            trail.startColor = color;
+            trail.endColor = color;
+        }
+    }
+}
