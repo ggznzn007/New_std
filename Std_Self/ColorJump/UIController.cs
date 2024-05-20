7f2f971ae4d222dc7ee27b0367f201d2a7d82351
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-05-20 15:49:00 +0900

    Create UIController.cs

---
+++ Std_Self/ColorJump/UIController.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using TMPro;
+
+public class UIController : MonoBehaviour
+{
+    [Header("게임메인")][SerializeField] private GameObject mainPanel;
+
+    [Header("게임중")]
+    [SerializeField] private GameObject inGamePanel;
+    [SerializeField] private TextMeshProUGUI textScore;
+
+    [Header("게임오버")]
+    [SerializeField] private GameObject gameOverPanel;
+    [SerializeField] private TextMeshProUGUI textHighScore;
+
+    [Header("볼륨옵션")]
+    [SerializeField] private GameObject volumePanel;
+
+    public void GameStart()
+    {
+        mainPanel.SetActive(false);
+        inGamePanel.SetActive(true);
+    }
+
+    public void UpdateScore(int score)
+    {
+        if(score<10)
+        {
+            textScore.text = score.ToString("D2");
+        }
+        else
+        {
+            textScore.text = score.ToString();
+        }
+    }
+
+    public void GameOver()
+    {
+        gameOverPanel.SetActive(true);
+
+        textHighScore.text = $"최고기록 : {PlayerPrefs.GetInt("HighScore")}";
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            GameController.GC.isPlaying = !GameController.GC.isPlaying;
+        }
+        if(!GameController.GC.isPlaying)
+        {
+            volumePanel.SetActive(true);            
+        }
+        else
+        {
+            volumePanel.SetActive(false);            
+        }
+    }
+}
