51def2da924a4a765a5bf0505ee1c9775a8bf714
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-05-23 17:02:20 +0900

    Create LobbyScenario.cs

---
+++ TheBackend_std/#03Lobby/LobbyScenario.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class LobbyScenario : MonoBehaviour
+{
+	[SerializeField]
+	private	UserInfo user;
+
+	private void Awake()
+	{
+		user.GetUserInfoFromBackend();
+	}
+	
+	private void Start()
+	{
+		BackendGameData.Instance.GameDataLoad();
+	}
+}
+
