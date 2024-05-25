cbefe9b196da98dd410407b9feb86c5ba5928df9
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-05-25 14:18:49 +0900

    Create DestroyByPosition.cs

---
+++ TheBackend_std/#04Game/DestroyByPosition.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class DestroyByPosition : MonoBehaviour
+{
+	[SerializeField]
+	private	StageData	stageData;
+	private	float		destroyWeight = 2;
+
+	private void LateUpdate()
+	{
+		if ( transform.position.y < stageData.LimitMin.y - destroyWeight ||
+			 transform.position.y > stageData.LimitMax.y + destroyWeight ||
+			 transform.position.x < stageData.LimitMin.x - destroyWeight ||
+			 transform.position.x > stageData.LimitMax.x + destroyWeight )
+		{
+			Destroy(gameObject);
+		}
+	}
+}
+
