ac1c60b2b5bf396a6c63ee61e6c1c210d48750ca
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-05-25 14:18:58 +0900

    Create Meteorite.cs

---
+++ TheBackend_std/#04Game/Meteorite.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Meteorite : MonoBehaviour
+{
+	private	GameController	gameController;
+
+	public void Setup(GameController gameController)
+	{
+		this.gameController = gameController;
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if ( collision.CompareTag("Player") )
+		{
+			gameController.GameOver();
+		}
+	}
+}
+
