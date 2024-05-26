af4be28561d7e12c6708646a3a65f94846be31cb
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-05-26 16:41:29 +0900

    Create Constants.cs

---
+++ TheBackend_std/#00Common/Constants.cs
@@ -0,0 +1,9 @@
+public static class Constants
+{
+	public static readonly string	USER_DATA_TABLE	= "USER_DATA";
+	public static readonly string	DAILY_RANK_UUID	= "0fa14490-085a-11ee-b506-7d5eaba96eff";
+	public static readonly int		MAX_RANK_LIST	= 20;
+
+	public static readonly string	LEVEL_CHART		= "82616";
+}
+
