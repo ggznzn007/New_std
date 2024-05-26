f939300f2362683b9a527bc95aafdf3bed7a49d1
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-05-26 16:42:07 +0900

    Update Enemy.cs

--- TheBackend_std/#04Game/Enemy.cs
+++ TheBackend_std/#04Game/Enemy.cs
@@ -2,19 +2,34 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-	private	GameController	gameController;
+    [SerializeField]
+    private int scorePoint = 100;       // 적을 처치했을 때 획득하는 점수
+    [SerializeField]
+    private GameObject explosionPrefab;     // 폭발 이펙트 프리팹
+    private GameController gameController;
 
-	public void Setup(GameController gameController)
-	{
-		this.gameController = gameController;
-	}
+    public void Setup(GameController gameController)
+    {
+        this.gameController = gameController;
+    }
 
-	private void OnTriggerEnter2D(Collider2D collision)
-	{
-		if ( collision.CompareTag("Player") )
-		{
-			gameController.GameOver();
-		}
-	}
+    public void OnDie()
+    {
+        // 폭발 이펙트 생성
+        Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+        // 플레이어의 점수를 scorePoint만큼 증가
+        gameController.Score += scorePoint;
+        // 적 캐릭터 삭제
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            OnDie();
+            gameController.GameOver();
+        }
+    }
 }
 
