9d8473d63ef13b1c7edbdfece11c5fa72c78ec9b
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-05-26 16:42:03 +0900

    Update PlayerProjectile.cs

--- TheBackend_std/#04Game/PlayerProjectile.cs
+++ TheBackend_std/#04Game/PlayerProjectile.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class PlayerProjectile : MonoBehaviour
 {
-	private void OnTriggerEnter2D(Collider2D collision)
-	{
-		if ( collision.CompareTag("Enemy") )
-		{
-			Destroy(collision.gameObject);
-			Destroy(gameObject);
-		}
-	}
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            collision.GetComponent<Enemy>().OnDie();
+
+            Destroy(gameObject);
+        }
+    }
 }
 
