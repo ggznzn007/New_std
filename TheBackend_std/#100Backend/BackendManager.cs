1247b343fa372099c4bac8c52ce62c9a4b0a91db
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-05-26 16:42:14 +0900

    Update BackendManager.cs

--- TheBackend_std/#100Backend/BackendManager.cs
+++ TheBackend_std/#100Backend/BackendManager.cs
@@ -3,41 +3,51 @@ using BackEnd;		// 뒤끝 SDK
 
 public class BackendManager : MonoBehaviour
 {
-	private void Awake()
-	{
-		// Update() 메소드의 Backend.AsyncPoll(); 호출을 위해 오브젝트를 파괴하지 않는다
-		DontDestroyOnLoad(gameObject);
+    private void Awake()
+    {
+        // Update() 메소드의 Backend.AsyncPoll(); 호출을 위해 오브젝트를 파괴하지 않는다
+        DontDestroyOnLoad(gameObject);
 
-		// 뒤끝 서버 초기화
-		BackendSetup();
-	}
+        // 뒤끝 서버 초기화
+        BackendSetup();
+    }
 
-	private void Update()
-	{
-		// 서버의 비동기 메소드 호출(콜백 함수 풀링)을 위해 작성
-		// 참고 : https://developer.thebackend.io/unity3d/guide/Async/AsyncFuncPoll/
-		if ( Backend.IsInitialized )
-		{
-			Backend.AsyncPoll();
-		}
-	}
+    private void Update()
+    {
+        // 서버의 비동기 메소드 호출(콜백 함수 풀링)을 위해 작성
+        // 참고 : https://developer.thebackend.io/unity3d/guide/Async/AsyncFuncPoll/
+        if (Backend.IsInitialized)
+        {
+            Backend.AsyncPoll();
+        }
 
-	private void BackendSetup()
-	{
-		// 뒤끝 초기화 (콜백 함수 풀링을 사용하려면 매개변수를 true로 설정)
-		var bro = Backend.Initialize(true);
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit(); // 어플리케이션 종료
+#endif
 
-		// 뒤끝 초기화에 대한 응답값
-		if ( bro.IsSuccess() )
-		{
-			// 초기화 성공 시 statusCode 204 Success
-			Debug.Log($"초기화 성공 : {bro}");
-		}
-		else
-		{
-			// 초기화 실패 시 statusCode 400대 에러 발생
-			Debug.LogError($"초기화 실패 : {bro}");
-		}
-	}
+        }
+    }
+
+    private void BackendSetup()
+    {
+        // 뒤끝 초기화 (콜백 함수 풀링을 사용하려면 매개변수를 true로 설정)
+        var bro = Backend.Initialize(true);
+
+        // 뒤끝 초기화에 대한 응답값
+        if (bro.IsSuccess())
+        {
+            // 초기화 성공 시 statusCode 204 Success
+            Debug.Log($"초기화 성공 : {bro}");
+        }
+        else
+        {
+            // 초기화 실패 시 statusCode 400대 에러 발생
+            Debug.LogError($"초기화 실패 : {bro}");
+        }
+    }
 }
 
