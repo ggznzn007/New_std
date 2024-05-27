5b4a679b3013368824fed303fff77550537cff21
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-05-27 15:14:45 +0900

    Update LobbyScenario.cs

--- TheBackend_std/#03Lobby/LobbyScenario.cs
+++ TheBackend_std/#03Lobby/LobbyScenario.cs
@@ -2,17 +2,18 @@ using UnityEngine;
 
 public class LobbyScenario : MonoBehaviour
 {
-	[SerializeField]
-	private	UserInfo user;
-
-	private void Awake()
-	{
-		user.GetUserInfoFromBackend();
-	}
+    [SerializeField]
+    private UserInfo user;
+    
+    private void Awake()
+    {
+        user.GetUserInfoFromBackend();
+    }
 
     private void Start()
     {
         BackendGameData.Instance.GameDataLoad();
     }
+  
 }
 
