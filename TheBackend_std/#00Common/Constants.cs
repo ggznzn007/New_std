26d4964972b75f129c373fb4a794a79fdb71b9a6
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-05-27 15:14:56 +0900

    Update Constants.cs

--- TheBackend_std/#00Common/Constants.cs
+++ TheBackend_std/#00Common/Constants.cs
@@ -1,9 +1,12 @@
 public static class Constants
 {
 	public static readonly string	USER_DATA_TABLE	= "USER_DATA";
-	public static readonly string	DAILY_RANK_UUID	= "0fa14490-085a-11ee-b506-7d5eaba96eff";
-	public static readonly int		MAX_RANK_LIST	= 20;
+	public static readonly string	DAILY_RANK_UUID	= "726b3350-199e-11ef-9daf-a73703f3dc37";
+	public static readonly string   TOTAL_RANK_UUID = "321e27a0-1a69-11ef-a7bb-a1c62064f2a4";
+	public static readonly int      MAX_RANK_LIST = 15;
 
-	public static readonly string	LEVEL_CHART		= "82616";
+	public static readonly string LEVEL_CHART = "123376";
+
+	public static readonly string GOODS_CHART_NAME = "재화차트";
 }
 
