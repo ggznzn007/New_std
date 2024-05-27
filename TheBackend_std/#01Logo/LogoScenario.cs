d8fff1ebdeffd37442d605ab8884879d97f1f3df
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-05-27 13:10:52 +0900

    Update LogoScenario.cs

--- TheBackend_std/#01Logo/LogoScenario.cs
+++ TheBackend_std/#01Logo/LogoScenario.cs
@@ -2,36 +2,34 @@ using UnityEngine;
 
 public class LogoScenario : MonoBehaviour
 {
-	[SerializeField]
-	private	Progress	progress;
-	[SerializeField]
-	private	SceneNames	nextScene;
+    [SerializeField] private Progress progress;
+    [SerializeField] private SceneNames nextScene;
 
-	private void Awake()
-	{
-		SystemSetup();
-	}
+    private void Awake()
+    {
+        SystemSetup();
+    }
 
-	private void SystemSetup()
-	{
-		// 활성화되지 않은 상태에서도 게임이 계속 진행
-		Application.runInBackground = true;
+    private void SystemSetup()
+    {
+        // 활성화되지 않은 상태에서도 게임이 계속 진행
+        Application.runInBackground = true;
 
-		// 해상도 설정 (9:18.5, 1440x2960, 갤럭시 노트 8)
-		int width	= Screen.width;
-		int height	= (int)(Screen.width * 18.5f / 9);
-		Screen.SetResolution(width, height, true);
+        // 해상도 설정 (9:18.5, 1440x2960, 갤럭시 노트 8)
+        int width = Screen.width;
+        int height = (int)(Screen.width * 16 / 9);
+        Screen.SetResolution(width, height, true);
 
-		// 화면이 꺼지지 않도록 설정
-		Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        // 화면이 꺼지지 않도록 설정
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
 
-		// 로딩 애니메이션 시작, 재생 완료시 OnAfterProgress() 메소드 실행
-		progress.Play(OnAfterProgress);
-	}
+        // 로딩 애니메이션 시작, 재생 완료시 OnAfterProgress() 메소드 실행
+        progress.Play(OnAfterProgress);
+    }
 
-	private void OnAfterProgress()
-	{
-		Utils.LoadScene(nextScene);
-	}
+    private void OnAfterProgress()
+    {
+        Utils.LoadScene(nextScene);
+    }
 }
 
