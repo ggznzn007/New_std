22f7001c4adbd109e1e36bad41ea755b0d6f7ae3
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-05-30 15:52:07 +0900

    Update FriendData.cs

--- TheBackend_std/#100Backend/FriendData.cs
+++ TheBackend_std/#100Backend/FriendData.cs
@@ -2,16 +2,22 @@ public class FriendData
 {
 	public	string	nickname;	// 닉네임
 	public	string	inDate;		// 해당 유저의 inDate
-	public	string	createdAt;	// 친구 요청 보낸 시간 / 친구 요청 받은 시간 / 친구가 된 시간
+	public	string	createdAt;  // 친구 요청 보낸 시간 / 친구 요청 받은 시간 / 친구가 된 시간
+    public string lastLogin;	// 마지막 접속 날짜
 
-	public override string ToString()
+    // level과 같이 출력하고 싶은 유저정보가 있으면 추가
+    public string level;        // 해당 유저의 level
+
+    public override string ToString()
 	{
 		string result = string.Empty;
 		result += $"닉네임 : {nickname}\n";
 		result += $"inDate : {inDate}\n";
 		result += $"createdAt : {createdAt}\n";
+        result += $"lastLogin : {lastLogin}\n";
+        result += $"level : {level}\n";
 
-		return result;
+        return result;
 	}
 }
 
