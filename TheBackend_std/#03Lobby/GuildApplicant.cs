f8b83c53b90b2c53cea12d943c2e2c5fb838f273
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-06-01 13:53:06 +0900

    Create GuildApplicant.cs

---
+++ TheBackend_std/#03Lobby/GuildApplicant.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using TMPro;
+
+public class GuildApplicant : MonoBehaviour
+{
+	[SerializeField]
+	private	TextMeshProUGUI		textNickname;
+	[SerializeField]
+	private	TextMeshProUGUI		textLevel;
+
+	private	BackendGuildSystem	backendGuildSystem;
+	private	GuildApplicantsPage	guildApplicantsPage;
+	private	GuildMemberData		guildMemberData;
+
+	public void Setup(BackendGuildSystem guildSystem, GuildApplicantsPage applicantsPage, GuildMemberData memberData)
+	{
+		textNickname.text	= memberData.nickname;
+		textLevel.text		= $"Lv. {memberData.level}";
+
+		backendGuildSystem	= guildSystem;
+		guildApplicantsPage	= applicantsPage;
+		guildMemberData		= memberData;
+	}
+
+	public void OnClickApproveApplicant()
+	{
+		// 해당 유저의 UI 오브젝트 삭제
+		guildApplicantsPage.Deactivate(gameObject);
+		// 해당 유저 길드 가입 요청 수락
+		backendGuildSystem.ApproveApplicant(guildMemberData.inDate);
+	}
+
+	public void OnClickRejectApplicant()
+	{
+		// 해당 유저의 UI 오브젝트 삭제
+		guildApplicantsPage.Deactivate(gameObject);
+		// 해당 유저 길드 가입 요청 거절
+		backendGuildSystem.RejectApplicant(guildMemberData.inDate);
+	}
+}
+
