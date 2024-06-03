7fc7c7ea6f8f30767aa8bb947ad680e74e2295a6
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-06-03 18:46:38 +0900

    Update GuildMemberData.cs

--- TheBackend_std/#100Backend/GuildMemberData.cs
+++ TheBackend_std/#100Backend/GuildMemberData.cs
@@ -1,18 +1,24 @@
 public class GuildMemberData
 {
-	public	string	nickname;	// 닉네임
-	public	string	inDate;		// 해당 유저의 inDate
-	public	string	level;		// 해당 유저의 level
+    public string nickname; // 닉네임
+    public string inDate;       // 해당 유저의 inDate
+    public string level;        // 해당 유저의 level
+    public string position; // 길드 직책
+    public int goodsCount;  // 굿즈 개수
+    public string lastLogin;    // 마지막 접속 날짜
 
-	public override string ToString()
-	{
-		string result = string.Empty;
-		result += $"nickname : {nickname}\n";
-		result += $"inDate : {inDate}\n";
-		result += $"level : {level}\n";
+    public override string ToString()
+    {
+        string result = string.Empty;
+        result += $"nickname : {nickname}\n";
+        result += $"inDate : {inDate}\n";
+        result += $"level : {level}\n";
+        result += $"position : {position}\n";
+        result += $"goodsCount : {goodsCount}\n";
+        result += $"lastLogin : {lastLogin}\n";
 
-		return result;
-	}
+        return result;
+    }
 }
 
 
