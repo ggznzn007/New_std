5bb69a8389cedd920a853d3557572c74ebc66eb5
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-06-03 18:46:29 +0900

    Update Constants.cs

--- TheBackend_std/#00Common/Constants.cs
+++ TheBackend_std/#00Common/Constants.cs
@@ -10,5 +10,7 @@ public static class Constants
 	public static readonly string GOODS_CHART_NAME = "재화차트";
 
     public static readonly int EXPIRATION_DAYS = 3;
+
+    public static readonly string RANDOM_GUILD_UUID = "ea6bade0-199d-11ef-a200-ab9b7a37ba3e";
 }
 
