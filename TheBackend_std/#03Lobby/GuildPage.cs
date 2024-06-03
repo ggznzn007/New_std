19788458d03eebbafdee903b34e93729c68b24f1
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-06-03 18:46:32 +0900

    Update GuildPage.cs

--- TheBackend_std/#03Lobby/GuildPage.cs
+++ TheBackend_std/#03Lobby/GuildPage.cs
@@ -3,24 +3,71 @@ using TMPro;
 
 public class GuildPage : MonoBehaviour
 {
-	[SerializeField]
-	private	BackendGuildSystem	backendGuildSystem;
-	[SerializeField]
-	private	TextMeshProUGUI		textGuildName;				// Popup 상단에 출력되는 길드 이름 Text UI
+    [SerializeField]
+    private BackendGuildSystem backendGuildSystem;
+    [SerializeField]
+    private TextMeshProUGUI textGuildName;              // Popup 상단에 출력되는 길드 이름 Text UI
+    [SerializeField]
+    private Notice notice;
+    [SerializeField]
+    private GameObject executivesOption;
+    [SerializeField]
+    private TextMeshProUGUI textMemberCount;
 
-	private	string				guildName = string.Empty;	// 길드 이름
+    [SerializeField]
+    private GameObject memberPrefab;
+    [SerializeField]
+    private Transform parentContent;
 
-	public void Activate(string guildName)
-	{
-		gameObject.SetActive(true);
+    private string guildName = string.Empty;    // 길드 이름
+    private MemoryPool memoryPool;
 
-		textGuildName.text	= guildName;
-		this.guildName		= guildName;
-	}
+    private void Awake()
+    {
+        memoryPool = new MemoryPool(memberPrefab, parentContent);
+    }
 
-	public void OnClickApplyGuild()
-	{
-		backendGuildSystem.ApplyGuild(guildName);
-	}
+    public void Setup(string guildName, bool isMaster = false, bool isOtherGuild = false)
+    {
+        notice.Setup(isMaster, isOtherGuild);
+        executivesOption.SetActive(isMaster);
+
+        gameObject.SetActive(true);
+
+        textGuildName.text = guildName;
+        this.guildName = guildName;
+
+        if (isOtherGuild == true)
+        {
+            textMemberCount.text = $"길드 인원 {backendGuildSystem.otherGuildData.memberCount}/100";
+            backendGuildSystem.GetGuildMemeberList(backendGuildSystem.otherGuildData.guildInDate);
+        }
+        else
+        {
+            textMemberCount.text = $"길드 인원 {backendGuildSystem.myGuildData.memberCount}/100";
+            backendGuildSystem.GetGuildMemeberList(backendGuildSystem.myGuildData.guildInDate);
+        }
+    }
+
+    public void Activate(GuildMemberData member)
+    {
+        GameObject item = memoryPool.ActivatePoolItem();
+        item.GetComponent<GuildMember>().Setup(member);
+    }
+
+    public void Deactivate(GameObject member)
+    {
+        memoryPool.DeactivatePoolItem(member);
+    }
+
+    public void DeactivateAll()
+    {
+        memoryPool.DeactivateAllPoolItems();
+    }
+
+    public void OnClickApplyGuild()
+    {
+        backendGuildSystem.ApplyGuild(guildName);
+    }
 }
 
