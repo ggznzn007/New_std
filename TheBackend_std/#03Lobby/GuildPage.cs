802b55c4151763218a2a8e135189574cd87d3de7
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-06-06 14:11:53 +0900

    Update GuildPage.cs

--- TheBackend_std/#03Lobby/GuildPage.cs
+++ TheBackend_std/#03Lobby/GuildPage.cs
@@ -3,71 +3,88 @@ using TMPro;
 
 public class GuildPage : MonoBehaviour
 {
-    [SerializeField]
-    private BackendGuildSystem backendGuildSystem;
-    [SerializeField]
-    private TextMeshProUGUI textGuildName;              // Popup 상단에 출력되는 길드 이름 Text UI
-    [SerializeField]
-    private Notice notice;
-    [SerializeField]
-    private GameObject executivesOption;
-    [SerializeField]
-    private TextMeshProUGUI textMemberCount;
+	[SerializeField]
+	private	BackendGuildSystem	backendGuildSystem;
+	[SerializeField]
+	private	TextMeshProUGUI		textGuildName;				// Popup 상단에 출력되는 길드 이름 Text UI
+	[SerializeField]
+	private	Notice				notice;
+	[SerializeField]
+	private	GameObject			executivesOption;
+	[SerializeField]
+	private	TextMeshProUGUI		textMemberCount;
+	[SerializeField]
+	private	GameObject			overlayBackground;
+	[SerializeField]
+	private	GuildMemberEdit		popupMemberEdit;
 
-    [SerializeField]
-    private GameObject memberPrefab;
-    [SerializeField]
-    private Transform parentContent;
+	[SerializeField]
+	private	GameObject			memberPrefab;
+	[SerializeField]
+	private	Transform			parentContent;
 
-    private string guildName = string.Empty;    // 길드 이름
-    private MemoryPool memoryPool;
+	private	string				guildName = string.Empty;	// 길드 이름
+	private	MemoryPool			memoryPool;
 
-    private void Awake()
-    {
-        memoryPool = new MemoryPool(memberPrefab, parentContent);
-    }
+	private void Awake()
+	{
+		memoryPool = new MemoryPool(memberPrefab, parentContent);
+	}
 
-    public void Setup(string guildName, bool isMaster = false, bool isOtherGuild = false)
-    {
-        notice.Setup(isMaster, isOtherGuild);
-        executivesOption.SetActive(isMaster);
+	public void Setup(string guildName, bool isMaster=false, bool isOtherGuild=false)
+	{
+		notice.Setup(isMaster, isOtherGuild);
+		executivesOption.SetActive(isMaster);
 
-        gameObject.SetActive(true);
+		gameObject.SetActive(true);
 
-        textGuildName.text = guildName;
-        this.guildName = guildName;
+		textGuildName.text		= guildName;
+		this.guildName			= guildName;
 
-        if (isOtherGuild == true)
-        {
-            textMemberCount.text = $"길드 인원 {backendGuildSystem.otherGuildData.memberCount}/100";
-            backendGuildSystem.GetGuildMemeberList(backendGuildSystem.otherGuildData.guildInDate);
-        }
-        else
-        {
-            textMemberCount.text = $"길드 인원 {backendGuildSystem.myGuildData.memberCount}/100";
-            backendGuildSystem.GetGuildMemeberList(backendGuildSystem.myGuildData.guildInDate);
-        }
-    }
+		if ( isOtherGuild == true )
+		{
+			textMemberCount.text = $"길드 인원 {backendGuildSystem.otherGuildData.memberCount}/100";
+			backendGuildSystem.GetGuildMemeberList(backendGuildSystem.otherGuildData.guildInDate);
+		}
+		else
+		{
+			textMemberCount.text	= $"길드 인원 {backendGuildSystem.myGuildData.memberCount}/100";
+			backendGuildSystem.GetGuildMemeberList(backendGuildSystem.myGuildData.guildInDate);
+		}
+	}
 
-    public void Activate(GuildMemberData member)
-    {
-        GameObject item = memoryPool.ActivatePoolItem();
-        item.GetComponent<GuildMember>().Setup(member);
-    }
+	public void Activate(GuildMemberData member)
+	{
+		GameObject item = memoryPool.ActivatePoolItem();
+		item.GetComponent<GuildMember>().Setup(backendGuildSystem, this, member);
+	}
 
-    public void Deactivate(GameObject member)
-    {
-        memoryPool.DeactivatePoolItem(member);
-    }
+	public void Deactivate(GameObject member)
+	{
+		memoryPool.DeactivatePoolItem(member);
+	}
 
-    public void DeactivateAll()
-    {
-        memoryPool.DeactivateAllPoolItems();
-    }
+	public void DeactivateAll()
+	{
+		memoryPool.DeactivateAllPoolItems();
+	}
 
-    public void OnClickApplyGuild()
-    {
-        backendGuildSystem.ApplyGuild(guildName);
-    }
+	public void OnClickApplyGuild()
+	{
+		backendGuildSystem.ApplyGuild(guildName);
+	}
+
+	public void SuccessWithdrawGuild()
+	{
+		gameObject.SetActive(false);
+	}
+
+	public void OnClickMemberEdit(GuildMemberData memberData)
+	{
+		overlayBackground.SetActive(true);
+		popupMemberEdit.gameObject.SetActive(true);
+
+		popupMemberEdit.Setup(memberData);
+	}
 }
 
