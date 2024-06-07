560c727a653288ff323d185ef01e37630f35fac4
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-06-07 16:13:21 +0900

    Update PoolManager.cs

--- VRock_Soft/ObjectPool/PoolManager.cs
+++ VRock_Soft/ObjectPool/PoolManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PoolManager : MonoBehaviour// 싱글 플레이 풀방식
+public class PoolManager : MonoBehaviour // 싱글 플레이 풀방식
 {
     /*private static PoolManager PM;
     public static PoolManager PoolingManager
