e7053ecb9250b1bb887b72f7fb4f98351725e5ff
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-06-08 20:35:30 +0900

    Update DonDestroy.cs

--- VRock_Soft/Photon/DonDestroy.cs
+++ VRock_Soft/Photon/DonDestroy.cs
@@ -4,16 +4,8 @@ using UnityEngine;
 
 public class DonDestroy : MonoBehaviour
 {
-
-    // Start is called before the first frame update
     void Start()
     {
         DontDestroyOnLoad(this.gameObject);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    }    
 }
