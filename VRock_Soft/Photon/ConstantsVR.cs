d8ca61a85e59bc7c5a623ae017f35ce697d76b08
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-06-08 20:34:49 +0900

    Update ConstantsVR.cs

--- VRock_Soft/Photon/ConstantsVR.cs
+++ VRock_Soft/Photon/ConstantsVR.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConstantsVR : MonoBehaviour
+public class ConstantsVR : MonoBehaviour  // VR게임에서 정적 문자열
 {
     public const string MAP_KEY = "map";
     public const string MAP_VALUE_LOBBY = "Lobby";
