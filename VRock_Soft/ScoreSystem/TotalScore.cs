3e552019dcfefdc6395433c7787edf882745e11f
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-06-12 10:19:15 +0900

    Update TotalScore.cs

--- VRock_Soft/ScoreSystem/TotalScore.cs
+++ VRock_Soft/ScoreSystem/TotalScore.cs
@@ -13,6 +13,7 @@ using Hashtable = ExitGames.Client.Photon.Hashtable;
 using System.Security.Cryptography;
 using Unity.VisualScripting;
 using static UnityEngine.UIElements.UxmlAttributeDescription;
+
 public class TotalScore : MonoBehaviourPunCallbacks//, IPunObservable
 {
     public static TotalScore TS;
