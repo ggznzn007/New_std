cfbcb02110c91c83d59bfdfbfe4dc7337a2521c7
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-06-15 12:04:57 +0900

    Update TextSetter.cs

--- ScriptableObject/TextSetter.cs
+++ ScriptableObject/TextSetter.cs
@@ -6,18 +6,11 @@ using UnityEngine.UI;
 public class TextSetter : MonoBehaviour
 {
     public Text text;
-
     
     void Start()
     {
         text.font = UI_Setting.Instance.defaultFont;
         text.fontSize = UI_Setting.Instance.defaultFontSize;
         text.color = UI_Setting.Instance.defaultFontColor;
-    }
-
-   
-    void Update()
-    {
-        
     }
 }
