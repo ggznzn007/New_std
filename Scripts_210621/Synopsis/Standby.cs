4af64c0454828061792e9a542133cf062a418f08
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-06-20 09:58:49 +0900

    Update Standby.cs

--- Scripts_210621/Synopsis/Standby.cs
+++ Scripts_210621/Synopsis/Standby.cs
@@ -3,15 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
+
 public class Standby : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
+{   
     void Update()
     {
         if (Input.GetMouseButton(0))
