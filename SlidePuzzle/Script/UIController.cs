6f481c0756a05da07f4e61a767e59d80010e2872
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-06-25 17:22:49 +0900

    Update UIController.cs

--- SlidePuzzle/Script/UIController.cs
+++ SlidePuzzle/Script/UIController.cs
@@ -16,7 +16,6 @@ public class UIController : MonoBehaviour
     public void OnResultPanel()
     {
         resultPanel.SetActive(true);
-
         textPlaytime.text = $"플레이 타임 : {board.Playtime / 60:D2}:{board.Playtime % 60:D2}";
         textMoveCount.text = "타일이동 횟수 : " + board.MoveCount;
     }
