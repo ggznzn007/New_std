c3aa23d955374613dab8714aa620f29703e85820
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-06-29 15:05:01 +0900

    Update Make_Button.cs

--- Supersell/Code/Earth/Make_Button.cs
+++ Supersell/Code/Earth/Make_Button.cs
@@ -8,6 +8,7 @@ public class Make_Button : MonoBehaviour
 {
     public UnityEvent unityEvent = new();
     public GameObject button;
+
     void Start()
     {
         button = this.gameObject;
@@ -15,27 +16,23 @@ public class Make_Button : MonoBehaviour
 
     void Update()
     {
-        if(!DataManager.DM.isInfoOpen)
+        if (!DataManager.DM.isInfoOpen)
         {
             if (DataManager.DM.isInfoOpen) { return; }
             ButtonClick();
         }
     }
 
-   public void ButtonClick()
-    {        
+    public void ButtonClick()
+    {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (Input.GetMouseButtonDown(0))
-        {            
+        {
             if (Physics.Raycast(ray, out RaycastHit hit) && hit.collider.gameObject == gameObject)
             {
                 unityEvent.Invoke();
             }
         }
-        
     }
-
-  
-
 }
