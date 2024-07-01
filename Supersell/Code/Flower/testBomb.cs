7b70db3c3266e9a67b5dc9db1882369c4311853b
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-07-01 16:25:03 +0900

    Update testBomb.cs

--- Supersell/Code/Flower/testBomb.cs
+++ Supersell/Code/Flower/testBomb.cs
@@ -5,41 +5,41 @@ using UnityEngine;
 public class testBomb : MonoBehaviour
 {
     public GameObject exp;
-    public float expForce , radius;
+    public float expForce, radius;
     public Renderer rend;
-    // Start is called before the first frame update
-    private void OnCollisionEnter(Collision other){
+
+    private void OnCollisionEnter(Collision other)
+    {
         // GameObject _exp = Instantiate(exp,transform.position,transform.rotation);
         // Destroy(_exp,3);
         knockBack();
         Destroy(exp);
     }
-    void knockBack(){
-        Collider[] colliders = Physics.OverlapSphere(transform.position,radius);
-        Vector3 newVector = transform.position-transform.position ;
-        foreach (Collider nearby in colliders){
+
+    void knockBack()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, radius);
+        Vector3 newVector = transform.position - transform.position;
+        foreach (Collider nearby in colliders)
+        {
             Rigidbody rigg = nearby.GetComponent<Rigidbody>();
-            if (rigg != null){
+            if (rigg != null)
+            {
                 Debug.Log(rigg);
                 //  rigg.WakeUp();
                 rigg.isKinematic = false;
                 // rigg.velocity = Vector3.zero;
                 // rigg.AddExplosionForce(expForce,transform.position,radius,0f, ForceMode.Force);
-                newVector.y=1;
-                rigg.AddForce(newVector*5.0f*Time.deltaTime,ForceMode.Impulse);
+                newVector.y = 1;
+                rigg.AddForce(newVector * 5.0f * Time.deltaTime, ForceMode.Impulse);
                 // this.transform.position = Vector3.MoveTowards(this.transform.position ,initialLoc,20.0f*Time.deltaTime);
             }
         }
     }
+
     void Start()
     {
         rend = GetComponent<Renderer>();
         rend.enabled = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    }   
 }
