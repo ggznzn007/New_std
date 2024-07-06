201851acba43af64621f2358b3aab53e5b2ab3c4
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-07-06 09:37:01 +0900

    Update Control_Slot.cs

--- Supersell/Code/LiveSketch/Control_Slot.cs
+++ Supersell/Code/LiveSketch/Control_Slot.cs
@@ -12,13 +12,9 @@ public class Control_Slot : PlayerControl
 
         waitTime = Setting.startWaitTime;
     }
+
     private void Start()
     {       
         anim = GetComponent<Animator>();
-    }
-
-    private void Update()
-    {
-      
     }
 }
