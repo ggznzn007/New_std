c46c1a714c26e01ca652a5d2de0dca295e56aeac
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-07-07 10:53:47 +0900

    Update DestroySelf.cs

--- Supersell/Code/LiveSketch/DestroySelf.cs
+++ Supersell/Code/LiveSketch/DestroySelf.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DestroySelf : MonoBehaviour
-{
-   
+{   
     void Start()
     {
         Destroy(gameObject, 10f);
-    }
-
-    
+    }    
 }
