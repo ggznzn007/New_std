0770bf307673fb5f3201c75eb1471712a36d2254
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-07-07 10:54:01 +0900

    Update Line.cs

--- Supersell/Code/LiveSketch/Line.cs
+++ Supersell/Code/LiveSketch/Line.cs
@@ -27,7 +27,6 @@ public class Line : MonoBehaviour
     void SetPoint(Vector2 point)
     {
         points.Add(point);
-
         lineRenderer.positionCount = points.Count;
         lineRenderer.SetPosition(points.Count - 1, point);
     }
