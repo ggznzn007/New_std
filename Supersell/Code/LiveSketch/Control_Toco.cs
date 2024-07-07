a9a0fa14e46c86da2f2ee81211e66458c5a52f2d
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-07-07 10:53:50 +0900

    Update Control_Toco.cs

--- Supersell/Code/LiveSketch/Control_Toco.cs
+++ Supersell/Code/LiveSketch/Control_Toco.cs
@@ -12,13 +12,9 @@ public class Control_Toco : PlayerControl
 
         waitTime = Setting.startWaitTime;
     }
+
     private void Start()
     {
         anim = GetComponent<Animator>();
     }
-
-    private void Update()
-    {
-    
-    }    
 }
