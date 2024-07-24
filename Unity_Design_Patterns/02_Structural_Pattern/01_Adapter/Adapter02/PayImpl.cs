630ed3aad35b9e361e72182d9b90f3e71c4379d7
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-07-24 16:31:45 +0900

    Update PayImpl.cs

--- Unity_Design_Patterns/02_Structural_Pattern/01_Adapter/Adapter02/PayImpl.cs
+++ Unity_Design_Patterns/02_Structural_Pattern/01_Adapter/Adapter02/PayImpl.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 // 우리 회사에서 PayY 인터페이스 구현
 public class PayImpl : PayY, PayX // 다중 상속  = 어댑터가 됨
 {
-
     // for PayY
     private string customerCardNum;
 
@@ -18,7 +17,6 @@ public class PayImpl : PayY, PayX // 다중 상속  = 어댑터가 됨
         Debug.Log("PayY (Set)");
         this.customerCardNum = customerCardNum;
     }
-
     // ------------------------------------------------------
 
     // for PayX Method
