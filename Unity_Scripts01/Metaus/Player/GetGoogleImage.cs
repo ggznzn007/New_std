226ec5f8f6608ced8505032504ae4c1ef491708c
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-07-31 14:15:09 +0900

    Update GetGoogleImage.cs

--- Unity_Scripts01/Metaus/Player/GetGoogleImage.cs
+++ Unity_Scripts01/Metaus/Player/GetGoogleImage.cs
@@ -8,17 +8,15 @@ public class GetGoogleImage : MonoBehaviour
 {
     private SpriteRenderer parentsRenderer;
     public RawImage userImage;
-    // Start is called before the first frame update
+   
     void Start()
     {
         parentsRenderer = transform.parent.GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         Texture2D texture2D = Social.localUser.image;
         userImage.texture = texture2D;
-
     }
 }
