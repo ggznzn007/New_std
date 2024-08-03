866311a27ef5f2557d0649cb226caba6ea042c9b
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-08-03 10:47:03 +0900

    Update DayTime.cs

--- Unity_Scripts01/Metaus/UI/DayTime.cs
+++ Unity_Scripts01/Metaus/UI/DayTime.cs
@@ -15,17 +15,16 @@ public class DayTime : MonoBehaviour
    
     public void ClickQuit()
     {
-
         LoadingUIController.Instance.LoadScene("Main");
         ((PlayGamesPlatform)Social.Active).SignOut();
-
     }
+
     public void DayTimePlayerSpawn()
     {
         GameObject player1 = Instantiate(Resources.Load("InPlayer"), new Vector3(-6, 14, 0), Quaternion.identity) as GameObject;
         player1.transform.localScale = new Vector3(0.3f, 0.3f, 1);
-
     }
+
     public void ClickBack()
     {
         LoadingUIController.Instance.LoadScene("Main");
@@ -35,9 +34,9 @@ public class DayTime : MonoBehaviour
     {
         this.gameObject.SetActive(true);
     }
+
     public void SetActiveFalse()
     {
         this.gameObject.SetActive(false);
-    }
-        
+    }        
 }
