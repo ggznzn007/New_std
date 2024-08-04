cf156dc6d6ae4b67f82854bdc79ee5f9be69904b
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-08-04 09:06:07 +0900

    Update LeanTween_Text.cs

--- Unity_Scripts01/Metaus/UI/LeanTween_Text.cs
+++ Unity_Scripts01/Metaus/UI/LeanTween_Text.cs
@@ -3,12 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LeanTween_Text : MonoBehaviour
-{
-    
+{    
     void Start()
     {
         this.transform.LeanScale(new Vector2(.9f, .9f), 0.5f).setLoopPingPong();
-    }
-
-   
+    }   
 }
