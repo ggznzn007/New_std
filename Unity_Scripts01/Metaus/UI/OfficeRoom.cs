b36dbbc349e8e14d4463ae5a40d0766a49c833f3
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-08-07 12:25:15 +0900

    Update OfficeRoom.cs

--- Unity_Scripts01/Metaus/UI/OfficeRoom.cs
+++ Unity_Scripts01/Metaus/UI/OfficeRoom.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class OfficeRoom : MonoBehaviour
 {
     [SerializeField] GameObject player;
+
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
@@ -16,13 +17,11 @@ public class OfficeRoom : MonoBehaviour
     {
         GameObject player1 = Instantiate(Resources.Load("InPlayer"), new Vector3(0f, 40.5f, 0), Quaternion.identity) as GameObject;
         player1.transform.localScale = new Vector3(0.3f, 0.3f, 1);
-        
     }
 
     public void ClickBack()
     {
         //LoadingSceneCtrl.LoadScene("Main");
-
         LoadingUIController.Instance.LoadScene("Main");
     }
 
