633936cf8cb1eca75a01a455551f2c3d8ac3c79a
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-09-05 12:54:36 +0900

    Update GameController.cs

--- FSM/GameController.cs
+++ FSM/GameController.cs
@@ -5,76 +5,72 @@ public enum Locations { SweetHome = 0, Library, LectureRoom, PCRoom, Pub };
 
 public class GameController : MonoBehaviour
 {
-	[SerializeField]
-	private	string[]	arrayStudents;	// Student들의 이름 배열
-	[SerializeField]
-	private	GameObject	studentPrefab;	// Student 타입의 프리팹
-	
-	[SerializeField]
-	private	string[]	arrayUnemployeds;	// Unemployed들의 이름 배열
-	[SerializeField]
-	private	GameObject	unemployedPrefab;	// Unemployed 타입의 프리팹
+    [SerializeField] private string[] arrayStudents;    // Student들의 이름 배열
+    [SerializeField] private GameObject studentPrefab;  // Student 타입의 프리팹
 
-	// 재생 제어를 위한 모든 에이전트 리스트
-	private	List<BaseGameEntity>	entitys;
+    [SerializeField] private string[] arrayUnemployeds; // Unemployed들의 이름 배열
+    [SerializeField] private GameObject unemployedPrefab;   // Unemployed 타입의 프리팹
 
-	public static bool	IsGameStop { set; get; } = false;
+    // 재생 제어를 위한 모든 에이전트 리스트
+    private List<BaseGameEntity> entitys;
 
-	private void Awake()
-	{
-		entitys = new List<BaseGameEntity>();
+    public static bool IsGameStop { set; get; } = false;
 
-		for ( int i = 0; i < arrayStudents.Length; ++ i )
-		{
-			// 에이전트 생성, 초기화 메소드 호출
-			GameObject	clone	= Instantiate(studentPrefab);
-			Student		entity	= clone.GetComponent<Student>();
-			entity.Setup(arrayStudents[i]);
+    private void Awake()
+    {
+        entitys = new List<BaseGameEntity>();
 
-			// 에이전트들의 재생 제어를 위해 리스트에 저장
-			entitys.Add(entity);
-		}
+        for (int i = 0; i < arrayStudents.Length; ++i)
+        {
+            // 에이전트 생성, 초기화 메소드 호출
+            GameObject clone = Instantiate(studentPrefab);
+            Student entity = clone.GetComponent<Student>();
+            entity.Setup(arrayStudents[i]);
 
-		// Unemployed 에이전트 생성
-		for ( int i = 0; i < arrayUnemployeds.Length; ++ i )
-		{
-			GameObject	clone	= Instantiate(unemployedPrefab);
-			Unemployed	entity	= clone.GetComponent<Unemployed>();
-			entity.Setup(arrayUnemployeds[i]);
+            // 에이전트들의 재생 제어를 위해 리스트에 저장
+            entitys.Add(entity);
+        }
 
-			entitys.Add(entity);
-		}
+        // Unemployed 에이전트 생성
+        for (int i = 0; i < arrayUnemployeds.Length; ++i)
+        {
+            GameObject clone = Instantiate(unemployedPrefab);
+            Unemployed entity = clone.GetComponent<Unemployed>();
+            entity.Setup(arrayUnemployeds[i]);
 
-		// 에이전트 데이터베이스 초기화 및 모든 에이전트 등록
-		EntityDatabase.Instance.Setup();
-		for ( int i = 0; i < entitys.Count; ++ i )
-		{
-			EntityDatabase.Instance.RegisterEntity(entitys[i]);
-		}
+            entitys.Add(entity);
+        }
 
-		// 메시지 관리자 초기화
-		MessageDispatcher.Instance.Setup();
-	}
+        // 에이전트 데이터베이스 초기화 및 모든 에이전트 등록
+        EntityDatabase.Instance.Setup();
+        for (int i = 0; i < entitys.Count; ++i)
+        {
+            EntityDatabase.Instance.RegisterEntity(entitys[i]);
+        }
 
-	private void Update()
-	{
-		if ( IsGameStop == true ) return;
+        // 메시지 관리자 초기화
+        MessageDispatcher.Instance.Setup();
+    }
 
-		// 지연 발송되어야 하는 메시지 관리
-		MessageDispatcher.Instance.DispatchDelayedMessages();
+    private void Update()
+    {
+        if (IsGameStop == true) return;
 
-		// 모든 에이전트의 Updated()를 호출해 에이전트 구동
-		for ( int i = 0; i < entitys.Count; ++ i )
-		{
-			entitys[i].Updated();
-		}
-	}
+        // 지연 발송되어야 하는 메시지 관리
+        MessageDispatcher.Instance.DispatchDelayedMessages();
 
-	public static void Stop(BaseGameEntity entity)
-	{
-		IsGameStop = true;
+        // 모든 에이전트의 Updated()를 호출해 에이전트 구동
+        for (int i = 0; i < entitys.Count; ++i)
+        {
+            entitys[i].Updated();
+        }
+    }
 
-		entity.PrintText("100점 획득으로 프로그램을 종료합니다.");
-	}
+    public static void Stop(BaseGameEntity entity)
+    {
+        IsGameStop = true;
+
+        entity.PrintText("100점 획득으로 프로그램을 종료합니다.");
+    }
 }
 
