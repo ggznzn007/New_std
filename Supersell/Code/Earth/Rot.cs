56e05e5b312f78606e7cb45b0be01441999250a6
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-10-10 10:11:31 +0900

    Update Rot.cs

--- Supersell/Code/Earth/Rot.cs
+++ Supersell/Code/Earth/Rot.cs
@@ -8,6 +8,6 @@ public class Rot : MonoBehaviour
     {
         //if(Input.GetKeyDown(KeyCode.Return))
         //this.transform.rotation = new Quaternion(0,0,0,0);
-       //this.transform.localEulerAngles = new Vector3(0, 0, 90);
+        //this.transform.localEulerAngles = new Vector3(0, 0, 90);
     }
 }
