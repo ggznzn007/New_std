b2ac7db23f51c52f82e694cf6655fdfa51259de6
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-11-04 13:34:15 +0900

    Update LeafRotate.cs

--- Supersell/Code/FourSeasons/LeafRotate.cs
+++ Supersell/Code/FourSeasons/LeafRotate.cs
@@ -17,12 +17,12 @@ public class LeafRotate : MonoBehaviour
     }
 
     void Update()
-    {       
+    {
         if (startPos != transform.position)
         {
             if (Vector3.Distance(transform.position, startPos) < 10)
-            {               
-               transform.parent.Rotate(Vector3.up, rotSpeed * Time.deltaTime);
+            {
+                transform.parent.Rotate(Vector3.up, rotSpeed * Time.deltaTime);
                 rotSpeed = 70;
                 StartCoroutine(DelRot());
             }
@@ -30,47 +30,46 @@ public class LeafRotate : MonoBehaviour
             {
                 this.transform.Rotate(Vector3.forward, rotSpeed * Time.deltaTime);
                 rotSpeed = 1;
-            }            
-        }        
+            }
+        }
     }
 
     IEnumerator DelRot()
     {
         yield return new WaitForSeconds(0.8f);
         startPos = this.transform.position;
-        //this.transform.Rotate(Vector3.forward, rotSpeed * Time.deltaTime);
     }
 
     IEnumerator DelayStopRotate()
-    {       
+    {
         yield return new WaitForSeconds(2);
         rotSpeed = 90;
     }
 
-  /*  private void OnTriggerEnter(Collider coll)
-    {
-        if(coll.CompareTag("Player"))
-        {
-            rotSpeed = 70;          
-            Debug.Log("플레이어 태그 시작");
-        }       
-    }
+    /*  private void OnTriggerEnter(Collider coll)
+      {
+          if(coll.CompareTag("Player"))
+          {
+              rotSpeed = 70;          
+              Debug.Log("플레이어 태그 시작");
+          }       
+      }
 
-    private void OnTriggerStay(Collider coll)
-    {
-        if (coll.CompareTag("Player"))
-        {
-            rotSpeed = 70;
-            Debug.Log("플레이어 태그 중");
-        }
-    }
+      private void OnTriggerStay(Collider coll)
+      {
+          if (coll.CompareTag("Player"))
+          {
+              rotSpeed = 70;
+              Debug.Log("플레이어 태그 중");
+          }
+      }
 
-    private void OnTriggerExit(Collider coll)
-    {
-        if(coll.CompareTag("Player"))
-        {
-            rotSpeed = 0;
-            Debug.Log("플레이어 아웃");
-        }
-    }*/
+      private void OnTriggerExit(Collider coll)
+      {
+          if(coll.CompareTag("Player"))
+          {
+              rotSpeed = 0;
+              Debug.Log("플레이어 아웃");
+          }
+      }*/
 }
