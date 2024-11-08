8552a4976cc62f724429d5eac2b8ec6faa8100ad
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-11-08 13:35:12 +0900

    Update Control_Camp.cs

--- Supersell/Code/LiveSketch/Control_Camp.cs
+++ Supersell/Code/LiveSketch/Control_Camp.cs
@@ -9,7 +9,6 @@ public class Control_Camp : PlayerControl
     {
         sr = GetComponent<SpriteRenderer>();
         sk = GetComponent<SpriteSkin>();
-
         waitTime = Setting.startWaitTime;
     }
 
