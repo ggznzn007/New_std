a1a7e8c6cda54ecfe2fff71a255634a4e98d20e6
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2024-11-22 15:30:48 +0900

    Update Control_Shun.cs

--- Supersell/Code/LiveSketch/Control_Shun.cs
+++ Supersell/Code/LiveSketch/Control_Shun.cs
@@ -9,7 +9,6 @@ public class Control_Shun : PlayerControl
     {
         sr = GetComponent<SpriteRenderer>();
         sk = GetComponent<SpriteSkin>();
-
         waitTime = Setting.startWaitTime;
     }
 
