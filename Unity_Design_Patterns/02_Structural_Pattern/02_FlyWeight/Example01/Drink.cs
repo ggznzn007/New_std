a74ba6c55f39eb339dd8d958d26e5bcd3dfaba91
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2025-01-25 18:31:29 +0900

    Update Drink.cs

--- Unity_Design_Patterns/02_Structural_Pattern/02_FlyWeight/Example01/Drink.cs
+++ Unity_Design_Patterns/02_Structural_Pattern/02_FlyWeight/Example01/Drink.cs
@@ -2,20 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface Drink
-{
-    string getName();
-}
+public interface Drink { string getName(); }
 
 public class Cola : Drink
 {
     string name;
     int price;
 
-    public Cola(string name)
-    {
-        this.name = name;
-    }
+    public Cola(string name) { this.name = name; }
 
     public string getName() // 오버라이딩
     {
