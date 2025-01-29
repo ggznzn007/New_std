d43e1d8618f56c4d31d381ebf306335ae271e479
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2025-01-29 16:43:29 +0900

    Update Cider.cs

--- Unity_Design_Patterns/02_Structural_Pattern/02_FlyWeight/Example02/Cider.cs
+++ Unity_Design_Patterns/02_Structural_Pattern/02_FlyWeight/Example02/Cider.cs
@@ -7,13 +7,7 @@ public class Cider : MonoBehaviour, CarbonatedDrink
     public string name;
     public int price;
 
-    public void setName(string name)
-    {
-        this.name = name;
-    }
+    public void setName(string name) { this.name = name; }
 
-    public string getName()
-    {
-        return name;
-    }
+    public string getName() { return name; }
 }
