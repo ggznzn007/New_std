188594e0ef3fbdc191551815250954f789c0d069
Pooleun Jeong <65937620+ggznzn007@users.noreply.github.com>
2025-02-07 14:30:15 +0900

    Update CommandDefense.cs

--- Unity_Design_Patterns/03_Behavioral_Pattern/01_Command/Example01/CommandDefense.cs
+++ Unity_Design_Patterns/03_Behavioral_Pattern/01_Command/Example01/CommandDefense.cs
@@ -5,30 +5,27 @@ using UnityEngine;
 // Concrete Command 객체 : 직접적으로 동작하는 객체
 public class CommandDefense : CommandKey
 {
-	public CommandDefense(MonoBehaviour _mono, GameObject _shield,
-						  GameObject _cannon, Transform _firePos)
-	{
-		this.shield = _shield;
-		this.cannon = _cannon;
-		this.firePos = _firePos;
-		this.mono = _mono;
-	}
+    public CommandDefense(MonoBehaviour _mono, GameObject _shield,
+                          GameObject _cannon, Transform _firePos)
+    {
+        this.shield = _shield;
+        this.cannon = _cannon;
+        this.firePos = _firePos;
+        this.mono = _mono;
+    }
 
-	public override void Execute()
-	{
-		Defense();
-	}
+    public override void Execute() { Defense(); }
 
-	void Defense()
-	{
-		Debug.Log("Defense");
-		shield.SetActive(true);
-		mono.StartCoroutine(Defense(1f));
-	}
+    void Defense()
+    {
+        Debug.Log("Defense");
+        shield.SetActive(true);
+        mono.StartCoroutine(Defense(1f));
+    }
 
-	IEnumerator Defense(float second)
-	{
-		yield return new WaitForSeconds(second);
-		this.shield.SetActive(false);
-	}
+    IEnumerator Defense(float second)
+    {
+        yield return new WaitForSeconds(second);
+        this.shield.SetActive(false);
+    }
 }
